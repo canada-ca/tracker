db9f572ddca74ae01f6697be1bca720fecd8a835
evadb <eva.demers-brett@cds-snc.ca>
2018-06-13 13:18:51 -0700

    Modal window (#131)
    * Start some basic stylin
    
    * focus management working
    
    * Clean up focus management, exit state

--- track_digital/track/static/js/main.js
+++ track_digital/track/static/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
 
+  var KEYCODE_ESC = 27;
+
+  // Mobile menu
+
   $('#menu-btn, .overlay, .sliding-panel-close').on('click touchstart',function (e) {
     $('#menu-content, .overlay').toggleClass('is-visible');
 
@@ -12,5 +16,55 @@ $(function(){
 
     e.preventDefault();
   });
+
+  // Modal
+
+  $('#modal-btn').on('click touchstart', function (e) {
+    if ($('#modal').hasClass('hidden')) toggleModal('show');
+
+    e.preventDefault();
+  });
+
+  $('#close-btn').on('click touchstart', function (e) {
+    toggleModal('hide');
+
+    e.preventDefault();
+  });
+
+  $(document).keyup(function(e) {
+    if(e.keyCode === KEYCODE_ESC && $('#modal').hasClass('flex')) {
+      toggleModal('hide')
+    }
+  })
+
+  $(document).focusin(function(e) {
+    var modal = document.getElementById("modal");
+    
+    if($('#modal').hasClass('flex') && !$.contains(modal, e.target)) {
+      e.stopPropagation();
+      $('#modal-content').focus();
+    }
+
+  });
+
+  var toggleModal = function(sh){ 
+    if(sh == "show") {
+      $('#modal').toggleClass('hidden flex');
+
+      $('#modal-header').focus();
+
+      $('#header, #main-content, #goc-footer').attr('aria-hidden', 'true');
+      
+    }
+
+    else {
+      $('#modal').toggleClass('hidden flex');
+
+      $('#header, #main-content, #goc-footer').attr('aria-hidden', 'false');
+
+      $('#close-btn').focus();
+    }
+
+  };
 
 });
