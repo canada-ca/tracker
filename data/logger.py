4267145aee33628f077ec0aca67b649400b65886
David Buckley <buckley.w.david@gmail.com>
2018-07-17 13:14:17 -0400

    Refactor logger module (#32)
    move handlers to the root logger, add syslog handler
    pipe stderr from subprocess call into the update logger

--- data/logger.py
+++ data/logger.py
@@ -1,4 +1,60 @@
 import logging
+from logging.handlers import SysLogHandler
+import threading
+import os
+
+# Configure the root logger with handlers
+_syslog_handler = SysLogHandler(address=os.environ.get("TRACKER_SYSLOG", "/dev/log"))
+_syslog_handler.setLevel(logging.ERROR)
+
+_stream_handler = logging.StreamHandler()
+_stream_handler.setLevel(logging.INFO)
+
+_stream_formatter = logging.Formatter(
+    fmt="%(asctime)s - %(levelname)s - %(name)s: %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S%z",
+)
+_syslog_formatter = logging.Formatter(
+    fmt="%(name)s: [%(levelname)s] %(message)s",
+)
+_syslog_handler.setFormatter(_syslog_formatter)
+_stream_handler.setFormatter(_stream_formatter)
+logging.getLogger().addHandler(_syslog_handler)
+logging.getLogger().addHandler(_stream_handler)
+
+# https://codereview.stackexchange.com/questions/6567/redirecting-subprocesses-output-stdout-and-stderr-to-the-logging-module
+# To redirect stderr & stdout from subprocess calls into a logger
+class LogPipe(threading.Thread):
+
+    def __init__(self, logger, level):
+        """Setup the object with a logger and a loglevel
+        and start the thread
+        """
+        super(LogPipe, self).__init__()
+        self.daemon = False
+        self.logger = logger
+        self.level = level
+        self.fd_read, self.fd_write = os.pipe()
+        self.pipe_reader = os.fdopen(self.fd_read, encoding='utf-8', errors='ignore')
+        self.start()
+
+    def fileno(self):
+        """Return the write file descriptor of the pipe
+        """
+        return self.fd_write
+
+    def run(self):
+        """Run the thread, logging everything.
+        """
+        for line in iter(self.pipe_reader.readline, ''):
+            self.logger.log(self.level, line.strip('\n'))
+
+        self.pipe_reader.close()
+
+    def close(self):
+        """Close the write end of the pipe.
+        """
+        os.close(self.fd_write)
 
 
 def unwrap_exception_message(exc: BaseException, join: str = " - ") -> str:
@@ -12,14 +68,5 @@ def unwrap_exception_message(exc: BaseException, join: str = " - ") -> str:
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-
-    if not logger.hasHandlers():
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            fmt="%(asctime)s - %(levelname)s - %(name)s: %(message)s",
-            datefmt="%Y-%m-%dT%H:%M:%S%z",
-        )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
 
     return logger
