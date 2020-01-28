88f73957d12dbe77aa99876049251f3223c66598
Mike Williamson <mike@korora.ca>
2020-01-28 15:21:20 -0500

    Add useful comment for the future

--- api/schema_export.py
+++ api/schema_export.py
@@ -1,6 +1,10 @@
 import json
 from queries import schema
 
+# TODO: revisit when Graphene 3 is out, and graphql-core 3 can be used
+# instead. This will be much nicer.
+# https://graphql-core-next.readthedocs.io/en/latest/usage/introspection.html
+
 introspection_dict = schema.introspect()
 
 with open("schema.json", "w") as fp:
