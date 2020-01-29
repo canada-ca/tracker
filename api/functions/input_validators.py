b8cffe5f7293a1cbf7a10a7f9b241904ddf5fef0
Nicholas <neatnick27@gmail.com>
2020-01-29 11:52:10 -0400

    Moved business logic into their own functions (#49)
    * Moved business logic into their own functions
    
    * Changed password requirements to meet GoC requirements

--- api/functions/input_validators.py
+++ api/functions/input_validators.py
@@ -9,25 +9,7 @@ import html
 # Password validators
 ##
 def is_strong_password(password):
-	return has_min_chars(password) and contains_special_char(password)\
-		and contains_uppercase(password) and contains_number(password)
-
-
-def has_min_chars(password):
-	return len(password) >= 8
-
-
-def contains_uppercase(password):
-	return any(char.isupper() for char in password)
-
-
-def contains_number(password):
-	return any(char.isdigit() for char in password)
-
-
-def contains_special_char(password):
-	string_check = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
-	return not string_check.search(password) is None
+	return len(password) >= 12
 
 
 ##
--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,27 +1,12 @@
 import graphene
 import json
-from flask_graphql_auth import create_refresh_token
 from graphene import relay, Mutation
-from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from graphql import GraphQLError
-from flask import current_app as app
-from flask_bcrypt import Bcrypt
+from graphene_sqlalchemy import SQLAlchemyObjectType
 
 from models import Users as User
-from db import db_session
-from functions.input_validators import *
 
-from flask_graphql_auth import (
-	AuthInfoField,
-	GraphQLAuth,
-	get_jwt_identity,
-	get_raw_jwt,
-	create_access_token,
-	create_refresh_token,
-	query_jwt_required,
-	mutation_jwt_refresh_token_required,
-	mutation_jwt_required,
-)
+from functions.create_user import create_user
+from functions.sign_in_user import sign_in_user
 
 
 class UserObject(SQLAlchemyObjectType):
@@ -46,35 +31,7 @@ class CreateUser(graphene.Mutation):
 
 	@staticmethod
 	def mutate(self, info, username, password, confirm_password, email):
-
-		username = cleanse_input(username)
-		password = cleanse_input(password)
-		confirm_password = cleanse_input(confirm_password)
-		email = cleanse_input(email)
-
-		if not is_strong_password(password):
-			raise GraphQLError("Password does not meet minimum requirements (Min. 8 chars, Uppercase, Number, Special Char)")
-
-		if password != confirm_password:
-			raise GraphQLError("Passwords do not match")
-
-		bcrypt = Bcrypt(app)
-
-		user = User(
-			username=username,
-			user_email=email,
-			preferred_lang='English',
-			display_name='Default Display Name',
-			user_password=bcrypt.generate_password_hash(password=password).decode('UTF-8')  # Hash the password
-		)
-
-		db_session.add(user)
-		try:
-			db_session.commit()
-		except Exception as e:
-			db_session.rollback()
-			db_session.flush()
-		raise GraphQLError("Error creating account, please try again")
+		create_user(username, password, confirm_password, email)
 
 
 class SignInUser(graphene.Mutation):
@@ -87,22 +44,8 @@ class SignInUser(graphene.Mutation):
 
 	@classmethod
 	def mutate(cls, _, info, email, password):
-		email = cleanse_input(email)
-		password = cleanse_input(password)
-		user = User.query.filter(User.user_email == email).first()
-
-		if user is None:
-			raise GraphQLError("User does not exist, please register")
-
-		bcrypt = Bcrypt(app)
-
-		email_match = email == user.user_email
-		password_match = bcrypt.check_password_hash(user.user_password, password)
-
-		if email_match and password_match:
-			return SignInUser(
-				auth_token=create_access_token(user.id),
-				user=user
-			)
-		else:
-			raise GraphQLError("Incorrect email or password")
+		user_dict = sign_in_user(email, password)
+		return SignInUser(
+			auth_token=user_dict['auth_token'],
+			user=user_dict['user']
+		)
