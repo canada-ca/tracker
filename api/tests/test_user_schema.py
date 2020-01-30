7ec362f292686c3bcc1ce4f1ead4f7c6445b1f7c
Matt Peachey <peacheym@dal.ca>
2020-01-30 07:28:31 -0400

    Added unit tests for new Mutation. Todo: Test actual DB connection and interaction. These Unit Tests only test errors and not successes.

--- api/tests/test_user_schema.py
+++ api/tests/test_user_schema.py
@@ -52,3 +52,73 @@ class TestUserSchemaPassword:
 		assert executed['errors'][0]
 		assert executed['errors'][0]['message'] == error_passwords_do_not_match()
 
+	def test_updated_passwords_do_not_match(self):
+		client = Client(schema)
+		executed = client.execute(
+			'''
+			mutation {
+				updatePassword(email: "test@test-email.ca", password: "a-super-long-password",
+					confirmPassword: "another-super-long-password") {
+					user {
+						username
+					}
+				}
+			}
+			''')
+
+		assert executed['errors']
+		assert executed['errors'][0]
+		assert executed['errors'][0]['message'] == error_passwords_do_not_match()
+
+	def test_updated_password_too_short(self):
+		client = Client(schema)
+		executed = client.execute(
+			'''
+			mutation {
+				updatePassword(email: "test@test-email.ca", password: "password", confirmPassword: "password") {
+					user {
+						username
+					}
+				}
+			}
+			''')
+
+		assert executed['errors']
+		assert executed['errors'][0]
+		assert executed['errors'][0]['message'] == error_password_does_not_meet_requirements()
+
+	def test_updated_password_no_user_email(self):
+		client = Client(schema)
+		executed = client.execute(
+			'''
+			mutation {
+				updatePassword(email: "", password: "valid-password", confirmPassword: "valid-password") {
+					user {
+						username
+					}
+				}
+			}
+			''')
+
+		assert executed['errors']
+		assert executed['errors'][0]
+		assert executed['errors'][0]['message'] == error_user_does_not_exist()
+
+	def test_updated_password_no_user(self):
+		client = Client(schema)
+		executed = client.execute(
+			'''
+			mutation {
+				updatePassword(email: "testing-fake-email-no-such-user@test.ca",
+					password: "valid-password", confirmPassword: "valid-password") {
+					user {
+						username
+					}
+				}
+			}
+			''')
+
+		assert executed['errors']
+		assert executed['errors'][0]
+		assert executed['errors'][0]['message'] == error_user_does_not_exist()
+
