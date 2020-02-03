4155ba223142f3bbe5d0b39ab697cd48c5e9ee59
Mike Williamson <mike@korora.ca>
2020-02-03 15:05:31 -0500

    Add react router
    This commit adds React Router and client side routing for just a single
    route: /.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,25 @@
 import React from 'react'
 import { i18n } from '@lingui/core'
-import { LocaleSwitcher } from './LocaleSwitcher'
 import { I18nProvider } from '@lingui/react'
-import { SignatureBlock } from './SignatureBlock'
 import { theme } from './theme'
 import { Main } from './Main'
-import { Box, Button, Heading, Grommet, Grid, ResponsiveContext } from 'grommet'
+import { Layout } from './Layout'
+import { Grommet } from 'grommet'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 export const App = () => {
   return (
     <I18nProvider i18n={i18n}>
       <Grommet full theme={theme}>
-        <ResponsiveContext.Consumer>
-          {size => {
-            return (
-              <Grid
-                fill
-                rows={['auto', 'flex']}
-                columns={['auto', 'flex']}
-                areas={[
-                  { name: 'header', start: [0, 0], end: [1, 0] },
-                  { name: 'main', start: [1, 1], end: [1, 1] },
-                ]}
-              >
-                <Box
-                  gridArea="header"
-                  direction="row"
-                  align="center"
-                  justify="between"
-                  pad={{ horizontal: 'medium', vertical: 'small' }}
-                  background="white"
-                >
-                  <Heading level="3" margin="none">
-                    <Button>
-                      <SignatureBlock />
-                    </Button>
-                  </Heading>
-                  <LocaleSwitcher />
-                </Box>
-                <Box
-                  gridArea="main"
-                  justify="start"
-                  pad={size}
-                  margin={size}
-                  align="center"
-                >
-                  <Main />
-                </Box>
-              </Grid>
-            )
-          }}
-        </ResponsiveContext.Consumer>
+        <Layout>
+          <Router>
+            <Switch>
+              <Route path="/">
+                <Main />
+              </Route>
+            </Switch>
+          </Router>
+        </Layout>
       </Grommet>
     </I18nProvider>
   )
--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
 import { render, cleanup } from '@testing-library/react'
 import { App } from '../App'
 
 describe('<App/>', () => {
   afterEach(cleanup)
-  it('renders without crashing', () => {
-    render(<App />)
+
+  describe('routes', () => {
+    describe('/', () => {
+      it('renders the main page', () => {
+        const { getByRole } = render(
+          <MemoryRouter initialEntries={['/']} initialIndex={1}>
+            <App />
+          </MemoryRouter>,
+        )
+        expect(getByRole('heading')).toHaveTextContent(/track web/i)
+      })
+    })
   })
 })
