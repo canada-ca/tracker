94fa157aeeccbf6d6eea95534693e21ecb780cd6
nick <neatnick27@gmail.com>
2020-02-05 10:29:22 -0400

    Initial testing of db migrations

--- api/tests/test_db_migrate.py
+++ api/tests/test_db_migrate.py
@@ -4,43 +4,52 @@ from os.path import dirname, join, expanduser, normpath, realpath
 
 import pytest
 
-import unittest
-
 # This is the only way I could get imports to work for unit testing.
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = dirname(realpath(join(os.getcwd(), expanduser(__file__))))
 sys.path.append(normpath(join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from models import Sectors
-from app import app, db
+from db import *
+
+from manage import app
 
 
 @pytest.fixture(scope='class')
 def build_db():
+
+	app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/track_dmarc'
+	app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+	db.init_app(app)
+
+	sector = Sectors(
+		sector='GC_F',
+		zone='GC',
+		description='Future Government of Canada'
+	)
 	with app.app_context():
-		from flask_migrate import init as _init
-		from flask_migrate import migrate as _migrate
-		from flask_migrate import upgrade as _upgrade
-	_init()
-	_migrate()
-	_upgrade()
+		db.session.add(sector)
+		db.session.commit()
 
 
 @pytest.mark.usefixtures('build_db')
 class TestDBCreation:
 	def test_db_create(self):
-		from db import db
 
 		sector = Sectors(
 			sector='GC_F',
 			zone='GC',
 			description='Future Government of Canada'
 		)
-		db.session.add(sector)
-		db.session.commit()
+		with app.app_context():
+			db.session.add(sector)
+			db.session.commit()
 
-		ret_sector = Sectors.query().filter(
-			sector='GC_F'
-		).first()
+		with app.app_context():
+			ret_sector = Sectors.query.filter(
+				Sectors.sector == 'GC_F'
+			)
 
-		assert sector.zone == ret_sector.zone
+		assert len(ret_sector.all())
