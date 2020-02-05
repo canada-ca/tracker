7178666d42558b71ae373418409ad22b9d7b920e
nick <neatnick27@gmail.com>
2020-02-05 14:11:18 -0400

    Migrate testing now cleaned up and working properly

--- api/tests/test_db_migrate.py
+++ api/tests/test_db_migrate.py
@@ -11,25 +11,23 @@ PACKAGE_PARENT = '..'
 SCRIPT_DIR = dirname(realpath(join(os.getcwd(), expanduser(__file__))))
 sys.path.append(normpath(join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-from models import Sectors
 from db import *
 from manage import app
 
 
-@pytest.fixture(scope='class')
-def initialize_db():
-
-	db.init_app(app)
-
-
-@pytest.mark.usefixtures('initialize_db')
 class TestDBCreation:
-	def test_db_create(self):
+	def test_created_schema_contains_expected_tables(self):
+		"""This test determines that the postgres db is created with the expected tables"""
+		# Arrange
+		db.init_app(app)
+		engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}')
 		tables = set()
+
+		# Act
 		tables.add("alembic_version")
 
 		for key in db.metadata.tables.keys():
 			tables.add(key)
 
-		engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}')
+		# Assert
 		assert tables == set(engine.table_names())
