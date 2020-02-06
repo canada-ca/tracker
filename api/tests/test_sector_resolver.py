1cd091a75648ee4c872c10fd7dfe976d6fd0be1a
nick <neatnick27@gmail.com>
2020-02-06 07:24:13 -0400

    Sector tests are now working with the new database system

--- api/tests/test_sector_resolver.py
+++ api/tests/test_sector_resolver.py
@@ -13,199 +13,187 @@ PACKAGE_PARENT = '..'
 SCRIPT_DIR = dirname(realpath(join(os.getcwd(), expanduser(__file__))))
 sys.path.append(normpath(join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-from app import schema
+from app import app, schema
+from db import db
+from models import Sectors
 
 
 @pytest.fixture(scope='class')
-def build_sectors_table():
-	"""Build database to allow proper testing of sector resolvers"""
-	url = "postgresql+psycopg2://postgres:postgres@postgres:5432/auth"
+def sector_test_resolver_db_init():
+	db.init_app(app)
 
-	engine = create_engine(url, echo=True)
-	connection = engine.connect()
+	with app.app_context():
+		if Sectors.query.first() is None:
+			sector = Sectors(
+				sector="GC",
+				zone="GC_A",
+				description="Arts"
+			)
+			db.session.add(sector)
+			db.session.commit()
 
-	meta = MetaData()
+			sector = Sectors(
+				sector="GC_F",
+				zone="GC",
+				description="Future Government of Canada"
+			)
+			db.session.add(sector)
+			db.session.commit()
 
-	sectors = Table(
-		'sectors', meta,
-		Column('id', Integer, primary_key=True),
-		Column('sector', String),
-		Column('zone', String),
-		Column('description', String),
-	)
-
-	meta.create_all(engine)
-
-	from models import Sectors
-	from db import db_session
-
-	if Sectors.query.first() is None:
-		sector = Sectors(
-			sector="GC",
-			zone="GC_A",
-			description="Arts"
-		)
-		db_session.add(sector)
-		db_session.commit()
-
-		sector = Sectors(
-			sector="GC_F",
-			zone="GC",
-			description="Future Government of Canada"
-		)
-
-		db_session.add(sector)
-		db_session.commit()
-
-		sector = Sectors(
-			sector="GC",
-			zone="GC_GA",
-			description="Government Administration"
-		)
-
-		db_session.add(sector)
-		db_session.commit()
-
-		# This fixture just needs to run to setup a postgres testing instance
-		return connection
+			sector = Sectors(
+				sector="GC",
+				zone="GC_GA",
+				description="Government Administration"
+			)
+			db.session.add(sector)
+			db.session.commit()
 
 
-@pytest.mark.usefixtures('build_sectors_table')
+@pytest.mark.usefixtures('sector_test_resolver_db_init')
 class TestSectorResolver(TestCase):
-	def test_get_sector_by_id(self):
+	def test_get_sector_resolver_by_id(self):
 		"""Test get_sector_by_id resolver"""
-		client = Client(schema)
-		query = """
-			query{
-				getSectorById(id: 1) {
-					sector
-					zone
-					description
+		with app.app_context():
+			client = Client(schema)
+			query = """
+				query{
+					getSectorById(id: 1) {
+						sector
+						zone
+						description
+					}
+				}
+				"""
+
+			result_refr = {
+				"data": {
+					"getSectorById": [
+						{
+							"sector": "GC",
+							"zone": "GC_A",
+							"description": "Arts"
+						}
+					]
 				}
 			}
-			"""
 
-		result_refr = {
-			"data": {
-				"getSectorById": [
-					{
-						"sector": "GC",
-						"zone": "GC_A",
-						"description": "Arts"
-					}
-				]
-			}
-		}
-
-		result_eval = client.execute(query)
+			result_eval = client.execute(query)
 
 		self.assertDictEqual(result_refr, result_eval)
 
-	def test_get_sector_by_sector(self):
+	def test_get_sector_resolver_by_sector(self):
 		"""Test get_sector_by_sector resolver"""
-		client = Client(schema)
-		query = """
-			query{
-				getSectorsBySector(sector: "GC_F") {
-					zone
-					description
+		with app.app_context():
+			client = Client(schema)
+			query = """
+				query{
+					getSectorsBySector(sector: "GC_F") {
+						zone
+						description
+					}
+				}
+				"""
+
+			result_refr = {
+				"data": {
+					"getSectorsBySector": [
+						{
+							"zone": "GC",
+							"description": "Future Government of Canada"
+						}
+					]
 				}
 			}
-			"""
 
-		result_refr = {
-			"data": {
-				"getSectorsBySector": [
-					{
-						"zone": "GC",
-						"description": "Future Government of Canada"
-					}
-				]
-			}
-		}
-
-		result_eval = client.execute(query)
+			result_eval = client.execute(query)
 
 		self.assertDictEqual(result_refr, result_eval)
 
-	def test_get_sector_by_zone(self):
+	def test_get_sector_resolver_by_zone(self):
 		"""Test get_sector_by_zone resolver"""
-		client = Client(schema)
-		query = """
-			query{
-				getSectorByZone(zone: "GC_GA"){
-					sector
-					zone
-					description
+		with app.app_context():
+			client = Client(schema)
+			query = """
+				query{
+					getSectorByZone(zone: "GC_GA"){
+						sector
+						zone
+						description
+					}
+				}
+				"""
+
+			result_refr = {
+				"data": {
+					"getSectorByZone": [
+						{
+							"sector": "GC",
+							"zone": "GC_GA",
+							"description": "Government Administration"
+						}
+					]
 				}
 			}
-			"""
 
-		result_refr = {
-			"data": {
-				"getSectorByZone": [
-					{
-						"sector": "GC",
-						"zone": "GC_GA",
-						"description": "Government Administration"
-					}
-				]
-			}
-		}
-
-		result_eval = client.execute(query)
+			result_eval = client.execute(query)
 
 		self.assertDictEqual(result_refr, result_eval)
 
-	def test_sector_by_id_invalid(self):
+	def test_sector_resolver_by_id_invalid(self):
 		"""Test get_sector_by_id invalid ID error handling"""
-		client = Client(schema)
-		query = """
-			query{
-				getSectorById(id: 55){
-					id
-					sector
-					zone
-					description
+		with app.app_context():
+			client = Client(schema)
+			query = """
+				query{
+					getSectorById(id: 55){
+						id
+						sector
+						zone
+						description
+					}
 				}
-			}
-			"""
-		executed = client.execute(query)
+				"""
+			executed = client.execute(query)
+
 		assert executed['errors']
 		assert executed['errors'][0]
 		assert executed['errors'][0]['message'] == "Error, Invalid ID"
 
 	def test_sector_by_zone_invalid(self):
 		"""Test get_sector_by_zone invalid Zone error handling"""
-		client = Client(schema)
-		query = """
+		with app.app_context():
+			client = Client(schema)
+			query = """
+				query{
+					getSectorByZone(zone: "fdsfa"){
+						id
+						sector
+						zone
+						description
+					}
+				}
+				"""
+			executed = client.execute(query)
+
+		assert executed['errors']
+		assert executed['errors'][0]
+		assert executed['errors'][0]['message'] == "Error, Zone does not exist"
+
+	def test_sector_resolver_by_sector_invalid(self):
+		"""Test get_sector_by_sector invalid sector error handling"""
+		with app.app_context():
+			client = Client(schema)
+			query = """
 			query{
-				getSectorByZone(zone: "fdsfa"){
+				getSectorsBySector(sector: "Should Not Work") {
 					id
-					sector
 					zone
 					description
 				}
 			}
 			"""
-		executed = client.execute(query)
-		assert executed['errors']
-		assert executed['errors'][0]
-		assert executed['errors'][0]['message'] == "Error, Zone does not exist"
+			executed = client.execute(query)
 
-	def test_sector_by_sector_invalid(self):
-		"""Test get_sector_by_sector invalid sector error handling"""
-		client = Client(schema)
-		query = """
-		query{
-			getSectorsBySector(sector: "Should Not Work") {
-				id
-				zone
-				description
-			}
-		}
-		"""
-		executed = client.execute(query)
 		assert executed['errors']
 		assert executed['errors'][0]
 		assert executed['errors'][0]['message'] == "Error, Sector does not exist"
