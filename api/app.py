bf5afff0f586e61ee4b90134f33fa3c2bc62a505
Matt Peachey <PeacheyM@dal.ca>
2020-02-10 09:35:18 -0400

    Use DB_PORT env when connecting to postgres (#79)

--- api/app.py
+++ api/app.py
@@ -10,14 +10,15 @@ from db import (
 	DB_NAME,
 	DB_HOST,
 	DB_PASS,
-	DB_USER
+	DB_USER,
+	DB_PORT
 )
 
 from queries import schema
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config["JWT_SECRET_KEY"] = os.getenv('SUPER_SECRET_KEY')
