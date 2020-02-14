311bc2c5ff5acb7cdc3b5a8ff7957c8435c16af3
Matt Peachey <peacheym@dal.ca>
2020-02-14 12:10:22 -0400

    Switch from append to extend for proper list formatting

--- api/user_roles.py
+++ api/user_roles.py
@@ -7,10 +7,12 @@ TODO: Improve how roles interact with one another across levels of permission
 super_admin_roles = ['super_admin']
 
 # All actions accessible to an admin are accessible to a super admin.
-admin_roles = ['admin', super_admin_roles]
+admin_roles = ['admin']
+admin_roles.extend(super_admin_roles)
 
 # All actions accessible to users are accessible to any admin or super admin.
-user_roles = ['user', admin_roles]
+user_roles = ['user']
+user_roles.extend(admin_roles)
 
 
 def is_super_admin(user_role):
