686dbec90c953ee91872492196dc66d044209e6f
Nicholas <neatnick27@gmail.com>
2020-02-20 18:10:01 +0000

    Graphql resolvers scans (#121)
    * Started initial work on scans resolvers
    
    * Completed initial scans resolvers
    
    * Tests completed for initial scans resolvers
    
    * Continued work on tests
    
    * - Tests have been updated to match with new standards
    
    * Switched to mock data

--- api/queries.py
+++ api/queries.py
@@ -21,138 +21,178 @@ from schemas.sectors import Sectors
 from schemas.groups import Groups
 from schemas.organizations import Organizations
 from schemas.domains import Domains
-
+from schemas.scans import Scans
 
 from resolvers.sectors import (
-	resolve_get_sector_by_id,
-	resolve_get_sectors_by_sector,
-	resolve_get_sector_by_zone
+    resolve_get_sector_by_id,
+    resolve_get_sectors_by_sector,
+    resolve_get_sector_by_zone
 )
 
 from resolvers.groups import (
-	resolve_get_group_by_id,
-	resolve_get_group_by_group,
-	resolve_get_group_by_sector
+    resolve_get_group_by_id,
+    resolve_get_group_by_group,
+    resolve_get_group_by_sector
 
 )
 
 from resolvers.users import (
-	resolve_test_user_claims,
-	resolve_generate_otp_url,
+    resolve_test_user_claims,
+    resolve_generate_otp_url,
 )
 
 from resolvers.organizations import (
-	resolve_get_org_by_id,
-	resolve_get_org_by_org,
-	resolve_get_orgs_by_group
+    resolve_get_org_by_id,
+    resolve_get_org_by_org,
+    resolve_get_orgs_by_group
 )
 
 from resolvers.domains import (
-	resolve_get_domain_by_id,
-	resolve_get_domain_by_domain,
-	resolve_get_domain_by_organization
+    resolve_get_domain_by_id,
+    resolve_get_domain_by_domain,
+    resolve_get_domain_by_organization
 )
 
+from resolvers.scans import (
+    resolve_get_scan_by_id,
+    resolve_get_scans_by_date,
+    resolve_get_scans_by_date_range,
+    resolve_get_scans_by_domain,
+    resolve_get_scans_by_user_id
+)
+
+
 class Query(graphene.ObjectType):
-	"""The central gathering point for all of the GraphQL queries."""
-	node = relay.Node.Field()
-	all_users = SQLAlchemyConnectionField(UserConnection, sort=None)
-	# all_users = graphene.List(UserObject, failedAttempts=graphene.Int(), resolver=resolve_all_users)
-	# all_sectors = SQLAlchemyConnectionField(SectorsConnection, sort=None)
-	# all_groups = SQLAlchemyConnectionField(GroupsConnection, sort=None)
-	get_sector_by_id = graphene.List(
-		of_type=Sectors,
-		id=graphene.Argument(graphene.Int, required=True),
-		resolver=resolve_get_sector_by_id,
-		description="Allows selection of a sector from a given sector ID"
-	)
-	get_sectors_by_sector = graphene.List(
-		of_type=Sectors,
-		sector=graphene.Argument(SectorEnums, required=True),
-		resolver=resolve_get_sectors_by_sector,
-		description="Allows selection of sector information from a given sector enum"
-	)
-	get_sector_by_zone = graphene.List(
-		of_type=Sectors,
-		zone=graphene.Argument(ZoneEnums, required=True),
-		resolver=resolve_get_sector_by_zone,
-		description="Allows selection of all sectors from a given zone enum"
-	)
-	get_group_by_id = graphene.List(
-		of_type=Groups,
-		id=graphene.Argument(graphene.Int, required=True),
-		resolver=resolve_get_group_by_id,
-		description="Allows selection of a group from a given group ID"
-	)
-	get_group_by_group = graphene.List(
-		of_type=Groups,
-		group=graphene.Argument(GroupEnums, required=True),
-		resolver=resolve_get_group_by_group,
-		description="Allows the selection of group information from a given group enum"
-	)
-	get_group_by_sector = graphene.List(
-		of_type=Groups,
-		sector=graphene.Argument(SectorEnums, required=True),
-		resolver=resolve_get_group_by_sector,
-		description="Allows selection of groups from a given sector enum"
-	)
-	get_org_by_id = graphene.List(
-		of_type=Organizations,
-		id=graphene.Argument(graphene.Int, required=True),
-		resolver=resolve_get_org_by_id,
-		description="Allows the selection of an organization from a given ID"
-	)
-	get_org_by_org = graphene.List(
-		of_type=Organizations,
-		org=graphene.Argument(OrganizationsEnum, required=True),
-		resolver=resolve_get_org_by_org,
-		description="Allows the selection of an organization from its given organization code"
-	)
-	get_org_by_group = graphene.List(
-		of_type=Organizations,
-		group=graphene.Argument(GroupEnums, required=True),
-		resolver=resolve_get_orgs_by_group,
-		description="Allows the selection of organizations from a given group"
-	)
-	get_domain_by_id = graphene.List(
-		of_type=Domains,
-		id=graphene.Argument(graphene.Int, required=True),
-		resolver=resolve_get_domain_by_id,
-		description="Allows the selection of a domain from a given ID"
-	)
-	get_domain_by_domain = graphene.List(
-		of_type=Domains,
-		url=graphene.Argument(URL, required=True),
-		resolver=resolve_get_domain_by_domain,
-		description="Allows the selection of a domain from a given domain"
-	)
-	get_domain_by_organization = graphene.List(
-		of_type=Domains,
-		org=graphene.Argument(OrganizationsEnum, required=True),
-		resolver=resolve_get_domain_by_organization,
-		description="Allows the selection of domains under an organization"
-	)
+    """The central gathering point for all of the GraphQL queries."""
+    node = relay.Node.Field()
+    all_users = SQLAlchemyConnectionField(UserConnection, sort=None)
+    # all_users = graphene.List(UserObject, failedAttempts=graphene.Int(), resolver=resolve_all_users)
+    # all_sectors = SQLAlchemyConnectionField(SectorsConnection, sort=None)
+    # all_groups = SQLAlchemyConnectionField(GroupsConnection, sort=None)
+    get_sector_by_id = graphene.List(
+        of_type=Sectors,
+        id=graphene.Argument(graphene.Int, required=True),
+        resolver=resolve_get_sector_by_id,
+        description="Allows selection of a sector from a given sector ID"
+    )
+    get_sectors_by_sector = graphene.List(
+        of_type=Sectors,
+        sector=graphene.Argument(SectorEnums, required=True),
+        resolver=resolve_get_sectors_by_sector,
+        description="Allows selection of sector information from a given sector enum"
+    )
+    get_sector_by_zone = graphene.List(
+        of_type=Sectors,
+        zone=graphene.Argument(ZoneEnums, required=True),
+        resolver=resolve_get_sector_by_zone,
+        description="Allows selection of all sectors from a given zone enum"
+    )
+    get_group_by_id = graphene.List(
+        of_type=Groups,
+        id=graphene.Argument(graphene.Int, required=True),
+        resolver=resolve_get_group_by_id,
+        description="Allows selection of a group from a given group ID"
+    )
+    get_group_by_group = graphene.List(
+        of_type=Groups,
+        group=graphene.Argument(GroupEnums, required=True),
+        resolver=resolve_get_group_by_group,
+        description="Allows the selection of group information from a given group enum"
+    )
+    get_group_by_sector = graphene.List(
+        of_type=Groups,
+        sector=graphene.Argument(SectorEnums, required=True),
+        resolver=resolve_get_group_by_sector,
+        description="Allows selection of groups from a given sector enum"
+    )
+    get_org_by_id = graphene.List(
+        of_type=Organizations,
+        id=graphene.Argument(graphene.Int, required=True),
+        resolver=resolve_get_org_by_id,
+        description="Allows the selection of an organization from a given ID"
+    )
+    get_org_by_org = graphene.List(
+        of_type=Organizations,
+        org=graphene.Argument(OrganizationsEnum, required=True),
+        resolver=resolve_get_org_by_org,
+        description="Allows the selection of an organization from its given organization code"
+    )
+    get_org_by_group = graphene.List(
+        of_type=Organizations,
+        group=graphene.Argument(GroupEnums, required=True),
+        resolver=resolve_get_orgs_by_group,
+        description="Allows the selection of organizations from a given group"
+    )
+    get_domain_by_id = graphene.List(
+        of_type=Domains,
+        id=graphene.Argument(graphene.Int, required=True),
+        resolver=resolve_get_domain_by_id,
+        description="Allows the selection of a domain from a given ID"
+    )
+    get_domain_by_domain = graphene.List(
+        of_type=Domains,
+        url=graphene.Argument(URL, required=True),
+        resolver=resolve_get_domain_by_domain,
+        description="Allows the selection of a domain from a given domain"
+    )
+    get_domain_by_organization = graphene.List(
+        of_type=Domains,
+        org=graphene.Argument(OrganizationsEnum, required=True),
+        resolver=resolve_get_domain_by_organization,
+        description="Allows the selection of domains under an organization"
+    )
+    get_scan_by_id = graphene.List(
+        of_type=Scans,
+        id=graphene.Argument(graphene.Int, required=True),
+        resolver=resolve_get_scan_by_id,
+        description="Allows the selection of a scan from a given scan ID"
+    )
+    get_scans_by_date = graphene.List(
+        of_type=Scans,
+        date=graphene.Argument(graphene.Date, required=True),
+        resolver=resolve_get_scans_by_date,
+        description="Allows selection of scans on a given date"
+    )
+    get_scans_by_date_range = graphene.List(
+        of_type=Scans,
+        startDate=graphene.Argument(graphene.Date, required=True),
+        endDate=graphene.Argument(graphene.Date, required=True),
+        resolver=resolve_get_scans_by_date_range,
+        description="Allows selection of scans from a given date range"
+    )
+    get_scans_by_domain = graphene.List(
+        of_type=Scans,
+        url=graphene.Argument(URL, required=True),
+        resolver=resolve_get_scans_by_domain,
+        description="Allows selection of scans from a given URL"
+    )
+    get_scans_by_user_id = graphene.List(
+        of_type=Scans,
+        id=graphene.Argument(graphene.Int, required=True),
+        resolver=resolve_get_scans_by_user_id,
+        description="Allows selection of scans initiated by a given user"
+    )
 
-	generate_otp_url = graphene.String(
-		email=graphene.Argument(EmailAddress, required=True),
-		resolver=resolve_generate_otp_url,
-		description="An api endpoint used to generate a OTP url used for two factor authentication."
-	)
+    generate_otp_url = graphene.String(
+        email=graphene.Argument(EmailAddress, required=True),
+        resolver=resolve_generate_otp_url,
+        description="An api endpoint used to generate a OTP url used for two factor authentication."
+    )
 
-	test_user_claims = graphene.String(
-		token=graphene.Argument(graphene.String, required=True),
-		resolver=resolve_test_user_claims,
-		description="An api endpoint to view a current user's claims -- Requires an active JWT."
-	)
+    test_user_claims = graphene.String(
+        token=graphene.Argument(graphene.String, required=True),
+        resolver=resolve_test_user_claims,
+        description="An api endpoint to view a current user's claims -- Requires an active JWT."
+    )
 
 
 class Mutation(graphene.ObjectType):
-	"""The central gathering point for all of the GraphQL mutations."""
-	create_user = CreateUser.Field()
-	sign_in = SignInUser.Field()
-	update_password = UpdateUserPassword.Field()
-	authenticate_two_factor = ValidateTwoFactor.Field()
-	update_user_role = UpdateUserRole.Field()
+    """The central gathering point for all of the GraphQL mutations."""
+    create_user = CreateUser.Field()
+    sign_in = SignInUser.Field()
+    update_password = UpdateUserPassword.Field()
+    authenticate_two_factor = ValidateTwoFactor.Field()
+    update_user_role = UpdateUserRole.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
---
+++ api/resolvers/scans.py
@@ -0,0 +1,94 @@
+from graphql import GraphQLError
+from sqlalchemy.orm import load_only
+import sqlalchemy as sq
+from sqlalchemy import between
+
+from schemas.scans import (
+    Scans,
+    ScanModel
+)
+
+from schemas.domains import (
+    Domains,
+    DomainModel
+)
+
+from schemas.user import (
+    User as UserModel,
+    UserObject as User
+)
+
+
+# Resolvers
+def resolve_get_scan_by_id(self, info, **kwargs):
+    """Return a scan by its ID"""
+    scan_id = kwargs.get('id')
+    query = Scans.get_query(info).filter(
+        ScanModel.id == scan_id
+    )
+    if not len(query.all()):
+        raise GraphQLError("Error, Invalid ID")
+    return query.all()
+
+
+def resolve_get_scans_by_date(self, info, **kwargs):
+    """Return a list of scans on a certain date"""
+    date = kwargs.get('date')
+    query = Scans.get_query(info).filter(
+        sq.func.date_trunc('day', ScanModel.scan_date) == date
+    )
+    if not len(query.all()):
+        raise GraphQLError("Error, No scans occurred on that date")
+    return query.all()
+
+
+def resolve_get_scans_by_date_range(self, info, **kwargs):
+    """Return a list of scans from a date range"""
+    start_date = kwargs.get('startDate')
+    end_date = kwargs.get('endDate')
+    query = Scans.get_query(info).filter(
+        sq.func.date_trunc('day', ScanModel.scan_date) >= start_date
+    ).filter(
+        sq.func.date_trunc('day', ScanModel.scan_date) <= end_date
+    )
+    if not len(query.all()):
+        raise GraphQLError("Error, No scans in that date range")
+    return query.all()
+
+
+def resolve_get_scans_by_domain(self, info, **kwargs):
+    """Return a list of scans based on a domain id"""
+    domain = kwargs.get('url')
+    domain_id = Domains.get_query(info).filter(
+        DomainModel.domain == domain
+    ).options(load_only('id'))
+
+    if not len(domain_id.all()):
+        raise GraphQLError("Error, no domain associated with that URL")
+
+    query = Scans.get_query(info).filter(
+        ScanModel.domain_id == domain_id
+    )
+
+    if not len(query.all()):
+        raise GraphQLError("Error, no scans associated with that domain")
+    return query.all()
+
+
+def resolve_get_scans_by_user_id(self, info, **kwargs):
+    """Return a list of scans based on a user id"""
+    user = kwargs.get('id')
+    user_id = User.get_query(info).filter(
+        UserModel.id == user
+    ).options(load_only('id'))
+
+    if not len(user_id.all()):
+        raise GraphQLError("Error, cannot find user")
+
+    query = Scans.get_query(info).filter(
+        ScanModel.initiated_by == user_id
+    )
+
+    if not len(query.all()):
+        raise GraphQLError("Error, no scans initiated by that user")
+    return query.all()
