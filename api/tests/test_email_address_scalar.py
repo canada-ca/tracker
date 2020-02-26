a518244273266841e99297ce8a3d5620470baf1b
nick <neatnick27@gmail.com>
2020-02-26 14:00:38 -0400

    Styling

--- api/tests/test_email_address_scalar.py
+++ api/tests/test_email_address_scalar.py
@@ -15,8 +15,11 @@ SCRIPT_DIR = os.path.dirname(
     os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-from scalars.email_address import EmailAddress, scalar_error_type, \
+from scalars.email_address import (
+    EmailAddress,
+    scalar_error_type,
     scalar_error_only_types
+)
 
 
 class TestEmailAddressScalar(unittest.TestCase):
