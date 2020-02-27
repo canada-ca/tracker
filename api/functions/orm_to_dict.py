02df3c111a17c8d5458f8b7d020d34e4b1bd7543
nick <neatnick27@gmail.com>
2020-02-27 09:20:20 -0400

    Removed counter

--- api/functions/orm_to_dict.py
+++ api/functions/orm_to_dict.py
@@ -1,10 +1,8 @@
 def orm_to_dict(sql):
     rtr_list = []
-    counter = 0
     for row in sql:
         row_contents = {}
         for column in row.__table__.columns:
             row_contents[column.name] = getattr(row, column.name)
-        counter += 1
         rtr_list.append(row_contents)
     return rtr_list
