e0de9e7d49b3a1f3f3d857a8d90d34896dc5fd16
Ethan Foss <fossliam@outlook.com>
2020-02-28 11:42:31 -0400

    Rough outline of result parsing for SSL and HTTPS

--- platform/overlays/knative/images/dkim/dkim_scanner.py
+++ platform/overlays/knative/images/dkim/dkim_scanner.py
@@ -3,10 +3,7 @@ import sys
 import requests
 import logging
 import json
-from sslyze.ssl_settings import TlsWrappedProtocolEnum
-from sslyze.server_connectivity_tester import ServerConnectivityError, ServerConnectivityTester
-from sslyze.plugins.openssl_cipher_suites_plugin import Tlsv12ScanCommand, Tlsv10ScanCommand
-from sslyze.synchronous_scanner import SynchronousScanner
+from checkdmarc import *
 from flask import Flask, request
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -29,9 +26,10 @@ def dispatch():
         domain = request.json['domain']
         res = scan(scan_id, domain)
         if res is not None:
-            payload = json.dumps({"results": str(res), "scan_type": "ssl"})
+            payload = json.dumps({"results": str(res), "scan_type": "dkim"})
+
         else:
-            raise Exception("(SCAN: %s) - An error occurred while attempting to establish SSL connection" % scan_id)
+            raise Exception("(SCAN: %s) - An error occurred while attempting to perform checkdmarc scan" % scan_id)
         try:
             response = requests.post('http://34.67.57.19/dispatch', headers=headers, data=payload)
             logging.info("Scan %s completed. Results queued for processing...\n" % scan_id)
@@ -45,26 +43,12 @@ def dispatch():
 def scan(scan_id, domain):
 
     try:
-        server_tester = ServerConnectivityTester(hostname=domain)
-
-        logging.info("\nTesting connectivity with %s:%s..." % (server_tester.hostname, server_tester.port))
-        server_info = server_tester.perform()
-        logging.info("Server Info %s\n" % server_info)
-    except ServerConnectivityError as e:
-        # Could not establish an SSL connection to the server
-        logging.error("Could not connect to %s: %s" % (e.server_info.hostname, e.error_message))
+        scan_result = check_domains(domain)
+    except (DNSException, SPFError, DMARCError) as e:
+        logging.error("(SCAN: %s) - Failed to check the given domains for DMARC/SPF records: %s" % (scan_id,e))
         return None
 
-    command = Tlsv10ScanCommand()
-
-    synchronous_scanner = SynchronousScanner()
-
-    scan_result = synchronous_scanner.run_scan_command(server_info, command)
-    for cipher in scan_result.accepted_cipher_list:
-        logging.info("    %s" % cipher.name)
-
     return scan_result
-
 
 if __name__ == "__main__":
     # Port number defaults to 8080, can be configured as an ENV
