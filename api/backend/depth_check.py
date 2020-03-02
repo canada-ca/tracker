fda995b574ea3c6d49648fd12d4c0d1af7cb9105
nick <neatnick27@gmail.com>
2020-03-02 09:31:51 -0400

    Implemented better depth check analysis

--- api/backend/depth_check.py
+++ api/backend/depth_check.py
@@ -1,47 +1,100 @@
+from typing import (
+    Union,
+    Optional,
+    Any,
+    Dict,
+    List
+)
+from graphql import GraphQLDocument, GraphQLSchema
 from graphql.backend.core import GraphQLCoreBackend
-from graphql.language.ast import FragmentSpread, FragmentDefinition
+from graphql.language.ast import (
+    Document,
+    FragmentSpread,
+    FragmentDefinition,
+    OperationDefinition,
+    Node,
+    FragmentSpread,
+    Field,
+    InlineFragment
+)
 
 
-def measure_depth(selection_set, level=1):
+class DepthLimitReadched(Exception):
+    pass
+
+
+def get_fragments(definitions) -> Dict[str, FragmentDefinition]:
+    return {
+        definition.name.value: definition
+        for definition in definitions
+        if isinstance(definition, FragmentDefinition)
+    }
+
+
+def get_queries_and_mutations(definitions) -> List[OperationDefinition]:
+    return [
+        definition
+        for definition in definitions
+        if isinstance(definition, OperationDefinition)
+    ]
+
+
+def measure_depth(node: Node, fragments: Dict[str, FragmentDefinition]) -> int:
     """
-    This function calculates the depth of queries that have been requested from
-    the user.
-    :param selection_set: The query requested by a user
-    :param level: This is the max depth you want to check for
-    :return: Returns the depth of the current selection
+    A function which recursively measures the depth of a Graphene Query
+    :type node: Node
+    :param node: Graphql-core object used for query traversal/indexing
+    :type fragments: dict
+    :param fragments: The fragments of the query
+    :rtype: int
+    :return: The max depth of the node
     """
-    max_depth = level
-    for field in selection_set.selections:
-        if not isinstance(field, FragmentSpread):
-            if field.selection_set:
-                new_depth = measure_depth(field.selection_set, level=level + 1)
-                if new_depth > max_depth:
-                    max_depth = new_depth
-    return max_depth
+    if isinstance(node, FragmentSpread):
+        fragment = fragments.get(node.name.value)
+        return measure_depth(node=fragment, fragments=fragments)
+
+    elif isinstance(node, Field):
+        if node.name.value.lower() in ["__schema", "__introspection"]:
+            return 0
+        if not node.selection_set:
+            return 1
+        depths = []
+        for selection in node.selection_set.selections:
+            depth = measure_depth(node=selection, fragments=fragments)
+            depths.append(depth)
+        return 1 + max(depths)
+    elif (
+        isinstance(node, FragmentDefinition)
+        or isinstance(node, OperationDefinition)
+        or isinstance(node, InlineFragment)
+    ):
+        depths = []
+        for selection in node.selection_set.selections:
+            depth = measure_depth(node=selection, fragments=fragments)
+            depths.append(depth)
+        return max(depths)
+    else:
+        raise Exception("Unknown node")
+
+
+def check_max_depth(max_depth: int, document: Document):
+    fragments = get_fragments(document.definitions)
+    queries = get_queries_and_mutations(document.definitions)
+
+    for query in queries:
+        depth = measure_depth(query, fragments)
+        if depth > max_depth:
+            raise DepthLimitReadched(
+                'Query is too complex'
+            )
 
 
 class DepthAnalysisBackend(GraphQLCoreBackend):
-    def __init__(self, max_depth=10):
-        super().__init__()
+    def __init__(self, max_depth=10, executor: Optional[Any] = None):
+        super().__init__(executor=executor)
         self.max_depth = max_depth
 
-    def document_from_string(self, schema, document_string):
-        """
-        This function checks to see if the current query maxes the maximum depth
-        to prevent complexity attacks
-        :param schema: The schema of the application
-        :param document_string: The request from the user
-        :return: If the test passes it wil return the requested information, if
-        the test fails it will raise an exception and inform the user
-        """
+    def document_from_string(self, schema: GraphQLSchema, document_string: Union[Document, str]) -> GraphQLDocument:
         document = super().document_from_string(schema, document_string)
-        ast = document.document_ast
-        for definition in ast.definitions:
-            # We are only interested in queries
-            if not isinstance(definition, FragmentDefinition):
-                if not isinstance(definition, FragmentSpread):
-                    depth = measure_depth(definition.selection_set)
-                    if depth > self.max_depth:  # set your depth max here
-                        raise Exception('Query is too complex')
-
+        check_max_depth(max_depth=self.max_depth, document=document.document_ast)
         return document
