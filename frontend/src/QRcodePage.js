bda3e761dc09ba002acbc30e155254f1464da7e7
Matt Peachey <peacheym@dal.ca>
2020-03-06 08:35:50 -0400

    Update props and pretty code

--- frontend/src/QRcodePage.js
+++ frontend/src/QRcodePage.js
@@ -1,18 +1,18 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import {Box, Stack, Text, Button} from '@chakra-ui/core'
-import {Link as RouteLink} from "react-router-dom";
+import { Box, Stack, Text, Button } from '@chakra-ui/core'
+import { Link as RouteLink } from "react-router-dom";
 
 var QRCode = require('qrcode.react');
 
-export function QRcodePage(props){
+export function QRcodePage({url}){
   return (
     <Stack spacing={4} mx="auto" alignItems="center">
 
       <Text alignItems="center" mx="auto" fontSize="2xl" >Scan this QR code with a 2FA app like Authy or Google Authenticator</Text>
 
       <Box mt={6} mx="auto">
-        <QRCode value={props.url} size={256} />
+        <QRCode value={url} size={256} />
       </Box>
 
       <Text mt={6} alignItems="center" mx="auto" fontSize="lg" >Your 2FA app will then have a valid code that you can use when you sign in.</Text>
