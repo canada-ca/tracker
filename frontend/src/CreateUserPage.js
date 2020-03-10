5c35251851d6067bcc28277620a1940ee3325be8
Matt Peachey <PeacheyM@dal.ca>
2020-03-10 14:01:02 -0300

    QRcode generated via GQL backend & Test (#154)
    * Generate an OTP QR code from userName prop
    
    * User is redirected to the 2FA code page on successful account creation
    
    * Re-add npm package qrcode.react to git
    
    * Add render test for this component
    
    * Add fake data to generate OTP code
    
    * Update CreateUserPage.js
    
    * Update CreateUserPage.js

--- frontend/src/CreateUserPage.js
+++ frontend/src/CreateUserPage.js
@@ -30,8 +30,12 @@ export function CreateUserPage(){
     if(data.error){
       console.log(error);
     }
-    console.log(data.createUser.user)
-    // Do something with the data.  Ie: Redirect if no error?
+
+    /* Check that a user's 2fa needs validation before this redirection.
+    return <Redirect to={{
+      pathname: '/two-factor-code',
+      state: {userName: data.createUser.userName},
+    }}/> */
   }
 
   /* A function for the Formik to validate fields in the form */
