f3e975073aa5e44c60620517302c75e30ad46f46
nick <neatnick27@gmail.com>
2020-03-11 10:05:49 -0300

    Started work on SSL tags

--- api/schemas/domain/www_scan/ssl/__init__.py
+++ api/schemas/domain/www_scan/ssl/__init__.py
@@ -6,6 +6,8 @@ from models import Ssl_scans
 
 from scalars.url import URL
 
+from schemas.domain.www_scan.ssl.ssl_tags import SSLTags
+
 from functions.get_domain import get_domain
 from functions.get_timestamp import get_timestamp
 
@@ -20,6 +22,7 @@ class SSL(SQLAlchemyObjectType):
     id = graphene.ID()
     domain = URL()
     timestamp = graphene.DateTime()
+    ssl_guidance_tags = graphene.List(lambda: SSLTags)
 
     with app.app_context():
         def resolve_domain(self, info):
@@ -27,3 +30,6 @@ class SSL(SQLAlchemyObjectType):
 
         def resolve_timestamp(self, info):
             return get_timestamp(self, info)
+
+        def resolve_ssl_guidance_tags(self, info):
+            return SSLTags.get_query(info).all()
---
+++ api/schemas/domain/www_scan/ssl/ssl_tags.py
@@ -0,0 +1,41 @@
+import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType
+
+from app import app
+from models import Ssl_scans
+
+
+class SSLTags(SQLAlchemyObjectType):
+    class Meta:
+        model = Ssl_scans
+        exclude_fields = (
+            "id",
+            "ssl_scan"
+        )
+    value = graphene.String()
+
+    with app.app_context():
+        def resolve_value(self: Ssl_scans, info):
+            tags = {}
+
+            if "missing" in self.ssl_scan:
+                return tags.update({"ssl2": "SSL-missing"})
+
+            if self.ssl_scan["ssl"]["rc4"] == "True":
+                tags.update({"ssl4": "SSL-rc4"})
+
+            if self.ssl_scan["ssl"]["3des"] == "True":
+                tags.update({"ssl5": "SSL-3des"})
+
+            if self.ssl_scan["ssl"]["starttls"] == "True":
+                tags.update({"ssl6": "SSL-starttls"})
+
+            # Check Ciphers
+
+            if self.ssl_scan["ssl"]["heartbleed"] == "True":
+                tags.update({"ssl9": "Vulnerability-heartbleed"})
+
+            if self.ssl_scan["ssl"]["openssl_ccs_injection"] == "True":
+                tags.update({"ssl10": "Vulnerability-ccs-injection"})
+
+            return tags
