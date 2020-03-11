6e6f84b3afe0710d60dc24900a96afa880247a68
nick <neatnick27@gmail.com>
2020-03-11 09:44:13 -0300

    Worked continued on dkim tags

--- api/schemas/domain/email_scan/dkim/dkim_tags.py
+++ api/schemas/domain/email_scan/dkim/dkim_tags.py
@@ -1,8 +1,15 @@
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType
+import datetime
+from dateutil.relativedelta import relativedelta
 
 from app import app
-from models import Dkim_scans
+from db import db
+from models import (
+    Scans,
+    Dkim_scans,
+    Mx_scans
+)
 
 
 class DkimTags(SQLAlchemyObjectType):
@@ -21,7 +28,10 @@ class DkimTags(SQLAlchemyObjectType):
             if "missing" in self.dkim_scan:
                 return tags.update({"dkim2": "missing"})
 
-            if self.dkim_scan["dkim"]["key_size"] >= 2048 \
+            if self.dkim_scan["dkim"]["key_size"] >= 4096 \
+            and self.dkim_scan["dkim"]["key_type"] == "rsa":
+                tags.update({"dkim8": "P-4096"})
+            elif self.dkim_scan["dkim"]["key_size"] == 2048 \
             and self.dkim_scan["dkim"]["key_type"] == "rsa":
                 tags.update({"dkim5": "P-2048"})
             elif self.dkim_scan["dkim"]["key_size"] == 1024 \
@@ -32,3 +42,34 @@ class DkimTags(SQLAlchemyObjectType):
                 tags.update({"dkim3": "P-sub1024"})
             else:
                 tags.update({"dkim6": "P-invalid"})
+
+            # Get Current Scan Timestamp
+            current_scan = db.session.query(Scans).filter(
+                Scans.id == self.id
+            ).first()
+
+            # Get One Year Difference
+            curr_date_time = datetime.datetime.fromtimestamp(current_scan.scan_date)
+            pre_date_time = curr_date_time - relativedelta(years=1)
+
+            # Get all scans in one year range
+            scans = db.session.query(Scans).filter(
+                Scans.scan_date < curr_date_time
+            ).filter(
+                Scans.scan_date >= pre_date_time
+            ).all()
+
+            # Get the current modulus for checking and assign bool check
+            curr_modulus = self.dkim_scan["dkim"]["something"]
+            needs_rotation = True
+
+            # Loop through all the scans and compare their modulus
+            for scan in scans:
+                check_modulus = db.session.query(Dkim_scans).filter(
+                    Dkim_scans.id == scan.id
+                ).first()
+                if curr_modulus != check_modulus.dkim_scan["dkim"]["something"]:
+                    needs_rotation = False
+
+            if needs_rotation:
+                tags.update({"dkim10": "P-update-recommended"})
