668b9acd3d87623db950d0f1aa0c9ac4d93285f3
nick <neatnick27@gmail.com>
2020-03-12 08:13:25 -0300

    Fixed missing check

--- api/schemas/domain/email_scan/spf/spf_tags.py
+++ api/schemas/domain/email_scan/spf/spf_tags.py
@@ -21,7 +21,7 @@ class SPFTags(SQLAlchemyObjectType):
         def resolve_value(self: Spf_scans, info):
             tags = {}
 
-            if self.spf_scan["spf"]["missing"]:
+            if 'missing' in self.spf_scan["spf"]:
                 return tags.update({"spf2": "missing"})
 
             # Check all tag
