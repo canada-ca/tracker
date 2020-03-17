6040ce94bbf9bef92cc802fb6028c5401f552ded
nick <neatnick27@gmail.com>
2020-03-17 13:13:10 -0300

    Added comments to user resolver

--- api/resolvers/user.py
+++ api/resolvers/user.py
@@ -30,10 +30,10 @@ def resolve_user(self, info, **kwargs):
     This function is used to resolve a users information such as user profile,
     etc. If an email address argument is present then the user will have to be
     an admin belonging to the same org to see the users information.
-    :param self:
-    :param info:
-    :param kwargs:
-    :return:
+    :param self: User SQLAlchemyObject type defined in the schemas directory
+    :param info: Request information sent to the sever from a client
+    :param kwargs: Field arguments and user_roles
+    :return: Filtered User SQLAlchemyObject Type
     """
     # Get information from kwargs
     user_id = kwargs.get('user_id')
@@ -48,40 +48,71 @@ def resolve_user(self, info, **kwargs):
     # Get initial query
     query = UserSchema.get_query(info)
 
+    # Check to see if user is requesting a specific user
     if user_name is not None:
+        # Get the requested user
         req_user_orm = db.session.query(Users).filter(
             Users.user_name == user_name
         ).options(load_only('id')).first()
 
+        # Check to see if user actually exists
         if req_user_orm is None:
             raise GraphQLError('Error, user cannot be found')
         else:
             req_user_id = req_user_orm.id
 
+        # Get org id's that the user belongs to
         req_org_orm = db.session.query(User_affiliations).filter(
             User_affiliations.user_id == req_user_id
-        ).options(load_only('organization_id')).first()
+        ).options(load_only('organization_id')).all()
 
+        # Check to ensure the user belongs to at least one organization
         if req_org_orm is None:
             raise GraphQLError("Error, user does not belong to any organization")
         else:
-            req_org_id = req_org_orm.organization_id
+            # Compile list of org id's the user belongs to
+            req_user_org_id_list = []
+            for org_id in req_org_orm:
+                req_user_org_id_list.append(org_id.organization_id)
 
+        # Check to see if the requested user is a super admin and if true return
         if is_super_admin(user_role=user_roles):
             return query.filter(Users.id == req_user_id)
 
-        if req_org_id in org_id_list:
-            if is_admin(user_role=user_roles, org_id=req_org_id):
-                return query.filter(Users.id == req_user_id)
-            else:
-                raise GraphQLError(
-                    "Error, you do not have permission to view this users"
-                    " information"
-                )
-        else:
+        # Declare return list, and check
+        rtn_query = []
+        user_check = True
+
+        # Go through each org id that the user belongs to
+        for req_org_id in req_user_org_id_list:
+            # Check to see if the requesting user and requested user belong to
+            # the same org
+            if req_org_id in org_id_list:
+                # Set check flag
+                user_check = False
+
+                # Check to see if the requesting user has admin rights to org
+                if is_admin(user_role=user_roles, org_id=req_org_id):
+                    # If admin and user share multiple orgs compile list and
+                    # return
+                    rtn_query.append(
+                        query.filter(
+                            Users.id == req_user_id
+                        ).first()
+                    )
+                else:
+                    raise GraphQLError(
+                        "Error, you do not have permission to view this users"
+                        " information"
+                    )
+                return rtn_query
+
+        # Give error if requesting user and requested user do not share an org
+        if user_check:
             raise GraphQLError(
                 "Error, user does not belong to any of your organizations"
             )
+    # Return user profile of requesting user
     else:
         return query.filter(Users.id == user_id)
 
