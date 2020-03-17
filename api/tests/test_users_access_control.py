252ef95bf30fc8e304cfaded039c82de630de0e7
nick <neatnick27@gmail.com>
2020-03-17 14:57:04 -0300

    Continued working on users query testings

---
+++ api/tests/test_users_access_control.py
@@ -0,0 +1,323 @@
+import sys
+import os
+from os.path import dirname, join, expanduser, normpath, realpath
+
+import pytest
+from flask import Request
+from graphene.test import Client
+from flask_bcrypt import Bcrypt
+
+from unittest import TestCase
+
+from werkzeug.test import create_environ
+
+from manage import seed, remove_seed
+
+seed()
+from app import app
+from db import db
+from models import Organizations, Domains, Users, User_affiliations
+from queries import schema
+from backend.security_check import SecurityAnalysisBackend
+remove_seed()
+
+# This is the only way I could get imports to work for unit testing.
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = dirname(realpath(join(os.getcwd(), expanduser(__file__))))
+sys.path.append(normpath(join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+
+@pytest.fixture(scope='class')
+def org_perm_test_db_init():
+    db.init_app(app)
+    bcrypt = Bcrypt(app)
+
+    with app.app_context():
+        test_user = Users(
+            id=1,
+            display_name="testuserread",
+            user_name="testuserread@testemail.ca",
+            user_password=bcrypt.generate_password_hash(
+                password="testpassword123").decode("UTF-8"),
+        )
+        db.session.add(test_user)
+        test_super_admin = Users(
+            id=2,
+            display_name="testsuperadmin",
+            user_name="testsuperadmin@testemail.ca",
+            user_password=bcrypt.generate_password_hash(
+                password="testpassword123").decode("UTF-8")
+        )
+        db.session.add(test_super_admin)
+        test_admin = Users(
+            id=3,
+            display_name="testadmin",
+            user_name="testadmin@testemail.ca",
+            user_password=bcrypt.generate_password_hash(
+                password="testpassword123").decode("UTF-8")
+        )
+        db.session.add(test_admin)
+        test_admin = Users(
+            id=4,
+            display_name="testadmin2",
+            user_name="testadmin2@testemail.ca",
+            user_password=bcrypt.generate_password_hash(
+                password="testpassword123").decode("UTF-8")
+        )
+        db.session.add(test_admin)
+
+        org = Organizations(
+            id=1,
+            acronym='ORG1'
+        )
+        db.session.add(org)
+        org = Organizations(
+            id=2,
+            acronym='ORG2'
+        )
+        db.session.add(org)
+        org = Organizations(
+            id=3,
+            acronym='ORG3'
+        )
+        db.session.add(org)
+
+        test_admin_role = User_affiliations(
+            user_id=1,
+            organization_id=1,
+            permission='user_read'
+        )
+        db.session.add(test_admin_role)
+        test_admin_role = User_affiliations(
+            user_id=2,
+            organization_id=2,
+            permission='super_admin'
+        )
+        db.session.add(test_admin_role)
+        test_admin_role = User_affiliations(
+            user_id=3,
+            organization_id=1,
+            permission='admin'
+        )
+        db.session.add(test_admin_role)
+        test_admin_role = User_affiliations(
+            user_id=4,
+            organization_id=2,
+            permission='admin'
+        )
+        db.session.add(test_admin_role)
+        db.session.commit()
+
+    yield
+
+    with app.app_context():
+        User_affiliations.query.delete()
+        Organizations.query.delete()
+        Users.query.delete()
+        db.session.commit()
+
+
+@pytest.mark.usefixtures('org_perm_test_db_init')
+class TestOrgResolverWithOrgsAndValues(TestCase):
+    # Super Admin Tests
+    def test_get_users_as_super_admin(self):
+        """
+        Test to see if users resolver access control allows super admin to
+        request users outside of organization
+        """
+        with app.app_context():
+            backend = SecurityAnalysisBackend()
+            client = Client(schema)
+            get_token = client.execute(
+                '''
+                mutation{
+                    signIn(userName:"testsuperadmin@testemail.ca", password:"testpassword123"){
+                        authToken
+                    }
+                }
+                ''', backend=backend)
+            assert get_token['data']['signIn']['authToken'] is not None
+            token = get_token['data']['signIn']['authToken']
+            assert token is not None
+
+            environ = create_environ()
+            environ.update(
+                HTTP_AUTHORIZATION=token
+            )
+            request_headers = Request(environ)
+
+            executed = client.execute(
+                '''
+                {
+                    users(org: ORG1) {
+                        edges {
+                            node {
+                                displayName
+                            }
+                        }
+                    }
+                }
+                ''', context_value=request_headers, backend=backend)
+            result_refr = {
+                "data": {
+                    "users": {
+                        "edges": [
+                            {
+                                "node": {
+                                    "displayName": "testuserread"
+                                }
+                            },
+                            {
+                                "node": {
+                                    "displayName": "testadmin"
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+            self.assertDictEqual(result_refr, executed)
+
+    # Admin Same Org
+    def test_get_users_from_same_org(self):
+        """
+        Test users query to see if an admin from the corresponding org can
+        retrieve the information
+        """
+        with app.app_context():
+            backend = SecurityAnalysisBackend()
+            client = Client(schema)
+            get_token = client.execute(
+                '''
+                mutation{
+                    signIn(userName:"testadmin@testemail.ca", password:"testpassword123"){
+                        authToken
+                    }
+                }
+                ''', backend=backend)
+            assert get_token['data']['signIn']['authToken'] is not None
+            token = get_token['data']['signIn']['authToken']
+            assert token is not None
+
+            environ = create_environ()
+            environ.update(
+                HTTP_AUTHORIZATION=token
+            )
+            request_headers = Request(environ)
+
+            executed = client.execute(
+                '''
+                {
+                    users(org: ORG1) {
+                        edges {
+                            node {
+                                displayName
+                            }
+                        }
+                    }
+                }
+                ''', context_value=request_headers, backend=backend)
+            result_refr = {
+                "data": {
+                    "users": {
+                        "edges": [
+                            {
+                                "node": {
+                                    "displayName": "testuserread"
+                                }
+                            },
+                            {
+                                "node": {
+                                    "displayName": "testadmin"
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+            self.assertDictEqual(result_refr, executed)
+
+    # Admin different org
+    def test_get_users_admin_from_different_org(self):
+        """
+        Test users query to see if an admin from anther org cannot
+        retrieve the information
+        """
+        with app.app_context():
+            backend = SecurityAnalysisBackend()
+            client = Client(schema)
+            get_token = client.execute(
+                '''
+                mutation{
+                    signIn(userName:"testadmin2@testemail.ca", password:"testpassword123"){
+                        authToken
+                    }
+                }
+                ''', backend=backend)
+            assert get_token['data']['signIn']['authToken'] is not None
+            token = get_token['data']['signIn']['authToken']
+            assert token is not None
+
+            environ = create_environ()
+            environ.update(
+                HTTP_AUTHORIZATION=token
+            )
+            request_headers = Request(environ)
+
+            executed = client.execute(
+                '''
+                {
+                    users(org: ORG1) {
+                        edges {
+                            node {
+                                displayName
+                            }
+                        }
+                    }
+                }
+                ''', context_value=request_headers, backend=backend)
+            assert executed['errors']
+            assert executed['errors'][0]
+            assert executed['errors'][0]['message'] == "Error, you do not have access to view this organization"
+
+    # User read tests
+    def test_get_users_user_read(self):
+        """
+        Ensure user read cannot access this query
+        """
+        with app.app_context():
+            backend = SecurityAnalysisBackend()
+            client = Client(schema)
+            get_token = client.execute(
+                '''
+                mutation{
+                    signIn(userName:"testuserread@testemail.ca", password:"testpassword123"){
+                        authToken
+                    }
+                }
+                ''', backend=backend)
+            assert get_token['data']['signIn']['authToken'] is not None
+            token = get_token['data']['signIn']['authToken']
+            assert token is not None
+
+            environ = create_environ()
+            environ.update(
+                HTTP_AUTHORIZATION=token
+            )
+            request_headers = Request(environ)
+
+            executed = client.execute(
+                '''
+                {
+                    users(org: ORG1) {
+                        edges {
+                            node {
+                                displayName
+                            }
+                        }
+                    }
+                }
+                ''', context_value=request_headers, backend=backend)
+            assert executed['errors']
+            assert executed['errors'][0]
+            assert executed['errors'][0]['message'] == "Error, you do not have access to view this organization"
