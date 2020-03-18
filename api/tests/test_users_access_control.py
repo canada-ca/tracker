7656628a236485d473a3a4855a9667dbe2a9f799
nick <neatnick27@gmail.com>
2020-03-18 06:53:55 -0300

    Completed access control tests for users resolvers

--- api/tests/test_users_access_control.py
+++ api/tests/test_users_access_control.py
@@ -28,19 +28,19 @@ sys.path.append(normpath(join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 
 @pytest.fixture(scope='class')
-def org_perm_test_db_init():
+def users_resolver_test_db_init():
     db.init_app(app)
     bcrypt = Bcrypt(app)
 
     with app.app_context():
-        test_user = Users(
+        test_read = Users(
             id=1,
             display_name="testuserread",
             user_name="testuserread@testemail.ca",
             user_password=bcrypt.generate_password_hash(
                 password="testpassword123").decode("UTF-8"),
         )
-        db.session.add(test_user)
+        db.session.add(test_read)
         test_super_admin = Users(
             id=2,
             display_name="testsuperadmin",
@@ -65,6 +65,14 @@ def org_perm_test_db_init():
                 password="testpassword123").decode("UTF-8")
         )
         db.session.add(test_admin)
+        test_write = Users(
+            id=5,
+            display_name="testuserwrite",
+            user_name="testuserwrite@testemail.ca",
+            user_password=bcrypt.generate_password_hash(
+                password="testpassword123").decode("UTF-8")
+        )
+        db.session.add(test_write)
 
         org = Organizations(
             id=1,
@@ -82,18 +90,18 @@ def org_perm_test_db_init():
         )
         db.session.add(org)
 
-        test_admin_role = User_affiliations(
+        test_user_read_role = User_affiliations(
             user_id=1,
             organization_id=1,
             permission='user_read'
         )
-        db.session.add(test_admin_role)
-        test_admin_role = User_affiliations(
+        db.session.add(test_user_read_role)
+        test_super_admin_role = User_affiliations(
             user_id=2,
             organization_id=2,
             permission='super_admin'
         )
-        db.session.add(test_admin_role)
+        db.session.add(test_super_admin_role)
         test_admin_role = User_affiliations(
             user_id=3,
             organization_id=1,
@@ -106,6 +114,12 @@ def org_perm_test_db_init():
             permission='admin'
         )
         db.session.add(test_admin_role)
+        test_user_write_role = User_affiliations(
+            user_id=5,
+            organization_id=1,
+            permission='user_write'
+        )
+        db.session.add(test_user_write_role)
         db.session.commit()
 
     yield
@@ -118,7 +132,7 @@ def org_perm_test_db_init():
 
 
 @pytest.mark.usefixtures('org_perm_test_db_init')
-class TestOrgResolverWithOrgsAndValues(TestCase):
+class TestUsersResolverAccessControl(TestCase):
     # Super Admin Tests
     def test_get_users_as_super_admin(self):
         """
@@ -170,6 +184,11 @@ class TestOrgResolverWithOrgsAndValues(TestCase):
                             {
                                 "node": {
                                     "displayName": "testadmin"
+                                }
+                            },
+                            {
+                                "node": {
+                                    "displayName": "testuserwrite"
                                 }
                             }
                         ]
@@ -230,6 +249,11 @@ class TestOrgResolverWithOrgsAndValues(TestCase):
                                 "node": {
                                     "displayName": "testadmin"
                                 }
+                            },
+                            {
+                                "node": {
+                                    "displayName": "testuserwrite"
+                                }
                             }
                         ]
                     }
@@ -250,6 +274,48 @@ class TestOrgResolverWithOrgsAndValues(TestCase):
                 '''
                 mutation{
                     signIn(userName:"testadmin2@testemail.ca", password:"testpassword123"){
+                        authToken
+                    }
+                }
+                ''', backend=backend)
+            assert get_token['data']['signIn']['authToken'] is not None
+            token = get_token['data']['signIn']['authToken']
+            assert token is not None
+
+            environ = create_environ()
+            environ.update(
+                HTTP_AUTHORIZATION=token
+            )
+            request_headers = Request(environ)
+
+            executed = client.execute(
+                '''
+                {
+                    users(org: ORG1) {
+                        edges {
+                            node {
+                                displayName
+                            }
+                        }
+                    }
+                }
+                ''', context_value=request_headers, backend=backend)
+            assert executed['errors']
+            assert executed['errors'][0]
+            assert executed['errors'][0]['message'] == "Error, you do not have access to view this organization"
+
+    # User write tests
+    def test_get_users_user_write(self):
+        """
+        Ensure user write cannot access this query
+        """
+        with app.app_context():
+            backend = SecurityAnalysisBackend()
+            client = Client(schema)
+            get_token = client.execute(
+                '''
+                mutation{
+                    signIn(userName:"testuserwrite@testemail.ca", password:"testpassword123"){
                         authToken
                     }
                 }
