0ad0e380070bc9ff93abde36d86c5fc7a318536e
Ethan Foss <fossliam@outlook.com>
2020-03-23 17:01:32 -0300

    Amendment to result parsing for SSL

--- platform/overlays/knative/images/results/utils.py
+++ platform/overlays/knative/images/results/utils.py
@@ -1,14 +1,5 @@
 import ast
 
-
-def boolean_for(string):
-    if string == "false":
-        return False
-    elif string == "true":
-        return True
-    return None
-
-
 def formatted_dictionary(data):
     formatted = data.replace("false", "False").replace("true", "True").replace("null", "None").replace("none", "None")
     formatted_dict = ast.literal_eval(formatted)
