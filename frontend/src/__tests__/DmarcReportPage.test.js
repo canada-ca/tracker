72988fab3e68e4abd02be86c2aeb02263ac2dadf
Matt Peachey <peacheym@dal.ca>
2020-03-23 10:10:59 -0300

    Add failIcon Test

--- frontend/src/__tests__/DmarcReportPage.test.js
+++ frontend/src/__tests__/DmarcReportPage.test.js
@@ -34,7 +34,7 @@ describe('<DmarcReportPage />', () => {
     expect(render).toBeTruthy()
   })
 
-  it('renders pass icons in headers correctly', async () => {
+  it('renders pass icons in sub-headers correctly', async () => {
     const mocks = [
       {
         request: {
@@ -76,7 +76,7 @@ describe('<DmarcReportPage />', () => {
           data: {
             dmarcReport: {
               reportId: 'string',
-              orgName: 'Margot',
+              orgName: 'test-org',
               endDate: '<DateTime>',
               dmarcResult: 'pass',
               dkimResult: 'pass',
@@ -146,6 +146,120 @@ describe('<DmarcReportPage />', () => {
     expect(dmarcHeader.children[1]).toHaveAttribute('role', 'passIcon')
     expect(dkimHeader.children[1]).toHaveAttribute('role', 'passIcon')
     expect(spfHeader.children[1]).toHaveAttribute('role', 'passIcon')
+  })
+
+  it('renders fail icons in sub-headers correctly', async () => {
+    const mocks = [
+      {
+        request: {
+          query: gql`
+            {
+              dmarcReport {
+                reportId
+                orgName
+                endDate
+                dmarcResult
+                dkimResult
+                spfResult
+                passPercentage
+                count
+                dkim {
+                  domain
+                  selector
+                  result
+                }
+                spf {
+                  domain
+                  scope
+                  result
+                }
+                source {
+                  ipAddress
+                  country
+                  reverseDns
+                  baseDomain
+                }
+                identifiers {
+                  headerFrom
+                }
+              }
+            }
+          `,
+        },
+        result: {
+          data: {
+            dmarcReport: {
+              reportId: 'string',
+              orgName: 'test-org',
+              endDate: '<DateTime>',
+              dmarcResult: 'fail',
+              dkimResult: 'fail',
+              spfResult: 'fail',
+              passPercentage: 95,
+              count: 47832,
+              dkim: [
+                {
+                  domain: 'geovanni.name',
+                  selector: 'test-selector',
+                  result: 'pass',
+                  __typename: 'DkimResult',
+                },
+              ],
+              spf: [
+                {
+                  domain: 'deonte.org',
+                  scope: 'test-scope',
+                  result: 'pass',
+                  __typename: 'SpfResult',
+                },
+              ],
+              source: {
+                ipAddress: '127.0.0.1',
+                country: 'Canada',
+                reverseDns: 'string',
+                baseDomain: 'werner.biz',
+                __typename: 'Source',
+              },
+              identifiers: {
+                headerFrom: 'dortha.biz',
+                __typename: 'Identifiers',
+              },
+              __typename: 'DmarcReport',
+            },
+          },
+        },
+      },
+    ]
+
+    const { container, getByRole } = render(
+      <ThemeProvider theme={theme}>
+        <I18nProvider i18n={i18n}>
+          <MemoryRouter initialEntries={['/']} initialIndex={0}>
+            <MockedProvider mocks={mocks}>
+              <DmarcReportPage />
+            </MockedProvider>
+          </MemoryRouter>
+        </I18nProvider>
+      </ThemeProvider>,
+    )
+    expect(render).toBeTruthy()
+    expect(container).toBeTruthy()
+
+    await waitForElement(() => getByText(container, /DMARC Report/i), {
+      container,
+    })
+
+    const dmarcHeader = getByRole('dmarcHeader')
+    const dkimHeader = getByRole('dkimHeader')
+    const spfHeader = getByRole('spfHeader')
+
+    expect(dmarcHeader).toBeTruthy()
+    expect(dkimHeader).toBeTruthy()
+    expect(spfHeader).toBeTruthy()
+
+    expect(dmarcHeader.children[1]).toHaveAttribute('role', 'failIcon')
+    expect(dkimHeader.children[1]).toHaveAttribute('role', 'failIcon')
+    expect(spfHeader.children[1]).toHaveAttribute('role', 'failIcon')
 
 
   })
