e06c4e19e9aaab6ed15ccbf333ac9b8421c132b5
IdezHD <neatnick27@gmail.com>
2020-03-24 14:55:41 -0300

    Updated model, see migration commit

--- api/models.py
+++ api/models.py
@@ -17,6 +17,7 @@ class Domains(db.Model):
     organization_id = Column(Integer, ForeignKey('organizations.id'))
     organization = relationship("Organizations", back_populates="domains", cascade="all, delete")
     scans = relationship("Scans", back_populates="domain", cascade="all, delete")
+    dmarc_reports = relationship("Dmarc_Reports", back_populates="domain", cascade="all, delete")
 
 
 class Organizations(db.Model):
@@ -57,10 +58,12 @@ class User_affiliations(db.Model):
 class Dmarc_Reports(db.Model):
     __tablename__ = 'dmarc_reports'
 
-    id = Column(Integer, ForeignKey('domains.id'), primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    domain_id = Column(Integer, ForeignKey('domains.id'))
     start_date = Column(DateTime)
     end_date = Column(DateTime)
     report = Column(JSONB)
+    domain = relationship("Domains", back_populates="dmarc_reports", cascade="all, delete")
 
 
 class Scans(db.Model):
