5e0340e9b549b51530ce3d37831b696dbb466d22
Matt Peachey <peacheym@dal.ca>
2020-03-27 09:43:06 -0300

    Fill inital layout for userList. Add reder test.

---
+++ frontend/src/__tests__/UserList.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { UserList } from '../UserList'
+import { i18n } from '@lingui/core'
+import {
+  render,
+  cleanup,
+} from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { ThemeProvider, theme } from '@chakra-ui/core'
+import { I18nProvider } from '@lingui/react'
+import { MockedProvider } from '@apollo/react-testing'
+
+describe('<UserList />', () => {
+  afterEach(cleanup)
+
+  it('the component renders', async () => {
+    const { container } = render(
+      <MockedProvider>
+        <MemoryRouter initialEntries={['/']}>
+          <ThemeProvider theme={theme}>
+            <I18nProvider i18n={i18n}>
+              <UserList/>
+            </I18nProvider>
+          </ThemeProvider>
+        </MemoryRouter>
+      </MockedProvider>,
+    )
+    expect(container).toBeTruthy()
+  })
+})
