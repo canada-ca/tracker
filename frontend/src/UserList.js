f1229d4bfe9d7387b6e96a689779413cb4ee68ce
Matt Peachey <peacheym@dal.ca>
2020-03-27 14:07:35 -0300

    Format doc

--- frontend/src/UserList.js
+++ frontend/src/UserList.js
@@ -3,6 +3,7 @@ import React from 'react'
 import {
   Badge,
   Stack,
+  SimpleGrid,
   Divider,
   Box,
   Text,
@@ -17,8 +18,15 @@ import {
 export function UserList() {
   return (
     <Stack mx="auto" mb={6}>
-      <Stack isInline mb={6}>
+      <SimpleGrid mb={6} columns={{ md: 1, lg: 2 }} spacing="15px">
+        <InputGroup>
+          <InputLeftElement>
+            <Icon name="search" color="gray.300" />
+          </InputLeftElement>
+          <Input type="text" placeholder="Search for user" />
+        </InputGroup>
         <Button
+          width={['100%', '30%']}
           leftIcon="add"
           variantColor="blue"
           onClick={() => {
@@ -27,13 +35,7 @@ export function UserList() {
         >
           Create User
         </Button>
-        <InputGroup>
-          <InputLeftElement>
-            <Icon name="search" color="gray.300" />
-          </InputLeftElement>
-          <Input type="text" placeholder="Search for user" />
-        </InputGroup>
-      </Stack>
+      </SimpleGrid>
       <Divider />
       <Box
         p={4}
@@ -54,7 +56,7 @@ export function UserList() {
         <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
           <Badge variantColor="green">Active</Badge>
           <Badge variantColor="red" ml="10px">
-            Not Admin
+            Admin
           </Badge>
         </Box>
         <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
@@ -108,7 +110,44 @@ export function UserList() {
           <Badge variantColor="red">TwoFactor</Badge>
         </Box>
       </Box>
-      <Divider />{' '}
+      <Divider />
+      <Box
+        p={4}
+        display={{ md: 'flex' }}
+        alignItems="center"
+        onClick={() => {
+          window.alert('clicked box')
+        }}
+      >
+        <Box flexShrink="0" mr={{ md: 6 }}>
+          <Text mt={1} fontSize="lg" fontWeight="semibold">
+            Test User
+          </Text>
+        </Box>
+        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
+          <Text fontSize="lg">testuser@email.gc.ca</Text>
+        </Box>
+        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
+          <Badge variantColor="green">Active</Badge>
+          <Badge variantColor="red" ml="10px">
+            Admin
+          </Badge>
+        </Box>
+        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
+          <Text mt={2} color="gray.500">
+            Orgs: DND | GC | CA
+          </Text>
+        </Box>
+        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
+          <Text mt={2} color="gray.500">
+            Preferred Language: English
+          </Text>
+        </Box>
+        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }} mt={2}>
+          <Badge variantColor="green">TwoFactor</Badge>
+        </Box>
+      </Box>
+      <Divider />
       <Stack isInline justifyContent="end">
         <IconButton
           variantColor="blue"
