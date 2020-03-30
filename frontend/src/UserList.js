559bc5a357e2c81372df70e59cac4e6b3d4b2b09
Matt Peachey <peacheym@dal.ca>
2020-03-30 09:10:19 -0300

    Begin to dynamically render contents of userList. Add displayName, userName, permissions.

--- frontend/src/UserList.js
+++ frontend/src/UserList.js
@@ -24,10 +24,19 @@ export function UserList() {
     gql`
       query Users($org: Acronym!) {
         users(org: $org) {
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+            startCursor
+            endCursor
+          }
           edges {
+            cursor
             node {
+              id
               userName
               permission
+              displayName
             }
           }
         }
@@ -41,160 +50,104 @@ export function UserList() {
   if (error) {
     console.log(error)
   }
-  if (data) {
-    console.log(data)
-  }
 
   return (
-    <Stack mx="auto" mb={6}>
-      <SimpleGrid mb={6} columns={{ md: 1, lg: 2 }} spacing="15px">
-        <InputGroup>
-          <InputLeftElement>
-            <Icon name="search" color="gray.300" />
-          </InputLeftElement>
-          <Input type="text" placeholder="Search for user" />
-        </InputGroup>
-        <Button
-          width={['100%', '30%']}
-          leftIcon="add"
-          variantColor="blue"
-          onClick={() => {
-            window.alert('create user')
-          }}
-        >
-          Create User
-        </Button>
-      </SimpleGrid>
-      <Divider />
-      <Box
-        p={4}
-        display={{ md: 'flex' }}
-        alignItems="center"
-        onClick={() => {
-          window.alert('clicked box')
-        }}
-      >
-        <Box flexShrink="0" mr={{ md: 6 }}>
-          <Text mt={1} fontSize="lg" fontWeight="semibold">
-            Test User
-          </Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Text fontSize="lg">testuser@email.gc.ca</Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Badge variantColor="green">Active</Badge>
-          <Badge variantColor="red" ml="10px">
-            Admin
-          </Badge>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Text mt={2} color="gray.500">
-            Orgs: DND | GC | CA
-          </Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Text mt={2} color="gray.500">
-            Preferred Language: English
-          </Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }} mt={2}>
-          <Badge variantColor="green">TwoFactor</Badge>
-        </Box>
-      </Box>
-      <Divider />
-      <Box
-        p={4}
-        display={{ md: 'flex' }}
-        alignItems="center"
-        onClick={() => {
-          window.alert('clicked box')
-        }}
-      >
-        <Box flexShrink="0" mr={{ md: 6 }}>
-          <Text mt={1} fontSize="lg" fontWeight="semibold">
-            Different User
-          </Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Text fontSize="lg">differentuser@email.gc.ca</Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Badge variantColor="green">Active</Badge>
-          <Badge variantColor="green" ml="10px">
-            Admin
-          </Badge>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Text mt={2} color="gray.500">
-            Orgs: TEST | CA | DND
-          </Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Text mt={2} color="gray.500">
-            Preferred Language: English
-          </Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }} mt={2}>
-          <Badge variantColor="red">TwoFactor</Badge>
-        </Box>
-      </Box>
-      <Divider />
-      <Box
-        p={4}
-        display={{ md: 'flex' }}
-        alignItems="center"
-        onClick={() => {
-          window.alert('clicked box')
-        }}
-      >
-        <Box flexShrink="0" mr={{ md: 6 }}>
-          <Text mt={1} fontSize="lg" fontWeight="semibold">
-            Test User
-          </Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Text fontSize="lg">testuser@email.gc.ca</Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Badge variantColor="green">Active</Badge>
-          <Badge variantColor="red" ml="10px">
-            Admin
-          </Badge>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Text mt={2} color="gray.500">
-            Orgs: DND | GC | CA
-          </Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
-          <Text mt={2} color="gray.500">
-            Preferred Language: English
-          </Text>
-        </Box>
-        <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }} mt={2}>
-          <Badge variantColor="green">TwoFactor</Badge>
-        </Box>
-      </Box>
-      <Divider />
-      <Stack isInline justifyContent="end">
-        <IconButton
-          variantColor="blue"
-          aria-label="Previous page"
-          icon="arrow-back"
-          onClick={() => {
-            window.alert('previous page')
-          }}
-        />
-        <IconButton
-          variantColor="blue"
-          aria-label="Next page"
-          icon="arrow-forward"
-          onClick={() => {
-            window.alert('next page')
-          }}
-        />
+    <>
+      <Stack mx="auto" mb={6}>
+        <SimpleGrid mb={6} columns={{ md: 1, lg: 2 }} spacing="15px">
+          <InputGroup>
+            <InputLeftElement>
+              <Icon name="search" color="gray.300" />
+            </InputLeftElement>
+            <Input type="text" placeholder="Search for user" />
+          </InputGroup>
+          <Button
+            width={['100%', '30%']}
+            leftIcon="add"
+            variantColor="blue"
+            onClick={() => {
+              window.alert('create user')
+            }}
+          >
+            Create User
+          </Button>
+        </SimpleGrid>
+        <Divider />
+
+        {data
+          ? data.users.edges.map(edge => {
+              console.log(edge.node.permission)
+              return (
+                <Box key={edge.node.id}>
+                  <Box
+                    p={4}
+                    display={{ md: 'flex' }}
+                    alignItems="center"
+                    onClick={() => {
+                      window.alert('clicked box')
+                    }}
+                  >
+                    <Box flexShrink="0" mr={{ md: 6 }}>
+                      <Text mt={1} fontSize="lg" fontWeight="semibold">
+                        {edge.node.displayName}
+                      </Text>
+                    </Box>
+                    <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
+                      <Text fontSize="lg">{edge.node.userName}</Text>
+                    </Box>
+                    <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
+                      <Badge variantColor="green">Active</Badge>
+                      <Badge
+                        variantColor={
+                          edge.node.permission === 'ADMIN' ||
+                          edge.node.permission === 'SUPER_ADMIN'
+                            ? 'green'
+                            : 'red'
+                        }
+                        ml="10px"
+                      >
+                        Admin
+                      </Badge>
+                    </Box>
+                    <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
+                      <Text mt={2} color="gray.500">
+                        Orgs: DND | GC | CA
+                      </Text>
+                    </Box>
+                    <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }}>
+                      <Text mt={2} color="gray.500">
+                        Preferred Language: English
+                      </Text>
+                    </Box>
+                    <Box flexShrink="0" ml={{ md: 6 }} mr={{ md: 6 }} mt={2}>
+                      <Badge variantColor="green">TwoFactor</Badge>
+                    </Box>
+                  </Box>
+                  <Divider />
+                </Box>
+              )
+            })
+          : null}
+
+        <Stack isInline justifyContent="end">
+          <IconButton
+            variantColor="blue"
+            aria-label="Previous page"
+            icon="arrow-back"
+            onClick={() => {
+              window.alert('previous page')
+            }}
+          />
+          <IconButton
+            variantColor="blue"
+            aria-label="Next page"
+            icon="arrow-forward"
+            onClick={() => {
+              window.alert('next page')
+            }}
+          />
+        </Stack>
       </Stack>
-    </Stack>
+    </>
   )
 }
