90f85437c100534d19ee857a0c291594219712f4
IdezHD <neatnick27@gmail.com>
2020-03-31 11:45:11 -0300

    Fixed tear down closing session

--- api/application.py
+++ api/application.py
@@ -9,7 +9,7 @@ app = create_application()
 add_graphql_endpoint(app)
 
 
-@app.teardown_appcontext
+@app.teardown_request
 def shutdown_session(exception=None):
     db_session.remove()
 
--- api/db.py
+++ api/db.py
@@ -11,7 +11,7 @@ DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
-engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}', convert_unicode=True)
+engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine)
