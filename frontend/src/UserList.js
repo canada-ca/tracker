1c4376620505c7d0d386a482ef8750b788a680dd
Matt Peachey <peacheym@dal.ca>
2020-04-03 10:46:46 -0300

    Remove redundant <BOX> and update test to include barebone mocked userlist test.

--- frontend/src/UserList.js
+++ frontend/src/UserList.js
@@ -29,7 +29,7 @@ export function UserList() {
     return <p>Loading...</p>
   }
   if (error) {
-    console.log(error)
+    return <p>Error :(</p>
   }
   if (data) {
     console.log(data)
@@ -61,95 +61,79 @@ export function UserList() {
         ? data.user.affiliations.edges[0].node.organization.affiliatedUsers.edges.map(
             (edge) => {
               return (
-                <Box key={edge.node.id} width="100%">
-                  <PseudoBox
-                    display={{ md: 'flex' }}
-                    alignItems="center"
-                    onClick={() => {
-                      history.push({
-                        pathname: '/user',
-                        state: { detail: edge.node.user.userName },
-                      })
-                    }}
-                    _hover={{ borderColor: 'gray.200', bg: 'gray.200' }}
-                    p="30px"
-                  >
-                    <Box flexShrink="0" minW="15%">
-                      <Text mt={1} fontSize="lg" fontWeight="semibold">
-                        {edge.node.user.displayName}
-                      </Text>
-                    </Box>
-                    <Box
-                      flexShrink="0"
-                      ml={{ md: 4 }}
-                      mr={{ md: 4 }}
-                      minW="35%"
-                    >
-                      <Text fontSize="lg" minW="10%">
-                        {edge.node.user.userName}
-                      </Text>
-                    </Box>
-                    <Box
-                      flexShrink="0"
-                      ml={{ md: 4 }}
-                      mr={{ md: 4 }}
-                      minW="25%"
-                    >
-                      <Box mt={2} color="gray.500">
-                        Orgs:&nbsp;
-                        {// Populate the user-orgs list.
-                        edge.node.user.affiliations.edges.map(
-                          (edge, i, arr) => {
-                            if (arr.length - 1 === i) {
-                              return (
-                                <Text display="inline" key={edge.node.id + i}>
-                                  {edge.node.organization.acronym}
-                                </Text>
-                              )
-                            }
-                            return (
-                              <Text display="inline" key={edge.node.id + i}>
-                                {edge.node.organization.acronym + ' | '}
-                              </Text>
-                            )
-                          },
-                        )}
-                      </Box>
-                    </Box>
-                    <Box
-                      flexShrink="0"
-                      ml={{ md: 4 }}
-                      mr={{ md: 4 }}
-                      minW="15%"
-                    >
-                      <Badge
-                        variantColor={edge.node.user.tfa ? 'green' : 'red'}
-                        minW="15%"
-                      >
-                        <Trans>TwoFactor</Trans>
-                      </Badge>
-                      <Badge
-                        variantColor={
-                          edge.node.permission === 'ADMIN' ||
-                          edge.node.permission === 'SUPER_ADMIN'
-                            ? 'green'
-                            : 'red'
+                <PseudoBox
+                  key={edge.node.id}
+                  role="userCard"
+                  width="100%"
+                  display={{ md: 'flex' }}
+                  alignItems="center"
+                  onClick={() => {
+                    history.push({
+                      pathname: '/user',
+                      state: { detail: edge.node.user.userName },
+                    })
+                  }}
+                  _hover={{ borderColor: 'gray.200', bg: 'gray.200' }}
+                  p="30px"
+                >
+                  <Box flexShrink="0" minW="15%">
+                    <Text mt={1} fontSize="lg" fontWeight="semibold">
+                      {edge.node.user.displayName}
+                    </Text>
+                  </Box>
+                  <Box flexShrink="0" ml={{ md: 4 }} mr={{ md: 4 }} minW="35%">
+                    <Text fontSize="lg" minW="10%">
+                      {edge.node.user.userName}
+                    </Text>
+                  </Box>
+                  <Box flexShrink="0" ml={{ md: 4 }} mr={{ md: 4 }} minW="25%">
+                    <Box mt={2} color="gray.500">
+                      Orgs:&nbsp;
+                      {// Populate the user-orgs list.
+                      edge.node.user.affiliations.edges.map((edge, i, arr) => {
+                        if (arr.length - 1 === i) {
+                          return (
+                            <Text display="inline" key={edge.node.id + i}>
+                              {edge.node.organization.acronym}
+                            </Text>
+                          )
                         }
-                        ml="10px"
-                        mr={{ md: 4 }}
-                      >
-                        <Trans>Admin</Trans>
-                      </Badge>
+                        return (
+                          <Text display="inline" key={edge.node.id + i}>
+                            {edge.node.organization.acronym + ' | '}
+                          </Text>
+                        )
+                      })}
                     </Box>
-                    <Box
-                      flexShrink="0"
-                      ml={{ md: 4 }}
-                      mr={{ md: 4 }}
-                      mt={2}
+                  </Box>
+                  <Box flexShrink="0" ml={{ md: 4 }} mr={{ md: 4 }} minW="15%">
+                    <Badge
+                      variantColor={edge.node.user.tfa ? 'green' : 'red'}
                       minW="15%"
-                    ></Box>
-                  </PseudoBox>
-                </Box>
+                    >
+                      <Trans>TwoFactor</Trans>
+                    </Badge>
+                    <Badge
+                      variantColor={
+                        edge.node.permission === 'ADMIN' ||
+                        edge.node.permission === 'SUPER_ADMIN'
+                          ? 'green'
+                          : 'red'
+                      }
+                      ml="10px"
+                      mr={{ md: 4 }}
+                    >
+                      <Trans>Admin</Trans>
+                    </Badge>
+                  </Box>
+                  <Box
+                    flexShrink="0"
+                    ml={{ md: 4 }}
+                    mr={{ md: 4 }}
+                    mt={2}
+                    minW="15%"
+                  ></Box>
+                </PseudoBox>
               )
             },
           )
--- frontend/src/__tests__/UserList.test.js
+++ frontend/src/__tests__/UserList.test.js
@@ -7,10 +7,12 @@ import { ThemeProvider, theme } from '@chakra-ui/core'
 import { I18nProvider } from '@lingui/react'
 import { MockedProvider } from '@apollo/react-testing'
 
+import { QUERY_USERLIST } from '../graphql/queries'
+
 describe('<UserList />', () => {
   afterEach(cleanup)
 
-  it('the component renders', async () => {
+  it('renders', async () => {
     const { container } = render(
       <MockedProvider>
         <MemoryRouter initialEntries={['/']}>
@@ -23,5 +25,97 @@ describe('<UserList />', () => {
       </MockedProvider>,
     )
     expect(container).toBeTruthy()
+  })
+
+  it('the component renders correctly with mocked data', async () => {
+    const mocks = [
+      {
+        request: {
+          query: QUERY_USERLIST,
+        },
+        result: {
+          data: {
+            user: {
+              affiliations: {
+                edges: [
+                  {
+                    node: {
+                      organization: {
+                        acronym: 'TEST',
+                        affiliatedUsers: {
+                          pageInfo: {
+                            hasNextPage: true,
+                            hasPreviousPage: true,
+                            startCursor: 'string',
+                            endCursor: 'string',
+                          },
+                          edges: [
+                            {
+                              node: {
+                                id: 'NzYzMzQ1MzQ1Ng==',
+                                user: {
+                                  userName: 'testuser@testemail.ca',
+                                  displayName: 'Test User',
+                                  tfa: false,
+                                  affiliations: {
+                                    edges: [
+                                      {
+                                        node: {
+                                          id: 'ODAyOTY1MDMyMQ==',
+                                          organization: {
+                                            acronym: 'GC',
+                                          },
+                                          permission: 'SUPER_ADMIN',
+                                        },
+                                      },
+                                      {
+                                        node: {
+                                          id: 'NjI3NzcyNDQ=',
+                                          organization: {
+                                            acronym: 'BC',
+                                          },
+                                          permission: 'USER_WRITE',
+                                        },
+                                      },
+                                      {
+                                        node: {
+                                          id: 'NjkyMzAyOTAx',
+                                          organization: {
+                                            acronym: 'BC',
+                                          },
+                                          permission: 'ADMIN',
+                                        },
+                                      },
+                                    ],
+                                  },
+                                },
+                              },
+                            },
+                          ],
+                        },
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    ]
+    const { container } = render(
+      <MockedProvider mocks={mocks} addTypename={false} removeTypename>
+        <MemoryRouter initialEntries={['/']}>
+          <ThemeProvider theme={theme}>
+            <I18nProvider i18n={i18n}>
+              <UserList />
+            </I18nProvider>
+          </ThemeProvider>
+        </MemoryRouter>
+      </MockedProvider>,
+    )
+    expect(container).toBeTruthy()
+
+    // TODO: Write test for this.
   })
 })
