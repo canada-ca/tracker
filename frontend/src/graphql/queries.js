c66987d43e5bbe8c5a18931e2470f8a8ddc74865
Matt Peachey <PeacheyM@dal.ca>
2020-04-09 11:38:43 -0300

    Create Custom type and sub-types for dmarc page. (#280)
    Update file and tests to use new types.
    Move type to graphql/queries file.

--- frontend/src/graphql/queries.js
+++ frontend/src/graphql/queries.js
@@ -17,3 +17,37 @@ export const GENERATE_OTP_URL = gql`
     generateOtpUrl(email: $email)
   }
 `
+
+export const QUERY_DMARC_REPORT = gql`
+  query QueryDmarcReport($reportId: String!) {
+    queryDmarcReport(reportId: $reportId) {
+      reportId
+      orgName
+      endDate
+      dmarcResult
+      dkimResult
+      spfResult
+      passPercentage
+      count
+      dkim {
+        domain
+        selector
+        result
+      }
+      spf {
+        domain
+        scope
+        result
+      }
+      source {
+        ipAddress
+        country
+        reverseDns
+        baseDomain
+      }
+      identifiers {
+        headerFrom
+      }
+    }
+  }
+`
