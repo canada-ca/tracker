c4a80685cf8742f731db4998e18269ee087c9cca
IdezHD <nc575894@dal.ca>
2020-04-16 13:34:35 -0300

    Quick Update To GraphQL Scalars (#277)
    * Fixed node type return value
    
    * Cleaned up email address scalar test
    
    * Added more URL tests
    
    * Added another test for acronym scalar

--- api/scalars/url.py
+++ api/scalars/url.py
@@ -11,37 +11,38 @@ URL_REGEX_CHECK = compile(URL_REGEX)
 
 
 class URL(Scalar):
-	"""
-	A field whose value conforms to the standard URL format as specified in RFC3986:
-	https://www.ietf.org/rfc/rfc3986.txt.
-	"""
+    """
+    A field whose value conforms to the standard URL format as specified in RFC3986:
+    https://www.ietf.org/rfc/rfc3986.txt.
+    """
 
-	@staticmethod
-	def serialize(value):
-		if not isinstance(value, str):
-			raise GraphQLError(scalar_error_type("String", value))
+    @staticmethod
+    def serialize(value):
+        if not isinstance(value, str):
+            raise GraphQLError(scalar_error_type("String", value))
 
-		if not URL_REGEX_CHECK.search(value):
-			raise GraphQLError(scalar_error_type("URL", value))
+        if not URL_REGEX_CHECK.search(value):
+            raise GraphQLError(scalar_error_type("URL", value))
 
-		return value
+        return value
 
-	@staticmethod
-	def parse_value(value):
-		if not isinstance(value, str):
-			raise GraphQLError(scalar_error_type("String", value))
+    @staticmethod
+    def parse_value(value):
+        if not isinstance(value, str):
+            raise GraphQLError(scalar_error_type("String", value))
 
-		if not URL_REGEX_CHECK.search(value):
-			raise GraphQLError(scalar_error_type("URL", value))
+        if not URL_REGEX_CHECK.search(value):
+            raise GraphQLError(scalar_error_type("URL", value))
 
-		return value
+        return value
 
-	@staticmethod
-	def parse_literal(node):
-		if not isinstance(node, ast.StringValue):
-			raise GraphQLError(scalar_error_only_types("strings", "URLs", str(ast.Type)))
+    @staticmethod
+    def parse_literal(node):
+        if not isinstance(node, ast.StringValue):
+            raise GraphQLError(
+                scalar_error_only_types("strings", "URLs", str(type(node))))
 
-		if not URL_REGEX_CHECK.search(node.value):
-			raise GraphQLError(scalar_error_type("URL", node.value))
+        if not URL_REGEX_CHECK.search(node.value):
+            raise GraphQLError(scalar_error_type("URL", node.value))
 
-		return node.value
+        return node.value
