25073f1819741591047efd59a69532228401ef3e
IdezHD <nc575894@dal.ca>
2020-04-21 12:53:49 +0000

    Scan Mutations Here We Come (#312)
    * Created initial mutation for sending a scan
    
    * Formatting
    
    * Moved fire scan function into own file in the functions directory
    
    * Added in domain id
    
    * Removed sending data through request, moved to header
    
    * Scans now working with GCP cluster
    
    * Added db update for domains table last run to happen during fire scan functionality
    
    * Added in test argument
    
    * Added test header for scan firing
    
    * Updated scan mutation for new db setup
    
    * Created migration for default super admin account
    
    * Added ENV, and secret for firing scan
    
    * Added token_key
    
    * Added istio gateway key
    
    * Make all id's 100
    
    * Using python to create sa account on api start up rather then a migration (because it gets removed during tests)
    
    * Switched to env's
    
    * added new env's
    
    * corrected test headers

---
+++ api/functions/fire_scan.py
@@ -0,0 +1,81 @@
+import requests
+import jwt
+import os
+import datetime
+
+from flask import request
+
+from app import app
+from db import db_session
+
+from models import Scans, Domains
+
+TOKEN_KEY = os.getenv('TOKEN_KEY')
+DISPATCHER_URL = os.getenv('ISTIO_INGRESS')
+
+
+def fire_scan(user_id: int, domain_id: int, url: str, dkim: bool, test: bool):
+    """
+    Functionality to send request to scanners and request a domain to get scanned
+    :param user_id: The id of the requesting user
+    :param domain_id: The id of the domain
+    :param url: URL passed in through the request
+    :param dkim: Bool to see if url needs to be put through the dkim scanner
+    :param test: Bool to send for testing purposes
+    :return: Status code returned from request
+    """
+    # Get Time
+    scan_datetime = datetime.datetime.utcnow()
+
+    # Create Scan Object
+    new_scan = Scans(
+        domain_id=domain_id,
+        scan_date=scan_datetime,
+        initiated_by=user_id
+    )
+    db_session.add(new_scan)
+
+    # Update Domain Tables Last Run
+    Domains.query.filter(
+        Domains.id == domain_id
+    ).update({'last_run': scan_datetime})
+
+    db_session.commit()
+
+    # Get latest scan entry
+    scan_orm = db_session.query(Scans).order_by(
+        Scans.id.desc()
+    ).first()
+    scan_id = scan_orm.id
+
+    payload = {
+        'exp': datetime.datetime.utcnow()+datetime.timedelta(days=0, seconds=10),
+        "scan_id": scan_id,
+        "domain": url,
+        "dkim": dkim,
+        "user_init": True
+    }
+
+    encoded_payload = jwt.encode(
+        payload,
+        TOKEN_KEY,
+        algorithm='HS256'
+    ).decode('utf-8')
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Host': 'dispatcher.tracker.example.com',
+        'Data': encoded_payload
+    }
+
+    if test:
+        headers['Test'] = 'true'
+    else:
+        headers['Test'] = 'false'
+
+    status = requests.post(
+        DISPATCHER_URL + "/receive",
+        headers=headers
+    )
+
+    return str(status.text)
---
+++ api/schemas/scans_mutation.py
@@ -0,0 +1,81 @@
+import graphene
+
+from graphql import GraphQLError
+
+from app import app
+from db import db_session
+
+from functions.auth_wrappers import require_token
+from functions.auth_functions import is_admin
+from functions.fire_scan import fire_scan
+from functions.input_validators import cleanse_input
+
+from models import Domains
+
+from scalars.url import URL
+
+
+class RequestScan(graphene.Mutation):
+    """
+    This mutation is used to send a domain to the scanners to be scanned
+    """
+    class Arguments:
+        url = URL(
+            description="The domain that you would like the scan to be ran on."
+        )
+        dkim = graphene.Boolean(
+            description="If this is a DKIM scan please set to true."
+        )
+        test = graphene.Boolean(
+            description="If this scan if for testing purposes set to true."
+        )
+
+    status = graphene.String()
+
+    with app.app_context():
+        @require_token
+        def mutate(self, info, **kwargs):
+            """
+            Process the request from the user
+            :param info: The request from the user
+            :param kwargs: Arguments passed in from token and request
+            :return: Request Scan object with status of request
+            """
+            # Get variables from kwargs
+            user_id = kwargs.get('user_id')
+            user_roles = kwargs.get('user_roles')
+            url = cleanse_input(kwargs.get('url'))
+            dkim = kwargs.get('dkim')
+            test = kwargs.get('test', False)
+
+            # Get Domain ORM related to requested domain
+            domain_orm = db_session.query(Domains).filter(
+                Domains.domain == url
+            ).first()
+
+            # Check to make sure domain exists
+            if domain_orm is None:
+                raise GraphQLError("Error, domain does not exist")
+
+            # Check to ensure user has admin rights
+            org_id = domain_orm.organization_id
+            domain_id = domain_orm.id
+            if is_admin(user_role=user_roles, org_id=org_id):
+                # Fire scan and get status from request
+                status = fire_scan(
+                    user_id=user_id,
+                    domain_id=domain_id,
+                    url=url,
+                    dkim=dkim,
+                    test=test
+                )
+
+                # Return status information to user
+                return RequestScan(status=status)
+
+            # If user doesn't have rights error out
+            else:
+                raise GraphQLError(
+                    "Error, you do not have permission to request a scan on "
+                    "that domain"
+                )
