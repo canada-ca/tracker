23e42ab085a9d3c4eb602d4b48b0d50403fe4689
Ethan Foss <fossliam@outlook.com>
2020-05-08 07:59:07 -0300

    Connect to postgres with cluster-local address

--- scanners/results/result_processor.py
+++ scanners/results/result_processor.py
@@ -19,7 +19,7 @@ DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@postgres.api.svc.cluster.local:{DB_PORT}/{DB_NAME}'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
