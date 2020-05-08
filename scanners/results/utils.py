d34c6bd0f2e2565c45bddfd16954f9e96a1c09ac
Ethan Foss <fossliam@outlook.com>
2020-05-08 12:27:28 -0300

    Run black

--- scanners/results/utils.py
+++ scanners/results/utils.py
@@ -1,6 +1,12 @@
 import ast
 
+
 def formatted_dictionary(data):
-    formatted = data.replace("false", "False").replace("true", "True").replace("null", "None").replace("none", "None")
+    formatted = (
+        data.replace("false", "False")
+        .replace("true", "True")
+        .replace("null", "None")
+        .replace("none", "None")
+    )
     formatted_dict = ast.literal_eval(formatted)
     return formatted_dict
