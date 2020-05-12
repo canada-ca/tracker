637f07574269c65e51557a5d74625bcde097d078
Mike Williamson <mike@korora.ca>
2020-05-12 14:03:46 -0400

    Handle jwt/other errors for DomainsPage
    As advertised, this adds more fulsome error handling to the DomainsPage.

--- frontend/src/DomainsPage.js
+++ frontend/src/DomainsPage.js
@@ -2,22 +2,32 @@ import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { Trans } from '@lingui/macro'
 import { Layout } from './Layout'
-import { Heading, Text, Stack, List, ListItem } from '@chakra-ui/core'
+import { Heading, Text, Stack, List, ListItem, useToast } from '@chakra-ui/core'
 import { DOMAINS } from './graphql/queries'
 import { useUserState } from './UserState'
 
 export default function DomainsPage() {
   const { currentUser } = useUserState()
-  const { loading, error, data } = useQuery(DOMAINS, {
+  const toast = useToast()
+  const { loading, _error, data } = useQuery(DOMAINS, {
     context: {
       headers: {
         authorization: currentUser.jwt,
       },
     },
+    onError: (error) => {
+      const [_, message] = error.message.split(': ')
+      toast({
+        title: 'Error',
+        description: message,
+        status: 'failure',
+        duration: 9000,
+        isClosable: true,
+      })
+    },
   })
 
   if (loading) return <p>Loading...</p>
-  if (error) return <p>Error :(</p>
 
   return (
     <Layout>
@@ -25,27 +35,37 @@ export default function DomainsPage() {
         <Heading as="h1">
           <Trans>Domains</Trans>
         </Heading>
-        <Stack spacing={4}>
-          <Stack spacing={4} direction="row" flexWrap="wrap">
-            <List>
-              {data.domains.edges.map((edge, i) => {
-                if (edge.node) {
-                  return (
-                    <ListItem key={edge.node.url + i}>
-                      <Text>{edge.node.url}</Text>
-                    </ListItem>
-                  )
-                } else {
-                  return (
-                    <ListItem key={"edge" + i}>
-                      <Trans>No domains scanned yet.</Trans>
-                    </ListItem>
-                  )
-                }
-              })}
-            </List>
+        {data && data.domains && (
+          <Stack spacing={4}>
+            <Stack spacing={4} direction="row" flexWrap="wrap">
+              <List>
+                {data.domains &&
+                  data.domains.edges.map((edge, i) => {
+                    if (edge.node) {
+                      return (
+                        <ListItem key={edge.node.url + i}>
+                          <Text>{edge.node.url}</Text>
+                        </ListItem>
+                      )
+                    } else {
+                      return (
+                        <ListItem key={'edge' + i}>
+                          <Trans>No domains scanned yet.</Trans>
+                        </ListItem>
+                      )
+                    }
+                  })}
+              </List>
+            </Stack>
           </Stack>
-        </Stack>
+        )}
+        {data && !data.domains && (
+          <Stack spacing={4} direction="row" flexWrap="wrap">
+            <ListItem>
+              <Trans>No domains scanned yet.</Trans>
+            </ListItem>
+          </Stack>
+        )}
       </Stack>
     </Layout>
   )
--- frontend/src/__tests__/DomainsPage.test.js
+++ frontend/src/__tests__/DomainsPage.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { GraphQLError } from 'graphql'
 import { ThemeProvider, theme } from '@chakra-ui/core'
 import { render, waitFor } from '@testing-library/react'
 import { MockedProvider } from '@apollo/react-testing'
@@ -8,24 +9,56 @@ import { DOMAINS } from '../graphql/queries'
 import { I18nProvider } from '@lingui/react'
 import { setupI18n } from '@lingui/core'
 
-const mocks = [
-  {
-    request: {
-      query: DOMAINS,
-    },
-    result: {
-      data: {
-        domains: {
-          edges: [{ node: null, __typename: 'DomainEdge' }],
-          __typename: 'DomainConnection',
+describe('<DomainsPage>', () => {
+  it('notifies the user of errors', async () => {
+    const invalidTokenMessage = 'Invalid token, please login again'
+    const mocks = [
+      {
+        request: {
+          query: DOMAINS,
+        },
+        result: {
+          data: { domains: null },
+          errors: [new GraphQLError(invalidTokenMessage)],
         },
       },
-    },
-  },
-]
+    ]
+    const { queryByText } = render(
+      <UserStateProvider
+        initialState={{ userName: 'me', jwt: 'longstring', tfa: null }}
+      >
+        <ThemeProvider theme={theme}>
+          <I18nProvider i18n={setupI18n()}>
+            <MockedProvider mocks={mocks}>
+              <DomainsPage />
+            </MockedProvider>
+          </I18nProvider>
+        </ThemeProvider>
+      </UserStateProvider>,
+    )
 
-describe('<DomainsPage>', () => {
+    await waitFor(() => {
+      const errorMessage = queryByText(invalidTokenMessage)
+      expect(errorMessage).toBeInTheDocument()
+    })
+  })
+
   it(`gracefully handles no domains being returned`, async () => {
+    const mocks = [
+      {
+        request: {
+          query: DOMAINS,
+        },
+        result: {
+          data: {
+            domains: {
+              edges: [{ node: null, __typename: 'DomainEdge' }],
+              __typename: 'DomainConnection',
+            },
+          },
+        },
+      },
+    ]
     const { queryByText } = render(
       <UserStateProvider
         initialState={{ userName: 'me', jwt: 'longstring', tfa: null }}
