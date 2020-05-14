42e2f0573b5f515bc1554ba3a12c9317b15c0c7c
IdezHD <neatnick27@gmail.com>
2020-05-14 10:47:55 -0300

    Removed roles from JWT

--- api/json_web_token.py
+++ api/json_web_token.py
@@ -8,14 +8,13 @@ from os import environ
 
 def tokenize(
     user_id,
-    roles,
     iat=dt.utcnow(),
     exp=(dt.utcnow() + timedelta(hours=1)),
     # TODO: SUPER_SECRET_SALT isn't actually a salt! Give this a better name.
     secret=environ.get("SUPER_SECRET_SALT", ""),
 ):
     return jwt.encode(
-        {"exp": exp, "iat": iat, "user_id": user_id, "roles": roles},
+        {"exp": exp, "iat": iat, "user_id": user_id},
         secret,
         algorithm="HS256",
     ).decode("utf-8")
