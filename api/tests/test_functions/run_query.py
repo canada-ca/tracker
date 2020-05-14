588278b63e4873b450e1b7f5788c7a3e3f3ea15e
IdezHD <neatnick27@gmail.com>
2020-05-14 14:40:02 -0300

    Completed refactor of test_notification_emails.py, and added header check if no as_user is passed in context_value will be none

--- api/tests/test_functions/run_query.py
+++ api/tests/test_functions/run_query.py
@@ -7,8 +7,10 @@ default_backend = SecurityAnalysisBackend()
 
 
 def run(query=None, mutation=None, as_user=None, schema=schema, backend=default_backend):
+    header = auth_header(tokenize(user_id=as_user.id)) if as_user is not None else None
+
     return Client(schema).execute(
         query if query else mutation,
-        context_value=auth_header(tokenize(user_id=as_user.id)),
+        context_value=header,
         backend=backend
     )
