76b7dcd2a2e8aa113442954cdcff216f051c9626
IdezHD <neatnick27@gmail.com>
2020-05-14 14:22:44 -0300

    Completed refactor of test_email_address_scalar.py

--- api/tests/test_email_address_scalar.py
+++ api/tests/test_email_address_scalar.py
@@ -1,11 +1,8 @@
-import sys
-import os
 import pytest
-from graphene.test import Client
-from graphql import GraphQLScalarType
+
 from graphql.language import ast
 from graphql import GraphQLError
-from unittest import TestCase
+
 from scalars.email_address import (
     EmailAddress,
     scalar_error_type,
@@ -13,59 +10,66 @@ from scalars.email_address import (
 )
 
 
-class TestEmailAddressScalar(TestCase):
-    def test_valid_email_serialize(self):
-        test_email = "test.account@canada.ca"
-        assert EmailAddress.serialize(test_email)
+def test_valid_email_serialize():
+    test_email = "test.account@canada.ca"
+    assert EmailAddress.serialize(test_email)
 
-    def test_valid_email_parse_value(self):
-        test_email = "test.account@canada.ca"
-        assert EmailAddress.parse_value(test_email)
 
-    def test_valid_email_parse_literal(self):
-        assert EmailAddress.parse_literal(
-            ast.StringValue(value="test.account@canada.ca")
-        )
+def test_valid_email_parse_value():
+    test_email = "test.account@canada.ca"
+    assert EmailAddress.parse_value(test_email)
 
-    def test_invalid_email_serialize_not_email(self):
-        test_value = "This Will Fail"
-        with self.assertRaisesRegex(
-            GraphQLError, scalar_error_type("email address", test_value)
-        ):
-            EmailAddress.serialize(test_value)
 
-    def test_invalid_email_serialize_wrong_type(self):
-        test_value = 1234
-        with self.assertRaisesRegex(
-            GraphQLError, scalar_error_type("String", test_value)
-        ):
-            EmailAddress.serialize(test_value)
+def test_valid_email_parse_literal():
+    assert EmailAddress.parse_literal(
+        ast.StringValue(value="test.account@canada.ca")
+    )
 
-    def test_invalid_email_parse_value_not_email(self):
-        test_value = "This Will Fail"
-        with self.assertRaisesRegex(
-            GraphQLError, scalar_error_type("email address", test_value)
-        ):
-            EmailAddress.parse_value(test_value)
 
-    def test_invalid_email_parse_value_wrong_type(self):
-        test_value = 1234
-        with self.assertRaisesRegex(
-            GraphQLError, scalar_error_type("String", test_value)
-        ):
-            EmailAddress.parse_value(test_value)
+def test_invalid_email_serialize_not_email():
+    test_value = "This Will Fail"
+    with pytest.raises(
+        GraphQLError, match=scalar_error_type("email address", test_value)
+    ):
+        EmailAddress.serialize(test_value)
 
-    def test_invalid_email_parse_literal_not_email(self):
-        test_value = ast.StringValue(value="This Will Fail")
-        with self.assertRaisesRegex(
-            GraphQLError, scalar_error_type("email address", test_value.value)
-        ):
-            EmailAddress.parse_literal(test_value)
 
-    def test_invalid_email_parse_literal_wrong_ast_type(self):
-        test_value = ast.IntValue(value="1234")
-        with self.assertRaisesRegex(
-            GraphQLError,
-            scalar_error_only_types("strings", "email address", str(ast.Type)),
-        ):
-            EmailAddress.parse_literal(test_value)
+def test_invalid_email_serialize_wrong_type():
+    test_value = 1234
+    with pytest.raises(
+        GraphQLError, match=scalar_error_type("String", test_value)
+    ):
+        EmailAddress.serialize(test_value)
+
+
+def test_invalid_email_parse_value_not_email():
+    test_value = "This Will Fail"
+    with pytest.raises(
+        GraphQLError, match=scalar_error_type("email address", test_value)
+    ):
+        EmailAddress.parse_value(test_value)
+
+
+def test_invalid_email_parse_value_wrong_type():
+    test_value = 1234
+    with pytest.raises(
+        GraphQLError, match=scalar_error_type("String", test_value)
+    ):
+        EmailAddress.parse_value(test_value)
+
+
+def test_invalid_email_parse_literal_not_email():
+    test_value = ast.StringValue(value="This Will Fail")
+    with pytest.raises(
+        GraphQLError, match=scalar_error_type("email address", test_value.value)
+    ):
+        EmailAddress.parse_literal(test_value)
+
+
+def test_invalid_email_parse_literal_wrong_ast_type():
+    test_value = ast.IntValue(value="1234")
+    with pytest.raises(
+        GraphQLError,
+        match=scalar_error_only_types("strings", "email address", str(ast.Type)),
+    ):
+        EmailAddress.parse_literal(test_value)
