db791fbbbdbfcebb180cfda48da451483ff3d383
IdezHD <neatnick27@gmail.com>
2020-05-14 10:57:56 -0300

    Added default backend security to run function

--- api/tests/test_functions/run_query.py
+++ api/tests/test_functions/run_query.py
@@ -1,9 +1,13 @@
 from graphene.test import Client
 from json_web_token import tokenize, auth_header
+from backend.security_check import SecurityAnalysisBackend
+
+default_backend = SecurityAnalysisBackend()
 
 
-def run(query=None, mutation=None, as_user=None, schema=None):
+def run(query=None, mutation=None, as_user=None, schema=None, backend=default_backend):
     return Client(schema).execute(
         query if query else mutation,
         context_value=auth_header(tokenize(user_id=as_user.id)),
+        backend=backend
     )
