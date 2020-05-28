8e2a0183c0d41f287e98938ef0e86bac08c4a524
IdezHD <neatnick27@gmail.com>
2020-05-28 08:46:14 -0300

    Completed test for get_yearly_dmarc_report_summaries query

---
+++ api/tests/testdata/get_yearly_dmarc_report_summaries/__init__.py
@@ -0,0 +1,2 @@
+from tests.testdata.get_yearly_dmarc_report_summaries.get_yearly_dmarc_report_summaries_data import api_return_data
+from tests.testdata.get_yearly_dmarc_report_summaries.get_yearly_dmarc_report_summaries_expected_data import api_expected_data
