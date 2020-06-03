a6380bd688bc6f2f4c4e3f44f5afc1a2f00d2cdc
IdezHD <neatnick27@gmail.com>
2020-06-03 10:57:36 -0300

    Create list item

---
+++ api/schemas/dmarc_report_detailed_tables/__init__.py
@@ -0,0 +1,16 @@
+import graphene
+
+from enums.period import PeriodEnums
+from scalars.slug import Slug
+from scalars.year import Year
+from schemas.dmarc_report_detailed_tables.dmarc_report_detailed_tables import DmarcReportDetailedTables
+from schemas.dmarc_report_detailed_tables.resolver import resolve_dmarc_report_detailed_tables
+
+get_dmarc_report_detailed_tables = graphene.List(
+    lambda: DmarcReportDetailedTables,
+    domain_slug=graphene.Argument(Slug, description=""),
+    period=graphene.Argument(PeriodEnums, description=""),
+    year=graphene.Argument(Year, description=""),
+    resolver=resolve_dmarc_report_detailed_tables,
+    description=""
+)
