49a14cb87a51861944da6ee6ec3a6d64642b3d06
IdezHD <neatnick27@gmail.com>
2020-06-05 13:48:47 -0300

    Dkim tags re-factor

--- api/schemas/domain/email_scan/dkim/dkim_tags.py
+++ api/schemas/domain/email_scan/dkim/dkim_tags.py
@@ -9,28 +9,28 @@ class DkimTags(SQLAlchemyObjectType):
         model = Dkim_scans
         exclude_fields = ("id", "dkim_scan")
 
-    value = graphene.String(description="Key tags found during scan")
+    value = graphene.List(
+        lambda: graphene.String,
+        description="Key tags found during scan",
+    )
 
     def resolve_value(self: Dkim_scans, info):
-        tags = {}
+        tags = []
 
-        if "missing" in self.dkim_scan:
+        if self.dkim_scan.get("missing", None) is not None:
             return tags.update({"dkim2": "missing"})
 
-        if (
-            self.dkim_scan["dkim"]["key_size"] >= 2048
-            and self.dkim_scan["dkim"]["key_type"] == "rsa"
-        ):
+        # Get Key Size, and Key Type
+        key_size = self.dkim_scan.get("dkim", {}) \
+            .get("key_size", None)
+        key_type = self.dkim_scan.get("dkim", {}) \
+            .get("key_type", None)
+
+        if key_size >= 2048 and key_type == "rsa":
             tags.update({"dkim5": "P-2048"})
-        elif (
-            self.dkim_scan["dkim"]["key_size"] == 1024
-            and self.dkim_scan["dkim"]["key_type"] == "rsa"
-        ):
+        elif key_size == 1024 and key_type == "rsa":
             tags.update({"dkim4": "P-1024"})
-        elif (
-            self.dkim_scan["dkim"]["key_size"] < 1024
-            and self.dkim_scan["dkim"]["key_type"] == "rsa"
-        ):
+        elif key_size < 1024 and key_type == "rsa":
             tags.update({"dkim3": "P-sub1024"})
         else:
             tags.update({"dkim6": "P-invalid"})
