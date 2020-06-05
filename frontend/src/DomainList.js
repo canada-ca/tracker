0c6f9b05367a25f52b8c70ec4f6d5a38fcf99242
Mike Williamson <mike@korora.ca>
2020-06-05 18:34:55 -0400

    Rework domains page (#503)
    * Rework domains page
    
    This commit breaks down the domains page by extracting some smaller components
    from it and modifies the graphql query to fetch domains via the organization.
    There are some weird workarounds in here that will be removed in a followup
    commit after some of the api stuff is improved.
    
    * Remove hackery and use slimmed down domains query

---
+++ frontend/src/DomainList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Trans } from '@lingui/macro'
+import { List, ListItem } from '@chakra-ui/core'
+import { string, arrayOf, func, shape, object } from 'prop-types'
+
+export function DomainList({ domains = [], ...props }) {
+  const elements = []
+  if (!domains || (domains && domains.length === 0)) {
+    return (
+      <List {...props}>
+        <ListItem key={'no-domains-scanned'}>
+          <Trans>No domains scanned yet.</Trans>
+        </ListItem>
+      </List>
+    )
+  } else {
+    for (const domain of domains) {
+      elements.push(props.children(domain))
+    }
+    return <List {...props}>{elements}</List>
+  }
+}
+
+DomainList.propTypes = {
+  children: func,
+  domains: arrayOf(
+    shape({ node: shape({ organization: object, url: string, lastRan: string }) }),
+  ),
+}
