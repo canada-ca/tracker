fc8d58116b93e3e3ce61146bf524a07783dfdbc1
Ethan <fossliam@outlook.com>
2020-06-08 07:09:19 -0300

    Scanner fix (#512)
    * Amend result insertion for new database module and rework dispatcher to prevent nested Async calls outside of test environment
    
    * Amend fire_scan.py
    
    * initiate_scan -> synchronous
    
    * Manual scan return vals
    
    * Don't return PlainTextResponse in test
    
    * json -> string before ensuring dict format
    
    * Amend asynchronous calls and awaiting
    
    * Update dispatcher tests to reflect asynchronous dispatch functions
    
    * Update dispatcher tests to reflect asynchronous dispatch functions
    
    * Ran black

--- scanners/dispatcher/tests/test_dispatcher.py
+++ scanners/dispatcher/tests/test_dispatcher.py
@@ -1,39 +1,26 @@
 import pytest
+import asyncio
+from unittest.mock import MagicMock
 from pretend import stub
 from starlette.testclient import TestClient
-from starlette.responses import PlainTextResponse
 from dispatcher import Server
 from utils import *
 
 
 def test_web_scan():
-    test_dkim = stub(
-        stub_function=lambda payload, client: PlainTextResponse(
-            "Dispatched to dkim scanner"
-        )
-    )
-    test_dmarc = stub(
-        stub_function=lambda payload, client: PlainTextResponse(
-            "Dispatched to dmarc scanner"
-        )
-    )
-    test_https = stub(
-        stub_function=lambda payload, client: PlainTextResponse(
-            "Dispatched to https scanner"
-        )
-    )
-    test_ssl = stub(
-        stub_function=lambda payload, client: PlainTextResponse(
-            "Dispatched to ssl scanner"
-        )
-    )
+    test_dmarc = MagicMock(return_value=asyncio.Future())
+    test_dmarc.return_value.set_result("Dispatched to dmarc scanner")
+    test_https = MagicMock(return_value=asyncio.Future())
+    test_https.return_value.set_result("Dispatched to https scanner")
+    test_ssl = MagicMock(return_value=asyncio.Future())
+    test_ssl.return_value.set_result("Dispatched to ssl scanner")
 
     test_app = Server(
         scanners={
-            "dkim": Scanner(test_dkim.stub_function),
-            "dmarc": Scanner(test_dmarc.stub_function),
-            "https": Scanner(test_https.stub_function),
-            "ssl": Scanner(test_ssl.stub_function),
+            "dkim": {"auto": scan_dkim, "manual": manual_scan_dkim},
+            "dmarc": {"auto": test_dmarc, "manual": manual_scan_dmarc},
+            "https": {"auto": test_https, "manual": manual_scan_https},
+            "ssl": {"auto": test_ssl, "manual": manual_scan_ssl},
         },
     )
 
@@ -49,37 +36,21 @@ def test_web_scan():
 
     res = test_client.post("/receive", headers=headers)
 
-    assert res.text == "All scans successfully dispatched to designated scanners"
+    assert res.text == "Scan successfully dispatched to designated scanners"
 
 
 def test_mail_scan():
-    test_dkim = stub(
-        stub_function=lambda payload, client: PlainTextResponse(
-            "Dispatched to dkim scanner"
-        )
-    )
-    test_dmarc = stub(
-        stub_function=lambda payload, client: PlainTextResponse(
-            "Dispatched to dmarc scanner"
-        )
-    )
-    test_https = stub(
-        stub_function=lambda payload, client: PlainTextResponse(
-            "Dispatched to https scanner"
-        )
-    )
-    test_ssl = stub(
-        stub_function=lambda payload, client: PlainTextResponse(
-            "Dispatched to ssl scanner"
-        )
-    )
+    test_dmarc = MagicMock(return_value=asyncio.Future())
+    test_dmarc.return_value.set_result("Dispatched to dmarc scanner")
+    test_dkim = MagicMock(return_value=asyncio.Future())
+    test_dkim.return_value.set_result("Dispatched to dkim scanner")
 
     test_app = Server(
         scanners={
-            "dkim": Scanner(test_dkim.stub_function),
-            "dmarc": Scanner(test_dmarc.stub_function),
-            "https": Scanner(test_https.stub_function),
-            "ssl": Scanner(test_ssl.stub_function),
+            "dkim": {"auto": test_dkim, "manual": manual_scan_dkim},
+            "dmarc": {"auto": test_dmarc, "manual": manual_scan_dmarc},
+            "https": {"auto": scan_https, "manual": manual_scan_https},
+            "ssl": {"auto": scan_ssl, "manual": manual_scan_ssl},
         },
     )
 
@@ -99,92 +70,70 @@ def test_mail_scan():
 
     res = test_client.post("/receive", headers=headers)
 
-    assert res.text == "All scans successfully dispatched to designated scanners"
+    assert res.text == "Scan successfully dispatched to designated scanners"
 
 
-def test_https_dispatch():
-    client_stub = stub(post=lambda url, json: None)
-
-    test_app = Server(
-        scanners={
-            "dkim": Scanner(scan_type=scan_dkim),
-            "dmarc": Scanner(scan_type=scan_dmarc),
-            "https": Scanner(scan_type=scan_https),
-            "ssl": Scanner(scan_type=scan_ssl),
-        },
-        default_client=client_stub,
-    )
-
-    test_client = TestClient(test_app)
-
-    payload = {"scan_id": 1, "domain": "cyber.gc.ca"}
-
-    res = test_client.post("/https", json=payload)
-
-    assert res.text == "Dispatched to https scanner"
-
-
-def test_ssl_dispatch():
-    client_stub = stub(post=lambda url, json: None)
+def test_manual_web_scan():
+    test_dmarc = MagicMock(return_value=asyncio.Future())
+    test_dmarc.return_value.set_result("Dispatched to dmarc scanner")
+    test_https = MagicMock(return_value=asyncio.Future())
+    test_https.return_value.set_result("Dispatched to https scanner")
+    test_ssl = MagicMock(return_value=asyncio.Future())
+    test_ssl.return_value.set_result("Dispatched to ssl scanner")
 
     test_app = Server(
         scanners={
-            "dkim": Scanner(scan_type=scan_dkim),
-            "dmarc": Scanner(scan_type=scan_dmarc),
-            "https": Scanner(scan_type=scan_https),
-            "ssl": Scanner(scan_type=scan_ssl),
+            "dkim": {"auto": scan_dkim, "manual": manual_scan_dkim},
+            "dmarc": {"auto": scan_dmarc, "manual": test_dmarc},
+            "https": {"auto": scan_https, "manual": test_https},
+            "ssl": {"auto": scan_ssl, "manual": test_ssl},
         },
-        default_client=client_stub,
     )
 
     test_client = TestClient(test_app)
+    test_app.state.client = test_client
 
-    payload = {"scan_id": 1, "domain": "cyber.gc.ca"}
+    test_payload = {"scan_id": 1, "domain": "cyber.gc.ca", "user_init": True}
+    headers = {
+        "Content-Type": "application/json",
+        "Data": str(test_payload),
+        "Scan-Type": "web",
+    }
 
-    res = test_client.post("/ssl", json=payload)
+    res = test_client.post("/receive", headers=headers)
 
-    assert res.text == "Dispatched to ssl scanner"
+    assert res.text == "Scan successfully dispatched to designated scanners"
 
 
-def test_dmarc_dispatch():
-    client_stub = stub(post=lambda url, json: None)
+def test_manual_mail_scan():
+    test_dmarc = MagicMock(return_value=asyncio.Future())
+    test_dmarc.return_value.set_result("Dispatched to dmarc scanner")
+    test_dkim = MagicMock(return_value=asyncio.Future())
+    test_dkim.return_value.set_result("Dispatched to dkim scanner")
 
     test_app = Server(
         scanners={
-            "dkim": Scanner(scan_type=scan_dkim),
-            "dmarc": Scanner(scan_type=scan_dmarc),
-            "https": Scanner(scan_type=scan_https),
-            "ssl": Scanner(scan_type=scan_ssl),
+            "dkim": {"auto": scan_dkim, "manual": test_dkim},
+            "dmarc": {"auto": scan_dmarc, "manual": test_dmarc},
+            "https": {"auto": scan_https, "manual": manual_scan_https},
+            "ssl": {"auto": scan_ssl, "manual": manual_scan_ssl},
         },
-        default_client=client_stub,
     )
 
     test_client = TestClient(test_app)
+    test_app.state.client = test_client
 
-    payload = {"scan_id": 1, "domain": "cyber.gc.ca"}
+    test_payload = {
+        "scan_id": 1,
+        "domain": "selector1._domainkey.cyber.gc.ca",
+        "user_init": True,
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Data": str(test_payload),
+        "Scan-Type": "mail",
+    }
 
-    res = test_client.post("/dmarc", json=payload)
+    res = test_client.post("/receive", headers=headers)
 
-    assert res.text == "Dispatched to dmarc scanner"
-
-
-def test_dkim_dispatch():
-    client_stub = stub(post=lambda url, json: None)
-
-    test_app = Server(
-        scanners={
-            "dkim": Scanner(scan_type=scan_dkim),
-            "dmarc": Scanner(scan_type=scan_dmarc),
-            "https": Scanner(scan_type=scan_https),
-            "ssl": Scanner(scan_type=scan_ssl),
-        },
-        default_client=client_stub,
-    )
-
-    test_client = TestClient(test_app)
-
-    payload = {"scan_id": 1, "domain": "selector1._domainkey.cyber.gc.ca"}
-
-    res = test_client.post("/dkim", json=payload)
-
-    assert res.text == "Dispatched to dkim scanner"
+    assert res.text == "Scan successfully dispatched to designated scanners"
--- scanners/dispatcher/utils.py
+++ scanners/dispatcher/utils.py
@@ -1,47 +1,81 @@
-def Scanner(scan_type):
-    dispatch_function = scan_type
-
-    def dispatch(payload, client):
-        dispatch_function(payload, client)
-
-    return dispatch
-
-
-def scan_dkim(payload, client):
-    client.post(url="http://dkim-scanner.tracker.svc.cluster.local/scan", json=payload)
+async def scan_dkim(payload, client):
+    try:
+        client.post(
+            url="http://dkim-scanner.tracker.svc.cluster.local/scan", json=payload
+        )
+    except Exception as e:
+        return str(e)
+    return "Dispatched to dkim scanner"
 
 
-def scan_dmarc(payload, client):
-    client.post(url="http://dmarc-scanner.tracker.svc.cluster.local/scan", json=payload)
+async def scan_dmarc(payload, client):
+    try:
+        client.post(
+            url="http://dmarc-scanner.tracker.svc.cluster.local/scan", json=payload
+        )
+    except Exception as e:
+        return str(e)
+    return "Dispatched to dmarc scanner"
 
 
-def scan_https(payload, client):
-    client.post(url="http://https-scanner.tracker.svc.cluster.local/scan", json=payload)
+async def scan_https(payload, client):
+    try:
+        client.post(
+            url="http://https-scanner.tracker.svc.cluster.local/scan", json=payload
+        )
+    except Exception as e:
+        return str(e)
+    return "Dispatched to https scanner"
 
 
-def scan_ssl(payload, client):
-    client.post(url="http://ssl-scanner.tracker.svc.cluster.local/scan", json=payload)
+async def scan_ssl(payload, client):
+    try:
+        client.post(
+            url="http://ssl-scanner.tracker.svc.cluster.local/scan", json=payload
+        )
+    except Exception as e:
+        return str(e)
+    return "Dispatched to ssl scanner"
 
 
-def manual_scan_dkim(payload, client):
-    client.post(
-        url="http://dkim-scanner-manual.tracker.svc.cluster.local/scan", json=payload
-    )
+async def manual_scan_dkim(payload, client):
+    try:
+        client.post(
+            url="http://dkim-scanner-manual.tracker.svc.cluster.local/scan",
+            json=payload,
+        )
+    except Exception as e:
+        return str(e)
+    return "Dispatched to dkim scanner"
 
 
-def manual_scan_dmarc(payload, client):
-    client.post(
-        url="http://dmarc-scanner-manual.tracker.svc.cluster.local/scan", json=payload,
-    )
+async def manual_scan_dmarc(payload, client):
+    try:
+        client.post(
+            url="http://dmarc-scanner-manual.tracker.svc.cluster.local/scan",
+            json=payload,
+        )
+    except Exception as e:
+        return str(e)
+    return "Dispatched to dmarc scanner"
 
 
-def manual_scan_https(payload, client):
-    client.post(
-        url="http://https-scanner-manual.tracker.svc.cluster.local/scan", json=payload,
-    )
+async def manual_scan_https(payload, client):
+    try:
+        client.post(
+            url="http://https-scanner-manual.tracker.svc.cluster.local/scan",
+            json=payload,
+        )
+    except Exception as e:
+        return str(e)
+    return "Dispatched to https scanner"
 
 
-def manual_scan_ssl(payload, client):
-    client.post(
-        url="http://ssl-scanner-manual.tracker.svc.cluster.local/scan", json=payload
-    )
+async def manual_scan_ssl(payload, client):
+    try:
+        client.post(
+            url="http://ssl-scanner-manual.tracker.svc.cluster.local/scan", json=payload
+        )
+    except Exception as e:
+        return str(e)
+    return "Dispatched to ssl scanner"
