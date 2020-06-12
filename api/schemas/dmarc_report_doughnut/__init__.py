1b0cf1b9f0d29a9013bf0ab55b5b013956446b4d
IdezHD <neatnick27@gmail.com>
2020-06-12 14:20:55 -0300

    Create demo doughnut query

--- api/schemas/dmarc_report_doughnut/__init__.py
+++ api/schemas/dmarc_report_doughnut/__init__.py
@@ -4,7 +4,10 @@ from enums.period import PeriodEnums
 from scalars.year import Year
 from scalars.slug import Slug
 from schemas.dmarc_report_doughnut.dmarc_report_doughnut import DmarcReportDoughnut
-from schemas.dmarc_report_doughnut.resolver import resolve_get_dmarc_report_doughnut
+from schemas.dmarc_report_doughnut.resolver import (
+    resolve_get_dmarc_report_doughnut,
+    resolve_demo_get_dmarc_report_doughnut,
+)
 
 
 get_dmarc_report_doughnut = graphene.Field(
@@ -13,6 +16,16 @@ get_dmarc_report_doughnut = graphene.Field(
     period=graphene.Argument(PeriodEnums, required=True),
     year=graphene.Argument(Year, required=True),
     description="A query object used to grab the data to create dmarc report "
-                "doughnuts",
+    "doughnuts",
     resolver=resolve_get_dmarc_report_doughnut,
+)
+
+demo_get_dmarc_report_doughnut = graphene.Field(
+    lambda: DmarcReportDoughnut,
+    domain_slug=graphene.Argument(Slug, required=True),
+    period=graphene.Argument(PeriodEnums, required=True),
+    year=graphene.Argument(Year, required=True),
+    description="A query object used to grab the data to create dmarc report "
+    "doughnuts",
+    resolver=resolve_demo_get_dmarc_report_doughnut,
 )
