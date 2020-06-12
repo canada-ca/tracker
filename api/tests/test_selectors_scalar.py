1dc14a15c28ba5d19e57dd7661b908d484a65bca
Ethan Foss <fossliam@outlook.com>
2020-06-12 11:02:24 -0300

    Fix non-list tests

--- api/tests/test_selectors_scalar.py
+++ api/tests/test_selectors_scalar.py
@@ -16,18 +16,20 @@ def test_valid_selectors_parse_value_list():
 
 def test_valid_selectors_serialize_not_list():
     test_selectors = "selector1._domainkey"
-    assert Selectors.serialize(test_selectors)
+    with pytest.raises(GraphQLError, match=scalar_error_type("List", test_selectors)):
+        assert Selectors.serialize(test_selectors)
 
 
 def test_valid_selectors_parse_value_not_list():
     test_selectors = "selector1._domainkey"
-    assert Selectors.parse_value(test_selectors)
+    with pytest.raises(GraphQLError, match=scalar_error_type("List", test_selectors)):
+        assert Selectors.parse_value(test_selectors)
 
 
 def test_valid_selectors_parse_literal():
     assert Selectors.parse_literal(
         ast.ListValue(
-            value=[
+            values=[
                 ast.StringValue(value="selector1._domainkey"),
                 ast.StringValue(value="selector2._domainkey"),
             ]
@@ -60,7 +62,7 @@ def test_invalid_selectors_parse_value_wrong_type():
 
 
 def test_invalid_selectors_parse_literal_not_selector():
-    test_list = ast.ListValue(value=[ast.StringValue(value="This Will Fail")])
+    test_list = ast.ListValue(values=[ast.StringValue(value="This Will Fail")])
     with pytest.raises(
         GraphQLError, match=scalar_error_type("Selectors", test_list.value)
     ):
@@ -68,7 +70,7 @@ def test_invalid_selectors_parse_literal_not_selector():
 
 
 def test_invalid_selectors_parse_literal_wrong_ast_type():
-    test_list = ast.ListValue(value=[ast.IntValue(value=1234)])
+    test_list = ast.ListValue(values=[ast.IntValue(value=1234)])
     with pytest.raises(
         GraphQLError,
         match=scalar_error_only_types("strings", "selectors", str(ast.Type)),
