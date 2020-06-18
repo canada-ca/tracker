77ade859f77249c915b116fb89158c580382fb96
Kyle Sullivan <kyle.j.s.sullivan@gmail.com>
2020-06-18 15:25:26 -0300

    Remove old schema

--- api/tests/testdata/get_dmarc_report_churro_chart/__init__.py
+++
@@ -1,6 +0,0 @@
-from tests.testdata.get_dmarc_report_churro_chart.get_dmarc_report_churro_chart_data import (
-    dmarc_report_churro_chart_data,
-)
-from tests.testdata.get_dmarc_report_churro_chart.get_dmarc_report_churro_chart_expected_results import (
-    dmarc_report_churro_chart_expected_data,
-)
