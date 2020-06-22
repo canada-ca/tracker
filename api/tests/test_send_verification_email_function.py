3f093fad2c662432ad69a98834fb363a2f1647cf
IdezHD <neatnick27@gmail.com>
2020-06-22 08:53:42 -0300

    Fix send_verification_email_function tests

--- api/tests/test_send_verification_email_function.py
+++ api/tests/test_send_verification_email_function.py
@@ -70,17 +70,14 @@ def test_permanent_failure_send_verification_email(mocker, caplog):
             password="testpassword123",
             display_name="test account",
         )
-        with pytest.raises(
-            GraphQLError,
-            match="Error, when sending verification email, please try again.",
-        ):
-            caplog.set_level(logging.WARNING)
-            send_verification_email(user=temp_user, client=mock_client)
+        caplog.set_level(logging.WARNING)
+        response = send_verification_email(user=temp_user, client=mock_client)
 
-        assert (
-            f"permanent-failure occurred when attempting to send {temp_user.id}'s verification email."
-            in caplog.text
-        )
+    assert response == "Email Send Error: permanent-failure"
+    assert (
+        f"permanent-failure occurred when attempting to send {temp_user.id}'s verification email."
+        in caplog.text
+    )
 
 
 def test_temporary_failure_send_verification_email(mocker, caplog):
@@ -101,18 +98,23 @@ def test_temporary_failure_send_verification_email(mocker, caplog):
 
         mock_client.send_email_notification = MagicMock(return_value={})
 
-        temp_user = Users(
-            user_name="temp-fail@simulator.notify",
-            password="testpassword123",
-            display_name="test account",
-        )
-        with pytest.raises(
-            GraphQLError,
-            match="Error, when sending verification email, please try again.",
-        ):
-            caplog.set_level(logging.WARNING)
-            send_verification_email(user=temp_user, client=mock_client)
+        request_headers = {"Origin": "https://testserver.com"}
+        with app.test_request_context(headers=request_headers):
+            mock_client = NotificationsAPIClient(
+                api_key=NOTIFICATION_API_KEY, base_url=NOTIFICATION_API_URL
+            )
 
+            mock_client.send_email_notification = MagicMock(return_value={})
+
+            temp_user = Users(
+                user_name="temp-fail@simulator.notify",
+                password="testpassword123",
+                display_name="test account",
+            )
+            caplog.set_level(logging.WARNING)
+            response = send_verification_email(user=temp_user, client=mock_client)
+
+        assert response == "Email Send Error: temporary-failure"
         assert (
             f"temporary-failure occurred when attempting to send {temp_user.id}'s verification email."
             in caplog.text
@@ -142,13 +144,10 @@ def test_technical_failure_send_verification_email(mocker, caplog):
             password="testpassword123",
             display_name="test account",
         )
-        with pytest.raises(
-            GraphQLError,
-            match="Error, when sending verification email, please try again.",
-        ):
-            caplog.set_level(logging.WARNING)
-            send_verification_email(user=temp_user, client=mock_client)
+        caplog.set_level(logging.WARNING)
+        response = send_verification_email(user=temp_user, client=mock_client)
 
+        assert response == "Email Send Error: technical-failure"
         assert (
             f"technical-failure occurred when attempting to send {temp_user.id}'s verification email."
             in caplog.text
