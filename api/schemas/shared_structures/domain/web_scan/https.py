796d9d308c1b210842315b66b50a19e68f65e5d7
IdezHD <neatnick27@gmail.com>
2020-06-30 07:54:41 -0300

    Add extra none checks for httpsGuidanceTags

--- api/schemas/shared_structures/domain/web_scan/https.py
+++ api/schemas/shared_structures/domain/web_scan/https.py
@@ -72,39 +72,42 @@ class HTTPS(SQLAlchemyObjectType):
         # Implementation
         implementation = self.https_scan.get("https", {}).get("implementation", None)
 
-        if isinstance(implementation, str):
-            implementation = implementation.lower()
+        if implementation is not None:
+            if isinstance(implementation, str):
+                implementation = implementation.lower()
 
-        if implementation == "downgrades https":
-            tags.append("https3")
-        elif implementation == "bad chain":
-            tags.append("https4")
-        elif implementation == "bad hostname":
-            tags.append("https5")
+            if implementation == "downgrades https":
+                tags.append("https3")
+            elif implementation == "bad chain":
+                tags.append("https4")
+            elif implementation == "bad hostname":
+                tags.append("https5")
 
         # Enforced
         enforced = self.https_scan.get("https", {}).get("enforced", None)
 
-        if isinstance(enforced, str):
-            enforced = enforced.lower()
+        if enforced is not None:
+            if isinstance(enforced, str):
+                enforced = enforced.lower()
 
-        if enforced == "moderate":
-            tags.append("https8")
-        elif enforced == "weak":
-            tags.append("https7")
-        elif enforced == "not enforced":
-            tags.append("https6")
+            if enforced == "moderate":
+                tags.append("https8")
+            elif enforced == "weak":
+                tags.append("https7")
+            elif enforced == "not enforced":
+                tags.append("https6")
 
         # HSTS
         hsts = self.https_scan.get("https", {}).get("hsts", None)
 
-        if isinstance(hsts, str):
-            hsts = hsts.lower()
+        if hsts is not None:
+            if isinstance(hsts, str):
+                hsts = hsts.lower()
 
-        if hsts == "hsts max age too short":
-            tags.append("https10")
-        elif hsts == "no hsts":
-            tags.append("https9")
+            if hsts == "hsts max age too short":
+                tags.append("https10")
+            elif hsts == "no hsts":
+                tags.append("https9")
 
         # HSTS Age
         hsts_age = self.https_scan.get("https", {}).get("hsts_age", None)
@@ -116,26 +119,29 @@ class HTTPS(SQLAlchemyObjectType):
         # Preload Status
         preload_status = self.https_scan.get("https", {}).get("preload_status", None)
 
-        if isinstance(preload_status, str):
-            preload_status = preload_status.lower()
+        if preload_status is not None:
+            if isinstance(preload_status, str):
+                preload_status = preload_status.lower()
 
-        if preload_status == "hsts preload ready":
-            tags.append("https11")
-        elif preload_status == "hsts not preloaded":
-            tags.append("https12")
+            if preload_status == "hsts preload ready":
+                tags.append("https11")
+            elif preload_status == "hsts not preloaded":
+                tags.append("https12")
 
         # Expired Cert
         expired_cert = self.https_scan.get("https", {}).get("expired_cert", None)
 
-        if expired_cert:
-            tags.append("https13")
+        if expired_cert is not None:
+            if expired_cert is True:
+                tags.append("https13")
 
         # Self Signed Cert
         self_signed_cert = self.https_scan.get("https", {}).get(
             "self_signed_cert", None
         )
 
-        if self_signed_cert:
-            tags.append("https14")
+        if self_signed_cert is not None:
+            if self_signed_cert is True:
+                tags.append("https14")
 
         return tags
