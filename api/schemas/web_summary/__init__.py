56c7ad1ff038073582aaa39f3b9b476b44c7f4d4
IdezHD <neatnick27@gmail.com>
2020-07-02 14:16:14 -0300

    updated descriptions

--- api/schemas/web_summary/__init__.py
+++ api/schemas/web_summary/__init__.py
@@ -6,11 +6,11 @@ from schemas.web_summary.resolver import resolve_web_summary, resolve_demo_web_s
 web_summary = graphene.Field(
     lambda: CatagorizedSummary,
     resolver=resolve_web_summary,
-    description="Web summary computed values, used to build the landing page doughnuts.",
+    description="Web summary computed values, used to build summary cards.",
 )
 
 demo_web_summary = graphene.Field(
     lambda: CatagorizedSummary,
     resolver=resolve_demo_web_summary,
-    description="Demo web summary computed values, used to build the public landing page doughnuts.",
+    description="Demo web summary computed values, used to build summary cards.",
 )
