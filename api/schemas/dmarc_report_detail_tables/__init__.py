60b6c57c51261331dc2cc53d3a35e62e562ad326
IdezHD <neatnick27@gmail.com>
2020-07-06 07:26:36 -0300

    Started work on revision

--- api/schemas/dmarc_report_detail_tables/__init__.py
+++ api/schemas/dmarc_report_detail_tables/__init__.py
@@ -12,44 +12,19 @@ from schemas.dmarc_report_detail_tables.resolver import (
 )
 
 
-class DmarcReportDetailTablesInput(graphene.InputObjectType):
-    """
-    Input object containing fields which map to the required arguments for
-    dmarcReportDetailTablesInput
-    """
-
-    domain_slug = Slug(
-        description="The slugified version of the domain you wish to retrieve data for.",
-        required=True,
-    )
-    period = PeriodEnums(
-        description="The period in which the returned data is relevant to.",
-        required=True,
-    )
-    year = Year(
-        description="The year in which the returned data is relevant to.",
-        required=True,
-    )
-
-
-dmarc_report_detail_tables = graphene.Field(
-    lambda: DmarcReportDetailTables,
-    input=DmarcReportDetailTablesInput(
-        required=True,
-        description="Input object containing fields which map to the required "
-        "arguments for dmarcReportDetailTablesInput",
-    ),
-    resolver=resolve_dmarc_report_detail_tables,
-    description="Query used for gathering data for dmarc report detail tables.",
-)
-
-
 demo_dmarc_report_detail_tables = graphene.Field(
     lambda: DmarcReportDetailTables,
-    input=DmarcReportDetailTablesInput(
+    domain_slug=Slug(
+        description="The slugified version of the domain you wish to retrieve data for.",
         required=True,
-        description="Input object containing fields which map to the required "
-        "arguments for dmarcReportDetailTablesInput",
+    ),
+    period=PeriodEnums(
+        description="The period in which the returned data is relevant to.",
+        required=True,
+    ),
+    year=Year(
+        description="The year in which the returned data is relevant to.",
+        required=True,
     ),
     resolver=resolve_demo_dmarc_report_detail_tables,
     description="Query used for gathering data for dmarc report detail tables.",
