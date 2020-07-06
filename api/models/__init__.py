6af16e8008b701ed64edd6893243fbd960b304b1
IdezHD <neatnick27@gmail.com>
2020-07-06 07:28:26 -0300

    remove dmarc report table

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -13,19 +13,6 @@ from models.Organizations import Organizations
 from models.Summaries import Summaries
 
 
-class Dmarc_Reports(Base):
-    __tablename__ = "dmarc_reports"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    domain_id = Column(Integer, ForeignKey("domains.id"))
-    start_date = Column(DateTime)
-    end_date = Column(DateTime)
-    report = Column(JSONB)
-    domain = relationship(
-        "Domains", back_populates="dmarc_reports", cascade="all, delete"
-    )
-
-
 class Web_scans(Base):
     __tablename__ = "web_scans"
 
