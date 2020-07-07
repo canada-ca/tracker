865a354115e586a8fe31f26bd58822f8cee3553e
IdezHD <neatnick27@gmail.com>
2020-07-07 08:18:08 -0300

    Updated updatePassword mutation to use new reset link setup

--- api/schemas/update_user_password/update_password.py
+++ api/schemas/update_user_password/update_password.py
@@ -1,58 +1,48 @@
-import bcrypt
-
 from graphql import GraphQLError
 
 from app import logger
 from db import db_session
-from functions.input_validators import *
+from functions.input_validators import is_strong_password
 from functions.error_messages import *
 from models import Users
 
 
-def update_password(user_name, password, confirm_password):
+def update_password(user, password, confirm_password):
     """
     This function allows a user's password to be updated / reset.
-    :param user_name: Email address associated with the account who's password is being updated
+    :param user: The user who requested a password change
     :param password: The new password to be set.
     :param confirm_password: A confirmation of the new password -- must be identical to password
     :return user: Returns the freshly updated user object retrieved from the database
     """
-    password = cleanse_input(password)
-    confirm_password = cleanse_input(confirm_password)
-    user_name = cleanse_input(user_name)
 
-    user = Users.find_by_user_name(user_name)
-
-    if user is None:
-        logger.warning(
-            f"User attempted to update password for {user_name}, but the account does not exist."
-        )
-        raise GraphQLError(error_user_does_not_exist())
-
+    # Check to make sure password meets GoC requirements
     if not is_strong_password(password):
         logger.warning(
             f"User: {user.id} attempted to update password, but requirements were not met."
         )
         raise GraphQLError(error_password_does_not_meet_requirements())
 
+    # Check to ensure that both passwords match
     if password != confirm_password:
         logger.warning(
             f"User: {user.id} attempted to update password, but passwords did not match."
         )
         raise GraphQLError(error_passwords_do_not_match())
 
-    user = db_session.query(Users).filter(Users.user_name == user_name).first()
+    # Update the users password
     user.update_password(password=password)
 
+    # Try to update the db, and catch if there's an error
     try:
         db_session.commit()
-        user = Users.find_by_user_name(user_name)
+        user = Users.find_by_id(user.id)
         logger.info(f"User: {user.id} successfully updated their password.")
-        return user
+        return True
     except Exception as e:
         db_session.rollback()
         db_session.flush()
         logger.error(
             f"A database exception occurred when a user: {user.id} tried to update their password: {str(e)}"
         )
-        raise GraphQLError(error_password_not_updated())
+        raise GraphQLError("Error when updating password, please try again.")
--- api/schemas/update_user_password/update_user_password.py
+++ api/schemas/update_user_password/update_user_password.py
@@ -1,21 +1,118 @@
-import graphene
+import os
 
+import graphene
+import jwt
+
+from graphql import GraphQLError
+
+from app import logger
+from functions.input_validators import cleanse_input
+from models import Users
 from schemas.update_user_password.update_password import update_password
-from schemas.user.user import User
-from scalars.email_address import EmailAddress
+
+
+class UpdateUserPasswordInput(graphene.InputObjectType):
+    """
+    Input object containing all fields for the UpdateUserPassword mutation
+    """
+
+    reset_token = graphene.String(
+        required=True,
+        description="Reset token found in the url, which was sent to the users inbox, by the sendPasswordReset mutation.",
+    )
+    password = graphene.String(
+        required=True, description="The new password the user wishes to change to."
+    )
+    confirm_password = graphene.String(
+        required=True,
+        description="A confirmation of the password field to ensure the user has entered the new password correctly.",
+    )
 
 
 class UpdateUserPassword(graphene.Mutation):
-    class Arguments:
-        password = graphene.String(required=True)
-        confirm_password = graphene.String(required=True)
-        user_name = EmailAddress(required=True)
+    """
+    This mutation allows users to use a token sent to them by email to update
+    their account password.
+    """
 
-    user = graphene.Field(lambda: User)
+    class Arguments:
+        input = UpdateUserPasswordInput(
+            required=True,
+            description="An input object containing all fields for this mutation.",
+        )
+
+    successful_password_update = graphene.String(
+        description="Informs the user if the password update was successful."
+    )
 
     @staticmethod
-    def mutate(self, info, password, confirm_password, user_name):
-        user = update_password(
-            user_name=user_name, password=password, confirm_password=confirm_password
+    def mutate(self, info, **kwargs):
+        """
+        This mutation function allows the user to submit a password and a
+        password confirmation that allow their password to be updated. This
+        function will only return if the update was successful, otherwise
+        it will fail out, which properly informs the user that an error
+        occurred.
+        :param self: None
+        :param info: Request Information
+        :param kwargs: Various arguments passed in from the user
+        :return: UpdateUserPassword object with a message saying the update was successful
+        """
+        # Get arguments
+        reset_token = cleanse_input(kwargs.get("input", {}).get("reset_token"))
+        password = cleanse_input(kwargs.get("input", {}).get("password"))
+        confirm_password = cleanse_input(
+            kwargs.get("input", {}).get("confirm_password")
         )
-        return UpdateUserPassword(user=user)
+
+        # Decode token, and handle token errors
+        try:
+            payload = jwt.decode(
+                reset_token, os.getenv("SUPER_SECRET_SALT"), algorithms=["HS256"]
+            )
+        except jwt.ExpiredSignatureError:
+            logger.warning(f"User attempted to reset password, but token was expired.")
+            raise GraphQLError(
+                "Error, token has expired please request another password reset email."
+            )
+        except jwt.InvalidTokenError:
+            logger.warning(
+                f"User attempted to reset password, but the token was invalid."
+            )
+            raise GraphQLError(
+                "Error, token has expired please request another password reset email."
+            )
+
+        # Get the id from the token payload
+        user_id = payload.get("user_id")
+
+        # Check to see if the token returned none
+        if user_id is not None:
+            # Find the user who is requesting a password reset
+            user = Users.find_by_id(id=user_id)
+            # Ensure that user exists
+            if user is not None:
+                # Dispatch password updating to update_password functions
+                successful_update = update_password(
+                    user=user, password=password, confirm_password=confirm_password
+                )
+
+                # Check to make sure that update_password was successful, and inform the user
+                if successful_update is True:
+                    return UpdateUserPassword(
+                        status="Successfully updated user password, please sign in with new password."
+                    )
+            else:
+                logger.warning(
+                    f"User: {user_id} attempted to reset password, but there is no account affiliated with that id."
+                )
+                raise GraphQLError(
+                    "Error, token has expired please request another password reset email."
+                )
+        else:
+            logger.warning(
+                f"A user attempted to change password but user id was not found in the reset token."
+            )
+            raise GraphQLError(
+                "Error, token has expired please request another password reset email."
+            )
