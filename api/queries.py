d22a29c6a966bf9cc56db41d4805cf90a6788fde
IdezHD <neatnick27@gmail.com>
2020-07-09 11:55:54 -0300

    Add to queries for manual testing

--- api/queries.py
+++ api/queries.py
@@ -96,6 +96,9 @@ from schemas.user_affiliations import UpdateUserRole
 # Validate Two Factor
 from schemas.validate_two_factor.validate_two_factor import ValidateTwoFactor
 
+# Invite user to org
+from schemas.invite_user_to_org import InviteUserToOrg
+
 # --- End Mutation Imports ---
 
 
@@ -235,6 +238,8 @@ class Mutation(graphene.ObjectType):
     update_user_role = UpdateUserRole.Field(
         description="Updates the users permission to an organization."
     )
+
+    invite_user_to_org = InviteUserToOrg.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
