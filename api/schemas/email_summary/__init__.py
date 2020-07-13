4cba25f00c486b139156a470182bf43d9ab8c212
nslandolt <nc575894@dal.ca>
2020-07-13 06:54:37 -0300

    I think I can spell category now (#673)
    * I think I can spell category now
    
    * Update faker

--- api/schemas/email_summary/__init__.py
+++ api/schemas/email_summary/__init__.py
@@ -1,19 +1,19 @@
 import graphene
 
-from schemas.shared_structures.catagorized_summary import CatagorizedSummary
+from schemas.shared_structures.categorized_summary import CategorizedSummary
 from schemas.email_summary.resolver import (
     resolve_email_summary,
     resolve_demo_email_summary,
 )
 
 email_summary = graphene.Field(
-    lambda: CatagorizedSummary,
+    lambda: CategorizedSummary,
     resolver=resolve_email_summary,
     description="Email summary computed values, used to build summary cards.",
 )
 
 demo_email_summary = graphene.Field(
-    lambda: CatagorizedSummary,
+    lambda: CategorizedSummary,
     resolver=resolve_demo_email_summary,
     description="Demo email summary computed values, used to build summary cards.",
 )
