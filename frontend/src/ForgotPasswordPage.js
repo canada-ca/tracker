f167c637e2f5bb04f630f312fe7d28e968eb1b15
lcampbell2 <l.campbell144768@gmail.com>
2020-07-17 13:59:27 -0300

    removed unused import

--- frontend/src/ForgotPasswordPage.js
+++ frontend/src/ForgotPasswordPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Trans, t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
-import { Text, Stack, Button, Box, useToast, Divider } from '@chakra-ui/core'
+import { Text, Stack, Button, Box, useToast } from '@chakra-ui/core'
 import EmailField from './EmailField'
 import { object, string } from 'yup'
 import { Formik } from 'formik'
