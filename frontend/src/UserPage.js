a00f44ac70fe117b21daceddb660d57654027c0b
Kyle Sullivan <kyle.j.s.sullivan@gmail.com>
2020-07-24 09:49:13 -0300

    Code cleanup

--- frontend/src/UserPage.js
+++ frontend/src/UserPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Formik } from 'formik'
 import { useHistory, useLocation } from 'react-router-dom'
 import { string } from 'prop-types'
 import {
@@ -13,16 +12,9 @@ import {
   useToast,
   useDisclosure,
   Heading,
-  ModalFooter,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  Icon,
-  InputRightElement,
 } from '@chakra-ui/core'
 import { useMutation, useQuery } from '@apollo/client'
 import PasswordConfirmation from './PasswordConfirmation'
-import LanguageSelect from './LanguageSelect'
 import { useUserState } from './UserState'
 import { QUERY_USER } from './graphql/queries'
 import EmailField from './EmailField'
@@ -30,11 +22,9 @@ import DisplayNameField from './DisplayNameField'
 import { UPDATE_USER_PROFILE } from './graphql/mutations'
 import { Trans, t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
-import ConfirmPasswordModal from './ConfirmPasswordModal'
 import PasswordField from './PasswordField'
 import EditableUserDetail from './EditableUserDetail'
 import EditableUserLanguage from './EditableUserLanguage'
-import EditDisplayNameModal from './EditDisplayNameModal'
 import { object } from 'yup'
 import { fieldRequirements } from './fieldRequirements'
 
@@ -43,33 +33,9 @@ export default function UserPage() {
   const toast = useToast()
   const history = useHistory()
   const { currentUser, logout } = useUserState()
-  const { isOpen, onOpen, onClose } = useDisclosure()
   const { i18n } = useLingui()
   const changePasswordBtnRef = React.useRef()
   const currentPasswordRef = React.useRef()
-
-  const [updatePassword, { error: updatePasswordError }] = useMutation(
-    UPDATE_USER_PROFILE,
-    {
-      context: {
-        headers: {
-          authorization: currentUser.jwt,
-        },
-      },
-      onError() {
-        console.log(updatePasswordError)
-        toast({
-          title: i18n._(t`An error occurred.`),
-          description: i18n._(
-            t`Unable to update your password, please try again.`,
-          ),
-          status: 'error',
-          duration: 9000,
-          isClosable: true,
-        })
-      },
-    },
-  )
 
   const {
     loading: queryUserLoading,
@@ -161,104 +127,7 @@ export default function UserPage() {
         <Divider />
 
         <EditableUserLanguage currentLang={queryUserData.userPage.lang} />
-
-        {/*<EditableUserDetail detailName="Email:" iconName="email" detailValue="Test email"/>*/}
-
-        {/*<EditableUserDetail detailName="Password:" iconName="lock" detailValue="********"/>*/}
-
-        {/* <EditableUserLanguage detailName="Language:" />*/}
       </Stack>
-      {/*<Formik*/}
-      {/*  initialValues={{*/}
-      {/*    displayName: queryUserData.userPage.displayName,*/}
-      {/*    email: currentUser.userName,*/}
-      {/*    lang: queryUserData.userPage.lang,*/}
-      {/*  }}*/}
-      {/*  onSubmit={(values, actions) => {*/}
-      {/*    window.alert('coming soon!!\n' + JSON.stringify(values, null, 2))*/}
-      {/*    actions.setSubmitting(false)*/}
-      {/*  }}*/}
-      {/*>*/}
-      {/*  {({ handleSubmit, isSubmitting }) => (*/}
-      {/*    <form onSubmit={handleSubmit}>*/}
-      {/*      <Stack p={25} spacing={4}>*/}
-      {/*        <Heading as="h1" size="lg" textAlign="center">*/}
-      {/*          <Trans>User Profile</Trans>*/}
-      {/*        </Heading>*/}
-
-      {/*        <DisplayNameField*/}
-      {/*          name="displayName"*/}
-      {/*          value={queryUserData.userPage.displayName}*/}
-      {/*          isReadOnly={true}*/}
-      {/*          rightInputElement={*/}
-      {/*            <InputRightElement width="fit-content">*/}
-      {/*              <Button*/}
-      {/*                id="editButton"*/}
-      {/*                h="1.75rem"*/}
-      {/*                size="sm"*/}
-      {/*                px="2rem"*/}
-      {/*                mr="1rem"*/}
-      {/*              >*/}
-      {/*                Edit*/}
-      {/*              </Button>*/}
-      {/*            </InputRightElement>*/}
-      {/*          }*/}
-      {/*        />*/}
-
-      {/*        <EmailField*/}
-      {/*          name="email"*/}
-      {/*          value={currentUser.userName}*/}
-      {/*          isReadOnly={true}*/}
-      {/*          rightInputElement={*/}
-      {/*            <InputRightElement width="fit-content">*/}
-      {/*              <Button*/}
-      {/*                id="editButton"*/}
-      {/*                h="1.75rem"*/}
-      {/*                size="sm"*/}
-      {/*                px="2rem"*/}
-      {/*                mr="1rem"*/}
-      {/*              >*/}
-      {/*                Edit*/}
-      {/*              </Button>*/}
-      {/*            </InputRightElement>*/}
-      {/*          }*/}
-      {/*        />*/}
-
-      {/*        <PasswordField*/}
-      {/*          name="password"*/}
-      {/*          isReadOnly={true}*/}
-      {/*          type="password"*/}
-      {/*          value="examplepass"*/}
-      {/*          rightInputElement={*/}
-      {/*            <InputRightElement width="fit-content">*/}
-      {/*              <Button*/}
-      {/*                id="editButton"*/}
-      {/*                h="1.75rem"*/}
-      {/*                size="sm"*/}
-      {/*                px="2rem"*/}
-      {/*                mr="1rem"*/}
-      {/*              >*/}
-      {/*                Edit*/}
-      {/*              </Button>*/}
-      {/*            </InputRightElement>*/}
-      {/*          }*/}
-      {/*        />*/}
-
-      {/*<LanguageSelect name="lang" />*/}
-
-      {/*<Button*/}
-      {/*  type="submit"*/}
-      {/*  variantColor="teal"*/}
-      {/*  width="fit-content"*/}
-      {/*  px={8}*/}
-      {/*  isLoading={isSubmitting}*/}
-      {/*>*/}
-      {/*  <Trans>Save Changes</Trans>*/}
-      {/*</Button>*/}
-      {/*</Stack>*/}
-      {/*    </form>*/}
-      {/*  )}*/}
-      {/*</Formik>*/}
 
       <Stack Stack p={25} spacing={4}>
         <Heading as="h1" size="lg" textAlign="center">
@@ -318,71 +187,6 @@ export default function UserPage() {
           <Trans>Sign Out</Trans>
         </Button>
       </Stack>
-
-      <Formik
-        initialValues={{
-          password: '',
-          confirmPassword: '',
-          currentPassword: '',
-        }}
-        onSubmit={async values => {
-          // Submit update password mutation
-          await updatePassword({
-            variables: {
-              password: values.password,
-              confirmPassword: values.confirmPassword,
-              currentPassword: values.currentPassword,
-            },
-          })
-        }}
-      >
-        {({ handleSubmit, isSubmitting, values }) => (
-          <form id="form" onSubmit={handleSubmit}>
-            <Stack spacing={4} p={25}>
-              <Heading as="h1" size="lg" textAlign="center">
-                <Trans>Change Password</Trans>
-              </Heading>
-
-              <Text>
-                <Trans>
-                  Change your password below by entering and confirming a new
-                  password.
-                </Trans>
-              </Text>
-
-              <PasswordConfirmation />
-
-              <Button
-                onClick={onOpen}
-                variantColor="teal"
-                width="fit-content"
-                px={8}
-                ref={changePasswordBtnRef}
-              >
-                <Trans>Change Password</Trans>
-              </Button>
-              <Button
-                variantColor="teal"
-                isLoading={isSubmitting}
-                type="submit"
-                id="submitBtn"
-                mr={4}
-              >
-                <Trans>Confirm</Trans>
-              </Button>
-
-              <ConfirmPasswordModal
-                isOpen={isOpen}
-                onClose={onClose}
-                initialFocusRef={currentPasswordRef}
-                finalFocusRef={changePasswordBtnRef}
-                isSubmitting={isSubmitting}
-                values={values}
-              />
-            </Stack>
-          </form>
-        )}
-      </Formik>
     </SimpleGrid>
   )
 }
