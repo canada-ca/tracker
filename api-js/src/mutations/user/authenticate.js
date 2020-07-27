30cb9a50b5878b2f595b60d3d6a845894d6cea6a
nslandolt <nsdeschenes@outlook.com>
2020-07-27 14:50:27 -0300

    Start working on authentication mutation

--- api-js/src/mutations/user/authenticate.js
+++ api-js/src/mutations/user/authenticate.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const { GraphQLNonNull, GraphQLString } = require('graphql')
 const { mutationWithClientMutationId } = require('graphql-relay')
 const { EmailAddress } = require('../../scalars')
@@ -21,10 +23,52 @@ const authenticate = new mutationWithClientMutationId({
     authResult: {
       type: authResultType,
       description: 'The authenticated users information, and JWT.',
-      resolve: async () => {},
+      resolve: async ({ authResult }) => {
+        return authResult
+      },
     },
   }),
-  mutateAndGetPayload: async () => {},
+  mutateAndGetPayload: async (args, { query, functions: { cleanseInput }}) => {
+    // Cleanse Inputs
+    let userName = cleanseInput(args.userName)
+    let password = cleanseInput(args.password)
+
+    // Gather sign in user
+    let userCursor
+    try {
+      userCursor = await query`
+        FOR user IN users
+          FILTER user.userName == ${userName}
+          RETURN user
+      `
+    } catch (err) {
+      console.error(`Database error occurred when ${userName} attempted to authenticate: ${err}`)
+      throw new Error('Unable to authenticate, please try again.')
+    }
+
+    if (userCursor.count === 0) {
+      console.error(`User: ${userName} attempted to authenticate, no account is associated with this email.`)
+      throw new Error('Unable to authenticate, please try again.')
+    }
+
+    // Get users from cursor
+    let users
+    try {
+      users = await userCursor.all()
+    } catch (err) {
+      console.error(`Cursor error occurred when user was authenticated.`)
+      throw new Error('Unable to authenticate, please try again.')
+    }
+
+    // Get user from cursor
+    const user = users.next()
+
+    if (bcrypt.compareSync(password, user.password)) {
+      if (user.failedLoginAttempts > 5 && (user.failedLoginAttemptTime + 1800) <= Math.floor(new Date().getTime() / 1000)) {
+        
+      }
+    }
+  },
 })
 
 module.exports = {
