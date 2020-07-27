32ca8b96b16316f0926a77403e620debebb01e89
nslandolt <nsdeschenes@outlook.com>
2020-07-27 06:58:16 -0300

    forgot to run prettier on the test file

--- api-js/src/__tests__/server.test.js
+++ api-js/src/__tests__/server.test.js
@@ -2,19 +2,19 @@ const request = require('supertest')
 const { Server } = require('../server')
 
 describe('parse server', () => {
-    describe('/alive', () => {
-        it('responds with a 200', async () => {
-            const response = await request(Server({ query: jest.fn() })).get('/alive')
+  describe('/alive', () => {
+    it('responds with a 200', async () => {
+      const response = await request(Server({ query: jest.fn() })).get('/alive')
 
-            expect(response.status).toEqual(200)
-        })
+      expect(response.status).toEqual(200)
     })
+  })
 
-    describe('/ready', () => {
-        it('returns 200', async () => {
-            const response = await request(Server({ query: jest.fn() })).get('/ready')
+  describe('/ready', () => {
+    it('returns 200', async () => {
+      const response = await request(Server({ query: jest.fn() })).get('/ready')
 
-            expect(response.status).toEqual(200)
-        })
+      expect(response.status).toEqual(200)
     })
+  })
 })
