98c2240f434d1b417e65943bd0b51ee48548f04d
Mike Williamson <mike@korora.ca>
2020-07-27 17:24:16 -0400

    Clean up Apollo dependencies
    This commit removes a bunch of dependencies obsoleted by the update to Apollo
    Client 3.

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,13 +1,10 @@
-import { createHttpLink } from 'apollo-link-http'
-import { ApolloClient } from '@apollo/client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import fetch from 'isomorphic-unfetch'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
-export function Client(
-  { link = createHttpLink({ fetch }), cache = new InMemoryCache() } = {},
-) {
+export function Client() {
   return new ApolloClient({
-    link,
-    cache,
+    link: new HttpLink({
+      uri: '/graphql',
+    }),
+    cache: new InMemoryCache(),
   })
 }
