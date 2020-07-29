460266af0a9905e7cdd5523880a38e77695298cb
nslandolt <nsdeschenes@outlook.com>
2020-07-29 08:51:29 -0300

    add notifications-node-client, and create notifyClient in the context

--- api-js/index.js
+++ api-js/index.js
@@ -2,18 +2,23 @@ const dotenv = require('dotenv-safe')
 dotenv.config()
 
 const { ArangoTools } = require('arango-tools')
+const { NotifyClient } = require('notifications-node-client')
 const { Server } = require('./src/server')
 const { makeMigrations } = require('./migrations')
 
-const { PORT = 4000, DB_PASS: rootPass, DB_URL: url, DB_NAME: databaseName } = process.env
+const { PORT = 4000, DB_PASS: rootPass, DB_URL: url, DB_NAME: databaseName, NOTIFICATION_API_KEY, NOTIFICATION_API_URL } = process.env
 
 ;(async () => {
   const { migrate } = await ArangoTools({ rootPass, url })
   const { query } = await migrate(
     makeMigrations({ databaseName, rootPass }),
   )
+  const notifyClient = new NotifyClient(NOTIFICATION_API_URL, NOTIFICATION_API_KEY)
 
-  Server({ query }).listen(PORT, (err) => {
+  Server({ 
+    query,
+    notifyClient,
+  }).listen(PORT, (err) => {
     if (err) throw err
     console.log(`🚀 API listening on port ${PORT}`)
   })
