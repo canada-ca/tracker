e54ca16ac6e7ebe25ded421fadb49f0c0f190827
nslandolt <nsdeschenes@outlook.com>
2020-08-04 11:41:22 -0300

    generate-jwt test to 100 coverage

--- api-js/src/__tests__/generate-jwt.test.js
+++ api-js/src/__tests__/generate-jwt.test.js
@@ -6,11 +6,21 @@ const { JWT_KEY } = process.env
 const jwt = require('jsonwebtoken')
 const { tokenize } = require('../auth')
 
-describe('given a set of parameters token can be encoded', () => {
-  it('returns a vaild encoded token', () => {
-    const token = tokenize({ parameters: { userId: 1 } })
-
-    const decoded = jwt.verify(token, String(JWT_KEY))
-    expect(decoded.parameters.userId).toEqual(1)
+describe('given a token generator', () => {
+  describe('given a set of parameters token can be encoded', () => {
+    it('returns a vaild encoded token', () => {
+      const token = tokenize({parameters: { userId: 1 }})
+  
+      const decoded = jwt.verify(token, String(JWT_KEY))
+      expect(decoded.parameters.userId).toEqual(1)
+    })
+  })
+  describe('given no parameters, token can still be encoded', () => {
+    it('returns a valid encoded token', () => {
+      const token = tokenize({})
+  
+      const decoded = jwt.verify(token, String(JWT_KEY))
+      expect(decoded.parameters).toEqual({})
+    })
   })
 })
