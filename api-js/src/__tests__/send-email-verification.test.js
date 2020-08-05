ef6bad25bf2506669afc70dc0211f3d96763515b
nslandolt <nsdeschenes@outlook.com>
2020-08-05 11:25:49 -0300

    sendEmailVerification mutation completed and 100 coverage testing

---
+++ api-js/src/__tests__/send-email-verification.test.js
@@ -0,0 +1,263 @@
+const dotenv = require('dotenv-safe')
+dotenv.config()
+
+const { DB_PASS: rootPass, DB_URL: url } = process.env
+
+const { ArangoTools, dbNameFromFile } = require('arango-tools')
+const { graphql, GraphQLSchema } = require('graphql')
+const { makeMigrations } = require('../../migrations')
+const { createQuerySchema } = require('../queries')
+const { createMutationSchema } = require('../mutations')
+
+const bcrypt = require('bcrypt')
+const { cleanseInput } = require('../validators')
+const { tokenize } = require('../auth')
+const { userLoaderByUserName } = require('../loaders')
+
+const mockNotify = jest.fn()
+
+describe('user send password reset email', () => {
+  const originalInfo = console.info
+  afterEach(() => (console.info = originalInfo))
+
+  let query, drop, truncate, migrate, collections, schema, request
+
+  beforeAll(async () => {
+    ;({ migrate } = await ArangoTools({ rootPass, url }))
+    ;({ query, drop, truncate, collections } = await migrate(
+      makeMigrations({ databaseName: dbNameFromFile(__filename), rootPass }),
+    ))
+    schema = new GraphQLSchema({
+      query: createQuerySchema(),
+      mutation: createMutationSchema(),
+    })
+    request = {
+      protocol: 'https',
+      get: (text) => text,
+    }
+  })
+
+  let consoleOutput = []
+  const mockedInfo = (output) => consoleOutput.push(output)
+  const mockedWarn = (output) => consoleOutput.push(output)
+  beforeEach(async () => {
+    console.info = mockedInfo
+    console.warn = mockedWarn
+    await truncate()
+  })
+
+  afterEach(() => {
+    consoleOutput = []
+  })
+
+  afterAll(async () => {
+    await drop()
+  })
+
+  describe('successfully sends verification email', () => {
+    describe('users preferred language is french', () => {
+      beforeEach(async () => {
+        await collections.users.save({
+          userName: 'test.account@istio.actually.exists',
+          displayName: 'Test Account',
+          preferredLang: 'french',
+          tfaValidated: false,
+          emailValidated: false,
+        })
+      })
+      it('returns status text', async () => {
+        const response = await graphql(
+          schema,
+          `
+            mutation {
+              sendEmailVerification(
+                input: { userName: "test.account@istio.actually.exists" }
+              ) {
+                status
+              }
+            }
+          `,
+          null,
+          {
+            request,
+            query,
+            auth: {
+              bcrypt,
+              tokenize,
+            },
+            functions: {
+              cleanseInput,
+            },
+            loaders: {
+              userLoaderByUserName: userLoaderByUserName(query),
+            },
+            notify: {
+              sendVerificationEmail: mockNotify,
+            },
+          },
+        )
+
+        const expectedResult = {
+          data: {
+            sendEmailVerification: {
+              status:
+                'If an account with this username is found, an email verification link will be found in your inbox.',
+            },
+          },
+        }
+
+        const cursor = await query`
+            FOR user IN users
+                FILTER user.userName == "test.account@istio.actually.exists"
+                RETURN user
+          `
+        const user = await cursor.next()
+
+        const token = tokenize({
+          parameters: { userId: user._key },
+        })
+        const verifyUrl = `${request.protocol}://${request.get(
+          'host',
+        )}/validate/${token}`
+
+        expect(response).toEqual(expectedResult)
+        expect(mockNotify).toHaveBeenCalledWith({
+          templateId: 'f2c9b64a-c754-4ffd-93e9-33fdb0b5ae0b',
+          user,
+          verifyUrl,
+        })
+        expect(consoleOutput).toEqual([
+          `User: ${user._key} successfully sent a verification email.`,
+        ])
+      })
+    })
+    describe('users preferred language is english', () => {
+      beforeEach(async () => {
+        await collections.users.save({
+          userName: 'test.account@istio.actually.exists',
+          displayName: 'Test Account',
+          preferredLang: 'english',
+          tfaValidated: false,
+          emailValidated: false,
+        })
+      })
+      it('returns status text', async () => {
+        const response = await graphql(
+          schema,
+          `
+            mutation {
+              sendEmailVerification(
+                input: { userName: "test.account@istio.actually.exists" }
+              ) {
+                status
+              }
+            }
+          `,
+          null,
+          {
+            request,
+            query,
+            auth: {
+              bcrypt,
+              tokenize,
+            },
+            functions: {
+              cleanseInput,
+            },
+            loaders: {
+              userLoaderByUserName: userLoaderByUserName(query),
+            },
+            notify: {
+              sendVerificationEmail: mockNotify,
+            },
+          },
+        )
+
+        const expectedResult = {
+          data: {
+            sendEmailVerification: {
+              status:
+                'If an account with this username is found, an email verification link will be found in your inbox.',
+            },
+          },
+        }
+
+        const cursor = await query`
+            FOR user IN users
+                FILTER user.userName == "test.account@istio.actually.exists"
+                RETURN user
+          `
+        const user = await cursor.next()
+
+        const token = tokenize({
+          parameters: { userId: user._key },
+        })
+        const verifyUrl = `${request.protocol}://${request.get(
+          'host',
+        )}/validate/${token}`
+
+        expect(response).toEqual(expectedResult)
+        expect(mockNotify).toHaveBeenCalledWith({
+          templateId: '6e3368a7-0d75-47b1-b4b2-878234e554c9',
+          user,
+          verifyUrl,
+        })
+        expect(consoleOutput).toEqual([
+          `User: ${user._key} successfully sent a verification email.`,
+        ])
+      })
+    })
+  })
+  describe('unsuccessful verification email send', () => {
+    describe('no user associated with account', () => {
+      it('returns status text', async () => {
+        const response = await graphql(
+          schema,
+          `
+            mutation {
+              sendEmailVerification(
+                input: {
+                  userName: "test.account@istio.does.not.actually.exists"
+                }
+              ) {
+                status
+              }
+            }
+          `,
+          null,
+          {
+            request,
+            query,
+            auth: {
+              bcrypt,
+              tokenize,
+            },
+            functions: {
+              cleanseInput,
+            },
+            loaders: {
+              userLoaderByUserName: userLoaderByUserName(query),
+            },
+            notify: {
+              sendVerificationEmail: mockNotify,
+            },
+          },
+        )
+
+        const expectedResult = {
+          data: {
+            sendEmailVerification: {
+              status:
+                'If an account with this username is found, an email verification link will be found in your inbox.',
+            },
+          },
+        }
+
+        expect(response).toEqual(expectedResult)
+        expect(consoleOutput).toEqual([
+          `A user attempted to send a verification email for test.account@istio.does.not.actually.exists but no account is affiliated with this user name.`,
+        ])
+      })
+    })
+  })
+})
--- api-js/src/mutations/user/send-email-verification.js
+++ api-js/src/mutations/user/send-email-verification.js
@@ -17,10 +17,58 @@ const sendEmailVerification = new mutationWithClientMutationId({
     status: {
       type: GraphQLString,
       description: 'Informs the user if the email was sent successfully.',
-      resolve: async () => {},
+      resolve: async (payload) => {
+        return payload.status
+      },
     },
   }),
-  mutateAndGetPayload: async () => {},
+  mutateAndGetPayload: async (
+    args,
+    {
+      request,
+      auth: { tokenize },
+      functions: { cleanseInput },
+      loaders: { userLoaderByUserName },
+      notify: { sendVerificationEmail },
+    },
+  ) => {
+    // Cleanse Input
+    const userName = cleanseInput(args.userName).toLowerCase()
+
+    // Get user from db
+    const user = await userLoaderByUserName.load(userName)
+
+    if (typeof user !== 'undefined') {
+      // Choose template based on users preferred language
+      let templateId
+      if (user.preferredLang === 'french') {
+        templateId = 'f2c9b64a-c754-4ffd-93e9-33fdb0b5ae0b'
+      } else {
+        templateId = '6e3368a7-0d75-47b1-b4b2-878234e554c9'
+      }
+
+      const token = tokenize({
+        parameters: { userId: user._key },
+      })
+
+      const verifyUrl = `${request.protocol}://${request.get(
+        'host',
+      )}/validate/${token}`
+
+      await sendVerificationEmail({ templateId, user, verifyUrl })
+
+      console.info(`User: ${user._key} successfully sent a verification email.`)
+    } else {
+      console.warn(
+        `A user attempted to send a verification email for ${userName} but no account is affiliated with this user name.`,
+      )
+    }
+
+    return {
+      status:
+        'If an account with this username is found, an email verification link will be found in your inbox.',
+    }
+  },
 })
 
 module.exports = {
