9ae711a28ed86d806eab3abcc168bf4f5a42f045
nslandolt <nsdeschenes@outlook.com>
2020-08-06 08:19:27 -0300

    add new document collections and edge collections

--- api-js/migrations.js
+++ api-js/migrations.js
@@ -2,15 +2,35 @@ const makeMigrations = ({ databaseName, rootPass }) => [
   {
     type: 'database',
     databaseName,
-    users: [{ username: 'root', passwd: rootPass }]
+    users: [{ username: 'root', passwd: rootPass }],
   },
   {
     type: 'documentcollection',
     databaseName,
-    name: 'users'
-  }
+    name: 'users',
+  },
+  {
+    type: 'documentcollection',
+    databaseName,
+    name: 'organizations',
+  },
+  {
+    type: 'documentcollection',
+    databaseName,
+    name: 'domains',
+  },
+  {
+    type: 'edgecollection',
+    databaseName,
+    name: 'affiliations',
+  },
+  {
+    type: 'edgecollection',
+    databaseName,
+    name: 'ownership',
+  },
 ]
 
 module.exports = {
-  makeMigrations
+  makeMigrations,
 }
