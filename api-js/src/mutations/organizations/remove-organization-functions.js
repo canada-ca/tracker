15f37ddb04a464604ae045f06feb69da593046d2
nslandolt <nsdeschenes@outlook.com>
2020-08-11 14:05:31 -0300

    start work on removeOrganization mutation

---
+++ api-js/src/mutations/organizations/remove-organization-functions.js
@@ -0,0 +1,97 @@
+const removeDKIM = String(function (params) {
+  const { query } = require('@arango')
+  return query`
+    LET domainEdges = (FOR v, e IN 1..1 ANY ${params.organizationId} claims RETURN { edgeKey: e._key, domainId: e._to })
+    FOR domainEdge in domainEdges
+      LET dkimEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsDKIM RETURN { edgeKey: e._key, dkimId: e._to })
+      LET removeDkimEdges = (FOR dkimEdge IN dkimEdges REMOVE dkimEdge.edgeKey IN domainsDKIM)
+      LET removeDkim = (FOR dkimEdge IN dkimEdges LET key = PARSE_IDENTIFIER(dkimEdge.dkimId).key REMOVE key IN dkim)
+    RETURN true
+  `
+})
+
+const removeDMARC = String(function (params) {
+  const { query } = require('@arango')
+  return query`
+  LET domainEdges = (FOR v, e IN 1..1 ANY ${params.organizationId} claims RETURN { edgeKey: e._key, domainId: e._to })
+  FOR domainEdge in domainEdges
+    LET dmarcEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsDMARC RETURN { edgeKey: e._key, dmarcId: e._to })
+    LET removeDmarcEdges = (FOR dmarcEdge IN dmarcEdges REMOVE dmarcEdge.edgeKey IN domainsDMARC)
+    LET removeDmarc = (FOR dmarcEdge IN dmarcEdges LET key = PARSE_IDENTIFIER(dmarcEdge.dmarcId).key REMOVE key IN dmarc)
+  RETURN true
+  `
+})
+
+const removeSPF = String(function (params) {
+  const { query } = require('@arango')
+  return query`
+  LET domainEdges = (FOR v, e IN 1..1 ANY ${params.organizationId} claims RETURN { edgeKey: e._key, domainId: e._to })
+  FOR domainEdge in domainEdges
+    LET spfEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsSPF RETURN { edgeKey: e._key, spfId: e._to })
+    LET removeSpfEdges = (FOR spfEdge IN spfEdges REMOVE spfEdge.edgeKey IN domainsSPF)
+    LET removeSpf = (FOR spfEdge IN spfEdges LET key = PARSE_IDENTIFIER(spfEdge.spfId).key REMOVE key IN spf)
+  RETURN true
+  `
+})
+
+const removeHTTPS = String(function (params) {
+  const { query } = require('@arango')
+  return query`
+  LET domainEdges = (FOR v, e IN 1..1 ANY ${params.organizationId} claims RETURN { edgeKey: e._key, domainId: e._to })
+  FOR domainEdge in domainEdges
+    LET httpsEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsHTTPS RETURN { edgeKey: e._key, httpsId: e._to })
+    LET removeHttpsEdges = (FOR httpsEdge IN httpsEdges REMOVE httpsEdge.edgeKey IN domainsHTTPS)
+    LET removeHttps = (FOR httpsEdge IN httpsEdges LET key = PARSE_IDENTIFIER(httpsEdge.dmarcId).key REMOVE key IN https)
+  RETURN true
+  `
+})
+
+const removeSSL = String(function (params) {
+  const { query } = require('@arango')
+  return query`
+  LET domainEdges = (FOR v, e IN 1..1 ANY ${params.organizationId} claims RETURN { edgeKey: e._key, domainId: e._to })
+  FOR domainEdge in domainEdges
+    LET sslEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsSSL RETURN { edgeKey: e._key, sslId: e._to})
+    LET removeSslEdges = (FOR sslEdge IN sslEdges REMOVE sslEdge.edgeKey IN domainsSSL)
+    LET removeSsl = (FOR sslEdge IN sslEdges LET key = PARSE_IDENTIFIER(sslEdge.dmarcId).key REMOVE key IN ssl)
+  RETURN true
+  `
+})
+
+const removeDomains = String(function (params) {
+  const { query } = require('@arango')
+  return query`
+  LET domainEdges = (FOR v, e IN 1..1 ANY ${params.organizationId} claims RETURN { edgeKey: e._key, domainId: e._to })
+  LET removeDomainEdges = (FOR domainEdge in domainEdges REMOVE domainEdge.domainId IN claims)
+  LET removeDomain = (FOR domainEdge in domainEdges LET key = PARSE_IDENTIFIER(domainEdge.domainId).key REMOVE key IN domains)
+  RETURN true
+  `
+})
+
+const removeUserAffiliations = String(function (params) {
+  const { query } = require('@arango')
+  return query`
+  LET userEdges = (FOR v, e IN 1..1 ANY ${params.organizationId} affiliations RETURN { edgeKey: e._key, userId: e._to })
+  LET removeUserEdges = (FOR userEdge IN userEdges REMOVE userEdge._key IN affiliations)
+  RETURN true
+  `
+})
+
+const removeOrganization = String(function (params) {
+  const { query } = require('@arango')
+  return query`
+  LET organizationKey = SPLIT(${params.organizationId}, '/')[1]
+  REMOVE organizationKey IN organizations
+  `
+})
+
+module.exports = {
+  removeDKIM,
+  removeDMARC,
+  removeSPF,
+  removeHTTPS,
+  removeSSL,
+  removeDomains,
+  removeUserAffiliations,
+  removeOrganization,
+}
--- api-js/src/mutations/organizations/remove-organization.js
+++ api-js/src/mutations/organizations/remove-organization.js
@@ -1,5 +1,5 @@
 const { GraphQLNonNull, GraphQLID, GraphQLString } = require('graphql')
-const { mutationWithClientMutationId } = require('graphql-relay')
+const { mutationWithClientMutationId, fromGlobalId } = require('graphql-relay')
 
 const removeOrganization = new mutationWithClientMutationId({
   name: 'RemoveOrganization',
@@ -15,10 +15,54 @@ const removeOrganization = new mutationWithClientMutationId({
       type: GraphQLNonNull(GraphQLString),
       description:
         'Status string to inform the user if the organization was successfully removed.',
-      resolve: async () => {},
+      resolve: async (payload) => {
+        return payload.status
+      },
     },
   }),
-  mutateAndGetPayload: async () => {},
+  mutateAndGetPayload: async (
+    args,
+    {
+      query,
+      userId,
+      auth: { checkPermission, userRequired },
+      validators: { cleanseInput },
+      loaders: { orgLoaderById, userLoaderById },
+    },
+  ) => {
+    // Cleanse Input
+    const { type: _orgType, id: orgId } = fromGlobalId(cleanseInput(args.id))
+
+    // Get user
+    const user = await userRequired(userId, userLoaderById)
+
+    // Get org from db
+    const organization = await orgLoaderById.load(orgId)
+
+    // Check to see if org exists
+    if (typeof organization === 'undefined') {
+      console.error(`User: ${userId} attempted to remove org: ${orgId}, but there is no org associated with that id.`)
+      throw new Error('Unable to remove organization. Please try again.')
+    }
+
+    // Get users permission
+    const permission = await checkPermission(user._id, organization._id, query)
+
+    // Check to see if org is blue check, and the user is super admin
+    if (organization.blueCheck && permission !== 'super_admin') {
+      console.warn(`User: ${userId} attempted to remove ${organization._key}, however the user is not a super admin.`)
+      throw new Error('Unable to remove organization. Please try again.')
+    }
+
+    if(permission !== 'super_admin' && permission !== 'admin') {
+      console.warn(`User: ${userId} attempted to remove ${organization._key}, however the user does not have permission to this organization.`)
+      throw new Error('Unable to remove organization. Please try again.')
+    }
+
+    return {
+      status: `Successfully remove ${organization.slug}.`,
+    }
+  },
 })
 
 module.exports = {
