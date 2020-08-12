dbc0645e6cda60eaa2c47c83bbf0c64f860da077
nslandolt <nsdeschenes@outlook.com>
2020-08-12 11:25:46 -0300

    create custom domain scalar

---
+++ api-js/src/scalars/domain.js
@@ -0,0 +1,30 @@
+const { Kind, GraphQLError, GraphQLScalarType } = require('graphql')
+
+const validate = (value) => {
+  const SLUG_REGEX = /\b((xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b/
+  if (typeof value !== typeof 'string') {
+    throw new TypeError(`Value is not a string: ${typeof value}`)
+  }
+  if (!SLUG_REGEX.test(value)) {
+    throw new TypeError(`Value is not a valid domain: ${value}`)
+  }
+
+  return value
+}
+
+module.exports.Domain = new GraphQLScalarType({
+  name: 'Domain',
+  description:
+    '',
+  serialize: validate,
+  parseValue: validate,
+
+  parseLiteral(ast) {
+    if (ast.kind !== Kind.STRING) {
+      throw new GraphQLError(
+        `Can only validate strings as domains but got a: ${ast.kind}`,
+      )
+    }
+    return validate(ast.value)
+  },
+})
--- api-js/src/scalars/index.js
+++ api-js/src/scalars/index.js
@@ -1,10 +1,12 @@
 const { Acronym } = require('./organization-acronym.js')
+const { Domain } = require('./domain')
 const { Selectors } = require('./selector.js')
 const { Slug } = require('./slug.js')
 const { Year } = require('./year.js')
 
 module.exports = {
   Acronym,
+  Domain,
   Selectors,
   Slug,
   Year,
