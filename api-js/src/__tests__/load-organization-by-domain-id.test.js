d3480dad8fd6623ade90cf9b15bf8bebb69f91e6
nslandolt <nsdeschenes@outlook.com>
2020-08-12 08:43:04 -0300

    Completed first edge based dataloader

--- api-js/src/__tests__/load-organization-by-domain-id.test.js
+++ api-js/src/__tests__/load-organization-by-domain-id.test.js
@@ -13,38 +13,14 @@ describe('given a orgLoaderById dataloader', () => {
   const mockedError = (output) => consoleOutput.push(output)
   beforeAll(async () => {
     console.error = mockedError
+  })
+
+  beforeEach(async () => {
     ;({ migrate } = await ArangoTools({ rootPass, url }))
     ;({ query, drop, truncate, collections } = await migrate(
       makeMigrations({ databaseName: dbNameFromFile(__filename), rootPass }),
     ))
-  })
-
-  beforeEach(async () => {
     await truncate()
-    await collections.organizations.save({
-      orgDetails: {
-        en: {
-          slug: 'communications-security-establishment',
-          acronym: 'CSE',
-          name: 'Communications Security Establishment',
-          zone: 'FED',
-          sector: 'DND',
-          country: 'Canada',
-          province: 'Ontario',
-          city: 'Ottawa',
-        },
-        fr: {
-          slug: 'centre-de-la-securite-des-telecommunications',
-          acronym: 'CST',
-          name: 'Centre de la Securite des Telecommunications',
-          zone: 'FED',
-          sector: 'DND',
-          country: 'Canada',
-          province: 'Ontario',
-          city: 'Ottawa',
-        },
-      },
-    })
     await collections.organizations.save({
       orgDetails: {
         en: {
@@ -69,45 +45,334 @@ describe('given a orgLoaderById dataloader', () => {
         },
       },
     })
-    collections.domains.save({
-      url: 'test.gc.ca',
+    await collections.organizations.save({
+      orgDetails: {
+        en: {
+          slug: 'communications-security-establishment',
+          acronym: 'CSE',
+          name: 'Communications Security Establishment',
+          zone: 'FED',
+          sector: 'DND',
+          country: 'Canada',
+          province: 'Ontario',
+          city: 'Ottawa',
+        },
+        fr: {
+          slug: 'centre-de-la-securite-des-telecommunications',
+          acronym: 'CST',
+          name: 'Centre de la Securite des Telecommunications',
+          zone: 'FED',
+          sector: 'DND',
+          country: 'Canada',
+          province: 'Ontario',
+          city: 'Ottawa',
+        },
+      },
     })
     const orgCursor = await query`
       FOR org IN organizations
         FILTER (LOWER("treasury-board-secretariat") == LOWER(TRANSLATE("en", org.orgDetails).slug))
         RETURN MERGE({ _id: org._id, _key: org._key, _rev: org._rev }, TRANSLATE("en", org.orgDetails))
     `
-    const domainCursor = await query`
-      FOR domain IN domains
-        FILTER domain.url == "test.gc.ca"
-        RETURN domain
-    `
-
     org = await orgCursor.next()
-    domain = await domainCursor.next()
-
-    collections.claims.save({
-      _to: domain._id,
-      _from: org._id,
-    })
-    
     consoleOutput = []
   })
 
-  afterAll(async () => {
+  afterEach(async () => {
     await drop()
   })
 
-  describe('test', () => {
-    it('', async () => {
-      const cursor = await query`
+  describe('provided a singled domain', () => {
+    beforeEach(async () => {
+      await collections.domains.save({
+        url: 'test.gc.ca',
+      })
+      const domainCursor = await query`
         FOR domain IN domains
           FILTER domain.url == "test.gc.ca"
           RETURN domain
       `
-      const domain = await cursor.next()
-      const loader = orgLoaderByDomainId(query, 'en')
-      await loader.load(domain._id)
+      domain = await domainCursor.next()
+
+      await collections.claims.save({
+        _to: domain._id,
+        _from: org._id,
+      })
+    })
+    describe('language is set to english', () => {
+      it('returns a single org', async () => {
+        const expectedDomainCursor = await query`
+          FOR domain IN domains
+            FILTER domain.url == 'test.gc.ca'
+            RETURN domain
+        `
+        const expectedDomain = await expectedDomainCursor.next()
+
+        const expectedOrgCursor = await query`
+          FOR org IN organizations
+          FILTER org.orgDetails.en.slug == "treasury-board-secretariat"
+          RETURN MERGE({ _id: org._id, _key: org._key, _rev: org._rev }, TRANSLATE("en", org.orgDetails))
+        `
+        const expectedOrg = await expectedOrgCursor.next()
+
+        const loader = orgLoaderByDomainId(query, 'en')
+        const org = await loader.load(expectedDomain._id)
+        expect(org).toEqual(expectedOrg)
+      })
+    })
+    describe('language is set to french', () => {
+      it('returns a single org', async () => {
+        const expectedDomainCursor = await query`
+          FOR domain IN domains
+            FILTER domain.url == 'test.gc.ca'
+            RETURN domain
+        `
+        const expectedDomain = await expectedDomainCursor.next()
+
+        const expectedOrgCursor = await query`
+          FOR org IN organizations
+          FILTER org.orgDetails.en.slug == "treasury-board-secretariat"
+          RETURN MERGE({ _id: org._id, _key: org._key, _rev: org._rev }, TRANSLATE("fr", org.orgDetails))
+        `
+        const expectedOrg = await expectedOrgCursor.next()
+
+        const loader = orgLoaderByDomainId(query, 'fr')
+        const org = await loader.load(expectedDomain._id)
+        expect(org).toEqual(expectedOrg)
+      })
+    })
+  })
+  describe('provided a list of domains', () => {
+    describe('domains belong to one org', () => {
+      beforeEach(async () => {
+        await collections.domains.save({
+          url: 'test.gc.ca',
+        })
+        let domainCursor = await query`
+          FOR domain IN domains
+            FILTER domain.url == 'test.gc.ca'
+            RETURN domain
+        `
+        domain = await domainCursor.next()
+        await collections.claims.save({
+          _to: domain._id,
+          _from: org._id,
+        })
+
+        await collections.domains.save({
+          url: 'test.canada.ca',
+        })
+        domainCursor = await query`
+          FOR domain IN domains
+            FILTER domain.url == 'test.canada.ca'
+            RETURN domain
+        `
+        domain = await domainCursor.next()
+        await collections.claims.save({
+          _to: domain._id,
+          _from: org._id,
+        })
+      })
+      describe('language is set to english', () => {
+        it('returns a list of orgs', async () => {
+          const domainIds = []
+          const expectedDomains = []
+          const expectedDomainCursor = await query`
+            FOR domain IN domains
+              RETURN domain
+          `
+          while (expectedDomainCursor.hasNext()) {
+            const tempDomain = await expectedDomainCursor.next()
+            domainIds.push(tempDomain._id)
+            expectedDomains.push(tempDomain)
+          }
+
+          const expectedOrgCursor = await query`
+            FOR org IN organizations
+            FILTER org.orgDetails.en.slug == "treasury-board-secretariat"
+            RETURN MERGE({ _id: org._id, _key: org._key, _rev: org._rev }, TRANSLATE("en", org.orgDetails))
+          `
+          const expectedOrg = await expectedOrgCursor.next()
+          const expectedOrgs = []
+          for (let i = 0; i < 2; i++) {
+            expectedOrgs.push(expectedOrg)
+          }
+
+          const loader = orgLoaderByDomainId(query, 'en')
+          const orgs = await loader.loadMany(domainIds)
+          expect(orgs).toEqual(expectedOrgs)
+        })
+      })
+      describe('language is set to french', () => {
+        it('returns a list of orgs', async () => {
+          const domainIds = []
+          const expectedDomains = []
+          const expectedDomainCursor = await query`
+            FOR domain IN domains
+              RETURN domain
+          `
+          while (expectedDomainCursor.hasNext()) {
+            const tempDomain = await expectedDomainCursor.next()
+            domainIds.push(tempDomain._id)
+            expectedDomains.push(tempDomain)
+          }
+
+          const expectedOrgCursor = await query`
+            FOR org IN organizations
+            FILTER org.orgDetails.en.slug == "treasury-board-secretariat"
+            RETURN MERGE({ _id: org._id, _key: org._key, _rev: org._rev }, TRANSLATE("fr", org.orgDetails))
+          `
+          const expectedOrg = await expectedOrgCursor.next()
+          const expectedOrgs = []
+          for (let i = 0; i < 2; i++) {
+            expectedOrgs.push(expectedOrg)
+          }
+
+          const loader = orgLoaderByDomainId(query, 'fr')
+          const orgs = await loader.loadMany(domainIds)
+          expect(orgs).toEqual(expectedOrgs)
+        })
+      })
+    })
+    describe('domains belong to different orgs', () => {
+      beforeEach(async () => {
+        await collections.domains.save({
+          url: 'test.gc.ca',
+        })
+        let domainCursor = await query`
+          FOR domain IN domains
+            FILTER domain.url == 'test.gc.ca'
+            RETURN domain
+        `
+        domain = await domainCursor.next()
+        await collections.claims.save({
+          _to: domain._id,
+          _from: org._id,
+        })
+
+        const orgCursor = await query`
+          FOR org IN organizations
+            FILTER (LOWER("centre-de-la-securite-des-telecommunications") == LOWER(TRANSLATE("fr", org.orgDetails).slug))
+            RETURN MERGE({ _id: org._id, _key: org._key, _rev: org._rev }, TRANSLATE("en", org.orgDetails))
+        `
+        org = await orgCursor.next()
+
+        await collections.domains.save({
+          url: 'test.canada.ca',
+        })
+        domainCursor = await query`
+          FOR domain IN domains
+            FILTER domain.url == 'test.canada.ca'
+            RETURN domain
+        `
+        domain = await domainCursor.next()
+        await collections.claims.save({
+          _to: domain._id,
+          _from: org._id,
+        })
+      })
+      describe('language is set to english', () => {
+        it('returns a list of orgs', async () => {
+          const domainIds = []
+          const expectedDomains = []
+          const expectedDomainCursor = await query`
+            FOR domain IN domains
+              RETURN domain
+          `
+          while (expectedDomainCursor.hasNext()) {
+            const tempDomain = await expectedDomainCursor.next()
+            domainIds.push(tempDomain._id)
+            expectedDomains.push(tempDomain)
+          }
+
+          const orgIds = []
+          const expectedOrgs = []
+          const expectedOrgCursor = await query`
+            FOR org IN organizations
+              RETURN MERGE({ _id: org._id, _key: org._key, _rev: org._rev }, TRANSLATE("en", org.orgDetails))
+          `
+          while (expectedOrgCursor.hasNext()) {
+            const tempOrg = await expectedOrgCursor.next()
+            orgIds.push(tempOrg._key)
+            expectedOrgs.push(tempOrg)
+          }
+
+          const loader = orgLoaderByDomainId(query, 'en')
+          const orgs = await loader.loadMany(domainIds)
+          expect(orgs).toEqual(expectedOrgs)
+        })
+      })
+      describe('language is set to french', () => {
+        it('returns a list of orgs', async () => {
+          const domainIds = []
+          const expectedDomains = []
+          const expectedDomainCursor = await query`
+            FOR domain IN domains
+              RETURN domain
+          `
+          while (expectedDomainCursor.hasNext()) {
+            const tempDomain = await expectedDomainCursor.next()
+            domainIds.push(tempDomain._id)
+            expectedDomains.push(tempDomain)
+          }
+
+          const orgIds = []
+          const expectedOrgs = []
+          const expectedOrgCursor = await query`
+            FOR org IN organizations
+              RETURN MERGE({ _id: org._id, _key: org._key, _rev: org._rev }, TRANSLATE("fr", org.orgDetails))
+          `
+          while (expectedOrgCursor.hasNext()) {
+            const tempOrg = await expectedOrgCursor.next()
+            orgIds.push(tempOrg._key)
+            expectedOrgs.push(tempOrg)
+          }
+
+          const loader = orgLoaderByDomainId(query, 'fr')
+          const orgs = await loader.loadMany(domainIds)
+          expect(orgs).toEqual(expectedOrgs)
+        })
+      })
+    })
+  })
+  describe('database error is raised', () => {
+    it('returns an error', async () => {
+      query = jest.fn().mockRejectedValue(new Error('Database error occurred.'))
+      const loader = orgLoaderByDomainId(query)
+
+      try {
+        await loader.load('1')
+      } catch (err) {
+        expect(err).toEqual(
+          new Error('Unable to find organization. Please try again.'),
+        )
+      }
+
+      expect(consoleOutput).toEqual([
+        `Database error occurred while running orgLoaderByDomainId: Error: Database error occurred.`,
+      ])
+    })
+  })
+  describe('cursor error is raised', () => {
+    it('returns an error', async () => {
+      const cursor = {
+        each() {
+          throw new Error('Cursor error occurred.')
+        },
+      }
+      query = jest.fn().mockReturnValue(cursor)
+      const loader = orgLoaderByDomainId(query)
+      try {
+        await loader.load('1')
+      } catch (err) {
+        expect(err).toEqual(
+          new Error('Unable to find organization. Please try again.'),
+        )
+      }
+
+      expect(consoleOutput).toEqual([
+        `Cursor error occurred during orgLoaderByDomainId: Error: Cursor error occurred.`,
+      ])
     })
   })
 })
--- api-js/src/loaders/organizations/load-organization-by-domain-id.js
+++ api-js/src/loaders/organizations/load-organization-by-domain-id.js
@@ -3,18 +3,33 @@ const DataLoader = require('dataloader')
 module.exports.orgLoaderByDomainId = (query, language) => 
   new DataLoader(async (ids) => {
     let cursor
-
+    
     try {
       cursor = await query`
       FOR id IN ${ids}
-        LET orgId = (FOR v, e IN 1..1 ANY id claims RETURN e._from )
-        FOR org IN organizations
-          FILTER orgId
-          RETURN MERGE({ _id: org._id, _key: org._key, _rev: org._rev }, TRANSLATE(${language}, org.orgDetails))
+        LET orgIds = (FOR v, e IN 1..1 ANY id claims RETURN { _from: e._from, _to: e._to })
+        FOR orgId IN orgIds
+          LET org = DOCUMENT(orgId._from)
+          RETURN MERGE({ _id: org._id, _key: org._key, _rev: org._rev, _to: orgId._to }, TRANSLATE(${language}, org.orgDetails))
       `
     } catch (err) {
-      console.log(err)
+      console.error(`Database error occurred while running orgLoaderByDomainId: ${err}`)
+      throw new Error('Unable to find organization. Please try again.')
     }
-    const key = await cursor.next()
-    console.log(key)
+
+    const orgMap = {}
+    try {
+      await cursor.each((org) => {
+        orgMap[org._to] = org
+      })
+    } catch (err) {
+      console.error(`Cursor error occurred during orgLoaderByDomainId: ${err}`)
+      throw new Error('Unable to find organization. Please try again.')
+    }
+
+    return ids.map((id) =>  {
+      const org = orgMap[id]
+      delete org._to
+      return org
+    })
 })
