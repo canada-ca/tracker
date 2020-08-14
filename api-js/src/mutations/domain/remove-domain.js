7f9bece31f98968ee34c6c4572f29ba8250455be
nslandolt <nsdeschenes@outlook.com>
2020-08-14 10:12:21 -0300

    started work on removing domains

--- api-js/src/mutations/domain/remove-domain.js
+++ api-js/src/mutations/domain/remove-domain.js
@@ -1,11 +1,11 @@
 const { GraphQLNonNull, GraphQLID, GraphQLString } = require('graphql')
-const { mutationWithClientMutationId } = require('graphql-relay')
+const { mutationWithClientMutationId, fromGlobalId } = require('graphql-relay')
 
 const removeDomain = new mutationWithClientMutationId({
   name: 'RemoveDomain',
   description: 'This mutation allows the removal of unused domains.',
   inputFields: () => ({
-    id: {
+    domainId: {
       type: GraphQLNonNull(GraphQLID),
       description: 'The global id of the domain you wish to remove.',
     },
@@ -15,10 +15,83 @@ const removeDomain = new mutationWithClientMutationId({
       type: GraphQLNonNull(GraphQLString),
       description:
         'Status string to inform the user if the domain was successfully removed.',
-      resolve: async () => {},
+      resolve: async (payload) => {
+        return payload.status
+      },
     },
   }),
-  mutateAndGetPayload: async () => {},
+  mutateAndGetPayload: async (
+    args,
+    {
+      query,
+      collections,
+      transaction,
+      userId,
+      auth: { checkPermission, userRequired },
+      validators: { cleanseInput },
+      loaders: {
+        domainLoaderById,
+        orgLoaderByDomainId,
+        userLoaderById,
+      },
+    },
+  ) => {
+    // Cleanse Input
+    const { type: _domainType, id: domainId } = fromGlobalId(
+      cleanseInput(args.domainId),
+    )
+
+    // Get User
+    const user = await userRequired(userId, userLoaderById)
+
+    // Get domain from db
+    const domain = await domainLoaderById.load(domainId)
+
+    // Check to see if domain exists
+    if (typeof domain === 'undefined') {
+      console.warn(``)
+      throw new Error('Unable to remove domain. Please try again.')
+    }
+
+    // Get Org from db
+    const org = await orgLoaderByDomainId.load(domain._id)
+
+    // Check to see if org exists
+    if (typeof org === 'undefined') {
+      console.warn(``)
+      throw new Error('Unable to remove domain. Please try again.')
+    }
+
+    // Get permission
+    const permission = await checkPermission(user._id, org._id, query)
+
+    // Check to see if domain belongs to blue check org
+    if (org.blueCheck && permission !== 'super_admin') {
+      console.warn(``)
+      throw new Error('Unable to remove domain. Please try again.')
+    }
+
+    if (permission !== 'super_admin' && permission !== 'admin') {
+      console.warn(``)
+      throw new Error('Unable to remove domain. Please try again.')
+    }
+
+    // Generate list of collections names
+    const collectionStrings = []
+    for (const property in collections) {
+      collectionStrings.push(property.toString())
+    }
+
+    // Setup Trans action
+    const trx = await transaction(collectionStrings)
+
+    // Remove scan data
+    try {
+      await 
+    } catch (err) {
+
+    }
+  },
 })
 
 module.exports = {
