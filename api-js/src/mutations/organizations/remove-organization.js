ce67de29a879ac51be36c77cac1cbd5dd91d231e
nslandolt <nsdeschenes@outlook.com>
2020-08-14 07:51:42 -0300

    removeOrganization mutation completed with 100 coverage testing

--- api-js/src/mutations/organizations/remove-organization.js
+++ api-js/src/mutations/organizations/remove-organization.js
@@ -5,14 +5,14 @@ const removeOrganization = new mutationWithClientMutationId({
   name: 'RemoveOrganization',
   description: 'This mutation allows the removal of unused organizations.',
   inputFields: () => ({
-    id: {
+    orgId: {
       type: GraphQLNonNull(GraphQLID),
       description: 'The global id of the organization you wish you remove.',
     },
   }),
   outputFields: () => ({
     status: {
-      type: GraphQLNonNull(GraphQLString),
+      type: GraphQLString,
       description:
         'Status string to inform the user if the organization was successfully removed.',
       resolve: async (payload) => {
@@ -24,6 +24,8 @@ const removeOrganization = new mutationWithClientMutationId({
     args,
     {
       query,
+      collections,
+      transaction,
       userId,
       auth: { checkPermission, userRequired },
       validators: { cleanseInput },
@@ -31,7 +33,8 @@ const removeOrganization = new mutationWithClientMutationId({
     },
   ) => {
     // Cleanse Input
-    const { type: _orgType, id: orgId } = fromGlobalId(cleanseInput(args.id))
+    const temp = cleanseInput(args.orgId)
+    const { type: _orgType, id: orgId } = fromGlobalId(temp)
 
     // Get user
     const user = await userRequired(userId, userLoaderById)
@@ -41,7 +44,7 @@ const removeOrganization = new mutationWithClientMutationId({
 
     // Check to see if org exists
     if (typeof organization === 'undefined') {
-      console.error(
+      console.warn(
         `User: ${userId} attempted to remove org: ${orgId}, but there is no org associated with that id.`,
       )
       throw new Error('Unable to remove organization. Please try again.')
@@ -65,8 +68,120 @@ const removeOrganization = new mutationWithClientMutationId({
       throw new Error('Unable to remove organization. Please try again.')
     }
 
+    // Generate list of collections names
+    const collectionStrings = []
+    for (const property in collections) {
+      collectionStrings.push(property.toString())
+    }
+
+    // Setup Trans action
+    const trx = await transaction(collectionStrings)
+
+    try {
+      await Promise.all([
+        trx.run(async () => {
+          await query`
+            LET domainEdges = (FOR v, e IN 1..1 ANY ${organization._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+            FOR domainEdge in domainEdges
+              LET dkimEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsDKIM RETURN { edgeKey: e._key, dkimId: e._to })
+              LET removeDkimEdges = (FOR dkimEdge IN dkimEdges REMOVE dkimEdge.edgeKey IN domainsDKIM)
+              LET removeDkim = (FOR dkimEdge IN dkimEdges LET key = PARSE_IDENTIFIER(dkimEdge.dkimId).key REMOVE key IN dkim)
+            RETURN true
+          `
+        }),
+        trx.run(async () => {
+          await query`
+            LET domainEdges = (FOR v, e IN 1..1 ANY ${organization._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+            FOR domainEdge in domainEdges
+              LET dmarcEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsDMARC RETURN { edgeKey: e._key, dmarcId: e._to })
+              LET removeDmarcEdges = (FOR dmarcEdge IN dmarcEdges REMOVE dmarcEdge.edgeKey IN domainsDMARC)
+              LET removeDmarc = (FOR dmarcEdge IN dmarcEdges LET key = PARSE_IDENTIFIER(dmarcEdge.dmarcId).key REMOVE key IN dmarc)
+            RETURN true
+          `
+        }),
+        trx.run(async () => {
+          await query`
+            LET domainEdges = (FOR v, e IN 1..1 ANY ${organization._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+            FOR domainEdge in domainEdges
+              LET spfEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsSPF RETURN { edgeKey: e._key, spfId: e._to })
+              LET removeSpfEdges = (FOR spfEdge IN spfEdges REMOVE spfEdge.edgeKey IN domainsSPF)
+              LET removeSpf = (FOR spfEdge IN spfEdges LET key = PARSE_IDENTIFIER(spfEdge.spfId).key REMOVE key IN spf)
+            RETURN true
+          `
+        }),
+        trx.run(async () => {
+          await query`
+            LET domainEdges = (FOR v, e IN 1..1 ANY ${organization._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+            FOR domainEdge in domainEdges
+              LET httpsEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsHTTPS RETURN { edgeKey: e._key, httpsId: e._to })
+              LET removeHttpsEdges = (FOR httpsEdge IN httpsEdges REMOVE httpsEdge.edgeKey IN domainsHTTPS)
+              LET removeHttps = (FOR httpsEdge IN httpsEdges LET key = PARSE_IDENTIFIER(httpsEdge.httpsId).key REMOVE key IN https)
+            RETURN true
+          `
+        }),
+        trx.run(async () => {
+          await query`
+            LET domainEdges = (FOR v, e IN 1..1 ANY ${organization._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+            FOR domainEdge in domainEdges
+              LET sslEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsSSL RETURN { edgeKey: e._key, sslId: e._to})
+              LET removeSslEdges = (FOR sslEdge IN sslEdges REMOVE sslEdge.edgeKey IN domainsSSL)
+              LET removeSsl = (FOR sslEdge IN sslEdges LET key = PARSE_IDENTIFIER(sslEdge.sslId).key REMOVE key IN ssl)
+            RETURN true
+          `
+        }),
+      ])
+    } catch (err) {
+      console.error(
+        `Transaction error occurred while attempting to remove scan results for org: ${organization._key}, error: ${err}`,
+      )
+      throw new Error('Unable to remove organization. Please try again.')
+    }
+
+    try {
+      await Promise.all([
+        trx.run(async () => {
+          await query`
+            LET domainEdges = (FOR v, e IN 1..1 ANY ${organization._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+            LET removeDomainEdges = (FOR domainEdge in domainEdges REMOVE domainEdge.edgeKey IN claims)
+            LET removeDomain = (FOR domainEdge in domainEdges LET key = PARSE_IDENTIFIER(domainEdge.domainId).key REMOVE key IN domains)
+            RETURN true
+          `
+        }),
+        trx.run(async () => {
+          await query`
+            LET userEdges = (FOR v, e IN 1..1 ANY ${organization._id} affiliations RETURN { edgeKey: e._key, userId: e._to })
+            LET removeUserEdges = (FOR userEdge IN userEdges REMOVE userEdge.edgeKey IN affiliations)
+            RETURN true
+          `
+        }),
+        trx.run(async () => {
+          await query`
+            REMOVE ${organization._key} IN organizations
+          `
+        }),
+      ])
+    } catch (err) {
+      console.error(
+        `Transaction error occurred while attempting to remove domain, affiliations, and the org for org: ${organization._key}, error: ${err}`,
+      )
+      throw new Error('Unable to remove organization. Please try again.')
+    }
+
+    try {
+      await trx.commit()
+    } catch (err) {
+      console.error(
+        `Transaction error occurred while attempting to commit removal of org: ${organization._key}, error: ${err}`,
+      )
+      throw new Error('Unable to remove organization. Please try again.')
+    }
+
+    console.info(
+      `User: ${userId} successfully removed org: ${organization._key}.`,
+    )
+
     return {
-      status: `Successfully remove ${organization.slug}.`,
+      status: `Successfully removed organization: ${organization.slug}.`,
     }
   },
 })
