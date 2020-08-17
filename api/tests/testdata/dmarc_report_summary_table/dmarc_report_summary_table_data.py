7fbfc338af9cd38fbeea8e4c1b5f49eb0535d52c
nslandolt <nsdeschenes@outlook.com>
2020-08-17 08:29:53 -0300

    Tests now completed

---
+++ api/tests/testdata/dmarc_report_summary_table/dmarc_report_summary_table_data.py
@@ -0,0 +1,31 @@
+api_return_data_1 = {
+    "getDmarcSummaryByPeriod": {
+        "period": {
+            "startDate": "2020-05-01",
+            "endDate": "2020-05-31",
+            "categoryTotals": {
+                "passSpfOnly": 168,
+                "passDkimOnly": 168,
+                "fullPass": 8331,
+                "fail": 570,
+                "total": 9237,
+            },
+        }
+    }
+}
+
+api_return_data_2 = {
+    "getDmarcSummaryByPeriod": {
+        "period": {
+            "startDate": "2020-05-01",
+            "endDate": "2020-05-31",
+            "categoryTotals": {
+                "passSpfOnly": 168,
+                "passDkimOnly": 168,
+                "fullPass": 8331,
+                "fail": 570,
+                "total": 9237,
+            },
+        }
+    }
+}
