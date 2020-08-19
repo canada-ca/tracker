5b6408342d442915b6031b4e7fe51bab0a0629df
nslandolt <nsdeschenes@outlook.com>
2020-08-19 11:09:51 -0300

    Just send the whole error to log

--- api/functions/external_graphql_api_request.py
+++ api/functions/external_graphql_api_request.py
@@ -65,23 +65,11 @@ def send_request(
         return data
 
     except Exception as e:
-        # Make sure the below stays like so
-        # error_str = e.__str__().replace("\'", '\"')
-        # Black will try and change it and it will break !!!
-        error_str = e.__str__().replace("\'", '\"')
-        try:
-            error_dict = json.loads(error_str)
-            if error_dict.get("message", None):
-                logger.error(
-                    f"Error occurred on the dmarc-report-api side: {str(error_dict.get('message'))}"
-                )
-                if summary_table:
-                    return {}
-                else:
-                    raise GraphQLError("Error when querying dmarc-report-api.")
+        logger.error(
+            f"Error occurred on the dmarc-report-api side: {str(e)}"
+        )
+        if summary_table:
+            return {}
+        else:
+            raise GraphQLError("Error when querying dmarc-report-api.")
 
-        except ValueError as ve:
-            logger.error(
-                f"Value Error occurred when receiving data from dmarc-report-api: {str(ve)}"
-            )
-            raise GraphQLError("Error, when querying dmarc-report-api.")
