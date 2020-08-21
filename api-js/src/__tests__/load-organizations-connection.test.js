d6be3c1659bf847104472eb8e29a8978a88842b7
nslandolt <nsdeschenes@outlook.com>
2020-08-21 10:14:10 -0300

    initial connection load function in working state

---
+++ api-js/src/__tests__/load-organizations-connection.test.js
@@ -0,0 +1,108 @@
+const dotenv = require('dotenv-safe')
+dotenv.config()
+const { DB_PASS: rootPass, DB_URL: url } = process.env
+
+const { ArangoTools, dbNameFromFile } = require('arango-tools')
+const { makeMigrations } = require('../../migrations')
+const { cleanseInput } = require('../validators')
+
+const { loadOrganizationsConnections } = require('../loaders/organizations/load-organizations-connection')
+
+describe('given the load organizations connection function', () => {
+  let query, drop, truncate, migrate, collections, transaction, user
+
+  let consoleOutput = []
+  // const mockedError = (output) => consoleOutput.push(output)
+  beforeAll(async () => {
+    // console.error = mockedError
+    ;({ migrate } = await ArangoTools({ rootPass, url }))
+    ;({ query, drop, truncate, collections, transaction } = await migrate(
+      makeMigrations({ databaseName: dbNameFromFile(__filename), rootPass }),
+    ))
+  })
+
+  beforeEach(async () => {
+    await truncate()
+    user = await collections.users.save({
+      userName: 'test.account@istio.actually.exists',
+      displayName: 'Test Account',
+      preferredLang: 'french',
+      tfaValidated: false,
+      emailValidated: false,
+    })
+    const orgTwo = await collections.organizations.save({
+      orgDetails: {
+        en: {
+          slug: 'treasury-board-secretariat',
+          acronym: 'TBS',
+          name: 'Treasury Board of Canada Secretariat',
+          zone: 'FED',
+          sector: 'TBS',
+          country: 'Canada',
+          province: 'Ontario',
+          city: 'Ottawa',
+        },
+        fr: {
+          slug: 'secretariat-conseil-tresor',
+          acronym: 'SCT',
+          name: 'Secrétariat du Conseil Trésor du Canada',
+          zone: 'FED',
+          sector: 'TBS',
+          country: 'Canada',
+          province: 'Ontario',
+          city: 'Ottawa',
+        },
+      },
+    })
+    const org = await collections.organizations.save({
+      orgDetails: {
+        en: {
+          slug: 'communications-security-establishment',
+          acronym: 'CSE',
+          name: 'Communications Security Establishment',
+          zone: 'FED',
+          sector: 'DND',
+          country: 'Canada',
+          province: 'Ontario',
+          city: 'Ottawa',
+        },
+        fr: {
+          slug: 'centre-de-la-securite-des-telecommunications',
+          acronym: 'CST',
+          name: 'Centre de la Securite des Telecommunications',
+          zone: 'FED',
+          sector: 'DND',
+          country: 'Canada',
+          province: 'Ontario',
+          city: 'Ottawa',
+        },
+      },
+    })
+    await collections.affiliations.save({
+      _from: org._id,
+      _to: user._id,
+      permission: 'user',
+    })
+    await collections.affiliations.save({
+      _from: orgTwo._id,
+      _to: user._id,
+      permission: 'user',
+    })
+    consoleOutput = []
+  })
+
+  afterAll(async () => {
+    await drop()
+  })
+
+  describe('', () => {
+    it('', async () => {
+      const connectionArgs = {
+        last: 1,
+      }
+      const organizations = await loadOrganizationsConnections(connectionArgs, query, user._key, cleanseInput)
+
+      expect(organizations).toEqual('')
+    })
+  })
+})
---
+++ api-js/src/loaders/organizations/load-organizations-connection.js
@@ -0,0 +1,120 @@
+const { fromGlobalId, toGlobalId } = require('graphql-relay')
+const { aql } = require('arangojs')
+
+const loadOrganizationsConnections = async (
+  { after, before, first, last },
+  query,
+  userId,
+  cleanseInput,
+) => {
+  let afterTemplate = aql``
+  let beforeTemplate = aql``
+
+  const userDBId = `users/${userId}`
+
+  if (typeof after !== 'undefined') {
+    const { id: afterId } = fromGlobalId(cleanseInput(after))
+    afterTemplate = aql`FILTER TO_NUMBER(org._key) > ${afterId}`
+  }
+
+  if (typeof before !== 'undefined') {
+    const { id: beforeId } = fromGlobalId(cleanseInput(before))
+    beforeTemplate = aql`FILTER TO_NUMBER(org._key) < ${beforeId}`
+  }
+
+  let limitTemplate = aql``
+  if (typeof first !== 'undefined' && typeof last === 'undefined') {
+    limitTemplate = aql`SORT org._key ASC LIMIT ${first + 1}`
+  } else if (typeof first === 'undefined' && typeof last !== 'undefined') {
+    limitTemplate = aql`SORT org._key DESC LIMIT ${last + 1}`
+  } else if (typeof first !== 'undefined' && typeof last !== 'undefined') {
+    console.warn(
+      `User: ${userId} tried to have first and last set in organizations connection query`,
+    )
+    throw new Error(
+      'Error, unable to have first, and last set at the same time.',
+    )
+  }
+
+  let acceptedOrgsCursor
+  try {
+    acceptedOrgsCursor = await query`
+    LET orgKeys = (FOR v, e IN 1..1 INBOUND ${userDBId} affiliations RETURN v._key )
+      RETURN orgKeys
+    `
+  } catch (err) {
+    console.error(`Database error occurred while user: ${userId} was trying to gather org information in loadOrganizationsConnections.`)
+    throw new Error('Unable to load organizations. Please try again.')
+  }
+
+  const acceptedOrgs = await acceptedOrgsCursor.next()
+
+  let orgCursor
+  try {
+    orgCursor = await query`
+    FOR org IN organizations
+      FILTER org._key IN ${acceptedOrgs}
+      ${afterTemplate} 
+      ${beforeTemplate} 
+      ${limitTemplate}
+      RETURN org
+    `
+  } catch (err) {
+    console.error(err)
+    throw new Error('Unable to retrieve organizations. Please try again.')
+  }
+
+  const organizations = await orgCursor.all()
+
+  const hasNextPage = !!(
+    typeof first !== 'undefined' && organizations.length > first
+  )
+  const hasPreviousPage = !!(
+    typeof last !== 'undefined' && organizations.length > last
+  )
+
+  if (organizations.length > last || organizations.length > first) {
+    organizations.pop()
+  }
+
+  const startCursor = toGlobalId('organizations', organizations[0]._key)
+  const endCursor = toGlobalId(
+    'organizations',
+    organizations[organizations.length - 1]._key,
+  )
+
+  const edges = []
+  organizations.forEach(async (organization) => {
+    organization.id = organization._key
+    edges.push({
+      cursor: toGlobalId('organizations', organization._key),
+      node: organization,
+    })
+  })
+
+  if (edges.length === 0) {
+    return {
+      edges: [],
+      pageInfo: {
+        hasNextPage: false,
+        hasPreviousPage: false,
+        startCursor: '',
+        endCursor: '',
+      },
+    }
+  }
+
+  return {
+    edges,
+    pageInfo: {
+      hasNextPage,
+      hasPreviousPage,
+      startCursor,
+      endCursor,
+    },
+  }
+}
+
+module.exports = {
+  loadOrganizationsConnections,
+}
