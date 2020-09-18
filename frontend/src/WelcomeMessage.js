0de264cc47b07d0fea272e8732d81b073db83bab
lcampbell2 <l.campbell144768@gmail.com>
2020-09-18 09:27:33 -0300

    switch logo position

--- frontend/src/WelcomeMessage.js
+++ frontend/src/WelcomeMessage.js
@@ -6,12 +6,6 @@ export function WelcomeMessage() {
   return (
     <Box bg="primary" color="gray.50" align="center">
       <SimpleGrid columns={[1, 2]}>
-        <Image
-          src="src/images/tracker-logo.png"
-          size={['30%', '80%']}
-          alignSelf="center"
-          mx="10"
-        />
         <Stack align="center" mx="10" my="10">
           <Text fontSize="5xl" fontWeight="semibold">
             <Trans>Track Web Security Compliance</Trans>
@@ -26,12 +20,12 @@ export function WelcomeMessage() {
             </Trans>
           </Text>
         </Stack>
-        {/* <Image
+        <Image
           src="src/images/tracker-logo.png"
           size={['30%', '80%']}
           alignSelf="center"
           mx="10"
-        /> */}
+        />
       </SimpleGrid>
     </Box>
   )
