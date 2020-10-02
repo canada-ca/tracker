01230fdde7413baa31c63d792d4c521405144554
nslandolt <nsdeschenes@outlook.com>
2020-10-02 10:01:25 -0300

    adjust emailScan object for new setup

--- api-js/src/types/base/scan/email-scan/index.js
+++ api-js/src/types/base/scan/email-scan/index.js
@@ -1,10 +1,10 @@
 const { GraphQLObjectType } = require('graphql')
-const { GraphQLDate } = require('graphql-scalars')
 const { dkimConnection } = require('./dkim')
 const { dmarcConnection } = require('./dmarc')
 const { spfConnection } = require('./spf')
 const { Domain } = require('../../../../scalars')
 const { connectionArgs } = require('graphql-relay')
+const { GraphQLDateTime } = require('graphql-scalars')
 
 const emailScanType = new GraphQLObjectType({
   name: 'EmailScan',
@@ -12,16 +12,22 @@ const emailScanType = new GraphQLObjectType({
     domain: {
       type: Domain,
       description: `The domain the scan was ran on.`,
-      resolve: async ({ domain }) => domain,
-    },
-    timestamp: {
-      type: GraphQLDate,
-      description: `The time the scan was initiated.`,
-      resolve: async ({ timestamp }) => timestamp,
+      resolve: async ({ _key }, _, { loaders: { domainLoaderByKey } }) => {
+        const domain = await domainLoaderByKey.load(_key)
+        return domain
+      },
     },
     dkim: {
       type: dkimConnection.connectionType,
       args: {
+        starDate: {
+          type: GraphQLDateTime,
+          description: 'Start date for date filter.',
+        },
+        endDate: {
+          type: GraphQLDateTime,
+          description: 'End date for date filter.',
+        },
         ...connectionArgs,
       },
       description: `DomainKeys Identified Mail (DKIM) Signatures scan results.`,
@@ -30,6 +36,14 @@ const emailScanType = new GraphQLObjectType({
     dmarc: {
       type: dmarcConnection.connectionType,
       args: {
+        starDate: {
+          type: GraphQLDateTime,
+          description: 'Start date for date filter.',
+        },
+        endDate: {
+          type: GraphQLDateTime,
+          description: 'End date for date filter.',
+        },
         ...connectionArgs,
       },
       description: `Domain-based Message Authentication, Reporting, and Conformance (DMARC) scan results.`,
@@ -38,6 +52,14 @@ const emailScanType = new GraphQLObjectType({
     spf: {
       type: spfConnection.connectionType,
       args: {
+        starDate: {
+          type: GraphQLDateTime,
+          description: 'Start date for date filter.',
+        },
+        endDate: {
+          type: GraphQLDateTime,
+          description: 'End date for date filter.',
+        },
         ...connectionArgs,
       },
       description: `Sender Policy Framework (SPF) scan results.`,
