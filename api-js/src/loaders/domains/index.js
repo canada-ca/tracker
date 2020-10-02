907cf3ded9ba43c4c6e13f9851ca23762c8bdcf7
Ethan Foss <fossliam@outlook.com>
2020-10-02 15:24:01 -0300

    Re-implement loadDomainByDomain changes within loaders/domains/index.js

--- api-js/src/loaders/domains/index.js
+++ api-js/src/loaders/domains/index.js
@@ -1,4 +1,5 @@
 const { domainLoaderByKey } = require('./load-domain-by-key')
+const { domainLoaderByDomain } = require('./load-domain-by-domain')
 const {
   domainLoaderConnectionsByOrgId,
 } = require('./load-domain-connections-by-organizations-id')
@@ -8,6 +9,7 @@ const {
 
 module.exports = {
   domainLoaderByKey,
+  domainLoaderByDomain,
   domainLoaderConnectionsByOrgId,
   domainLoaderConnectionsByUserId,
 }
