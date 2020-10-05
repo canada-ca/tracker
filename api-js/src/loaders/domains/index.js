e649d7248c86e92fdd9b4cf8b1d6715189e7cb00
Ethan <fossliam@outlook.com>
2020-10-05 17:53:56 -0300

    Implementation of 'Find my domains' query and underlying loader (#878)
    * Initial implementation of find-my-domains query and load-domain-connections-by-user-id loader
    
    * Removal of slug usages
    
    * Re-implement loadDomainByDomain changes within loaders/domains/index.js
    
    * Updated to reflect requested changes
    
    * Updated to reflect requested changes

--- api-js/src/loaders/domains/index.js
+++ api-js/src/loaders/domains/index.js
@@ -3,9 +3,13 @@ const { domainLoaderByDomain } = require('./load-domain-by-domain')
 const {
   domainLoaderConnectionsByOrgId,
 } = require('./load-domain-connections-by-organizations-id')
+const {
+  domainLoaderConnectionsByUserId,
+} = require('./load-domain-connections-by-user-id')
 
 module.exports = {
   domainLoaderByKey,
   domainLoaderByDomain,
   domainLoaderConnectionsByOrgId,
+  domainLoaderConnectionsByUserId,
 }
--- api-js/src/queries/domains/find-my-domains.js
+++ api-js/src/queries/domains/find-my-domains.js
@@ -7,7 +7,31 @@ const findMyDomains = {
   args: {
     ...connectionArgs,
   },
-  resolve: async () => {},
+  resolve: async (
+    _,
+    args,
+    {
+      userId,
+      loaders: {
+        domainLoaderConnectionsByUserId,
+      },
+    },
+  ) => {
+    let domainConnections
+
+    try {
+      domainConnections = await domainLoaderConnectionsByUserId(args)
+    } catch (err) {
+      console.error(
+        `Database error occurred while user: ${userId} was trying to gather domain connections in findMyDomains.`,
+      )
+      throw new Error('Unable to load domains. Please try again.')
+    }
+
+    console.info(`User ${userId} successfully retrieved their domains.`)
+
+    return domainConnections
+  },
 }
 
 module.exports = {
