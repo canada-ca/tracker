93b59394c74f67bdec5bb2d437f291c6e1f5b2c2
nslandolt <nsdeschenes@outlook.com>
2020-10-05 15:16:59 -0300

    move email scan stuff into base schema file

--- api-js/src/types/base/scan/index.js
+++ api-js/src/types/base/scan/index.js
@@ -1,8 +1,6 @@
-const { emailScanType } = require('./email-scan')
 const { webScanType, webScanConnection } = require('./web-scan')
 
 module.exports = {
-  emailScanType,
   webScanType,
   webScanConnection,
 }
