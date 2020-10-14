7a7dd630f0ba215c85693067286519079bd99e2b
nslandolt <nsdeschenes@outlook.com>
2020-10-14 07:46:17 -0300

    add jest config to ignore locale directory

---
+++ api-js/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'src/**/*.js',
+  ],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'test-config',
+    'jestGlobalMocks.js',
+    '.module.js',
+    'locale',
+  ],
+}
