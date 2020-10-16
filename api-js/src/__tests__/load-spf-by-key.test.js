c3ed144284c965805651badd6d1c0eea6c7a097d
nslandolt <nsdeschenes@outlook.com>
2020-10-16 12:41:27 -0300

    load spf by key translations completed

--- api-js/src/__tests__/load-spf-by-key.test.js
+++ api-js/src/__tests__/load-spf-by-key.test.js
@@ -5,11 +5,15 @@ require('dotenv-safe').config({
 const { DB_PASS: rootPass, DB_URL: url } = process.env
 
 const { ArangoTools, dbNameFromFile } = require('arango-tools')
+const { setupI18n } = require('@lingui/core')
+
+const englishMessages = require('../locale/en/messages')
+const frenchMessages = require('../locale/fr/messages')
 const { makeMigrations } = require('../../migrations')
 const { spfLoaderByKey } = require('../loaders')
 
 describe('given the spfLoaderByKey function', () => {
-  let query, drop, truncate, migrate, collections
+  let query, drop, truncate, migrate, collections, i18n
 
   const consoleErrorOutput = []
   const mockedError = (output) => consoleErrorOutput.push(output)
@@ -20,6 +24,15 @@ describe('given the spfLoaderByKey function', () => {
       makeMigrations({ databaseName: dbNameFromFile(__filename), rootPass }),
     ))
     console.error = mockedError
+    i18n = setupI18n({
+      language: 'en',
+      locales: ['en', 'fr'],
+      missing: 'Traduction manquante',
+      catalogs: {
+        en: englishMessages,
+        fr: frenchMessages,
+      },
+    })
   })
 
   beforeEach(async () => {
@@ -43,7 +56,7 @@ describe('given the spfLoaderByKey function', () => {
     `
       const expectedSpf = await expectedCursor.next()
 
-      const loader = spfLoaderByKey(query)
+      const loader = spfLoaderByKey(query, i18n)
       const spf = await loader.load(expectedSpf._key)
 
       expect(spf).toEqual(expectedSpf)
@@ -64,50 +77,121 @@ describe('given the spfLoaderByKey function', () => {
         expectedSpfScans.push(tempSpf)
       }
 
-      const loader = spfLoaderByKey(query)
+      const loader = spfLoaderByKey(query, i18n)
       const dkimScans = await loader.loadMany(spfKeys)
       expect(dkimScans).toEqual(expectedSpfScans)
     })
   })
-  describe('given a database error', () => {
-    it('raises an error', async () => {
-      query = jest.fn().mockRejectedValue(new Error('Database error occurred.'))
-      const loader = spfLoaderByKey(query)
+  describe('users language is set to english', () => {
+    beforeAll(() => {
+      i18n = setupI18n({
+        language: 'en',
+        locales: ['en', 'fr'],
+        missing: 'Traduction manquante',
+        catalogs: {
+          en: englishMessages,
+          fr: frenchMessages,
+        },
+      })
+    })
+    describe('given a database error', () => {
+      it('raises an error', async () => {
+        query = jest
+          .fn()
+          .mockRejectedValue(new Error('Database error occurred.'))
+        const loader = spfLoaderByKey(query, i18n)
 
-      try {
-        await loader.load('1')
-      } catch (err) {
-        expect(err).toEqual(
-          new Error('Unable to find spf scan. Please try again.'),
-        )
-      }
+        try {
+          await loader.load('1')
+        } catch (err) {
+          expect(err).toEqual(
+            new Error('Unable to find spf scan. Please try again.'),
+          )
+        }
 
-      expect(consoleErrorOutput).toEqual([
-        `Database error occurred when running spfLoaderByKey: Error: Database error occurred.`,
-      ])
+        expect(consoleErrorOutput).toEqual([
+          `Database error occurred when running spfLoaderByKey: Error: Database error occurred.`,
+        ])
+      })
+    })
+    describe('given a cursor error', () => {
+      it('raises an error', async () => {
+        const cursor = {
+          each() {
+            throw new Error('Cursor error occurred.')
+          },
+        }
+        query = jest.fn().mockReturnValue(cursor)
+        const loader = spfLoaderByKey(query, i18n)
+
+        try {
+          await loader.load('1')
+        } catch (err) {
+          expect(err).toEqual(
+            new Error('Unable to find spf scan. Please try again.'),
+          )
+        }
+
+        expect(consoleErrorOutput).toEqual([
+          `Cursor error occurred when running spfLoaderByKey: Error: Cursor error occurred.`,
+        ])
+      })
     })
   })
-  describe('given a cursor error', () => {
-    it('raises an error', async () => {
-      const cursor = {
-        each() {
-          throw new Error('Cursor error occurred.')
+  describe('users language is set to french', () => {
+    beforeAll(() => {
+      i18n = setupI18n({
+        language: 'fr',
+        locales: ['en', 'fr'],
+        missing: 'Traduction manquante',
+        catalogs: {
+          en: englishMessages,
+          fr: frenchMessages,
         },
-      }
-      query = jest.fn().mockReturnValue(cursor)
-      const loader = spfLoaderByKey(query)
+      })
+    })
+    describe('given a database error', () => {
+      it('raises an error', async () => {
+        query = jest
+          .fn()
+          .mockRejectedValue(new Error('Database error occurred.'))
+        const loader = spfLoaderByKey(query, i18n)
 
-      try {
-        await loader.load('1')
-      } catch (err) {
-        expect(err).toEqual(
-          new Error('Unable to find spf scan. Please try again.'),
-        )
-      }
+        try {
+          await loader.load('1')
+        } catch (err) {
+          expect(err).toEqual(
+            new Error('todo'),
+          )
+        }
 
-      expect(consoleErrorOutput).toEqual([
-        `Cursor error occurred when running spfLoaderByKey: Error: Cursor error occurred.`,
-      ])
+        expect(consoleErrorOutput).toEqual([
+          `Database error occurred when running spfLoaderByKey: Error: Database error occurred.`,
+        ])
+      })
+    })
+    describe('given a cursor error', () => {
+      it('raises an error', async () => {
+        const cursor = {
+          each() {
+            throw new Error('Cursor error occurred.')
+          },
+        }
+        query = jest.fn().mockReturnValue(cursor)
+        const loader = spfLoaderByKey(query, i18n)
+
+        try {
+          await loader.load('1')
+        } catch (err) {
+          expect(err).toEqual(
+            new Error('todo'),
+          )
+        }
+
+        expect(consoleErrorOutput).toEqual([
+          `Cursor error occurred when running spfLoaderByKey: Error: Cursor error occurred.`,
+        ])
+      })
     })
   })
 })
