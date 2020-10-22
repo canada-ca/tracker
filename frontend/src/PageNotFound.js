5bc39010277c86b5a76523ef63a22412ee0fa604
lcampbell2 <l.campbell144768@gmail.com>
2020-10-22 09:58:23 -0300

    small changes on 404 page

--- frontend/src/PageNotFound.js
+++ frontend/src/PageNotFound.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Heading, Box, Text } from '@chakra-ui/core'
+import { Heading, Box, Text, Icon, Stack, Divider } from '@chakra-ui/core'
 import { Trans } from '@lingui/macro'
 
 export default function PageNotFound() {
   return (
     <Box>
-      <Heading as="h1">
-        <Trans>Page not found</Trans>
-      </Heading>
-      <Text textAlign="center">
-        <Trans>Sorry, the page you were trying to view does not exist.</Trans>
+      <Stack isInline align="center">
+        <Icon name="warning-2" size="icons.xl" />
+        <Heading as="h1">
+          <Trans>404 - Page Not Found</Trans>
+        </Heading>
+      </Stack>
+      <Divider />
+      <Text textAlign="center" fontSize="2xl" fontWeight="bold">
+        <Trans>The page you are looking for has moved or does not exist.</Trans>
       </Text>
     </Box>
   )
