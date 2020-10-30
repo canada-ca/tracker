d1e044cdf9639b6abd65c8c45ec28c686cca9840
nsdeschenes <nsdeschenes@outlook.com>
2020-10-30 09:33:34 -0300

    remove import

--- api-js/src/queries/domains/index.js
+++ api-js/src/queries/domains/index.js
@@ -1,9 +1,7 @@
 const { findDomainByDomain } = require('./find-domain-by-domain')
-const { findDomainsByOrg } = require('./find-domains-by-org')
 const { findMyDomains } = require('./find-my-domains')
 
 module.exports = {
   findDomainByDomain,
-  findDomainsByOrg,
   findMyDomains,
 }
