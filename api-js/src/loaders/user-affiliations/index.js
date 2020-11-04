bb5fe25258b95fbee92251eae4e5535e9dda7ce9
Ethan <fossliam@outlook.com>
2020-11-04 08:27:23 -0400

    Implementation of user-affiliation loaders and tests (#1113)
    * Implementation of loaders and tests
    
    * Update and amend tests and loader functionality
    
    * Fix queries

---
+++ api-js/src/loaders/user-affiliations/index.js
@@ -0,0 +1,9 @@
+const { affiliationLoaderByKey } = require('./load-user-affiliations-by-key')
+const { affiliationLoaderByUserId } = require('./load-user-affiliations-by-user-id')
+const { affiliationLoaderByOrgId } = require('./load-user-affiliations-by-org-id')
+
+module.exports = {
+  affiliationLoaderByKey,
+  affiliationLoaderByUserId,
+  affiliationLoaderByOrgId,
+}
