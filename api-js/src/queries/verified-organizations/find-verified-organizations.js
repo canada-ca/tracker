cff402dd2649d9c1d401ed791e6015d81627f290
nsdeschenes <nsdeschenes@outlook.com>
2020-11-10 09:08:48 -0400

    verified organizations queries compeleted and tested

---
+++ api-js/src/queries/verified-organizations/find-verified-organizations.js
@@ -0,0 +1,22 @@
+const { connectionArgs } = require('graphql-relay')
+const { verifiedOrganizationConnections } = require('../../types')
+
+const findVerifiedOrganizations = {
+  type: verifiedOrganizationConnections.connectionType,
+  description: 'Select organizations a user has access to.',
+  args: {
+    ...connectionArgs,
+  },
+  resolve: async (
+    _,
+    args,
+    { loaders: { verifiedOrgLoaderConnections } },
+  ) => {
+    const orgConnections = await verifiedOrgLoaderConnections(args)
+    return orgConnections
+  },
+}
+
+module.exports = {
+  findVerifiedOrganizations,
+}
