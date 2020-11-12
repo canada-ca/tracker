30c4f43c8aee488843124a8f4631242ba9ab374f
nsdeschenes <nsdeschenes@outlook.com>
2020-11-12 07:00:18 -0400

    change test message

--- api-js/src/__tests__/server.test.js
+++ api-js/src/__tests__/server.test.js
@@ -3,7 +3,7 @@ const { Server } = require('../server')
 
 describe('parse server', () => {
   describe('/alive', () => {
-    it('responds with a 200', async () => {
+    it('returns 200', async () => {
       const response = await request(Server({ query: jest.fn() })).get('/alive')
       expect(response.status).toEqual(200)
     })
