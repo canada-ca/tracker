cf19763c5757f3203281c88da8987357086d568d
nsdeschenes <nsdeschenes@outlook.com>
2020-11-16 13:52:42 -0400

    update order of function calls

--- services/domain-ownership/index.js
+++ services/domain-ownership/index.js
@@ -8,16 +8,21 @@ const { DB_PASS: rootPass, DB_URL: url, DB_NAME: databaseName } = process.env
 const { ArangoTools } = require('arango-tools')
 const { makeMigrations } = require('./migrations')
 const fetch = require('isomorphic-fetch')
-const { loadDomainOwnership, upsertOwnership, removeOwnerships } = require('./src')
+const {
+  loadDomainOwnership,
+  upsertOwnership,
+  removeOwnerships,
+} = require('./src')
 
 ;(async () => {
   // Generate Database information
   const { migrate } = await ArangoTools({ rootPass, url })
   const { query } = await migrate(makeMigrations({ databaseName, rootPass }))
-  // Load ownership assignments from github
-  const ownerships = await loadDomainOwnership({ fetch })
 
   await removeOwnerships({ query })
+
+  // Load ownership assignments from github
+  const ownerships = await loadDomainOwnership({ fetch })
   await upsertOwnership({ ownerships, query })
 
   console.info('Completed assigning ownerships.')
