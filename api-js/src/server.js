810631b801a6cba014c5c347d09249a37cf3c882
nsdeschenes <nc575894@dal.ca>
2020-11-19 10:05:07 -0400

    We're Going Back To Apollo (#1216)
    * Update packages
    
    * move create context to its own directory
    
    * move createi18n function to its own directory
    
    * move back to apollo server express
    
    * fix start issue caused by unused envs
    
    * update server tests
    
    * fix verify token by actually creating it with factory func
    
    * write tests to check the userId being set in context creation

--- api-js/src/server.js
+++ api-js/src/server.js
@@ -1,337 +1,29 @@
 const cors = require('cors')
 const express = require('express')
-const bcrypt = require('bcrypt')
-const moment = require('moment')
-// const http = require('http')
-// const { createServer } = require('graphql-transport-ws')
-const { graphqlHTTP } = require('express-graphql')
-const { GraphQLSchema /*, execute, subscribe */ } = require('graphql')
-const expressPlayground = require('graphql-playground-middleware-express')
-  .default
+const http = require('http')
+const { ApolloServer } = require('apollo-server-express')
 const requestLanguage = require('express-request-language')
-const { setupI18n } = require('@lingui/core')
-const { t } = require('@lingui/macro')
-const fetch = require('isomorphic-fetch')
+const { GraphQLSchema } = require('graphql')
 const depthLimit = require('graphql-depth-limit')
 const { createComplexityLimitRule } = require('graphql-validation-complexity')
 
+const { createContext } = require('./create-context')
 const { createQuerySchema } = require('./queries')
 const { createMutationSchema } = require('./mutations')
-const englishMessages = require('./locale/en/messages')
-const frenchMessages = require('./locale/fr/messages')
 
-const { cleanseInput, slugify } = require('./validators')
-const {
-  checkDomainOwnership,
-  checkDomainPermission,
-  checkPermission,
-  checkUserIsAdminForUser,
-  tokenize,
-  userRequired,
-  verifyToken,
-} = require('./auth')
-const {
-  notifyClient,
-  sendAuthEmail,
-  sendAuthTextMsg,
-  sendOrgInviteCreateAccount,
-  sendOrgInviteEmail,
-  sendPasswordResetEmail,
-  sendTfaTextMsg,
-  sendVerificationEmail,
-} = require('./notify')
-
-const {
-  chartSummaryLoaderByKey,
-  generateDetailTableFields,
-  generateGqlQuery,
-  dmarcReportLoader,
-  domainLoaderByKey,
-  domainLoaderByDomain,
-  domainLoaderConnectionsByOrgId,
-  domainLoaderConnectionsByUserId,
-  dkimLoaderByKey,
-  dkimResultLoaderByKey,
-  dmarcLoaderByKey,
-  spfLoaderByKey,
-  dkimLoaderConnectionsByDomainId,
-  dkimResultsLoaderConnectionByDkimId,
-  dmarcLoaderConnectionsByDomainId,
-  spfLoaderConnectionsByDomainId,
-  dkimGuidanceTagLoader,
-  dkimGuidanceTagConnectionsLoader,
-  dmarcGuidanceTagLoader,
-  dmarcGuidanceTagConnectionsLoader,
-  httpsGuidanceTagLoader,
-  httpsGuidanceTagConnectionsLoader,
-  spfGuidanceTagLoader,
-  spfGuidanceTagConnectionsLoader,
-  sslGuidanceTagLoader,
-  sslGuidanceTagConnectionsLoader,
-  orgLoaderByKey,
-  orgLoaderBySlug,
-  orgLoaderConnectionArgsByDomainId,
-  orgLoaderConnectionsByUserId,
-  userLoaderByUserName,
-  userLoaderByKey,
-  httpsLoaderByKey,
-  httpsLoaderConnectionsByDomainId,
-  sslLoaderByKey,
-  sslLoaderConnectionsByDomainId,
-  affiliationLoaderByKey,
-  affiliationLoaderByUserId,
-  affiliationLoaderByOrgId,
-  verifiedDomainLoaderByDomain,
-  verifiedDomainLoaderByKey,
-  verifiedDomainLoaderConnections,
-  verifiedDomainLoaderConnectionsByOrgId,
-  verifiedOrgLoaderByKey,
-  verifiedOrgLoaderBySlug,
-  verifiedOrgLoaderConnectionsByDomainId,
-  verifiedOrgLoaderConnections,
-} = require('./loaders')
-
-const createSchema = ({ language }) =>
+const createSchema = () =>
   new GraphQLSchema({
-    query: createQuerySchema(createI18n(language)),
-    mutation: createMutationSchema(createI18n(language)),
+    query: createQuerySchema(),
+    mutation: createMutationSchema(),
   })
-
-const createI18n = (language) =>
-  setupI18n({
-    language: language,
-    locales: ['en', 'fr'],
-    missing: 'Traduction manquante',
-    catalogs: {
-      en: englishMessages,
-      fr: frenchMessages,
-    },
-  })
-
-const createContext = ({ context, request, response }) => {
-  const { query } = context
-  // Get user id from token
-  let userId
-  const token = request.headers.authorization || ''
-  if (token !== '') {
-    userId = verifyToken({ token }).userId
-  }
-
-  const i18n = createI18n(request.language)
-
-  return {
-    i18n,
-    ...context,
-    request,
-    response,
-    userId,
-    moment,
-    auth: {
-      bcrypt,
-      checkDomainOwnership: checkDomainOwnership({ i18n, userId, query }),
-      checkDomainPermission: checkDomainPermission({ i18n, userId, query }),
-      checkPermission: checkPermission({ i18n, userId, query }),
-      checkUserIsAdminForUser: checkUserIsAdminForUser({ i18n, userId, query }),
-      tokenize,
-      userRequired: userRequired({
-        i18n,
-        userId,
-        userLoaderByKey: userLoaderByKey(query),
-      }),
-      verifyToken: verifyToken({ i18n }),
-    },
-    validators: {
-      cleanseInput,
-      slugify,
-    },
-    notify: {
-      sendAuthEmail: sendAuthEmail(notifyClient, i18n),
-      sendAuthTextMsg: sendAuthTextMsg(notifyClient, i18n),
-      sendOrgInviteCreateAccount: sendOrgInviteCreateAccount(
-        notifyClient,
-        i18n,
-      ),
-      sendOrgInviteEmail: sendOrgInviteEmail(notifyClient, i18n),
-      sendPasswordResetEmail: sendPasswordResetEmail(notifyClient, i18n),
-      sendTfaTextMsg: sendTfaTextMsg(notifyClient, i18n),
-      sendVerificationEmail: sendVerificationEmail(notifyClient, i18n),
-    },
-    loaders: {
-      chartSummaryLoaderByKey: chartSummaryLoaderByKey(query, userId, i18n),
-      dmarcReportLoader: dmarcReportLoader({
-        generateGqlQuery,
-        generateDetailTableFields,
-        fetch,
-        i18n,
-      }),
-      domainLoaderByDomain: domainLoaderByDomain(query, userId, i18n),
-      domainLoaderByKey: domainLoaderByKey(query, userId, i18n),
-      domainLoaderConnectionsByOrgId: domainLoaderConnectionsByOrgId(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      domainLoaderConnectionsByUserId: domainLoaderConnectionsByUserId(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      dkimLoaderByKey: dkimLoaderByKey(query, userId, i18n),
-      dkimResultLoaderByKey: dkimResultLoaderByKey(query, userId, i18n),
-      dmarcLoaderByKey: dmarcLoaderByKey(query, userId, i18n),
-      spfLoaderByKey: spfLoaderByKey(query, userId, i18n),
-      dkimLoaderConnectionsByDomainId: dkimLoaderConnectionsByDomainId(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      dkimResultsLoaderConnectionByDkimId: dkimResultsLoaderConnectionByDkimId(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      dmarcLoaderConnectionsByDomainId: dmarcLoaderConnectionsByDomainId(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      spfLoaderConnectionsByDomainId: spfLoaderConnectionsByDomainId(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      httpsLoaderByKey: httpsLoaderByKey(query, userId, i18n),
-      httpsLoaderConnectionsByDomainId: httpsLoaderConnectionsByDomainId(
-        query,
-        userId,
-        cleanseInput,
-      ),
-      sslLoaderByKey: sslLoaderByKey(query, userId, i18n),
-      sslLoaderConnectionsByDomainId: sslLoaderConnectionsByDomainId(
-        query,
-        userId,
-        cleanseInput,
-      ),
-      dkimGuidanceTagLoader: dkimGuidanceTagLoader(query, userId, i18n),
-      dkimGuidanceTagConnectionsLoader: dkimGuidanceTagConnectionsLoader(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      dmarcGuidanceTagLoader: dmarcGuidanceTagLoader(query, userId, i18n),
-      dmarcGuidanceTagConnectionsLoader: dmarcGuidanceTagConnectionsLoader(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      httpsGuidanceTagLoader: httpsGuidanceTagLoader(query, userId, i18n),
-      httpsGuidanceTagConnectionsLoader: httpsGuidanceTagConnectionsLoader(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      spfGuidanceTagLoader: spfGuidanceTagLoader(query, userId, i18n),
-      spfGuidanceTagConnectionsLoader: spfGuidanceTagConnectionsLoader(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      sslGuidanceTagLoader: sslGuidanceTagLoader(query, userId, i18n),
-      sslGuidanceTagConnectionsLoader: sslGuidanceTagConnectionsLoader(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      orgLoaderByKey: orgLoaderByKey(query, request.language, userId, i18n),
-      orgLoaderBySlug: orgLoaderBySlug(query, request.language, userId, i18n),
-      orgLoaderConnectionArgsByDomainId: orgLoaderConnectionArgsByDomainId(
-        query,
-        request.language,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      orgLoaderConnectionsByUserId: orgLoaderConnectionsByUserId(
-        query,
-        userId,
-        cleanseInput,
-        request.language,
-        i18n,
-      ),
-      userLoaderByUserName: userLoaderByUserName(query, userId, i18n),
-      userLoaderByKey: userLoaderByKey(query, userId, i18n),
-      affiliationLoaderByKey: affiliationLoaderByKey(query, userId, i18n),
-      affiliationLoaderByUserId: affiliationLoaderByUserId(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      affiliationLoaderByOrgId: affiliationLoaderByOrgId(
-        query,
-        userId,
-        cleanseInput,
-        i18n,
-      ),
-      verifiedDomainLoaderByDomain: verifiedDomainLoaderByDomain(query, i18n),
-      verifiedDomainLoaderByKey: verifiedDomainLoaderByKey(query, i18n),
-      verifiedDomainLoaderConnections: verifiedDomainLoaderConnections(
-        query,
-        cleanseInput,
-        i18n,
-      ),
-      verifiedDomainLoaderConnectionsByOrgId: verifiedDomainLoaderConnectionsByOrgId(
-        query,
-        cleanseInput,
-        i18n,
-      ),
-      verifiedOrgLoaderByKey: verifiedOrgLoaderByKey(
-        query,
-        request.language,
-        i18n,
-      ),
-      verifiedOrgLoaderBySlug: verifiedOrgLoaderBySlug(
-        query,
-        request.language,
-        i18n,
-      ),
-      verifiedOrgLoaderConnectionsByDomainId: verifiedOrgLoaderConnectionsByDomainId(
-        query,
-        request.language,
-        cleanseInput,
-        i18n,
-      ),
-      verifiedOrgLoaderConnections: verifiedOrgLoaderConnections(
-        query,
-        request.language,
-        cleanseInput,
-        i18n,
-      ),
-    },
-  }
-}
 
 const createValidationRules = (
-  language,
   maxDepth,
   complexityCost,
   scalarCost,
   objectCost,
   listFactor,
 ) => {
-  const i18n = createI18n(language)
-
   return [
     depthLimit(maxDepth),
     createComplexityLimitRule(complexityCost, {
@@ -340,14 +32,14 @@ const createValidationRules = (
       listFactor,
       formatErrorMessage: (cost) => {
         console.warn(`User attempted a costly request: ${cost}`)
-        return i18n._(t`Query error, query is too complex.`)
+        return `Query error, query is too complex.`
       },
     }),
   ]
 }
 
 const Server = (
-  _PORT,
+  PORT,
   maxDepth,
   complexityCost,
   scalarCost,
@@ -373,47 +65,31 @@ const Server = (
     res.json({ ok: 'yes' })
   })
 
-  app.get(
-    '/graphql',
-    expressPlayground({
-      endpoint: '/graphql',
-    }),
+  const server = new ApolloServer({
+    schema: createSchema(),
+    context: async ({ req, res }) => createContext({ context, req, res }),
+    validationRules: createValidationRules(
+      maxDepth,
+      complexityCost,
+      scalarCost,
+      objectCost,
+      listFactor,
+    ),
+  })
+
+  server.applyMiddleware({ app })
+
+  const httpServer = http.createServer(app)
+
+  server.installSubscriptionHandlers(httpServer)
+
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`,
   )
-
-  app.use(
-    '/graphql',
-    graphqlHTTP(async (request, response, _graphQLParams) => ({
-      graphiql: false,
-      schema: createSchema({ language: request.language }),
-      context: createContext({ context, request, response }),
-      validationRules: createValidationRules(
-        request.language,
-        maxDepth,
-        complexityCost,
-        scalarCost,
-        objectCost,
-        listFactor,
-      ),
-    })),
+  console.log(
+    `🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`,
   )
-
-  // const httpServer = http.createServer(app)
-
-  // httpServer.listen(4001, () => {
-  //   createServer(
-  //     {
-  //       schema: createSchema('en'),
-  //       execute,
-  //       subscribe,
-  //     },
-  //     {
-  //       server: httpServer,
-  //       path: '/graphql',
-  //     },
-  //   )
-  // })
-
-  return app
+  return httpServer
 }
 
 module.exports = {
