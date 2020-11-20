61a3512972516e78e24a797d167f13f8276e77d5
Ethan <fossliam@outlook.com>
2020-11-20 13:03:01 -0400

    Core service test update (#1220)
    * Addition of summary calculation tests
    
    * Update tests and amend unhashable type error
    
    * Update expected document format to include _id and _rev, fix org update
    
    * Miscellaneous test fix

--- services/core/tests/test_core_service.py
+++ services/core/tests/test_core_service.py
@@ -11,9 +11,92 @@ sys_db.create_database("test")
 
 # Establish DB connection
 db = arango_client.db("test", username="", password="")
-db.create_collection("domains")
+graph = db.create_graph("compliance")
+domains = graph.create_vertex_collection("domains")
+orgs = graph.create_vertex_collection("organizations")
 
-db.collection("domains").insert({"domain": "cyber.gc.ca", "selectors": ["selector1"]})
+claims = graph.create_edge_definition(
+    edge_collection="claims",
+    from_vertex_collections=["organizations"],
+    to_vertex_collections=["domains"],
+)
+
+org = orgs.insert(
+    {
+        "_key": "testorg",
+        "verified": "true",
+        "summaries": {
+            "web": {"pass": 0, "fail": 0, "total": 0},
+            "mail": {"pass": 0, "fail": 0, "total": 0},
+        },
+        "orgDetails": {
+            "en": {
+                "slug": "communications-security-establishment",
+                "acronym": "CSE",
+                "name": "Communications Security Establishment",
+                "zone": "FED",
+                "sector": "DND",
+                "country": "Canada",
+                "province": "Ontario",
+                "city": "Ottawa",
+            },
+            "fr": {
+                "slug": "centre-de-la-securite-des-telecommunications",
+                "acronym": "CST",
+                "name": "Centre de la Securite des Telecommunications",
+                "zone": "FED",
+                "sector": "DND",
+                "country": "Canada",
+                "province": "Ontario",
+                "city": "Ottawa",
+            },
+        },
+    }
+)
+
+domain1 = domains.insert(
+    {
+        "domain": "cyber1.gc.ca",
+        "selectors": ["selector1"],
+        "status": {
+            "https": "pass",
+            "ssl": "pass",
+            "dmarc": "pass",
+            "spf": "pass",
+            "dkim": "fail",
+        },
+    }
+)
+domain2 = domains.insert(
+    {
+        "domain": "cyber2.gc.ca",
+        "selectors": ["selector1"],
+        "status": {
+            "https": "pass",
+            "ssl": "pass",
+            "dmarc": "pass",
+            "spf": "pass",
+            "dkim": "pass",
+        },
+    }
+)
+domain3 = domains.insert(
+    {
+        "domain": "cyber3.gc.ca",
+        "selectors": ["selector1"],
+        "status": {
+            "https": "fail",
+            "ssl": "fail",
+            "dmarc": "fail",
+            "spf": "fail",
+            "dkim": "fail",
+        },
+    }
+)
+
+claims.insert({"_from": org["_id"], "_to": domain1["_id"]})
+claims.insert({"_from": org["_id"], "_to": domain2["_id"]})
+claims.insert({"_from": org["_id"], "_to": domain3["_id"]})
 
 
 def test_update_guidance():
@@ -134,3 +217,91 @@ def test_update_guidance():
             "refLinksGuide": current_tag["refLinksGuide"],
             "refLinksTechnical": current_tag["refLinksTechnical"],
         }
+
+
+def test_update_scan_summaries():
+    update_scan_summaries(host="http://testdb:8529", name="test", user="", password="")
+
+    httpsScanSummary = db.collection("scanSummaries").get({"_key": "https"})
+    assert httpsScanSummary == {
+        "_id": "scanSummaries/https",
+        "_rev": httpsScanSummary["_rev"],
+        "_key": "https",
+        "pass": 2,
+        "fail": 1,
+        "total": 3,
+    }
+
+    sslScanSummary = db.collection("scanSummaries").get({"_key": "ssl"})
+    assert sslScanSummary == {
+        "_id": "scanSummaries/ssl",
+        "_rev": sslScanSummary["_rev"],
+        "_key": "ssl",
+        "pass": 2,
+        "fail": 1,
+        "total": 3,
+    }
+
+    dmarcScanSummary = db.collection("scanSummaries").get({"_key": "dmarc"})
+    assert dmarcScanSummary == {
+        "_id": "scanSummaries/dmarc",
+        "_rev": dmarcScanSummary["_rev"],
+        "_key": "dmarc",
+        "pass": 2,
+        "fail": 1,
+        "total": 3,
+    }
+
+    spfScanSummary = db.collection("scanSummaries").get({"_key": "spf"})
+    assert spfScanSummary == {
+        "_id": "scanSummaries/spf",
+        "_rev": spfScanSummary["_rev"],
+        "_key": "spf",
+        "pass": 2,
+        "fail": 1,
+        "total": 3,
+    }
+
+    dkimScanSummary = db.collection("scanSummaries").get({"_key": "dkim"})
+    assert dkimScanSummary == {
+        "_id": "scanSummaries/dkim",
+        "_rev": dkimScanSummary["_rev"],
+        "_key": "dkim",
+        "pass": 1,
+        "fail": 2,
+        "total": 3,
+    }
+
+
+def test_update_chart_summaries():
+    update_chart_summaries(host="http://testdb:8529", name="test", user="", password="")
+
+    webSummary = db.collection("chartSummaries").get({"_key": "web"})
+    assert webSummary == {
+        "_id": "chartSummaries/web",
+        "_rev": webSummary["_rev"],
+        "_key": "web",
+        "pass": 2,
+        "fail": 1,
+        "total": 3,
+    }
+
+    mailSummary = db.collection("chartSummaries").get({"_key": "mail"})
+    assert mailSummary == {
+        "_id": "chartSummaries/mail",
+        "_rev": mailSummary["_rev"],
+        "_key": "mail",
+        "pass": 1,
+        "fail": 2,
+        "total": 3,
+    }
+
+
+def test_update_org_summaries():
+    update_org_summaries(host="http://testdb:8529", name="test", user="", password="")
+
+    organization = db.collection("organizations").get({"_key": "testorg"})
+    assert organization["summaries"] == {
+        "web": {"pass": 2, "fail": 1, "total": 3},
+        "mail": {"pass": 1, "fail": 2, "total": 3},
+    }
