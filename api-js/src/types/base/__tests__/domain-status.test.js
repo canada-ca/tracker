a5aeebe2c4a854f5ea7e9dcbbb6829599bff401b
nsdeschenes <nsdeschenes@outlook.com>
2020-11-23 12:31:34 -0400

    refactored domainStatus tests

--- api-js/src/types/base/__tests__/domain-status.test.js
+++ api-js/src/types/base/__tests__/domain-status.test.js
@@ -1,255 +1,75 @@
-const { ArangoTools, dbNameFromFile } = require('arango-tools')
-const bcrypt = require('bcrypt')
-const { graphql, GraphQLSchema } = require('graphql')
-const { makeMigrations } = require('../../../../migrations')
-const { createQuerySchema } = require('../../../queries')
-const { createMutationSchema } = require('../../../mutations')
-const { cleanseInput } = require('../../../validators')
-const { tokenize } = require('../../../auth')
-const {
-  orgLoaderConnectionArgsByDomainId,
-  domainLoaderConnectionsByUserId,
-  domainLoaderByKey,
-  userLoaderByUserName,
-} = require('../../../loaders')
-const { DB_PASS: rootPass, DB_URL: url } = process.env
+const { StatusEnum } = require('../../../enums')
+const { domainStatus } = require('../domain-status')
 
-describe('given the domain status object', () => {
-  let query, drop, truncate, migrate, schema, collections, org, i18n
+describe('given the domainStatus object', () => {
+  describe('testing its field definitions', () => {
+    it('has a dkim field', () => {
+      const demoType = domainStatus.getFields()
 
-  beforeAll(async () => {
-    // Create GQL Schema
-    schema = new GraphQLSchema({
-      query: createQuerySchema(),
-      mutation: createMutationSchema(),
+      expect(demoType).toHaveProperty('dkim')
+      expect(demoType.dkim.type).toMatchObject(StatusEnum)
+    })
+    it('has a dmarc field', () => {
+      const demoType = domainStatus.getFields()
+
+      expect(demoType).toHaveProperty('dmarc')
+      expect(demoType.dmarc.type).toMatchObject(StatusEnum)
+    })
+    it('has a https field', () => {
+      const demoType = domainStatus.getFields()
+
+      expect(demoType).toHaveProperty('https')
+      expect(demoType.https.type).toMatchObject(StatusEnum)
+    })
+    it('has a spf field', () => {
+      const demoType = domainStatus.getFields()
+
+      expect(demoType).toHaveProperty('spf')
+      expect(demoType.spf.type).toMatchObject(StatusEnum)
+    })
+    it('has a ssl field', () => {
+      const demoType = domainStatus.getFields()
+
+      expect(demoType).toHaveProperty('ssl')
+      expect(demoType.ssl.type).toMatchObject(StatusEnum)
     })
   })
 
-  let consoleOutput = []
-  const mockedInfo = (output) => consoleOutput.push(output)
-  const mockedWarn = (output) => consoleOutput.push(output)
-  const mockedError = (output) => consoleOutput.push(output)
+  describe('testing its field resolvers', () => {
+    describe('testing the dkim resolver', () => {
+      it('returns the parsed value', () => {
+        const demoType = domainStatus.getFields()
 
-  beforeEach(async () => {
-    console.info = mockedInfo
-    console.warn = mockedWarn
-    console.error = mockedError
-    // Generate DB Items
-    ;({ migrate } = await ArangoTools({ rootPass, url }))
-    ;({ query, drop, truncate, collections } = await migrate(
-      makeMigrations({ databaseName: dbNameFromFile(__filename), rootPass }),
-    ))
-    await truncate()
-    await graphql(
-      schema,
-      `
-        mutation {
-          signUp(
-            input: {
-              displayName: "Test Account"
-              userName: "test.account@istio.actually.exists"
-              password: "testpassword123"
-              confirmPassword: "testpassword123"
-              preferredLang: FRENCH
-            }
-          ) {
-            authResult {
-              user {
-                id
-              }
-            }
-          }
-        }
-      `,
-      null,
-      {
-        query,
-        auth: {
-          bcrypt,
-          tokenize,
-        },
-        validators: {
-          cleanseInput,
-        },
-        loaders: {
-          userLoaderByUserName: userLoaderByUserName(query),
-        },
-      },
-    )
-    consoleOutput = []
-
-    org = await collections.organizations.save({
-      orgDetails: {
-        en: {
-          slug: 'treasury-board-secretariat',
-          acronym: 'TBS',
-          name: 'Treasury Board of Canada Secretariat',
-          zone: 'FED',
-          sector: 'TBS',
-          country: 'Canada',
-          province: 'Ontario',
-          city: 'Ottawa',
-        },
-        fr: {
-          slug: 'secretariat-conseil-tresor',
-          acronym: 'SCT',
-          name: 'Secrétariat du Conseil Trésor du Canada',
-          zone: 'FED',
-          sector: 'TBS',
-          country: 'Canada',
-          province: 'Ontario',
-          city: 'Ottawa',
-        },
-      },
-    })
-  })
-
-  afterEach(async () => {
-    await drop()
-  })
-
-  describe('given successful retrieval of domain status', () => {
-    let user, domainOne, domainTwo
-    beforeEach(async () => {
-      const userCursor = await query`
-        FOR user IN users
-          FILTER user.userName == "test.account@istio.actually.exists"
-          RETURN user
-      `
-      user = await userCursor.next()
-      await collections.affiliations.save({
-        _from: org._id,
-        _to: user._id,
-        permission: 'user',
-      })
-      domainOne = await collections.domains.save({
-        domain: 'test1.gc.ca',
-        lastRan: null,
-        selectors: ['selector1._domainkey', 'selector2._domainkey'],
-        status: {
-          dkim: 'pass',
-          dmarc: 'pass',
-          https: 'info',
-          spf: 'fail',
-          ssl: 'fail',
-        },
-      })
-      domainTwo = await collections.domains.save({
-        domain: 'test2.gc.ca',
-        lastRan: null,
-        selectors: ['selector1._domainkey', 'selector2._domainkey'],
-        status: {
-          dkim: 'pass',
-          dmarc: 'pass',
-          https: 'info',
-          spf: 'fail',
-          ssl: 'fail',
-        },
-      })
-      await collections.claims.save({
-        _to: domainOne._id,
-        _from: org._id,
-      })
-      await collections.claims.save({
-        _to: domainTwo._id,
-        _from: org._id,
+        expect(demoType.dkim.resolve({ dkim: 'pass' })).toEqual('pass')
       })
     })
-    afterEach(async () => {
-      await query`
-        LET userEdges = (FOR v, e IN 1..1 ANY ${org._id} affiliations RETURN { edgeKey: e._key, userKey: e._to })
-        LET removeUserEdges = (FOR userEdge IN userEdges REMOVE userEdge.edgeKey IN affiliations)
-        RETURN true
-      `
-      await query`
-        FOR affiliation IN affiliations
-          REMOVE affiliation IN affiliations
-      `
-      await query`
-        LET domainEdges = (FOR v, e IN 1..1 ANY ${org._id} claims RETURN { edgeKey: e._key, userKey: e._to })
-        LET removeDomainEdges = (FOR domainEdge IN domainEdges REMOVE domainEdge.edgeKey IN claims)
-        RETURN true
-      `
-      await query`
-        FOR claim IN claims
-          REMOVE claim IN claims
-      `
-    })
-    it('returns domains', async () => {
-      const response = await graphql(
-        schema,
-        `
-          query {
-            findMyDomains(first: 5) {
-              edges {
-                node {
-                  status {
-                    dkim
-                    dmarc
-                    https
-                    spf
-                    ssl
-                  }
-                }
-              }
-            }
-          }
-        `,
-        null,
-        {
-          i18n,
-          userKey: user._key,
-          loaders: {
-            domainLoaderByKey: domainLoaderByKey(query, user._key),
-            domainLoaderConnectionsByUserId: domainLoaderConnectionsByUserId(
-              query,
-              user._key,
-              cleanseInput,
-            ),
-            orgLoaderConnectionArgsByDomainId: orgLoaderConnectionArgsByDomainId(
-              query,
-              'en',
-              user._key,
-              cleanseInput,
-            ),
-          },
-        },
-      )
+    describe('testing the dmarc resolver', () => {
+      it('returns the parsed value', () => {
+        const demoType = domainStatus.getFields()
 
-      const expectedResponse = {
-        data: {
-          findMyDomains: {
-            edges: [
-              {
-                node: {
-                  status: {
-                    dkim: 'PASS',
-                    dmarc: 'PASS',
-                    https: 'INFO',
-                    spf: 'FAIL',
-                    ssl: 'FAIL',
-                  },
-                },
-              },
-              {
-                node: {
-                  status: {
-                    dkim: 'PASS',
-                    dmarc: 'PASS',
-                    https: 'INFO',
-                    spf: 'FAIL',
-                    ssl: 'FAIL',
-                  },
-                },
-              },
-            ],
-          },
-        },
-      }
-      expect(response).toEqual(expectedResponse)
-      expect(consoleOutput).toEqual([
-        `User ${user._key} successfully retrieved their domains.`,
-      ])
+        expect(demoType.dmarc.resolve({ dmarc: 'pass' })).toEqual('pass')
+      })
+    })
+    describe('testing the https resolver', () => {
+      it('returns the parsed value', () => {
+        const demoType = domainStatus.getFields()
+
+        expect(demoType.https.resolve({ https: 'pass' })).toEqual('pass')
+      })
+    })
+    describe('testing the spf resolver', () => {
+      it('returns the parsed value', () => {
+        const demoType = domainStatus.getFields()
+
+        expect(demoType.spf.resolve({ spf: 'pass' })).toEqual('pass')
+      })
+    })
+    describe('testing the ssl resolver', () => {
+      it('returns the parsed value', () => {
+        const demoType = domainStatus.getFields()
+
+        expect(demoType.ssl.resolve({ ssl: 'pass' })).toEqual('pass')
+      })
     })
   })
 })
