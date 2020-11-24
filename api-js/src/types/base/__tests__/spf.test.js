50cabc9bb301d5aaef8f55f5e18799599634cd8b
nsdeschenes <nsdeschenes@outlook.com>
2020-11-24 08:19:28 -0400

    spf type tests refactored

--- api-js/src/types/base/__tests__/spf.test.js
+++ api-js/src/types/base/__tests__/spf.test.js
@@ -1,308 +1,245 @@
 const { DB_PASS: rootPass, DB_URL: url } = process.env
 
 const { ArangoTools, dbNameFromFile } = require('arango-tools')
+const {
+  GraphQLNonNull,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLString,
+} = require('graphql')
 const { toGlobalId } = require('graphql-relay')
-const { graphql, GraphQLSchema } = require('graphql')
-
-const { createQuerySchema } = require('../../../queries')
-const { createMutationSchema } = require('../../../mutations')
+const { GraphQLDateTime } = require('graphql-scalars')
 const { makeMigrations } = require('../../../../migrations')
 const { cleanseInput } = require('../../../validators')
-const { checkDomainPermission, userRequired } = require('../../../auth')
 const {
-  spfLoaderConnectionsByDomainId,
-  spfLoaderByKey,
-  domainLoaderByDomain,
   domainLoaderByKey,
-  userLoaderByKey,
-  spfGuidanceTagLoader,
   spfGuidanceTagConnectionsLoader,
 } = require('../../../loaders')
+const { spfType, domainType, guidanceTagConnection } = require('../index')
 
 describe('given the spfType object', () => {
-  let query,
-    drop,
-    truncate,
-    migrate,
-    collections,
-    user,
-    domain,
-    schema,
-    org,
-    spf
+  describe('testing the field definitions', () => {
+    it('has an id field', () => {
+      const demoType = spfType.getFields()
 
-  const consoleInfoOutput = []
-  const mockedInfo = (output) => consoleInfoOutput.push(output)
+      expect(demoType).toHaveProperty('id')
+      expect(demoType.id.type).toMatchObject(GraphQLNonNull(GraphQLID))
+    })
+    it('has a domain field', () => {
+      const demoType = spfType.getFields()
 
-  const consoleWarnOutput = []
-  const mockedWarn = (output) => consoleWarnOutput.push(output)
+      expect(demoType).toHaveProperty('domain')
+      expect(demoType.domain.type).toMatchObject(domainType)
+    })
+    it('has a timestamp field', () => {
+      const demoType = spfType.getFields()
 
-  const consoleErrorOutput = []
-  const mockedError = (output) => consoleErrorOutput.push(output)
+      expect(demoType).toHaveProperty('timestamp')
+      expect(demoType.timestamp.type).toMatchObject(GraphQLDateTime)
+    })
+    it('has a lookups field', () => {
+      const demoType = spfType.getFields()
 
-  beforeAll(async () => {
-    console.info = mockedInfo
-    console.warn = mockedWarn
-    console.error = mockedError
-    ;({ migrate } = await ArangoTools({ rootPass, url }))
-    ;({ query, drop, truncate, collections } = await migrate(
-      makeMigrations({ databaseName: dbNameFromFile(__filename), rootPass }),
-    ))
-  })
+      expect(demoType).toHaveProperty('lookups')
+      expect(demoType.lookups.type).toMatchObject(GraphQLInt)
+    })
+    it('has a record field', () => {
+      const demoType = spfType.getFields()
 
-  beforeEach(async () => {
-    await truncate()
-    schema = new GraphQLSchema({
-      query: createQuerySchema(),
-      mutation: createMutationSchema(),
+      expect(demoType).toHaveProperty('record')
+      expect(demoType.record.type).toMatchObject(GraphQLString)
     })
-    consoleWarnOutput.length = 0
-    consoleErrorOutput.length = 0
-    consoleInfoOutput.length = 0
+    it('has a spfDefault field', () => {
+      const demoType = spfType.getFields()
 
-    user = await collections.users.save({
-      userName: 'test.account@istio.actually.exists',
-      displayName: 'Test Account',
-      preferredLang: 'french',
-      tfaValidated: false,
-      emailValidated: false,
+      expect(demoType).toHaveProperty('spfDefault')
+      expect(demoType.spfDefault.type).toMatchObject(GraphQLString)
     })
-    org = await collections.organizations.save({
-      orgDetails: {
-        en: {
-          slug: 'treasury-board-secretariat',
-          acronym: 'TBS',
-          name: 'Treasury Board of Canada Secretariat',
-          zone: 'FED',
-          sector: 'TBS',
-          country: 'Canada',
-          province: 'Ontario',
-          city: 'Ottawa',
-        },
-        fr: {
-          slug: 'secretariat-conseil-tresor',
-          acronym: 'SCT',
-          name: 'Secrétariat du Conseil Trésor du Canada',
-          zone: 'FED',
-          sector: 'TBS',
-          country: 'Canada',
-          province: 'Ontario',
-          city: 'Ottawa',
-        },
-      },
-    })
-    await collections.affiliations.save({
-      _from: org._id,
-      _to: user._id,
-      permission: 'admin',
-    })
-    domain = await collections.domains.save({
-      domain: 'test.domain.gc.ca',
-      slug: 'test-domain-gc-ca',
-    })
-    await collections.claims.save({
-      _from: org._id,
-      _to: domain._id,
-    })
-    spf = await collections.spf.save({
-      timestamp: '2020-10-02T12:43:39Z',
-      lookups: 5,
-      record: 'txtRecord',
-      spfDefault: 'default',
-      guidanceTags: ['spf1'],
-    })
-    await collections.domainsSPF.save({
-      _from: domain._id,
-      _to: spf._id,
-    })
-    await collections.spfGuidanceTags.save({
-      _key: 'spf1',
-      tagName: 'SPF-TAG',
-      guidance: 'Some Interesting Guidance',
-      refLinksGuide: [
-        {
-          description: 'refLinksGuide Description',
-          ref_link: 'www.refLinksGuide.ca',
-        },
-      ],
-      refLinksTechnical: [
-        {
-          description: 'refLinksTechnical Description',
-          ref_link: 'www.refLinksTechnical.ca',
-        },
-      ],
-    })
-  })
+    it('has a guidanceTags field', () => {
+      const demoType = spfType.getFields()
 
-  afterAll(async () => {
-    await drop()
-  })
-
-  describe('all fields can be queried', () => {
-    it('resolves all fields', async () => {
-      const response = await graphql(
-        schema,
-        `
-          query {
-            findDomainByDomain(domain: "test.domain.gc.ca") {
-              id
-              domain
-              email {
-                spf(first: 5) {
-                  edges {
-                    node {
-                      id
-                      domain {
-                        id
-                      }
-                      timestamp
-                      lookups
-                      record
-                      spfDefault
-                      guidanceTags(first: 5) {
-                        edges {
-                          node {
-                            id
-                            tagId
-                            tagName
-                            guidance
-                            refLinks {
-                              description
-                              refLink
-                            }
-                            refLinksTech {
-                              description
-                              refLink
-                            }
-                          }
-                        }
-                        totalCount
-                        pageInfo {
-                          hasNextPage
-                          hasPreviousPage
-                          startCursor
-                          endCursor
-                        }
-                      }
-                    }
-                  }
-                  totalCount
-                  pageInfo {
-                    hasNextPage
-                    hasPreviousPage
-                    startCursor
-                    endCursor
-                  }
-                }
-              }
-            }
-          }
-        `,
-        null,
-        {
-          userKey: user._key,
-          query: query,
-          auth: {
-            checkDomainPermission: checkDomainPermission({
-              query,
-              userKey: user._key,
-            }),
-            userRequired: userRequired({
-              userKey: user._key,
-              userLoaderByKey: userLoaderByKey(query),
-            }),
-          },
-          validators: {
-            cleanseInput,
-          },
-          loaders: {
-            spfLoaderConnectionsByDomainId: spfLoaderConnectionsByDomainId(
-              query,
-              user._key,
-              cleanseInput,
-            ),
-            spfLoaderByKey: spfLoaderByKey(query),
-            spfGuidanceTagLoader: spfGuidanceTagLoader(query),
-            spfGuidanceTagConnectionsLoader: spfGuidanceTagConnectionsLoader(
-              query,
-              user._key,
-              cleanseInput,
-            ),
-            domainLoaderByDomain: domainLoaderByDomain(query),
-            domainLoaderByKey: domainLoaderByKey(query),
-            userLoaderByKey: userLoaderByKey(query),
-          },
-        },
+      expect(demoType).toHaveProperty('guidanceTags')
+      expect(demoType.guidanceTags.type).toMatchObject(
+        guidanceTagConnection.connectionType,
       )
+    })
+  })
 
-      const expectedResponse = {
-        data: {
-          findDomainByDomain: {
-            id: toGlobalId('domains', domain._key),
-            domain: 'test.domain.gc.ca',
-            email: {
-              spf: {
-                edges: [
+  describe('testing the field resolvers', () => {
+    let query, drop, truncate, migrate, collections, domain, spf, spfGT
+
+    beforeAll(async () => {
+      ;({ migrate } = await ArangoTools({ rootPass, url }))
+      ;({ query, drop, truncate, collections } = await migrate(
+        makeMigrations({ databaseName: dbNameFromFile(__filename), rootPass }),
+      ))
+    })
+
+    beforeEach(async () => {
+      domain = await collections.domains.save({
+        domain: 'test.domain.gc.ca',
+        slug: 'test-domain-gc-ca',
+      })
+      spf = await collections.spf.save({
+        timestamp: '2020-10-02T12:43:39Z',
+        lookups: 5,
+        record: 'txtRecord',
+        spfDefault: 'default',
+        guidanceTags: ['spf1'],
+      })
+      await collections.domainsSPF.save({
+        _from: domain._id,
+        _to: spf._id,
+      })
+      spfGT = await collections.spfGuidanceTags.save({
+        _key: 'spf1',
+        tagName: 'SPF-TAG',
+        guidance: 'Some Interesting Guidance',
+        refLinksGuide: [
+          {
+            description: 'refLinksGuide Description',
+            ref_link: 'www.refLinksGuide.ca',
+          },
+        ],
+        refLinksTechnical: [
+          {
+            description: 'refLinksTechnical Description',
+            ref_link: 'www.refLinksTechnical.ca',
+          },
+        ],
+      })
+    })
+
+    afterEach(async () => {
+      await truncate()
+    })
+
+    afterAll(async () => {
+      await drop()
+    })
+
+    describe('testing the id resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = spfType.getFields()
+
+        expect(demoType.id.resolve({ id: '1' })).toEqual(toGlobalId('spf', '1'))
+      })
+    })
+    describe('testing the domain resolver', () => {
+      it('returns the resolved value', async () => {
+        const demoType = spfType.getFields()
+
+        const loader = domainLoaderByKey(query, '1', {})
+
+        const expectedResult = {
+          _id: domain._id,
+          _key: domain._key,
+          _rev: domain._rev,
+          id: domain._key,
+          domain: 'test.domain.gc.ca',
+          slug: 'test-domain-gc-ca',
+        }
+
+        await expect(
+          demoType.domain.resolve(
+            { domainId: domain._id },
+            {},
+            { loaders: { domainLoaderByKey: loader } },
+          ),
+        ).resolves.toEqual(expectedResult)
+      })
+    })
+    describe('testing the timestamp resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = spfType.getFields()
+
+        expect(
+          demoType.timestamp.resolve({ timestamp: '2020-10-02T12:43:39Z' }),
+        ).toEqual('2020-10-02T12:43:39Z')
+      })
+    })
+    describe('testing the lookups resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = spfType.getFields()
+
+        expect(demoType.lookups.resolve({ lookups: 1 })).toEqual(1)
+      })
+    })
+    describe('testing the record resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = spfType.getFields()
+
+        expect(demoType.record.resolve({ record: 'txtRecord' })).toEqual(
+          'txtRecord',
+        )
+      })
+    })
+    describe('testing the spfDefault resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = spfType.getFields()
+
+        expect(
+          demoType.spfDefault.resolve({ spfDefault: 'spfDefault' }),
+        ).toEqual('spfDefault')
+      })
+    })
+    describe('testing the guidanceTags resolver', () => {
+      it('returns the resolved value', async () => {
+        const demoType = spfType.getFields()
+
+        const loader = spfGuidanceTagConnectionsLoader(
+          query,
+          '1',
+          cleanseInput,
+          {},
+        )
+
+        const guidanceTags = ['spf1']
+
+        const expectedResult = {
+          edges: [
+            {
+              cursor: toGlobalId('guidanceTags', spfGT._key),
+              node: {
+                _id: spfGT._id,
+                _key: spfGT._key,
+                _rev: spfGT._rev,
+                id: spfGT._key,
+                guidance: 'Some Interesting Guidance',
+                refLinksGuide: [
                   {
-                    node: {
-                      id: toGlobalId('spf', spf._key),
-                      domain: {
-                        id: toGlobalId('domains', domain._key),
-                      },
-                      timestamp: new Date('2020-10-02T12:43:39.000Z'),
-                      lookups: 5,
-                      record: 'txtRecord',
-                      spfDefault: 'default',
-                      guidanceTags: {
-                        edges: [
-                          {
-                            node: {
-                              id: toGlobalId('guidanceTags', 'spf1'),
-                              tagId: 'spf1',
-                              tagName: 'SPF-TAG',
-                              guidance: 'Some Interesting Guidance',
-                              refLinks: [
-                                {
-                                  description: 'refLinksGuide Description',
-                                  refLink: 'www.refLinksGuide.ca',
-                                },
-                              ],
-                              refLinksTech: [
-                                {
-                                  description: 'refLinksTechnical Description',
-                                  refLink: 'www.refLinksTechnical.ca',
-                                },
-                              ],
-                            },
-                          },
-                        ],
-                        totalCount: 1,
-                        pageInfo: {
-                          hasNextPage: false,
-                          hasPreviousPage: false,
-                          startCursor: toGlobalId('guidanceTags', 'spf1'),
-                          endCursor: toGlobalId('guidanceTags', 'spf1'),
-                        },
-                      },
-                    },
+                    description: 'refLinksGuide Description',
+                    ref_link: 'www.refLinksGuide.ca',
                   },
                 ],
-                totalCount: 1,
-                pageInfo: {
-                  hasNextPage: false,
-                  hasPreviousPage: false,
-                  startCursor: toGlobalId('spf', spf._key),
-                  endCursor: toGlobalId('spf', spf._key),
-                },
+                refLinksTechnical: [
+                  {
+                    description: 'refLinksTechnical Description',
+                    ref_link: 'www.refLinksTechnical.ca',
+                  },
+                ],
+                tagId: 'spf1',
+                tagName: 'SPF-TAG',
               },
             },
+          ],
+          totalCount: 1,
+          pageInfo: {
+            hasNextPage: false,
+            hasPreviousPage: false,
+            startCursor: toGlobalId('guidanceTags', spfGT._key),
+            endCursor: toGlobalId('guidanceTags', spfGT._key),
           },
-        },
-      }
-      expect(response).toEqual(expectedResponse)
-      expect(consoleInfoOutput).toEqual([
-        `User ${user._key} successfully retrieved domain ${domain._key}.`,
-      ])
+        }
+
+        await expect(
+          demoType.guidanceTags.resolve(
+            { guidanceTags },
+            { first: 1 },
+            { loaders: { spfGuidanceTagConnectionsLoader: loader } },
+          ),
+        ).resolves.toEqual(expectedResult)
+      })
     })
   })
 })
