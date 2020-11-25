144fd52ae53fc636a0e254757578d7753ab00209
nsdeschenes <nsdeschenes@outlook.com>
2020-11-25 13:57:41 -0400

    category-totals tests refactored

--- api-js/src/types/base/dmarc-report/__tests__/category-totals.test.js
+++ api-js/src/types/base/dmarc-report/__tests__/category-totals.test.js
@@ -1,236 +1,62 @@
-const { ArangoTools, dbNameFromFile } = require('arango-tools')
-const { graphql, GraphQLSchema } = require('graphql')
-const { makeMigrations } = require('../../../../../migrations')
-const { createQuerySchema } = require('../../../../queries')
-const { createMutationSchema } = require('../../../../mutations')
-const bcrypt = require('bcrypt')
+const { GraphQLInt } = require('graphql')
+const { categoryTotalsType } = require('../category-totals')
 
-const { cleanseInput } = require('../../../../validators')
-const {
-  checkDomainPermission,
-  checkDomainOwnership,
-  tokenize,
-  userRequired,
-} = require('../../../../auth')
-const {
-  domainLoaderByDomain,
-  userLoaderByUserName,
-  userLoaderByKey,
-} = require('../../../../loaders')
-const { DB_PASS: rootPass, DB_URL: url } = process.env
+describe('testing the category totals gql object', () => {
+  describe('testing the field definitions', () => {
+    it('has a passSpfOnly field', () => {
+      const demoType = categoryTotalsType.getFields()
 
-describe('given findDomainByDomain query', () => {
-  let query, drop, truncate, migrate, schema, collections, domain, org
+      expect(demoType).toHaveProperty('passSpfOnly')
+      expect(demoType.passSpfOnly.type).toMatchObject(GraphQLInt)
+    })
+    it('has a passDkimOnly field', () => {
+      const demoType = categoryTotalsType.getFields()
 
-  beforeAll(async () => {
-    // Create GQL Schema
-    schema = new GraphQLSchema({
-      query: createQuerySchema(),
-      mutation: createMutationSchema(),
+      expect(demoType).toHaveProperty('passDkimOnly')
+      expect(demoType.passDkimOnly.type).toMatchObject(GraphQLInt)
+    })
+    it('has a fullPass field', () => {
+      const demoType = categoryTotalsType.getFields()
+
+      expect(demoType).toHaveProperty('fullPass')
+      expect(demoType.fullPass.type).toMatchObject(GraphQLInt)
+    })
+    it('has a fail field', () => {
+      const demoType = categoryTotalsType.getFields()
+
+      expect(demoType).toHaveProperty('fail')
+      expect(demoType.fail.type).toMatchObject(GraphQLInt)
     })
   })
 
-  let consoleOutput = []
-  const mockedInfo = (output) => consoleOutput.push(output)
-  const mockedWarn = (output) => consoleOutput.push(output)
-  const mockedError = (output) => consoleOutput.push(output)
+  describe('testing the field resolvers', () => {
+    describe('testing the passSpfOnly resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = categoryTotalsType.getFields()
 
-  beforeEach(async () => {
-    console.info = mockedInfo
-    console.warn = mockedWarn
-    console.error = mockedError
-    // Generate DB Items
-    ;({ migrate } = await ArangoTools({ rootPass, url }))
-    ;({ query, drop, truncate, collections } = await migrate(
-      makeMigrations({ databaseName: dbNameFromFile(__filename), rootPass }),
-    ))
-    await truncate()
-    await graphql(
-      schema,
-      `
-        mutation {
-          signUp(
-            input: {
-              displayName: "Test Account"
-              userName: "test.account@istio.actually.exists"
-              password: "testpassword123"
-              confirmPassword: "testpassword123"
-              preferredLang: FRENCH
-            }
-          ) {
-            authResult {
-              user {
-                id
-              }
-            }
-          }
-        }
-      `,
-      null,
-      {
-        query,
-        auth: {
-          bcrypt,
-          tokenize,
-        },
-        validators: {
-          cleanseInput,
-        },
-        loaders: {
-          userLoaderByUserName: userLoaderByUserName(query),
-        },
-      },
-    )
-    consoleOutput = []
-
-    org = await collections.organizations.save({
-      orgDetails: {
-        en: {
-          slug: 'treasury-board-secretariat',
-          acronym: 'TBS',
-          name: 'Treasury Board of Canada Secretariat',
-          zone: 'FED',
-          sector: 'TBS',
-          country: 'Canada',
-          province: 'Ontario',
-          city: 'Ottawa',
-        },
-        fr: {
-          slug: 'secretariat-conseil-tresor',
-          acronym: 'SCT',
-          name: 'Secrétariat du Conseil Trésor du Canada',
-          zone: 'FED',
-          sector: 'TBS',
-          country: 'Canada',
-          province: 'Ontario',
-          city: 'Ottawa',
-        },
-      },
-    })
-    domain = await collections.domains.save({
-      domain: 'test.gc.ca',
-      lastRan: null,
-      selectors: ['selector1._domainkey', 'selector2._domainkey'],
-    })
-    await collections.claims.save({
-      _to: domain._id,
-      _from: org._id,
-    })
-    await collections.ownership.save({
-      _to: domain._id,
-      _from: org._id,
-    })
-  })
-
-  afterEach(async () => {
-    await drop()
-  })
-
-  describe('find the dmarc report category totals for a given dmarc report period', () => {
-    let user
-    beforeEach(async () => {
-      const userCursor = await query`
-        FOR user IN users
-          FILTER user.userName == "test.account@istio.actually.exists"
-          RETURN user
-      `
-      user = await userCursor.next()
-      await collections.affiliations.save({
-        _from: org._id,
-        _to: user._id,
-        permission: 'user',
+        expect(demoType.passSpfOnly.resolve({ passSpfOnly: 5 })).toEqual(5)
       })
     })
+    describe('testing the passDkimOnly resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = categoryTotalsType.getFields()
 
-    afterEach(async () => {
-      await query`
-        LET userEdges = (FOR v, e IN 1..1 ANY ${org._id} affiliations RETURN { edgeKey: e._key, userKey: e._to })
-        LET removeUserEdges = (FOR userEdge IN userEdges REMOVE userEdge.edgeKey IN affiliations)
-        RETURN true
-      `
-      await query`
-        FOR affiliation IN affiliations
-          REMOVE affiliation IN affiliations
-      `
-    })
-    it('returns category totals', async () => {
-      const dmarcReportLoader = jest.fn().mockReturnValue({
-        data: {
-          dmarcSummaryByPeriod: {
-            categoryTotals: {
-              fail: 10,
-              fullPass: 53,
-              passDkimOnly: 123,
-              passSpfOnly: 135,
-            },
-          },
-        },
+        expect(demoType.passDkimOnly.resolve({ passDkimOnly: 5 })).toEqual(5)
       })
+    })
+    describe('testing the fullPass field', () => {
+      it('returns the resolved value', () => {
+        const demoType = categoryTotalsType.getFields()
 
-      const response = await graphql(
-        schema,
-        `
-          query {
-            findDomainByDomain(domain: "test.gc.ca") {
-              dmarcSummaryByPeriod(month: SEPTEMBER, year: "2020") {
-                categoryTotals {
-                  fail
-                  fullPass
-                  passDkimOnly
-                  passSpfOnly
-                }
-              }
-            }
-          }
-        `,
-        null,
-        {
-          userKey: user._key,
-          query: query,
-          auth: {
-            checkDomainPermission: checkDomainPermission({
-              query,
-              userKey: user._key,
-            }),
-            checkDomainOwnership: checkDomainOwnership({
-              query,
-              userKey: user._key,
-            }),
-            tokenize,
-            userRequired: userRequired({
-              userKey: user._key,
-              userLoaderByKey: userLoaderByKey(query),
-            }),
-          },
-          validators: {
-            cleanseInput,
-          },
-          loaders: {
-            domainLoaderByDomain: domainLoaderByDomain(query),
-            dmarcReportLoader,
-            userLoaderByKey: userLoaderByKey(query),
-          },
-        },
-      )
+        expect(demoType.fullPass.resolve({ fullPass: 5 })).toEqual(5)
+      })
+    })
+    describe('testing the fail field', () => {
+      it('returns the resolved value', () => {
+        const demoType = categoryTotalsType.getFields()
 
-      const expectedResponse = {
-        data: {
-          findDomainByDomain: {
-            dmarcSummaryByPeriod: {
-              categoryTotals: {
-                fail: 10,
-                fullPass: 53,
-                passDkimOnly: 123,
-                passSpfOnly: 135,
-              },
-            },
-          },
-        },
-      }
-      expect(response).toEqual(expectedResponse)
-      expect(consoleOutput).toEqual([
-        `User ${user._key} successfully retrieved domain ${domain._key}.`,
-      ])
+        expect(demoType.fail.resolve({ fail: 5 })).toEqual(5)
+      })
     })
   })
 })
