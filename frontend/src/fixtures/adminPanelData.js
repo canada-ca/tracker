be7250dfd2a294bf06e863a396ae672959229686
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2020-11-25 13:56:33 -0400

    Fix AdminPanel test

--- frontend/src/fixtures/adminPanelData.js
+++ frontend/src/fixtures/adminPanelData.js
@@ -1,23 +1,24 @@
 export const rawAdminPanelData = {
   data: {
     findOrganizationBySlug: {
-      id: 'NTc5MjU2MjY4',
+      id: 'NDY4MTc2NjcxMA==',
+      name: 'Buckridge Inc',
       domains: {
         edges: [
           {
             node: {
-              id: 'NjgxNjk3MTc5Nw==',
-              domain: 'rcmp-grc.gc.ca',
-              lastRan: '2020-11-19T18:11:20Z',
+              id: 'NjkzMTM1NjAwNQ==',
+              domain: 'tbs-sct.gc.ca',
+              lastRan: '2020-11-25T13:45:31Z',
               __typename: 'Domain',
             },
             __typename: 'DomainEdge',
           },
           {
             node: {
-              id: 'MTI0MjEyNjQ1Ng==',
-              domain: "jAva&Tab;script:alert('XSS')",
-              lastRan: '2020-11-19T11:22:51Z',
+              id: 'NDYwNjMxNzg2MQ==',
+              domain: 'cse-cst.gc.ca',
+              lastRan: '2020-11-25T06:50:01Z',
               __typename: 'Domain',
             },
             __typename: 'DomainEdge',
@@ -25,10 +26,50 @@ export const rawAdminPanelData = {
         ],
         pageInfo: {
           endCursor: 'string',
-          hasNextPage: false,
+          hasNextPage: true,
           __typename: 'PageInfo',
         },
+        totalCount: 14,
         __typename: 'DomainConnection',
+      },
+      affiliations: {
+        edges: [
+          {
+            node: {
+              userId: 'NDUxNzAzMDY4NQ==',
+              permission: 'SUPER_ADMIN',
+              user: {
+                userName: 'Winnifred_Legros19@yahoo.com',
+                displayName: 'Fredy',
+                tfaValidated: false,
+                __typename: 'User',
+              },
+              __typename: 'UserAffiliations',
+            },
+            __typename: 'UserAffiliationsEdge',
+          },
+          {
+            node: {
+              userId: 'NzkzMDM3ODg5NQ==',
+              permission: 'USER',
+              user: {
+                userName: 'Lonny46@hotmail.com',
+                displayName: 'Addie',
+                tfaValidated: true,
+                __typename: 'User',
+              },
+              __typename: 'UserAffiliations',
+            },
+            __typename: 'UserAffiliationsEdge',
+          },
+        ],
+        pageInfo: {
+          endCursor: 'string',
+          hasNextPage: true,
+          __typename: 'PageInfo',
+        },
+        totalCount: 1,
+        __typename: 'UserAffiliationsConnection',
       },
       __typename: 'Organization',
     },
