a6bd88640182017dbb6f2153ca9cae3f702ea1b5
nsdeschenes <nsdeschenes@outlook.com>
2020-11-25 13:08:10 -0400

    refactor full pass table tests

--- api-js/src/types/base/dmarc-report/detail-tables/__tests__/full-pass-table.test.js
+++ api-js/src/types/base/dmarc-report/detail-tables/__tests__/full-pass-table.test.js
@@ -1,294 +1,146 @@
-const { ArangoTools, dbNameFromFile } = require('arango-tools')
-const { graphql, GraphQLSchema } = require('graphql')
-const { makeMigrations } = require('../../../../../../migrations')
-const { createQuerySchema } = require('../../../../../queries')
-const { createMutationSchema } = require('../../../../../mutations')
-const bcrypt = require('bcrypt')
+const { GraphQLID, GraphQLInt, GraphQLString } = require('graphql')
 
-const { cleanseInput } = require('../../../../../validators')
-const {
-  checkDomainPermission,
-  checkDomainOwnership,
-  tokenize,
-  userRequired,
-} = require('../../../../../auth')
-const {
-  domainLoaderByDomain,
-  userLoaderByUserName,
-  userLoaderByKey,
-} = require('../../../../../loaders')
-const { DB_PASS: rootPass, DB_URL: url } = process.env
+const { fullPassTableType } = require('../full-pass-table')
 
-describe('given findDomainByDomain query', () => {
-  let query, drop, truncate, migrate, schema, collections, domain, org
+describe('given the fullPassTable gql object', () => {
+  describe('testing the field definitions', () => {
+    it('has an id field', () => {
+      const demoType = fullPassTableType.getFields()
 
-  beforeAll(async () => {
-    // Create GQL Schema
-    schema = new GraphQLSchema({
-      query: createQuerySchema(),
-      mutation: createMutationSchema(),
+      expect(demoType).toHaveProperty('id')
+      expect(demoType.id.type).toMatchObject(GraphQLID)
+    })
+    it('has a dkimDomains field', () => {
+      const demoType = fullPassTableType.getFields()
+
+      expect(demoType).toHaveProperty('dkimDomains')
+      expect(demoType.dkimDomains.type).toMatchObject(GraphQLString)
+    })
+    it('has a dkimSelectors field', () => {
+      const demoType = fullPassTableType.getFields()
+
+      expect(demoType).toHaveProperty('dkimSelectors')
+      expect(demoType.dkimSelectors.type).toMatchObject(GraphQLString)
+    })
+    it('has a dnsHost field', () => {
+      const demoType = fullPassTableType.getFields()
+
+      expect(demoType).toHaveProperty('dnsHost')
+      expect(demoType.dnsHost.type).toMatchObject(GraphQLString)
+    })
+    it('has an envelopeFrom field', () => {
+      const demoType = fullPassTableType.getFields()
+
+      expect(demoType).toHaveProperty('envelopeFrom')
+      expect(demoType.envelopeFrom.type).toMatchObject(GraphQLString)
+    })
+    it('has a headerFrom field', () => {
+      const demoType = fullPassTableType.getFields()
+
+      expect(demoType).toHaveProperty('headerFrom')
+      expect(demoType.headerFrom.type).toMatchObject(GraphQLString)
+    })
+    it('has a sourceIpAddress field', () => {
+      const demoType = fullPassTableType.getFields()
+
+      expect(demoType).toHaveProperty('sourceIpAddress')
+      expect(demoType.sourceIpAddress.type).toMatchObject(GraphQLString)
+    })
+    it('has a spfDomains field', () => {
+      const demoType = fullPassTableType.getFields()
+
+      expect(demoType).toHaveProperty('spfDomains')
+      expect(demoType.spfDomains.type).toMatchObject(GraphQLString)
+    })
+    it('has a totalMessages field', () => {
+      const demoType = fullPassTableType.getFields()
+
+      expect(demoType).toHaveProperty('totalMessages')
+      expect(demoType.totalMessages.type).toMatchObject(GraphQLInt)
     })
   })
 
-  let consoleOutput = []
-  const mockedInfo = (output) => consoleOutput.push(output)
-  const mockedWarn = (output) => consoleOutput.push(output)
-  const mockedError = (output) => consoleOutput.push(output)
+  describe('testing the field resolvers', () => {
+    describe('testing the id resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = fullPassTableType.getFields()
 
-  beforeEach(async () => {
-    console.info = mockedInfo
-    console.warn = mockedWarn
-    console.error = mockedError
-    // Generate DB Items
-    ;({ migrate } = await ArangoTools({ rootPass, url }))
-    ;({ query, drop, truncate, collections } = await migrate(
-      makeMigrations({ databaseName: dbNameFromFile(__filename), rootPass }),
-    ))
-    await truncate()
-    await graphql(
-      schema,
-      `
-        mutation {
-          signUp(
-            input: {
-              displayName: "Test Account"
-              userName: "test.account@istio.actually.exists"
-              password: "testpassword123"
-              confirmPassword: "testpassword123"
-              preferredLang: FRENCH
-            }
-          ) {
-            authResult {
-              user {
-                id
-              }
-            }
-          }
-        }
-      `,
-      null,
-      {
-        query,
-        auth: {
-          bcrypt,
-          tokenize,
-        },
-        validators: {
-          cleanseInput,
-        },
-        loaders: {
-          userLoaderByUserName: userLoaderByUserName(query),
-        },
-      },
-    )
-    consoleOutput = []
-
-    org = await collections.organizations.save({
-      orgDetails: {
-        en: {
-          slug: 'treasury-board-secretariat',
-          acronym: 'TBS',
-          name: 'Treasury Board of Canada Secretariat',
-          zone: 'FED',
-          sector: 'TBS',
-          country: 'Canada',
-          province: 'Ontario',
-          city: 'Ottawa',
-        },
-        fr: {
-          slug: 'secretariat-conseil-tresor',
-          acronym: 'SCT',
-          name: 'Secrétariat du Conseil Trésor du Canada',
-          zone: 'FED',
-          sector: 'TBS',
-          country: 'Canada',
-          province: 'Ontario',
-          city: 'Ottawa',
-        },
-      },
-    })
-    domain = await collections.domains.save({
-      domain: 'test.gc.ca',
-      lastRan: null,
-      selectors: ['selector1._domainkey', 'selector2._domainkey'],
-    })
-    await collections.claims.save({
-      _to: domain._id,
-      _from: org._id,
-    })
-    await collections.ownership.save({
-      _to: domain._id,
-      _from: org._id,
-    })
-  })
-
-  afterEach(async () => {
-    await drop()
-  })
-
-  describe('find the dmarc report full pass information', () => {
-    let user
-    beforeEach(async () => {
-      const userCursor = await query`
-        FOR user IN users
-          FILTER user.userName == "test.account@istio.actually.exists"
-          RETURN user
-      `
-      user = await userCursor.next()
-      await collections.affiliations.save({
-        _from: org._id,
-        _to: user._id,
-        permission: 'user',
+        expect(demoType.id.resolve({ id: '1' })).toEqual('1')
       })
     })
+    describe('testing the dkimDomains resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = fullPassTableType.getFields()
 
-    afterEach(async () => {
-      await query`
-        LET userEdges = (FOR v, e IN 1..1 ANY ${org._id} affiliations RETURN { edgeKey: e._key, userKey: e._to })
-        LET removeUserEdges = (FOR userEdge IN userEdges REMOVE userEdge.edgeKey IN affiliations)
-        RETURN true
-      `
-      await query`
-        FOR affiliation IN affiliations
-          REMOVE affiliation IN affiliations
-      `
-    })
-    it('returns full pass data', async () => {
-      const dmarcReportLoader = jest.fn().mockReturnValue({
-        data: {
-          dmarcSummaryByPeriod: {
-            detailTables: {
-              fullPass: {
-                edges: [
-                  {
-                    cursor: 'ZnVsbFBhc3M6MQ==',
-                    node: {
-                      dkimDomains: 'test.gc.ca',
-                      dkimSelectors: 'selector1',
-                      dnsHost: 'test.canada.ca',
-                      envelopeFrom: 'test.gc.ca',
-                      headerFrom: 'test.gc.ca',
-                      id: 'ZnVsbFBhc3M6MQ==',
-                      sourceIpAddress: '123.456.78.91',
-                      spfDomains: 'test.gc.ca',
-                      totalMessages: 50,
-                    },
-                  },
-                ],
-                pageInfo: {
-                  startCursor: 'ZnVsbFBhc3M6MQ==',
-                  endCursor: 'ZnVsbFBhc3M6MQ==',
-                  hasNextPage: true,
-                  hasPreviousPage: false,
-                },
-              },
-            },
-          },
-        },
+        expect(
+          demoType.dkimDomains.resolve({ dkimDomains: 'dkimDomains' }),
+        ).toEqual('dkimDomains')
       })
+    })
+    describe('testing the dkimSelectors resolvers', () => {
+      it('returns the resolved value', () => {
+        const demoType = fullPassTableType.getFields()
 
-      const response = await graphql(
-        schema,
-        `
-          query {
-            findDomainByDomain(domain: "test.gc.ca") {
-              dmarcSummaryByPeriod(month: SEPTEMBER, year: "2020") {
-                detailTables {
-                  fullPass(first: 1) {
-                    edges {
-                      cursor
-                      node {
-                        id
-                        dkimDomains
-                        dkimSelectors
-                        dnsHost
-                        envelopeFrom
-                        headerFrom
-                        sourceIpAddress
-                        spfDomains
-                        totalMessages
-                      }
-                    }
-                    pageInfo {
-                      startCursor
-                      endCursor
-                      hasNextPage
-                      hasPreviousPage
-                    }
-                  }
-                }
-              }
-            }
-          }
-        `,
-        null,
-        {
-          userKey: user._key,
-          query: query,
-          auth: {
-            checkDomainPermission: checkDomainPermission({
-              query,
-              userKey: user._key,
-            }),
-            checkDomainOwnership: checkDomainOwnership({
-              query,
-              userKey: user._key,
-            }),
-            tokenize,
-            userRequired: userRequired({
-              userKey: user._key,
-              userLoaderByKey: userLoaderByKey(query),
-            }),
-          },
-          validators: {
-            cleanseInput,
-          },
-          loaders: {
-            domainLoaderByDomain: domainLoaderByDomain(query),
-            dmarcReportLoader,
-            userLoaderByKey: userLoaderByKey(query),
-          },
-        },
-      )
+        expect(
+          demoType.dkimSelectors.resolve({ dkimSelectors: 'dkimSelectors' }),
+        ).toEqual('dkimSelectors')
+      })
+    })
+    describe('testing the dnsHost resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = fullPassTableType.getFields()
 
-      const expectedResponse = {
-        data: {
-          findDomainByDomain: {
-            dmarcSummaryByPeriod: {
-              detailTables: {
-                fullPass: {
-                  edges: [
-                    {
-                      cursor: 'ZnVsbFBhc3M6MQ==',
-                      node: {
-                        dkimDomains: 'test.gc.ca',
-                        dkimSelectors: 'selector1',
-                        dnsHost: 'test.canada.ca',
-                        envelopeFrom: 'test.gc.ca',
-                        headerFrom: 'test.gc.ca',
-                        id: 'ZnVsbFBhc3M6MQ==',
-                        sourceIpAddress: '123.456.78.91',
-                        spfDomains: 'test.gc.ca',
-                        totalMessages: 50,
-                      },
-                    },
-                  ],
-                  pageInfo: {
-                    startCursor: 'ZnVsbFBhc3M6MQ==',
-                    endCursor: 'ZnVsbFBhc3M6MQ==',
-                    hasNextPage: true,
-                    hasPreviousPage: false,
-                  },
-                },
-              },
-            },
-          },
-        },
-      }
-      expect(response).toEqual(expectedResponse)
-      expect(consoleOutput).toEqual([
-        `User ${user._key} successfully retrieved domain ${domain._key}.`,
-      ])
+        expect(demoType.dnsHost.resolve({ dnsHost: 'dnsHost' })).toEqual(
+          'dnsHost',
+        )
+      })
+    })
+    describe('testing the envelopeFrom resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = fullPassTableType.getFields()
+
+        expect(
+          demoType.envelopeFrom.resolve({ envelopeFrom: 'envelopeFrom' }),
+        ).toEqual('envelopeFrom')
+      })
+    })
+    describe('testing the headerFrom resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = fullPassTableType.getFields()
+
+        expect(
+          demoType.headerFrom.resolve({ headerFrom: 'headerFrom' }),
+        ).toEqual('headerFrom')
+      })
+    })
+    describe('testing the sourceIpAddress resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = fullPassTableType.getFields()
+
+        expect(
+          demoType.sourceIpAddress.resolve({
+            sourceIpAddress: 'sourceIpAddress',
+          }),
+        ).toEqual('sourceIpAddress')
+      })
+    })
+    describe('testing the spfDomains resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = fullPassTableType.getFields()
+
+        expect(
+          demoType.spfDomains.resolve({ spfDomains: 'spfDomains' }),
+        ).toEqual('spfDomains')
+      })
+    })
+    describe('testing the totalMessages resolver', () => {
+      it('returns the resolved value', () => {
+        const demoType = fullPassTableType.getFields()
+
+        expect(demoType.totalMessages.resolve({ totalMessages: 10 })).toEqual(
+          10,
+        )
+      })
     })
   })
 })
