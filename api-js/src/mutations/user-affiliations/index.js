1f1ef5e4766eff3c9a04de707192a93187178d70
nsdeschenes <nsdeschenes@outlook.com>
2020-11-27 06:57:36 -0400

    impory export new mutation

--- api-js/src/mutations/user-affiliations/index.js
+++ api-js/src/mutations/user-affiliations/index.js
@@ -1,7 +1,9 @@
 const { inviteUserToOrg } = require('./invite-user-to-org')
+const { removeUserFromOrg } = require('./remove-user-from-org')
 const { updateUserRole } = require('./update-user-role')
 
 module.exports = {
   inviteUserToOrg,
+  removeUserFromOrg,
   updateUserRole,
 }
