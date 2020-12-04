54050bed72eb47f3b44a373fc34b51b0c7c3e3ad
nsdeschenes <nc575894@dal.ca>
2020-12-04 09:00:39 -0400

    Refactor To CheckDomainPermission Function (#1302)
    * Add super admin checks
    
    * update translations

--- api-js/src/locale/en/messages.js
+++ api-js/src/locale/en/messages.js
@@ -78,6 +78,8 @@
     'Password was successfully reset.': 'Password was successfully reset.',
     'Password was successfully updated.': 'Password was successfully updated.',
     'Passwords do not match.': 'Passwords do not match.',
+    'Permission check error. Unable to request domain information.':
+      'Permission check error. Unable to request domain information.',
     'Permission error, not an admin for this user.':
       'Permission error, not an admin for this user.',
     'Profile successfully updated.': 'Profile successfully updated.',
