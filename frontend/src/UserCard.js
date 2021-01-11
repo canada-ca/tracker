963bd314b8e2c33a78f742e5b7d579f7a6231b5a
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-01-11 14:17:59 -0400

    Remove tfa badge from user card

--- frontend/src/UserCard.js
+++ frontend/src/UserCard.js
@@ -1,25 +1,14 @@
 import React from 'react'
 import { Badge, Box, Text, PseudoBox, Stack } from '@chakra-ui/core'
-import { Trans } from '@lingui/macro'
-import { string, oneOf } from 'prop-types'
+import { string } from 'prop-types'
 
-export function UserCard({ userName, tfaSendMethod, role }) {
-  const validationText =
-    tfaSendMethod === 'phone' ? (
-      <Trans>Phone 2FA</Trans>
-    ) : tfaSendMethod === 'email' ? (
-      <Trans>Email 2FA</Trans>
-    ) : (
-      <Trans>2FA not enabled</Trans>
-    )
-
+export function UserCard({ userName, role }) {
   return (
     <PseudoBox width="100%" p="8">
       <Stack isInline align="center" mb={['1', '0']}>
         <Box>
           <Text fontSize="md">{userName}</Text>
         </Box>
-        <Box ml="auto" /> {/* spacer */}
         {role && (
           <Box>
             <Badge
@@ -27,19 +16,12 @@ export function UserCard({ userName, tfaSendMethod, role }) {
               bg="transparent"
               borderColor="primary"
               borderWidth="1px"
+              ml="auto"
             >
               {role}
             </Badge>
           </Box>
         )}
-        <Box>
-          <Badge
-            variant="solid"
-            variantColor={tfaSendMethod !== null ? 'green' : 'red'}
-          >
-            {validationText}
-          </Badge>
-        </Box>
       </Stack>
     </PseudoBox>
   )
@@ -48,5 +30,4 @@ export function UserCard({ userName, tfaSendMethod, role }) {
 UserCard.propTypes = {
   userName: string.isRequired,
   role: string,
-  tfaSendMethod: oneOf(['phone', 'email']),
 }
