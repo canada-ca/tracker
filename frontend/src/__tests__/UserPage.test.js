893fc2ebe9d505b2b7bbbc48c92e2704b20aa699
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-01-11 15:04:02 -0400

    Fix UserPage.test.js

--- frontend/src/__tests__/UserPage.test.js
+++ frontend/src/__tests__/UserPage.test.js
@@ -34,8 +34,9 @@ describe('<UserPage />', () => {
             userName: userName,
             displayName: 'Test User',
             preferredLang: 'ENGLISH',
-            tfaValidated: false,
-            emailValidated: false,
+            tfaSendMethod: 'PHONE',
+            phoneValidated: true,
+            emailValidated: true,
           },
         },
       },
@@ -45,7 +46,7 @@ describe('<UserPage />', () => {
   it('renders without error', async () => {
     const { queryByText } = render(
       <UserStateProvider
-        initialState={{ userName, jwt: 'string', tfaSendMethod: false }}
+        initialState={{ userName, jwt: 'string', tfaSendMethod: null }}
       >
         <MockedProvider mocks={mocks} addTypename={false}>
           <MemoryRouter initialEntries={['/']}>
