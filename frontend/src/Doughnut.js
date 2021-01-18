73096a1ba9b80886587c6df65cf57994e146764d
Thomas Nickerson <>
2021-01-18 17:05:42 -0400

    Add first attempt at no scan data message for org pages

--- frontend/src/Doughnut.js
+++ frontend/src/Doughnut.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { object, arrayOf, number, string, func } from 'prop-types'
-import { Box } from '@chakra-ui/core'
+import { Box, Image, Text } from '@chakra-ui/core'
 import { scaleOrdinal } from 'd3'
 import { useArcs } from './useArcs'
 import { ZigZag, CrossHatch, Dots, Stripes } from './patterns'
+import trackerLogo from './images/tracker_v-03.png'
 
 export const Doughnut = ({
   data,
@@ -34,26 +35,46 @@ export const Doughnut = ({
     `url(#zigzag)`,
   ])
 
+  let chartContent;
+  if (data[0].total) { // check if there are scans (why does if(data.total) not work?)
+    chartContent = 
+    <svg height={height} width={width}>
+      <title>{title}</title>
+      <defs>
+        <ZigZag width={0.4} background="#F16D22" color="#fff" />
+        <Dots size={1} background="#B93B26" color="#fff" />
+        <Stripes angle={45} background="#F8991F" color="#fff" />
+        <CrossHatch width={0.8} background="#F16D22" color="#fff" />
+      </defs>
+      <g transform={`translate(${width / 2},${height / 2})`}>
+        {arcs.map((arc, index) => {
+          return children(
+            { d: arc.d, fill: patterns(index / data.length - 1) },
+            index,
+          )
+        })}
+      </g>
+    </svg>;
+  } else {
+    chartContent = 
+    <Box>
+      <Image
+          src={trackerLogo}
+          alt={'Tracker Logo'}
+        />
+      <Text
+          fontSize="l"
+          textAlign="center"
+          color="black"
+        >
+          No scan data for this organization.
+        </Text>
+      </Box>;
+  }
+
   return (
     <div {...rest}>
-      <svg height={height} width={width}>
-        <title>{title}</title>
-        <defs>
-          <ZigZag width={0.4} background="#F16D22" color="#fff" />
-          <Dots size={1} background="#B93B26" color="#fff" />
-          <Stripes angle={45} background="#F8991F" color="#fff" />
-          <CrossHatch width={0.8} background="#F16D22" color="#fff" />
-        </defs>
-        <g transform={`translate(${width / 2},${height / 2})`}>
-          {arcs.map((arc, index) => {
-            return children(
-              { d: arc.d, fill: patterns(index / data.length - 1) },
-              index,
-            )
-          })}
-        </g>
-      </svg>
-
+      {chartContent}
       {arcs.map(({ title, count, percentage }, index) => {
         if (percentage % 1 >= 0.5) {
           percentage = Math.ceil(percentage)
@@ -74,6 +95,12 @@ export const Doughnut = ({
               width={30}
               style={{ display: 'inline', marginRight: '1em' }}
             >
+            <defs>
+              <ZigZag width={0.4} background="#F16D22" color="#fff" />
+              <Dots size={1} background="#B93B26" color="#fff" />
+              <Stripes angle={45} background="#F8991F" color="#fff" />
+              <CrossHatch width={0.8} background="#F16D22" color="#fff" />
+              </defs>
               <g>
                 <rect
                   stroke="#fff"
