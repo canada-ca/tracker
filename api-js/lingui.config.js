5e29f637b7b0d30c7a5ac4bc1ab77fe254cc5b44
nsdeschenes <nsdeschenes@outlook.com>
2021-01-18 18:37:27 -0400

    update config

--- api-js/lingui.config.js
+++ api-js/lingui.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  catalogs: [{
-    'path': '<rootDir>/locale/{locale}/messages',
-    'include': ['<rootDir>'],
-    'exclude': ['**/node_modules/**'],
-  }],
+  catalogs: [
+    {
+      path: '<rootDir>/locale/{locale}/messages',
+      include: ['<rootDir>'],
+      exclude: ['**/node_modules/**'],
+    },
+  ],
   extractBabelOptions: {},
   fallbackLocales: {
     default: 'en',
   },
   format: 'po',
-  locales: [
-    'en',
-    'fr',
-  ],
+  locales: ['en', 'fr'],
   rootDir: './src',
   sourceLocale: 'en',
 }
