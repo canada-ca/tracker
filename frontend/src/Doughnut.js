dd91efdd782f5b805fc377681f9d4ea49a16a9d8
Thomas <64759920+ThomasNickerson@users.noreply.github.com>
2021-01-19 13:28:54 -0400

    Fe add message when no scan data for org (#1481)
    * Add first attempt at no scan data message for org pages
    
    * run prettier on last commit (whoops)
    
    * Extract chart contents JSX from if statement to variables
    
    * Add French translation of new message
    
    * update Doughnut.test.js for Lingui use in Doughnut.js
    
    * Add total attribute as in real data to test data in Doughnut.test.js
    
    * Unbreak localization files
    
    * Fix translations for DMARC strings on summary cards
    
    * re-run lingui extract with updated version of package
    
    Co-authored-by: Thomas Nickerson <>

--- frontend/src/Doughnut.js
+++ frontend/src/Doughnut.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { object, arrayOf, number, string, func } from 'prop-types'
-import { Box } from '@chakra-ui/core'
+import { Box, Image, Text } from '@chakra-ui/core'
 import { scaleOrdinal } from 'd3'
+import { Trans } from "@lingui/macro"
 import { useArcs } from './useArcs'
 import { ZigZag, CrossHatch, Dots, Stripes } from './patterns'
+import trackerLogo from './images/tracker_v-03.png'
 
 export const Doughnut = ({
   data,
@@ -34,26 +36,45 @@ export const Doughnut = ({
     `url(#zigzag)`,
   ])
 
+  const doughnutChart = (
+    <svg height={height} width={width}>
+      <title>{title}</title>
+      <defs>
+        <ZigZag width={0.4} background="#F16D22" color="#fff" />
+        <Dots size={1} background="#B93B26" color="#fff" />
+        <Stripes angle={45} background="#F8991F" color="#fff" />
+        <CrossHatch width={0.8} background="#F16D22" color="#fff" />
+      </defs>
+      <g transform={`translate(${width / 2},${height / 2})`}>
+        {arcs.map((arc, index) => {
+          return children(
+            { d: arc.d, fill: patterns(index / data.length - 1) },
+            index,
+          )
+        })}
+      </g>
+    </svg>
+  )
+
+  const noScanMessage = (
+    <Box>
+      <Image src={trackerLogo} alt={'Tracker Logo'} />
+      <Text fontSize="l" textAlign="center" color="black">
+        <Trans>No scan data for this organization.</Trans>
+      </Text>
+    </Box>
+  )
+
+  let chartContent
+  if (data[0].total) {
+    chartContent = doughnutChart
+  } else {
+    chartContent = noScanMessage
+  }
+
   return (
     <div {...rest}>
-      <svg height={height} width={width}>
-        <title>{title}</title>
-        <defs>
-          <ZigZag width={0.4} background="#F16D22" color="#fff" />
-          <Dots size={1} background="#B93B26" color="#fff" />
-          <Stripes angle={45} background="#F8991F" color="#fff" />
-          <CrossHatch width={0.8} background="#F16D22" color="#fff" />
-        </defs>
-        <g transform={`translate(${width / 2},${height / 2})`}>
-          {arcs.map((arc, index) => {
-            return children(
-              { d: arc.d, fill: patterns(index / data.length - 1) },
-              index,
-            )
-          })}
-        </g>
-      </svg>
-
+      {chartContent}
       {arcs.map(({ title, count, percentage }, index) => {
         if (percentage % 1 >= 0.5) {
           percentage = Math.ceil(percentage)
@@ -74,6 +95,12 @@ export const Doughnut = ({
               width={30}
               style={{ display: 'inline', marginRight: '1em' }}
             >
+              <defs>
+                <ZigZag width={0.4} background="#F16D22" color="#fff" />
+                <Dots size={1} background="#B93B26" color="#fff" />
+                <Stripes angle={45} background="#F8991F" color="#fff" />
+                <CrossHatch width={0.8} background="#F16D22" color="#fff" />
+              </defs>
               <g>
                 <rect
                   stroke="#fff"
--- frontend/src/__tests__/Doughnut.test.js
+++ frontend/src/__tests__/Doughnut.test.js
@@ -1,32 +1,46 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Doughnut, Segment } from '../Doughnut'
+import { I18nProvider } from '@lingui/react'
+import { setupI18n } from '@lingui/core'
+
+const i18n = setupI18n({
+  locale: 'en',
+  messages: {
+    en: {},
+  },
+  localeData: {
+    en: {},
+  },
+})
 
 describe('<Doughnut/>', () => {
   const data = [
-    { count: 1, title: 'good', percentage: 1 },
-    { count: 1, title: 'bad', percentage: 1 },
-    { count: 1, title: 'ugly', percentage: 1 },
+    { count: 1, title: 'good', percentage: 1, total: 3 },
+    { count: 1, title: 'bad', percentage: 1, total: 3 },
+    { count: 1, title: 'ugly', percentage: 1, total: 3 },
   ]
   it('renders segments for the data provided', () => {
     const { queryAllByTestId } = render(
-      <Doughnut
-        title="doughnut chart!"
-        data={data}
-        color="#2e2e40"
-        height={320}
-        width={320}
-        valueAccessor={(d) => d.count}
-        data-testid="doughnut"
-      >
-        {(segmentProps, index) => (
-          <Segment
-            data-testid="segment"
-            key={`segment:${index}`}
-            {...segmentProps}
-          />
-        )}
-      </Doughnut>,
+      <I18nProvider i18n={i18n}>
+        <Doughnut
+          title="doughnut chart!"
+          data={data}
+          color="#2e2e40"
+          height={320}
+          width={320}
+          valueAccessor={(d) => d.count}
+          data-testid="doughnut"
+        >
+          {(segmentProps, index) => (
+            <Segment
+              data-testid="segment"
+              key={`segment:${index}`}
+              {...segmentProps}
+            />
+          )}
+        </Doughnut>
+      </I18nProvider>,
     )
 
     const segments = queryAllByTestId('segment')
