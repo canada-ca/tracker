ad64829431c867cecf04e92ee0cd85e860564fee
Thomas Nickerson <>
2021-01-19 10:42:39 -0400

    update Doughnut.test.js for Lingui use in Doughnut.js

--- frontend/src/__tests__/Doughnut.test.js
+++ frontend/src/__tests__/Doughnut.test.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Doughnut, Segment } from '../Doughnut'
+import { I18nProvider } from '@lingui/react'
+import { setupI18n } from '@lingui/core'
+
+const i18n = setupI18n({
+  locale: 'en',
+  messages: {
+    en: {},
+  },
+  localeData: {
+    en: {},
+  },
+})
 
 describe('<Doughnut/>', () => {
   const data = [
@@ -10,23 +22,25 @@ describe('<Doughnut/>', () => {
   ]
   it('renders segments for the data provided', () => {
     const { queryAllByTestId } = render(
-      <Doughnut
-        title="doughnut chart!"
-        data={data}
-        color="#2e2e40"
-        height={320}
-        width={320}
-        valueAccessor={(d) => d.count}
-        data-testid="doughnut"
-      >
-        {(segmentProps, index) => (
-          <Segment
-            data-testid="segment"
-            key={`segment:${index}`}
-            {...segmentProps}
-          />
-        )}
-      </Doughnut>,
+      <I18nProvider i18n={i18n}>
+        <Doughnut
+          title="doughnut chart!"
+          data={data}
+          color="#2e2e40"
+          height={320}
+          width={320}
+          valueAccessor={(d) => d.count}
+          data-testid="doughnut"
+        >
+          {(segmentProps, index) => (
+            <Segment
+              data-testid="segment"
+              key={`segment:${index}`}
+              {...segmentProps}
+            />
+          )}
+        </Doughnut>
+      </I18nProvider>,
     )
 
     const segments = queryAllByTestId('segment')
