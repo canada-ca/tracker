137f0f172c53285adea27d407aa0231fee2f8c15
nsdeschenes <nsdeschenes@outlook.com>
2021-01-21 08:06:10 -0400

    add array equals to exports

--- services/domain-ownership/src/index.js
+++ services/domain-ownership/src/index.js
@@ -1,7 +1,9 @@
 const database = require('./database')
 const loaders = require('./loaders')
+const { arrayEquals } = require('./array-equals')
 
 module.exports = {
   ...database,
   ...loaders,
+  arrayEquals,
 }
