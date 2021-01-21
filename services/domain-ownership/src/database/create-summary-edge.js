3cc62481be42703bada120cd512944286db6d249
nsdeschenes <nsdeschenes@outlook.com>
2021-01-21 09:15:38 -0400

    create summary edge completed and tested

--- services/domain-ownership/src/database/create-summary-edge.js
+++ services/domain-ownership/src/database/create-summary-edge.js
@@ -2,14 +2,12 @@ const createSummaryEdge = (collections) => async ({
   domainId,
   summaryId,
   startDate,
-  thirtyDays,
 }) => {
   try {
     await collections.domainsToDmarcSummaries.save({
       _from: domainId,
       _to: summaryId,
       startDate: startDate,
-      thirtyDays: thirtyDays,
     })
   } catch (err) {
     throw new Error(err)
