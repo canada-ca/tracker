588986f0869bdd79b332f557a61c1a9826e36ef7
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-01-22 16:08:49 -0400

    Fix error when hovering over dmarc graph bar

--- frontend/src/CustomTooltipContent.js
+++ frontend/src/CustomTooltipContent.js
@@ -8,39 +8,46 @@ const CustomTooltipContent = ({
   formatter,
   calculatePercentages,
 }) => {
-  const totalValue = payload.reduce((acc, currentValue) => {
-    return currentValue.payload[currentValue.dataKey] + acc
-  }, 0)
+  const totalValue =
+    payload !== undefined
+      ? payload.reduce((acc, currentValue) => {
+          return currentValue.payload[currentValue.dataKey] + acc
+        }, 0)
+      : 1
 
   const listItems = []
-  payload.forEach((entry) => {
-    if (entry !== undefined) {
-      listItems.push(
-        <Text
-          key={`${entry.dataKey}-name`}
-          color={entry.color}
-        >{`${entry.name} :`}</Text>,
-      )
-      listItems.push(
-        <Text
-          mx="1rem"
-          textAlign="right"
-          key={`${entry.dataKey}-value`}
-          color={entry.color}
-        >{`${formatter(entry.payload[entry.dataKey])}`}</Text>,
-      )
-      if (calculatePercentages)
+  if (payload !== undefined)
+    payload.forEach((entry) => {
+      if (entry !== undefined) {
         listItems.push(
           <Text
-            textAlign="right"
-            key={`${entry.dataKey}-percent`}
+            key={`${entry.dataKey}-name`}
             color={entry.color}
-          >{`${Math.round(
-            formatter((entry.payload[entry.dataKey] / totalValue) * 100),
-          )}%`}</Text>,
+          >{`${entry.name} :`}</Text>,
         )
-    }
-  })
+        listItems.push(
+          <Text
+            mx="1rem"
+            textAlign="right"
+            key={`${entry.dataKey}-value`}
+            color={entry.color}
+          >{`${formatter(entry.payload[entry.dataKey])}`}</Text>,
+        )
+        if (calculatePercentages) {
+          const percentage =
+            Math.round(
+              formatter((entry.payload[entry.dataKey] / totalValue) * 100),
+            ) || 0
+          listItems.push(
+            <Text
+              textAlign="right"
+              key={`${entry.dataKey}-percent`}
+              color={entry.color}
+            >{`${percentage}%`}</Text>,
+          )
+        }
+      }
+    })
 
   const columns = calculatePercentages ? 3 : 2
   const templateColumns = `repeat(${columns}, auto)}`
