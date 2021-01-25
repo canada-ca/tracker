9b7f93f62d36591d4d1e726b1612e9fa7c5216b8
Mike Williamson <mike@korora.ca>
2021-01-25 16:59:46 -0500

    Verify jwt token in middleware
    This commit adds the [express-jwt
    middleware](https://github.com/auth0/express-jwt) to the api creating a central
    place to check the validity of tokens.
    
    The middleware gets the incoming request, validates/decodes the jwt and
    attaches the result to the request object as request.user.
    
    https://github.com/auth0/express-jwt#retrieving-the-decoded-payload
    
    The request object is already passed into the resolvers via the context so
    decisions can be made there based on `request.users`.

--- api-js/src/__tests__/create-context.test.js
+++ api-js/src/__tests__/create-context.test.js
@@ -1,12 +1,14 @@
 const { createContext } = require('../create-context')
-const { tokenize } = require('../auth')
 
 describe('given the create context function', () => {
   describe('request authorization token is not set', () => {
     it('returns object with userKey as undefined', async () => {
       const context = createContext({
         context: {},
-        req: { headers: { authorization: '' }, language: 'en' },
+        req: {
+          language: 'en',
+          user: {},
+        },
         res: {},
       })
 
@@ -15,10 +17,12 @@ describe('given the create context function', () => {
   })
   describe('request authorization token is set', () => {
     it('returns object with userKey as value', async () => {
-      const token = tokenize({ parameters: { userKey: '1234' } })
       const context = createContext({
         context: {},
-        req: { headers: { authorization: token }, language: 'en' },
+        req: {
+          language: 'en',
+          user: { userKey: '1234' },
+        },
         res: {},
       })
 
--- api-js/src/server.js
+++ api-js/src/server.js
@@ -2,11 +2,14 @@ import cors from 'cors'
 import express from 'express'
 import http from 'http'
 import { ApolloServer } from 'apollo-server-express'
+import jwt from 'express-jwt'
 import requestLanguage from 'express-request-language'
 import { GraphQLSchema } from 'graphql'
 import depthLimit from 'graphql-depth-limit'
 import { createComplexityLimitRule } from 'graphql-validation-complexity'
+import { defineMessage } from '@lingui/macro'
 
+import { createI18n } from './create-i18n'
 import { createContext } from './create-context'
 import { createQuerySchema } from './query'
 import { createMutationSchema } from './mutation'
@@ -38,6 +41,8 @@ const createValidationRules = (
   ]
 }
 
+const { AUTHENTICATED_KEY } = process.env
+
 export const Server = (
   // TODO refactor
   // no longer used but preserved because of coupling to argument order
@@ -58,6 +63,27 @@ export const Server = (
       languages: ['en', 'fr'],
     }),
   )
+
+  app.use(
+    jwt({
+      secret: AUTHENTICATED_KEY,
+      algorithms: ['HS256'],
+      credentialsRequired: false, // permissions are checked in the resolvers
+    }),
+  )
+
+  app.use(function handleJWTError(err, req, res, _next) {
+    // Per request i18n:
+    // https://github.com/lingui/js-lingui/discussions/954
+    const message = 'Invalid token, please request a new one.'
+    defineMessage({ message })
+
+    if (err.name === 'UnauthorizedError') {
+      const i18n = createI18n(req.language)
+      res.status(401).send(i18n._(message))
+      console.warn('JWT was attempted to be verified but secret was incorrect.')
+    }
+  })
 
   app.get('/alive', (_req, res) => {
     res.json({ ok: 'yes' })
