bff69f757be1cbe319ecdc388c1bfd6ada93d815
Thomas Nickerson <>
2021-01-25 17:20:32 -0400

    Almost correct formatting of response

--- clients/python/tracker_client.py
+++ clients/python/tracker_client.py
@@ -6,31 +6,38 @@ from gql.transport.aiohttp import AIOHTTPTransport
 
 SIGNIN_MUTATION = gql(
     """
-        mutation signIn($creds: SignInInput!) {
-            signIn (input: $creds) {
-                result {
-                    ... on RegularSignInResult {
-                        authResult {
-                            authToken
-                        }
+    mutation signIn($creds: SignInInput!) {
+        signIn (input: $creds) {
+            result {
+                ... on RegularSignInResult {
+                    authResult {
+                        authToken
                     }
                 }
             }
         }
-        """
+    }
+    """
 )
 
 ALL_DOMAINS_QUERY = gql(
     """
-  query Domains {
-    domains: findMyDomains(first: 100) {
-      edges {
-        node {
-          domain
+    query getAllDomainsByOrganization {
+        findMyOrganizations(first: 100) {
+            edges {
+                node {
+                    acronym
+                    domains(first: 100){
+                        edges{
+                            node{
+                                domain
+                            }
+                        }
+                    }     
+                }
+            }
         }
-      }
     }
-  }
     """
 )
 
@@ -39,9 +46,12 @@ def get_auth_token():
     """Takes in environment variables "TRACKER_UNAME" and "TRACKER_PASS", returns an auth token"""
     transport = AIOHTTPTransport(url="https://tracker.alpha.canada.ca/graphql")
     client = Client(transport=transport)
+
     username = os.environ.get("TRACKER_UNAME")
     password = os.environ.get("TRACKER_PASS")
+
     params = {"creds": {"userName": username, "password": password}}
+
     result = client.execute(SIGNIN_MUTATION, variable_values=params)
     auth_token = result["signIn"]["result"]["authResult"]["authToken"]
     return auth_token
@@ -53,9 +63,20 @@ def get_all_domains(auth_token):
         url="https://tracker.alpha.canada.ca/graphql",
         headers={"authorization": auth_token},
     )
+
     client = Client(transport=transport)
+
     result = client.execute(ALL_DOMAINS_QUERY)
-    return json.dumps(result)
+
+    # Avert your eyes
+    result2 = result["findMyOrganizations"]["edges"]
+    result3 = [n["node"] for n in result2]
+    for x in result3:
+        x["domains"] = x["domains"]["edges"]
+        x["domains"] = [n["node"] for n in x["domains"]]
+        x["domains"] = [n["domain"] for n in x["domains"]]
+
+    return json.dumps(result3)
 
 
 def main():
