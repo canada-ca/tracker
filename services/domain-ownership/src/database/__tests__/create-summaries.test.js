14236bc0b4db798d1e7e47c9071466be4126014d
nsdeschenes <nsdeschenes@outlook.com>
2021-01-26 07:20:40 -0400

    create summaries completed and tested

--- services/domain-ownership/src/database/__tests__/create-summaries.test.js
+++ services/domain-ownership/src/database/__tests__/create-summaries.test.js
@@ -1,21 +1,12 @@
 const { ArangoTools, dbNameFromFile } = require('arango-tools')
-const moment = require('moment')
 
-const { createSummaries } = require('../create-summaries')
 const { makeMigrations } = require('../../../migrations')
-const {
-  arrayEquals,
-  loadDates,
-  loadSummaryByDate,
-  createSummaryEdge,
-  createSummary,
-  removeSummaryEdge,
-  removeSummary,
-} = require('../../index')
+const { createSummaries } = require('../index')
+const { loadSummaryCountByDomain } = require('../../loaders')
 
 const { DB_PASS: rootPass, DB_URL: url } = process.env
 
-describe('given the createSummaries function', () => {
+describe('Given the createSummaries function', () => {
   let query, drop, truncate, migrate, collections
 
   const infoConsole = []
@@ -35,10 +26,9 @@ describe('given the createSummaries function', () => {
   afterAll(async () => {
     await drop()
   })
-
-  describe('dmarc summaries have to all be initialized', () => {
-    let domain, mockedContainer, mockedDates
-    beforeAll(async () => {
+  describe('the domain summary count is not equal to 14', () => {
+    let domain, dates
+    beforeEach(async () => {
       domain = await collections.domains.save({
         domain: 'domain.ca',
         selectors: '',
@@ -52,19 +42,7 @@ describe('given the createSummaries function', () => {
         lastRan: '2021-01-01 12:12:12.000000',
       })
 
-      mockedContainer = {
-        items: {
-          query: jest.fn().mockReturnValue({
-            fetchAll() {
-              return {
-                resources: [],
-              }
-            },
-          }),
-        },
-      }
-
-      mockedDates = jest.fn().mockReturnValue([
+      dates = [
         {
           startDate: '2020-01-01',
         },
@@ -104,490 +82,132 @@ describe('given the createSummaries function', () => {
         {
           startDate: '2021-01-01',
         },
-      ])
+      ]
     })
-    it('creates 14 dmarc summaries', async () => {
-      const createSummaryFunc = createSummaries(
-        query,
-        arrayEquals,
-        mockedDates,
-        loadSummaryByDate(mockedContainer),
-        createSummaryEdge(collections),
-        createSummary(query),
-        removeSummaryEdge(query),
-        removeSummary(query),
+    it('initializes summaries', async () => {
+      const mockedInitializeSummaries = jest.fn()
+      const mockedUpdateCurrentSummaries = jest.fn()
+      const mockedLoadDates = jest.fn().mockReturnValue(dates)
+
+      const createSummariesFunc = createSummaries(
+        mockedLoadDates,
+        loadSummaryCountByDomain(query),
+        mockedInitializeSummaries,
+        mockedUpdateCurrentSummaries,
       )
 
-      await createSummaryFunc({ domain: 'domain.ca' })
+      await createSummariesFunc({ domain: 'domain.ca' })
 
-      const summaryEdgeCursor = await query`
-        FOR summary IN domainsToDmarcSummaries
-          RETURN { startDate: summary.startDate, _from: summary._from }
-      `
-
-      const expectedSummaryEdges = [
-        {
-          _from: domain._id,
-          startDate: '2020-01-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-02-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-03-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-04-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-05-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-06-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-07-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-08-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-09-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-10-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-11-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2020-12-01',
-        },
-        {
-          _from: domain._id,
-          startDate: '2021-01-01',
-        },
-        {
-          _from: domain._id,
-          startDate: 'thirtyDays',
-        },
-      ]
-
-      await expect(summaryEdgeCursor.all()).resolves.toEqual(
-        expectedSummaryEdges,
-      )
-
-      const summaryCursor = await query`
-        FOR summary IN dmarcSummaries
-          RETURN summary
-      `
-
-      const expectedSummaries = [
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-        {
-          detailTables: {
-            dkimFailure: [],
-            dmarcFailure: [],
-            fullPass: [],
-            spfFailure: [],
-          },
-          categoryTotals: {
-            pass: 0,
-            fail: 0,
-            passDkimOnly: 0,
-            passSpfOnly: 0,
-          },
-        },
-      ]
-
-      await expect(summaryCursor.all()).resolves.toEqual(expectedSummaries)
+      expect(mockedInitializeSummaries).toHaveBeenCalledTimes(1)
+      expect(mockedInitializeSummaries).toHaveBeenNthCalledWith(1, {
+        domain: 'domain.ca',
+        domainId: domain._id,
+        dates: dates,
+      })
     })
   })
-  describe('thirty days and current month need to be updated', () => {
-    it('updates two records', async () => {
-      let domain, mockedContainer, mockedDates
-      beforeAll(async () => {
-        domain = await collections.domains.save({
-          domain: 'domain.ca',
-          selectors: '',
-          status: {
-            dkim: 'fail',
-            dmarc: 'fail',
-            https: 'fail',
-            spf: 'fail',
-            ssl: 'fail',
-          },
-          lastRan: '2021-01-01 12:12:12.000000',
-        })
-
-        mockedContainer = {
-          items: {
-            query: jest.fn().mockReturnValue({
-              fetchAll() {
-                return {
-                  resources: [],
-                }
-              },
-            }),
-          },
-        }
-
-        mockedDates = jest.fn().mockReturnValue([
-          {
-            startDate: '2020-01-01',
-          },
-          {
-            startDate: '2020-02-01',
-          },
-          {
-            startDate: '2020-03-01',
-          },
-          {
-            startDate: '2020-04-01',
-          },
-          {
-            startDate: '2020-05-01',
-          },
-          {
-            startDate: '2020-06-01',
-          },
-          {
-            startDate: '2020-07-01',
-          },
-          {
-            startDate: '2020-08-01',
-          },
-          {
-            startDate: '2020-09-01',
-          },
-          {
-            startDate: '2020-10-01',
-          },
-          {
-            startDate: '2020-11-01',
-          },
-          {
-            startDate: '2020-12-01',
-          },
-          {
-            startDate: '2021-01-01',
-          },
-        ])
+  describe('the domain summary count is equal to 14', () => {
+    let domain, dates
+    beforeEach(async () => {
+      domain = await collections.domains.save({
+        domain: 'domain.ca',
+        selectors: '',
+        status: {
+          dkim: 'fail',
+          dmarc: 'fail',
+          https: 'fail',
+          spf: 'fail',
+          ssl: 'fail',
+        },
+        lastRan: '2021-01-01 12:12:12.000000',
       })
 
-      const createSummaryFunc = createSummaries(
-        query,
-        arrayEquals,
-        mockedDates,
-        loadSummaryByDate(mockedContainer),
-        createSummaryEdge(collections),
-        createSummary(query),
-        removeSummaryEdge(query),
-        removeSummary(query),
+      dates = [
+        {
+          startDate: '2020-01-01',
+        },
+        {
+          startDate: '2020-02-01',
+        },
+        {
+          startDate: '2020-03-01',
+        },
+        {
+          startDate: '2020-04-01',
+        },
+        {
+          startDate: '2020-05-01',
+        },
+        {
+          startDate: '2020-06-01',
+        },
+        {
+          startDate: '2020-07-01',
+        },
+        {
+          startDate: '2020-08-01',
+        },
+        {
+          startDate: '2020-09-01',
+        },
+        {
+          startDate: '2020-10-01',
+        },
+        {
+          startDate: '2020-11-01',
+        },
+        {
+          startDate: '2020-12-01',
+        },
+        {
+          startDate: '2021-01-01',
+        },
+      ]
+    })
+    it('updates the given summary', async () => {
+      const mockedInitializeSummaries = jest.fn()
+      const mockedUpdateCurrentSummaries = jest.fn()
+      const mockedLoadDates = jest.fn().mockReturnValue(dates)
+      const mockedLoadSummaryCountByDomain = jest
+        .fn()
+        .mockReturnValue({ summaryCount: 14, domainId: domain._id })
+
+      const createSummariesFunc = createSummaries(
+        mockedLoadDates,
+        mockedLoadSummaryCountByDomain,
+        mockedInitializeSummaries,
+        mockedUpdateCurrentSummaries,
       )
 
-      await createSummaryFunc({ domain: 'domain.ca' })
-    })
-    describe('current month is a new month', () => {
-      it('removes oldest month', async () => {})
-      it('creates a new month record', async () => {})
+      await createSummariesFunc({ domain: 'domain.ca' })
+
+      expect(mockedUpdateCurrentSummaries).toHaveBeenCalledTimes(1)
+      expect(mockedUpdateCurrentSummaries).toHaveBeenNthCalledWith(1, {
+        domain: 'domain.ca',
+        domainId: domain._id,
+        dates: dates,
+      })
     })
   })
   describe('domain cannot be found', () => {
-    const consoleOutput = []
-    const mockedWarning = (output) => consoleOutput.push(output)
-    beforeAll(() => {
-      console.warn = mockedWarning
-    })
-    afterAll(() => {
-      consoleOutput.length = 0
-    })
-    it('logs to terminal', async () => {
-      const cursor = {
-        next() {
-          return undefined
-        },
-      }
+    it('returns undefined', async () => {
+      const mockedInitializeSummaries = jest.fn()
+      const mockedUpdateCurrentSummaries = jest.fn()
+      const mockedLoadDates = jest.fn()
+      const mockedLoadSummaryCountByDomain = jest
+        .fn()
+        .mockReturnValue({ summaryCount: 14, domainId: undefined })
 
-      const mockedQuery = jest.fn().mockReturnValue(cursor)
-
-      const createSummaryFunc = createSummaries(
-        mockedQuery,
-        arrayEquals,
-        loadDates(moment),
-        loadSummaryByDate(jest.fn()),
-        createSummaryEdge(collections),
-        createSummary(query),
-        removeSummaryEdge(query),
-        removeSummary(query),
+      const createSummariesFunc = createSummaries(
+        mockedLoadDates,
+        mockedLoadSummaryCountByDomain,
+        mockedInitializeSummaries,
+        mockedUpdateCurrentSummaries,
       )
 
-      await createSummaryFunc({ domain: 'domain.ca' })
+      const data = await createSummariesFunc({ domain: 'domain.ca' })
 
-      expect(consoleOutput).toEqual([`Could not find domain in DB: domain.ca`])
-    })
-  })
-  describe('database error occurs', () => {
-    describe('when checking for domain', () => {
-      it('throws an error', async () => {
-        const mockedQuery = jest
-          .fn()
-          .mockRejectedValue('Database error occurred.')
-
-        const createSummaryFunc = createSummaries(
-          mockedQuery,
-          arrayEquals,
-          loadDates(moment),
-          loadSummaryByDate(jest.fn()),
-          createSummaryEdge(collections),
-          createSummary(query),
-          removeSummaryEdge(query),
-          removeSummary(query),
-        )
-
-        try {
-          await createSummaryFunc({ domain: 'domain.ca' })
-        } catch (err) {
-          expect(err).toEqual(Error('Database error occurred.'))
-        }
-      })
-    })
-    describe('when checking for any dmarc summary connections', () => {
-      it('throws an error', async () => {
-        const cursor = {
-          next() {
-            return {
-              _id: 'domains/1',
-            }
-          },
-        }
-
-        const mockedQuery = jest
-          .fn()
-          .mockReturnValueOnce(cursor)
-          .mockRejectedValue('Database error occurred.')
-
-        const createSummaryFunc = createSummaries(
-          mockedQuery,
-          arrayEquals,
-          loadDates(moment),
-          loadSummaryByDate(jest.fn()),
-          createSummaryEdge(collections),
-          createSummary(query),
-          removeSummaryEdge(query),
-          removeSummary(query),
-        )
-
-        try {
-          await createSummaryFunc({ domain: 'domain.ca' })
-        } catch (err) {
-          expect(err).toEqual(Error('Database error occurred.'))
-        }
-      })
-    })
-  })
-  describe('cursor error occurs', () => {
-    describe('when checking for domain', () => {
-      it('throws an error', async () => {
-        const cursor = {
-          next: jest.fn().mockRejectedValue('Cursor error ocurred.'),
-        }
-
-        const mockedQuery = jest.fn().mockReturnValue(cursor)
-
-        const createSummaryFunc = createSummaries(
-          mockedQuery,
-          arrayEquals,
-          loadDates(moment),
-          loadSummaryByDate(jest.fn()),
-          createSummaryEdge(collections),
-          createSummary(query),
-          removeSummaryEdge(query),
-          removeSummary(query),
-        )
-
-        await expect(
-          createSummaryFunc({ domain: 'domain.ca' }),
-        ).rejects.toEqual(new Error('Cursor error ocurred.'))
-      })
+      expect(data).toBeUndefined()
     })
   })
 })
--- services/domain-ownership/src/database/create-summaries.js
+++ services/domain-ownership/src/database/create-summaries.js
@@ -1,21 +1,15 @@
 const moment = require('moment')
 
 const createSummaries = (
-  arrayEquals,
-  loadCurrentDates,
   loadDates,
-  loadSummaryByDate,
   loadSummaryCountByDomain,
-  createSummaryEdge,
-  createSummary,
-  removeSummaryEdge,
-  removeSummary,
+  initializeSummaries,
+  updateCurrentSummaries,
 ) => async ({ domain }) => {
-
   const { summaryCount, domainId } = await loadSummaryCountByDomain({ domain })
 
-  if (typeof summaryCount === 'undefined') {
-    return
+  if (typeof domainId === 'undefined') {
+    return undefined
   }
 
   const startDate = moment()
@@ -27,108 +21,9 @@ const createSummaries = (
 
   // Create all new summaries
   if (summaryCount !== 14) {
-    console.info(`First time initialization of dmarc summaries for: ${domain}`)
-
-    // Add thirty_days
-    dates.push({ startDate: 'thirty_days' })
-
-    dates.forEach(async ({ startDate }) => {
-      const currentSummary = await loadSummaryByDate({ domain, startDate })
-
-      const summaryDBInfo = await createSummary({ currentSummary })
-
-      if (startDate === 'thirty_days') {
-        startDate = 'thirtyDays'
-      }
-
-      await createSummaryEdge({
-        domainId,
-        summaryId: summaryDBInfo._id,
-        startDate: startDate,
-      })
-    })
+    await initializeSummaries({ domain, domainId, dates })
   } else {
-    console.info(`Updating thirty days, and current month data for: ${domain}`)
-    // Remove summary edge
-    const thirtyDayEdge = await removeSummaryEdge({
-      domainId,
-      monthToRemove: 'thirtyDays',
-    })
-
-    // Remove summary
-    await removeSummary({ summaryId: thirtyDayEdge._to })
-
-    const currentSummary = await loadSummaryByDate({
-      domain,
-      startDate: 'thirtyDays',
-    })
-
-    const summaryDBInfo = await createSummary({ currentSummary })
-
-    await createSummaryEdge({
-      domainId,
-      summaryId: summaryDBInfo._id,
-      startDate: 'thirtyDays',
-    })
-
-    // Get current start dates in db
-    const dbStartDates = await loadCurrentDates()
-
-    const dateArrEqual = arrayEquals(dates, dbStartDates)
-
-    if (dateArrEqual) {
-      // Update current month
-      const { startDate: currentStartDate } = dates[12]
-
-      // Remove summary edge
-      const currentMonthEdge = await removeSummaryEdge({
-        domainId,
-        monthToRemove: currentStartDate,
-      })
-
-      // Remove summary
-      await removeSummary({ summaryId: currentMonthEdge._to })
-
-      const currentSummary = await loadSummaryByDate({
-        domain,
-        startDate: dates[12],
-      })
-
-      const summaryDBInfo = await createSummary({ currentSummary })
-
-      await createSummaryEdge({
-        domainId,
-        summaryId: summaryDBInfo._id,
-        startDate: currentStartDate,
-      })
-    } else {
-      // Remove first month, and create new end month
-      const { startDate: monthToRemove } = dbStartDates[0]
-
-      // Remove summary edge
-      const monthToRemoveEdge = await removeSummaryEdge({
-        domainId,
-        monthToRemove,
-      })
-
-      // Remove summary
-      await removeSummary({ summaryId: monthToRemoveEdge._to })
-
-      const { startDate: newMonth } = dates[12]
-
-      const currentSummary = await loadSummaryByDate({
-        domain,
-        startDate: newMonth,
-      })
-
-      const summaryDBInfo = await createSummary({ currentSummary })
-
-      await createSummaryEdge({
-        domainId,
-        summaryId: summaryDBInfo._id,
-        startDate: newMonth,
-      })
-    }
+    await updateCurrentSummaries({ domain, domainId, dates })
   }
 }
 
