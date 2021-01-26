9c72e7afc9beb19cb68ecf2a10be3bb0308e9883
Thomas Nickerson <>
2021-01-26 09:56:44 -0400

    Clean up domain results formatting, factor out client and transport creation

--- clients/python/tracker_client.py
+++ clients/python/tracker_client.py
@@ -42,10 +42,46 @@ ALL_DOMAINS_QUERY = gql(
 )
 
 
+def create_transport(url, auth_token=None):
+    """Create and return a gql transport object
+
+    Arguments:
+    url -- the Tracker GraphQL endpoint url
+    auth_token -- JWT auth token string, omit when initially obtaining the token
+    """
+    if auth_token is None:
+        transport = AIOHTTPTransport(url=url)
+    else:
+        transport = AIOHTTPTransport(
+            url="https://tracker.alpha.canada.ca/graphql",
+            headers={"authorization": auth_token},
+        )
+    return transport
+
+
+def create_client(url, auth_token=None):
+    """Create and return a gql client object
+
+    Arguments:
+    url -- the Tracker GraphQL endpoint url
+    auth_token -- JWT auth token string, omit when initially obtaining the token
+    """
+    if auth_token is None:
+        client = Client(
+            transport=create_transport(url=url),
+            fetch_schema_from_transport=True,
+        )
+    else:
+        client = Client(
+            transport=create_transport(url=url, auth_token=auth_token),
+            fetch_schema_from_transport=True,
+        )
+    return client
+
+
 def get_auth_token():
     """Takes in environment variables "TRACKER_UNAME" and "TRACKER_PASS", returns an auth token"""
-    transport = AIOHTTPTransport(url="https://tracker.alpha.canada.ca/graphql")
-    client = Client(transport=transport)
+    client = create_client(url="https://tracker.alpha.canada.ca/graphql")
 
     username = os.environ.get("TRACKER_UNAME")
     password = os.environ.get("TRACKER_PASS")
@@ -59,23 +95,29 @@ def get_auth_token():
 
 def get_all_domains(auth_token):
     """Takes in an auth token, returns raw JSON result listing all domains you have membership in"""
-    transport = AIOHTTPTransport(
+    client = create_client(
         url="https://tracker.alpha.canada.ca/graphql",
-        headers={"authorization": auth_token},
+        auth_token=auth_token,
     )
-
-    client = Client(transport=transport)
 
     result = client.execute(ALL_DOMAINS_QUERY)
 
-    # Avert your eyes
-    result2 = result["findMyOrganizations"]["edges"]
-    result3 = [n["node"] for n in result2]
-    for x in result3:
+    # Extract the list of nodes from the resulting dict
+    result_list = result["findMyOrganizations"]["edges"]
+    # Move the dict value of "node" up a level
+    result_list = [
+        n["node"] for n in result_list
+    ]  # Move the dict value of "node" up a level
+
+    # For each dict element of the list, change the value of "domains"
+    # to the list of domains contained in the nodes of its edges
+    for x in result_list:
         x["domains"] = x["domains"]["edges"]
         x["domains"] = [n["node"] for n in x["domains"]]
         x["domains"] = [n["domain"] for n in x["domains"]]
-    result_dict = {x["acronym"]: {"domains": x["domains"]} for x in result3}
+
+    # Create a new dict in the desired format to return
+    result_dict = {x["acronym"]: {"domains": x["domains"]} for x in result_list}
     return json.dumps(result_dict, indent=4)
 
 
