a2bcaac825623403e60caa3ee0206db7c5addc57
Mike Williamson <mike@korora.ca>
2021-02-03 09:11:53 -0500

    Revert "Verify jwt token in middleware (#1519)"
    This reverts commit 7d49433f59aa6ee187f3b4784825ce7b03fd079c.

--- api-js/src/__tests__/create-context.test.js
+++ api-js/src/__tests__/create-context.test.js
@@ -1,14 +1,12 @@
 const { createContext } = require('../create-context')
+const { tokenize } = require('../auth')
 
 describe('given the create context function', () => {
   describe('request authorization token is not set', () => {
     it('returns object with userKey as undefined', async () => {
       const context = createContext({
         context: {},
-        req: {
-          language: 'en',
-          user: {},
-        },
+        req: { headers: { authorization: '' }, language: 'en' },
         res: {},
       })
 
@@ -17,12 +15,10 @@ describe('given the create context function', () => {
   })
   describe('request authorization token is set', () => {
     it('returns object with userKey as value', async () => {
+      const token = tokenize({ parameters: { userKey: '1234' } })
       const context = createContext({
         context: {},
-        req: {
-          language: 'en',
-          user: { userKey: '1234' },
-        },
+        req: { headers: { authorization: token }, language: 'en' },
         res: {},
       })
 
