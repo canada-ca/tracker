9bd02260cfeed9b8ebb0b5ffa5c2e5271f0cad9f
nsdeschenes <nsdeschenes@outlook.com>
2021-02-04 08:12:31 -0400

    update category percentage to use ints over floats

--- api-js/src/dmarc-summaries/objects/category-percentages.js
+++ api-js/src/dmarc-summaries/objects/category-percentages.js
@@ -1,26 +1,26 @@
-import { GraphQLObjectType, GraphQLInt, GraphQLFloat } from 'graphql'
+import { GraphQLObjectType, GraphQLInt } from 'graphql'
 
 export const categoryPercentagesType = new GraphQLObjectType({
   name: 'CategoryPercentages',
   description: 'This object displays the percentages of the category totals.',
   fields: () => ({
     failPercentage: {
-      type: GraphQLFloat,
+      type: GraphQLInt,
       description: 'Percentage of messages that are failing all checks.',
       resolve: ({ fail }) => fail,
     },
     fullPassPercentage: {
-      type: GraphQLFloat,
+      type: GraphQLInt,
       description: 'Percentage of messages that are passing all checks.',
       resolve: ({ pass }) => pass,
     },
     passDkimOnlyPercentage: {
-      type: GraphQLFloat,
+      type: GraphQLInt,
       description: 'Percentage of messages that are passing only dkim.',
       resolve: ({ passDkimOnly }) => passDkimOnly,
     },
     passSpfOnlyPercentage: {
-      type: GraphQLFloat,
+      type: GraphQLInt,
       description: 'Percentage of messages that are passing only spf.',
       resolve: ({ passSpfOnly }) => passSpfOnly,
     },
