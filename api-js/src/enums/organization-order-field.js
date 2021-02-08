70eb84fb68d35791b296d0c59cb08907490f411c
nsdeschenes <nsdeschenes@outlook.com>
2021-02-08 10:41:30 -0400

    progress

--- api-js/src/enums/organization-order-field.js
+++ api-js/src/enums/organization-order-field.js
@@ -13,59 +13,59 @@ export const OrganizationOrderField = new GraphQLEnumType({
     },
     SLUG: {
       value: 'slug',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by slug.',
     },
     ZONE: {
       value: 'zone',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by zone.',
     },
     SECTOR: {
       value: 'sector',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by sector.',
     },
     COUNTRY: {
       value: 'country',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by country.',
     },
     PROVINCE: {
       value: 'province',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by province.',
     },
     CITY: {
       value: 'city',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by city.',
     },
     VERIFIED: {
       value: 'verified',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by verified.',
     },
     SUMMARY_MAIL_PASS: {
       value: 'summary-mail-pass',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by summary mail pass count.',
     },
     SUMMARY_MAIL_FAIL: {
       value: 'summary-mail-fail',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by summary mail fail count.',
     },
     SUMMARY_MAIL_TOTAL: {
       value: 'summary-mail-total',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by summary mail total count.',
     },
     SUMMARY_WEB_PASS: {
       value: 'summary-web-pass',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by summary web pass count.',
     },
     SUMMARY_WEB_FAIL: {
       value: 'summary-web-fail',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by summary web fail count.',
     },
     SUMMARY_WEB_TOTAL: {
       value: 'summary-web-total',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by summary web total count.',
     },
     DOMAIN_COUNT: {
       value: 'domain-count',
-      description: 'Order organizations by acronym.',
+      description: 'Order organizations by domain count.',
     },
   },
   description: 'Properties by which organization connections can be ordered.',
