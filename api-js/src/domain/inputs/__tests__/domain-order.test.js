ccf5872a350a708ccf4a2d4d002616db1e6c85ce
nsdeschenes <nc575894@dal.ca>
2021-02-11 06:39:36 -0400

    Ordering Domain Connections (#1605)
    * laying down the ground work
    
    * progress
    
    * add ordering to domain connection loader by user id
    
    * add ordering to load domain by org id
    
    * add orderBy argument to domains field in org object
    
    * add test for input object

---
+++ api-js/src/domain/inputs/__tests__/domain-order.test.js
@@ -0,0 +1,25 @@
+import { GraphQLNonNull } from 'graphql'
+
+import { domainOrder } from '../domain-order'
+import { OrderDirection, DomainOrderField } from '../../../enums'
+
+describe('given the domainOrder input object', () => {
+  describe('testing fields', () => {
+    it('has a direction field', () => {
+      const demoType = domainOrder.getFields()
+
+      expect(demoType).toHaveProperty('direction')
+      expect(demoType.direction.type).toMatchObject(
+        GraphQLNonNull(OrderDirection),
+      )
+    })
+    it('has a field field', () => {
+      const demoType = domainOrder.getFields()
+
+      expect(demoType).toHaveProperty('field')
+      expect(demoType.field.type).toMatchObject(
+        GraphQLNonNull(DomainOrderField),
+      )
+    })
+  })
+})
---
+++ api-js/src/domain/inputs/domain-order.js
@@ -0,0 +1,18 @@
+import { GraphQLInputObjectType, GraphQLNonNull } from 'graphql'
+
+import { OrderDirection, DomainOrderField } from '../../enums'
+
+export const domainOrder = new GraphQLInputObjectType({
+  name: 'DomainOrder',
+  description: 'Ordering options for domain connections.',
+  fields: () => ({
+    field: {
+      type: GraphQLNonNull(DomainOrderField),
+      description: 'The field to order domains by.',
+    },
+    direction: {
+      type: GraphQLNonNull(OrderDirection),
+      description: 'The ordering direction.',
+    },
+  }),
+})
