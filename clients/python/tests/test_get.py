7058802b5dd474e2a1eab70a8f91d440e0f20b1a
Thomas Nickerson <>
2021-02-12 11:00:30 -0400

    Add scan results tests

--- clients/python/tests/test_get.py
+++ clients/python/tests/test_get.py
@@ -159,8 +159,8 @@ def test_get_summary_by_acronym_key_error(mocker, all_summaries_input):
     mocker.patch("tracker_client.client.execute_query", auto_spec=True, return_value=all_summaries_input)
 
     client = Client()
-    result = tc.get_domains_by_acronym(client, "foo")
-    tc.execute_query.assert_called_once_with(client, queries.ALL_DOMAINS_QUERY)
+    result = tc.get_summary_by_acronym(client, "foo")
+    tc.execute_query.assert_called_once_with(client, queries.ALL_ORG_SUMMARIES)
     assert result == json.dumps({
         "error": {"message": "No organization with the provided acronym could be found."}
     }, indent=4)
@@ -194,3 +194,45 @@ def test_get_summary_by_name(mocker, name_summary_input, org_summary_output):
     result = tc.get_summary_by_name(client, "foo bar")
     tc.execute_query.assert_called_once_with(client, queries.SUMMARY_BY_SLUG, expected_params)
     assert result == json.dumps(org_summary_output, indent=4)
+
+
+def test_get_domain_results_error(mocker):
+    mocker.patch("tracker_client.client.execute_query", auto_spec=True, return_value={
+        "error": {"message": "No organization with the provided slug could be found."}
+    })
+    client = Client()
+    result = tc.get_domain_results(client, "foo.bar")
+    assert result  == json.dumps({
+        "error": {"message": "No organization with the provided slug could be found."}
+    }, indent=4)
+
+
+def test_get_domain_results(mocker, scan_results_input, scan_results_output):
+    mocker.patch("tracker_client.client.execute_query", auto_spec=True, return_value=scan_results_input)
+    expected_params = {"domain": "foo.bar"}
+
+    client = Client()
+    result = tc.get_domain_results(client, "foo.bar")
+    tc.execute_query.assert_called_once_with(client, queries.DOMAIN_RESULTS, expected_params)
+    assert result == json.dumps(scan_results_output, indent=4)
+
+
+def test_get_domain_status_error(mocker):
+    mocker.patch("tracker_client.client.execute_query", auto_spec=True, return_value={
+        "error": {"message": "No organization with the provided slug could be found."}
+    })
+    client = Client()
+    result = tc.get_domain_status(client, "foo.bar")
+    assert result  == json.dumps({
+        "error": {"message": "No organization with the provided slug could be found."}
+    }, indent=4)
+
+
+def test_get_domain_status(mocker, domain_status_input, domain_status_output):
+    mocker.patch("tracker_client.client.execute_query", auto_spec=True, return_value=domain_status_input)
+    expected_params = {"domain": "foo.bar"}
+
+    client = Client()
+    result = tc.get_domain_status(client, "foo.bar")
+    tc.execute_query.assert_called_once_with(client, queries.DOMAIN_STATUS, expected_params)
+    assert result == json.dumps(domain_status_output, indent=4)
