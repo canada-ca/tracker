1d8e23494cdc4a928d02788c07047675ca006094
nsdeschenes <nsdeschenes@outlook.com>
2021-02-16 09:42:59 -0400

    dmarc ordering enums

---
+++ api-js/src/enums/dmarc-order.js
@@ -0,0 +1,32 @@
+import { GraphQLEnumType } from 'graphql'
+
+export const DmarcOrderField = new GraphQLEnumType({
+  name: 'DmarcOrderField',
+  description: 'Properties by which dmarc connections can be ordered.',
+  values: {
+    TIMESTAMP: {
+      value: 'timestamp',
+      description: 'Order dmarc summaries by timestamp.',
+    },
+    DMARC_PHASE: {
+      value: 'dmarc-phase',
+      description: 'Order dmarc summaries by dmarc phase.',
+    },
+    RECORD: {
+      value: 'record',
+      description: 'Order dmarc summaries by record.',
+    },
+    P_POLICY: {
+      value: 'p-policy',
+      description: 'Order dmarc summaries by p policy.',
+    },
+    SP_POLICY: {
+      value: 'sp-policy',
+      description: 'Order dmarc summaries by sp policy.',
+    },
+    PCT: {
+      value: 'pct',
+      description: 'Order dmarc summaries by percentage.',
+    },
+  },
+})
