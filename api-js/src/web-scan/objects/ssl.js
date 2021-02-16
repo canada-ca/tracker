1cf9e0fedc3743dcaf75de7fb24a15e840d0e656
nsdeschenes <nsdeschenes@outlook.com>
2021-02-16 09:31:47 -0400

    implement new fields on the ssl object

--- api-js/src/web-scan/objects/ssl.js
+++ api-js/src/web-scan/objects/ssl.js
@@ -1,4 +1,10 @@
-import { GraphQLInt, GraphQLObjectType, GraphQLString } from 'graphql'
+import {
+  GraphQLBoolean,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql'
 import {
   connectionArgs,
   connectionDefinitions,
@@ -14,6 +20,24 @@ export const sslType = new GraphQLObjectType({
   name: 'SSL',
   fields: () => ({
     id: globalIdField('ssl'),
+    acceptableCiphers: {
+      type: GraphQLList(GraphQLString),
+      description:
+        'List of ciphers in use by the server deemed to be "acceptable".',
+      resolve: ({ acceptable_ciphers: acceptableCiphers }) => acceptableCiphers,
+    },
+    acceptableCurves: {
+      type: GraphQLList(GraphQLString),
+      description:
+        'List of curves in use by the server deemed to be "acceptable".',
+      resolve: ({ acceptable_curves: acceptableCurves }) => acceptableCurves,
+    },
+    ccsInjectionVulnerable: {
+      type: GraphQLBoolean,
+      description: 'Denotes vulnerability to OpenSSL CCS Injection.',
+      resolve: ({ ccs_injection_vulnerable: ccsInjectionVulnerable }) =>
+        ccsInjectionVulnerable,
+    },
     domain: {
       type: domainType,
       description: `The domain the scan was ran on.`,
@@ -23,16 +47,6 @@ export const sslType = new GraphQLObjectType({
         domain.id = domain._key
         return domain
       },
-    },
-    timestamp: {
-      type: GraphQLString,
-      description: `The time when the scan was initiated.`,
-      resolve: ({ timestamp }) => timestamp,
-    },
-    rawJson: {
-      type: GraphQLJSON,
-      description: 'Raw scan result.',
-      resolve: ({ rawJson }) => JSON.stringify(rawJson),
     },
     guidanceTags: {
       type: guidanceTagConnection.connectionType,
@@ -51,6 +65,51 @@ export const sslType = new GraphQLObjectType({
         })
         return sslTags
       },
+    },
+    heartbleedVulnerable: {
+      type: GraphQLBoolean,
+      description: 'Denotes vulnerability to "Heartbleed" exploit.',
+      resolve: ({ heartbleed_vulnerable: heartbleedVulnerable }) =>
+        heartbleedVulnerable,
+    },
+    rawJson: {
+      type: GraphQLJSON,
+      description: 'Raw scan result.',
+      resolve: ({ rawJson }) => JSON.stringify(rawJson),
+    },
+    strongCiphers: {
+      type: GraphQLList(GraphQLString),
+      description:
+        'List of ciphers in use by the server deemed to be "strong".',
+      resolve: ({ strong_ciphers: strongCiphers }) => strongCiphers,
+    },
+    strongCurves: {
+      type: GraphQLList(GraphQLString),
+      description: 'List of curves in use by the server deemed to be "strong".',
+      resolve: ({ strong_ciphers: strongCiphers }) => strongCiphers,
+    },
+    supportsEcdhKeyExchange: {
+      type: GraphQLBoolean,
+      description: 'Denotes support for elliptic curve key pairs.',
+      resolve: ({ supports_ecdh_key_exchange: supportsEcdhKeyExchange }) =>
+        supportsEcdhKeyExchange,
+    },
+    timestamp: {
+      type: GraphQLString,
+      description: `The time when the scan was initiated.`,
+      resolve: ({ timestamp }) => timestamp,
+    },
+    weakCiphers: {
+      type: GraphQLList(GraphQLString),
+      description:
+        'List of ciphers in use by the server deemed to be "weak" or in other words, are not compliant with security standards.',
+      resolve: ({ weak_ciphers: weakCiphers }) => weakCiphers,
+    },
+    weakCurves: {
+      type: GraphQLList(GraphQLString),
+      description:
+        'List of curves in use by the server deemed to be "weak" or in other words, are not compliant with security standards.',
+      resolve: ({ weak_curves: weakCurves }) => weakCurves,
     },
   }),
   interfaces: [nodeInterface],
