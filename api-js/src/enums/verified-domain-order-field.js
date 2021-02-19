bf41c767f880e191509c2ded011b092626f19766
nsdeschenes <nc575894@dal.ca>
2021-02-19 09:13:01 -0400

    Ordering Verified Domains (#1683)
    * create verified domain ordering enum
    
    * create verified domain orderBy input object
    
    * add ordering functionality to verified domain connection loaders
    
    * add ordering arguments to queries and objects

---
+++ api-js/src/enums/verified-domain-order-field.js
@@ -0,0 +1,37 @@
+import { GraphQLEnumType } from 'graphql'
+
+export const VerifiedDomainOrderField = new GraphQLEnumType({
+  name: 'VerifiedDomainOrderField',
+  description:
+    'Properties by which verified domain connections can be ordered.',
+  values: {
+    DOMAIN: {
+      value: 'domain',
+      description: 'Order verified domain edges by domain.',
+    },
+    LAST_RAN: {
+      value: 'last-ran',
+      description: 'Order verified domain edges by last ran.',
+    },
+    DKIM_STATUS: {
+      value: 'dkim-status',
+      description: 'Order verified domain edges by dkim status.',
+    },
+    DMARC_STATUS: {
+      value: 'dmarc-status',
+      description: 'Order verified domain edges by dmarc status.',
+    },
+    HTTPS_STATUS: {
+      value: 'https-status',
+      description: 'Order verified domain edges by https status.',
+    },
+    SPF_STATUS: {
+      value: 'spf-status',
+      description: 'Order verified domain edges by spf status.',
+    },
+    SSL_STATUS: {
+      value: 'ssl-status',
+      description: 'Order verified domain edges by ssl status.',
+    },
+  },
+})
