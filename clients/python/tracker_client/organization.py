a5180d1b009991fb5e37d5ba0cb8b253370fbdf5
Thomas Nickerson <>
2021-02-22 16:37:22 -0400

    Organization slightly more fleshed out

--- clients/python/tracker_client/organization.py
+++ clients/python/tracker_client/organization.py
@@ -2,18 +2,26 @@ from client import get_domains_by_name, create_client, get_auth_token, get_summa
 
 class Organization:
     # Get list of domains outside and pass in, or fetch list at instantiation?
-    def __init__(self, name, acronym):
+    def __init__(self, name, acronym, client):
         self.name = name
         self.acronym = acronym
-
-        # Temporary, should be list of Domain objects 
-        client = create_client(auth_token=get_auth_token)
-        result = get_domains_by_name(client, name)
-        self.domains = result[acronym]
+        self.client = client
 
     def get_summary(self):
-        # Temporary, need to decide if client should be an attribute of this class or be passed in to method call
-        client = create_client(auth_token=get_auth_token)
-        return get_summary_by_name(client, self.name)
+        # Temporary, will move actual function here soon
+        return get_summary_by_name(self.client, self.name)
 
-    
+    # TODO: make this return a list of domain objects and create another function for JSON output
+    def get_domains(self):
+        return get_domains_by_name(self.client, self.name)
+
+
+def main():
+    client = create_client(auth_token=get_auth_token())
+    test_org = Organization("Communications Security Establishment Canada", "CSE", client)
+    print(test_org.get_domains())
+    print(test_org.get_summary())
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
