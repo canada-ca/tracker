14c2e73732092f98606be5027c26349bd2a5a45f
Thomas Nickerson <>
2021-02-23 09:28:20 -0400

    Update imports in domain.py, add more temporary methods

--- clients/python/tracker_client/domain.py
+++ clients/python/tracker_client/domain.py
@@ -1,4 +1,6 @@
-from client import get_domain_status, get_dmarc_summary, get_yearly_dmarc_summaries, get_domain_results, create_client, get_auth_token
+from core import create_client, get_auth_token
+from results import get_all_results, get_domain_status, get_email_results, get_web_results
+from dmarc import get_dmarc_summary, get_yearly_dmarc_summaries
 
 class Domain:
     def __init__(self, domain_name, client):
@@ -8,20 +10,31 @@ class Domain:
     def get_status(self):
         return get_domain_status(self.client, self.domain_name)
 
-    def get_dmarc_summary(self, month, year):
+    def get_monthly_dmarc(self, month, year):
         return get_dmarc_summary(self.client, self.domain_name, month, year)
 
+    def get_yearly_dmarc(self):
+        return get_yearly_dmarc_summaries(self.client, self.domain_name)
+
     def get_all_results(self):
-        return get_domain_results(self.client, self.domain_name)
+        return get_all_results(self.client, self.domain_name)
+
+    def get_web_results(self):
+        return get_web_results(self.client, self.domain_name)
+
+    def get_email_results(self):
+        return get_email_results(self.client, self.domain_name)
 
 
 def main():
     client = create_client(auth_token=get_auth_token())
     test_domain = Domain("cyber.gc.ca", client)
     print(test_domain.get_status())
-    print(test_domain.get_dmarc_summary("september", 2020))
+    print(test_domain.get_monthly_dmarc("september", 2020))
+    print(test_domain.get_yearly_dmarc())
     print(test_domain.get_all_results())
-
+    print(test_domain.get_web_results())
+    print(test_domain.get_email_results())
 
 
 if __name__ == "__main__":  # pragma: no cover
