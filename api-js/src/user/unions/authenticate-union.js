10ae59033e077edb81f55194ac0662ad14706368
nsdeschenes <nsdeschenes@outlook.com>
2021-03-03 07:56:50 -0400

    add description for authenticate union

--- api-js/src/user/unions/authenticate-union.js
+++ api-js/src/user/unions/authenticate-union.js
@@ -3,7 +3,8 @@ import { authResultType, authenticateError } from '../objects'
 
 export const authenticateUnion = new GraphQLUnionType({
   name: 'AuthenticateUnion',
-  description: '',
+  description:
+    'This union is used with the `authenticate` mutation, allowing for the user to authenticate, and support any errors that may occur',
   types: [authResultType, authenticateError],
   resolveType({ _type }) {
     if (_type === 'authResult') {
