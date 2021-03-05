e71a315e6bb53398063037b86762ff3f67ff2489
nsdeschenes <nsdeschenes@outlook.com>
2021-03-05 10:59:03 -0400

    create new send phone code objects

---
+++ api-js/src/user/objects/send-phone-code-result.js
@@ -0,0 +1,15 @@
+import { GraphQLObjectType, GraphQLString } from 'graphql'
+
+export const sendPhoneCodeResultType = new GraphQLObjectType({
+  name: 'ResetPasswordResult',
+  description:
+    'This object is used to inform the user that no errors were encountered while sending their phone code.',
+  fields: () => ({
+    status: {
+      type: GraphQLString,
+      description:
+        'Informs the user if their phone code was successfully sent.',
+      resolve: ({ status }) => status,
+    },
+  }),
+})
