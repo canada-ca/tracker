be9e6024f33b4f5ccb5fd1f1e0da22410f2154ce
nsdeschenes <nsdeschenes@outlook.com>
2021-03-08 08:25:58 -0400

    create new fragments for different return values

--- frontend/src/graphql/fragments.js
+++ frontend/src/graphql/fragments.js
@@ -14,3 +14,68 @@ export const Authorization = {
     `,
   },
 }
+
+export const UpdateUserProfileDisplayName = {
+  fragments: {
+    requiredFields: gql`
+      fragment UpdateUserProfileFields on UpdateUserProfileResult {
+        status
+        user {
+          displayName
+        }
+      }
+    `,
+  },
+}
+
+export const UpdateUserProfileUserName = {
+  fragments: {
+    requiredFields: gql`
+      fragment UpdateUserProfileFields on UpdateUserProfileResult {
+        status
+        user {
+          userName
+        }
+      }
+    `,
+  },
+}
+
+export const UpdateUserProfileLanguage = {
+  fragments: {
+    requiredFields: gql`
+      fragment UpdateUserProfileFields on UpdateUserProfileResult {
+        status
+        user {
+          preferredLang
+        }
+      }
+    `,
+  },
+}
+
+export const UpdateUserProfileTfaSendMethod = {
+  fragments: {
+    requiredFields: gql`
+      fragment UpdateUserProfileFields on UpdateUserProfileResult {
+        status
+        user {
+          tfaSendMethod
+        }
+      }
+    `,
+  },
+}
+
+export const UpdateUserProfilePhoneNumber = {
+  fragments: {
+    requiredFields: gql`
+      fragment UpdateUserProfileFields on UpdateUserProfileResult {
+        status
+        user {
+          phoneNumber
+        }
+      }
+    `,
+  },
+}
