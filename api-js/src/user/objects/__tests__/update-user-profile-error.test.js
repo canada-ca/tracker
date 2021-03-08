f27b0d2b226311b16870d7b5725caf72142b9301
nsdeschenes <nsdeschenes@outlook.com>
2021-03-08 07:02:12 -0400

    fix remaining tests

--- api-js/src/user/objects/__tests__/update-user-profile-error.test.js
+++ api-js/src/user/objects/__tests__/update-user-profile-error.test.js
@@ -1,17 +1,17 @@
 import { GraphQLInt, GraphQLString } from 'graphql'
 
-import { resetPasswordErrorType } from '../index'
+import { updateUserProfileErrorType } from '../index'
 
-describe('given the resetPasswordErrorType object', () => {
+describe('given the updateUserProfileErrorType object', () => {
   describe('testing the field definitions', () => {
     it('has an code field', () => {
-      const demoType = resetPasswordErrorType.getFields()
+      const demoType = updateUserProfileErrorType.getFields()
 
       expect(demoType).toHaveProperty('code')
       expect(demoType.code.type).toMatchObject(GraphQLInt)
     })
     it('has a description field', () => {
-      const demoType = resetPasswordErrorType.getFields()
+      const demoType = updateUserProfileErrorType.getFields()
 
       expect(demoType).toHaveProperty('description')
       expect(demoType.description.type).toMatchObject(GraphQLString)
@@ -21,14 +21,14 @@ describe('given the resetPasswordErrorType object', () => {
   describe('testing the field resolvers', () => {
     describe('testing the code resolver', () => {
       it('returns the resolved field', () => {
-        const demoType = resetPasswordErrorType.getFields()
+        const demoType = updateUserProfileErrorType.getFields()
 
         expect(demoType.code.resolve({ code: 400 })).toEqual(400)
       })
     })
     describe('testing the description field', () => {
       it('returns the resolved value', () => {
-        const demoType = resetPasswordErrorType.getFields()
+        const demoType = updateUserProfileErrorType.getFields()
 
         expect(
           demoType.description.resolve({ description: 'description' }),
