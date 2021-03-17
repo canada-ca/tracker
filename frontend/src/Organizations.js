b308c458255482c7c7e191a58914153360a27b74
Thomas Nickerson <>
2021-03-17 15:46:29 -0300

    Add sorting dropdowns to orgs

--- frontend/src/Organizations.js
+++ frontend/src/Organizations.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { number } from 'prop-types'
 import { Trans, t } from '@lingui/macro'
 import { Layout } from './Layout'
@@ -11,6 +11,9 @@ import {
   InputLeftElement,
   Icon,
   Input,
+  Select,
+  Stack,
+  Text,
 } from '@chakra-ui/core'
 import {
   PAGINATED_ORGANIZATIONS as FORWARD,
@@ -25,7 +28,10 @@ import { LoadingMessage } from './LoadingMessage'
 import { RelayPaginationControls } from './RelayPaginationControls'
 
 export default function Organisations({ orgsPerPage = 10 }) {
+  const [orderDirection, setOrderDirection] = useState('ASC')
+  const [orderField, setOrderField] = useState('NAME')
   const { currentUser } = useUserState()
+
   const {
     loading,
     error,
@@ -38,6 +44,7 @@ export default function Organisations({ orgsPerPage = 10 }) {
     fetchForward: FORWARD,
     fetchBackward: BACKWARD,
     fetchHeaders: { authorization: currentUser.jwt },
+    variables: { field: orderField, direction: orderDirection },
     recordsPerPage: orgsPerPage,
     relayRoot: 'findMyOrganizations',
   })
@@ -57,12 +64,49 @@ export default function Organisations({ orgsPerPage = 10 }) {
         <Trans>Organizations</Trans>
       </Heading>
       <ErrorBoundary FallbackComponent={ErrorFallbackMessage}>
-        <InputGroup width="100%" mb="8px">
-          <InputLeftElement>
-            <Icon name="search" color="gray.300" />
-          </InputLeftElement>
-          <Input type="text" placeholder={t`Search for an organization`} />
-        </InputGroup>
+        <Stack isInline spacing={3}>
+          <InputGroup width="57%" mb="8px">
+            <InputLeftElement>
+              <Icon name="search" color="gray.300" />
+            </InputLeftElement>
+            <Input type="text" placeholder={t`Search for an organization`} />
+          </InputGroup>
+          <Text
+            fontSize="md"
+            fontWeight="bold"
+            textAlign="center"
+            ml="8%"
+            mt={2}
+            isTruncated
+          >
+            <Trans>Sort by: </Trans>
+          </Text>
+          <Select
+            aria-label="Sort by"
+            w="10%"
+            size="md"
+            variant="filled"
+            onChange={(e) => {
+              setOrderField(e.target.value)
+            }}
+          >
+            <option key="NAME" value="NAME">Name</option>
+            <option key="DOMAIN_COUNT" value="DOMAIN_COUNT">Domains</option>
+            <option key="ACRONYM" value="ACRONYM">Acronym</option>
+          </Select>
+          <Select
+            aria-label="Sort order"
+            w="10%"
+            size="md"
+            variant="filled"
+            onChange={(e) => {
+              setOrderDirection(e.target.value)
+            }}
+          >
+            <option key="ASC" value="ASC">Asc.</option>
+            <option key="DESC" value="DESC">Desc.</option>
+          </Select>
+        </Stack>
         <ListOf
           elements={nodes}
           ifEmpty={() => <Trans>No Organizations</Trans>}
