6b9041bf2fb31f77da52d79c968a71ccf413d133
Mike Williamson <mike@korora.ca>
2021-03-19 21:11:42 -0400

    Remove Layout from Footer and LandingPage (#1945)
    The abstraction the Layout component creates is not a useful one. This is the
    first step in removing it, allowing us to shave some memory use and bytes
    transferred.

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -3,33 +3,43 @@ import PropTypes from 'prop-types'
 import { useLingui } from '@lingui/react'
 import wordmark from './images/canada-wordmark.svg'
 import { Box, Flex, List, Image, ListItem } from '@chakra-ui/core'
-import { Layout } from './Layout'
 
 export const Footer = (props) => {
   const { i18n } = useLingui()
 
   return (
-    <Flex as="footer" {...props} py="4" fontFamily="body" borderTop="2px solid" borderTopColor="gray.300">
-      <Layout>
-        <Flex align="center" direction="row">
-          <List px={0} d="flex" align="center" direction="row">
-            {React.Children.map(props.children, (child) => (
-              <ListItem>{child}</ListItem>
-            ))}
-          </List>
-          <Box py={4} width={{ base: 147.2 }} ml="auto">
-            <Image
-              src={wordmark}
-              width="100%"
-              alt={
-                i18n.locale === 'en'
-                  ? 'Symbol of the Government of Canada'
-                  : 'Symbole du gouvernement du Canada'
-              }
-            />
-          </Box>
-        </Flex>
-      </Layout>
+    <Flex
+      {...props}
+      py="4"
+      fontFamily="body"
+      borderTop="2px solid"
+      borderTopColor="gray.300"
+    >
+      <Flex
+        maxW={{ sm: 540, md: 768, lg: 960, xl: 1200 }}
+        mx="auto"
+        px={4}
+        w="100%"
+        align="center"
+        direction="row"
+      >
+        <List px={0} d="flex" align="center" direction="row">
+          {React.Children.map(props.children, (child) => (
+            <ListItem>{child}</ListItem>
+          ))}
+        </List>
+        <Box py={4} width={{ base: 147.2 }} ml="auto">
+          <Image
+            src={wordmark}
+            width="100%"
+            alt={
+              i18n.locale === 'en'
+                ? 'Symbol of the Government of Canada'
+                : 'Symbole du gouvernement du Canada'
+            }
+          />
+        </Box>
+      </Flex>
     </Flex>
   )
 }
--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -1,20 +1,17 @@
 import React from 'react'
-import { Layout } from './Layout'
 import { Image, Stack } from '@chakra-ui/core'
 import trackerLogo from './images/tracker_v-03.png'
 import { WelcomeMessage } from './WelcomeMessage'
 
 export function LandingPage() {
   return (
-    <Layout>
-      <Stack align="center">
-        <Image
-          src={trackerLogo}
-          alt={'Tracker Logo'}
-          size={['100%', '100%', '0%', '0%', '0%']}
-        />
-        <WelcomeMessage />
-      </Stack>
-    </Layout>
+    <Stack align="center">
+      <Image
+        src={trackerLogo}
+        alt={'Tracker Logo'}
+        size={['100%', '100%', '0%', '0%', '0%']}
+      />
+      <WelcomeMessage />
+    </Stack>
   )
 }
