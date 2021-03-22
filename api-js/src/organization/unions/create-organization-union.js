66029a8dc6d8f5aa5cc447a616b90e4d80582212
nsdeschenes <nsdeschenes@outlook.com>
2021-03-22 08:42:13 -0300

    create union for mutation

---
+++ api-js/src/organization/unions/create-organization-union.js
@@ -0,0 +1,16 @@
+import { GraphQLUnionType } from 'graphql'
+import { organizationErrorType, organizationType } from '../objects'
+
+export const createOrganizationUnion = new GraphQLUnionType({
+  name: 'CreateOrganizationUnion',
+  description: `This union is used with the \`CreateOrganization\` mutation, 
+allowing for users to create an organization, and support any errors that may occur`,
+  types: [organizationErrorType, organizationType],
+  resolveType({ _type }) {
+    if (_type === 'domain') {
+      return organizationType
+    } else {
+      return organizationErrorType
+    }
+  },
+})
