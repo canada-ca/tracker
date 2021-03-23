f28e535288b1e46ae956e6c6a16e64c35e83d323
Thomas Nickerson <64759920+ThomasNickerson@users.noreply.github.com>
2021-03-23 13:05:13 -0300

    Update org page to disable pagination while loading

--- frontend/src/Organizations.js
+++ frontend/src/Organizations.js
@@ -35,6 +35,7 @@ export default function Organisations({ orgsPerPage = 10 }) {
 
   const {
     loading,
+    isLoadingMore,
     error,
     nodes,
     next,
@@ -154,6 +155,7 @@ export default function Organisations({ orgsPerPage = 10 }) {
           hasPreviousPage={hasPreviousPage}
           next={next}
           previous={previous}
+          isDisabled={isLoadingMore}
         />
       </ErrorBoundary>
     </Layout>
