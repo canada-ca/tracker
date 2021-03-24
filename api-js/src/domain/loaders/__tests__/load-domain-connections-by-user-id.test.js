fa44f20f1bbfee2ab269ab95e09dd2e93d65e726
nsdeschenes <nsdeschenes@outlook.com>
2021-03-24 06:45:14 -0300

    tests are still failing

--- api-js/src/domain/loaders/__tests__/load-domain-connections-by-user-id.test.js
+++ api-js/src/domain/loaders/__tests__/load-domain-connections-by-user-id.test.js
@@ -31,7 +31,6 @@ describe('given the load domain connections by user id function', () => {
   })
 
   beforeEach(async () => {
-    await truncate()
     user = await collections.users.save({
       userName: 'test.account@istio.actually.exists',
       displayName: 'Test Account',
@@ -281,6 +280,45 @@ describe('given the load domain connections by user id function', () => {
               hasPreviousPage: true,
               startCursor: toGlobalId('domains', expectedDomains[1]._key),
               endCursor: toGlobalId('domains', expectedDomains[1]._key),
+            },
+          }
+
+          expect(domains).toEqual(expectedStructure)
+        })
+      })
+      describe('using search argument', () => {
+        it('returns filtered domains', async () => {
+          const connectionLoader = domainLoaderConnectionsByUserId(
+            query,
+            user._key,
+            cleanseInput,
+          )
+
+          const domainLoader = domainLoaderByKey(query)
+          const expectedDomain = await domainLoader.load(domainOne._key)
+
+          const connectionArgs = {
+            first: 1,
+            search: 'test1.gc.ca',
+          }
+
+          const domains = await connectionLoader({ ...connectionArgs })
+
+          const expectedStructure = {
+            edges: [
+              {
+                cursor: toGlobalId('domains', expectedDomain._key),
+                node: {
+                  ...expectedDomain,
+                },
+              },
+            ],
+            totalCount: 1,
+            pageInfo: {
+              hasNextPage: false,
+              hasPreviousPage: false,
+              startCursor: toGlobalId('domains', expectedDomain._key),
+              endCursor: toGlobalId('domains', expectedDomain._key),
             },
           }
 
