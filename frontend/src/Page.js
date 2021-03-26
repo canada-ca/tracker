fdd015de108edc5387367fc8965cf4dca4ee86e3
Thomas Nickerson <64759920+ThomasNickerson@users.noreply.github.com>
2021-03-26 17:15:09 -0300

    Allow for translation of Tracker in page title

--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { string } from 'prop-types'
+import { t } from '@lingui/macro'
 import { useDocumentTitle } from './useDocumentTitle'
 
 export const Page = ({ title, ...props }) => {
-  useDocumentTitle(`${title} - Tracker`)
+  useDocumentTitle(`${title} - ${t`Tracker`}`)
 
   return <Route {...props} />
 }
