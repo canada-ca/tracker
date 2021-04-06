6bf5570fc44c59501dcf57e063b27e79d6d0e15e
nsdeschenes <nsdeschenes@outlook.com>
2021-04-06 08:45:01 -0300

    implement new mutation to support removing phone numbers

---
+++ api-js/src/user/mutations/remove-phone-number.js
@@ -0,0 +1,82 @@
+import { t } from '@lingui/macro'
+import { mutationWithClientMutationId } from 'graphql-relay'
+
+import { removePhoneNumberUnion } from '../unions'
+
+export const removePhoneNumber = new mutationWithClientMutationId({
+  name: 'RemovePhoneNumber',
+  description: '',
+  outputFields: () => ({
+    result: {
+      type: removePhoneNumberUnion,
+      description:
+        '`RemovePhoneNumberUnion` returning either a `RemovePhoneNumberResult`, or `RemovePhoneNumberError` object.',
+      resolve: (payload) => payload,
+    },
+  }),
+  mutateAndGetPayload: async (
+    _args,
+    { i18n, collections, query, transaction, auth: { userRequired } },
+  ) => {
+    // Get requesting user
+    const user = await userRequired()
+
+    // Set TFA method to backup incase user gets logged out, so they're not locked out of their account
+    let tfaSendMethod = 'none'
+    if (user.emailValidated && user.tfaSendMethod !== 'none') {
+      tfaSendMethod = 'email'
+    }
+
+    // Generate list of collections names
+    const collectionStrings = []
+    for (const property in collections) {
+      collectionStrings.push(property.toString())
+    }
+
+    // Setup Transaction
+    const trx = await transaction(collectionStrings)
+
+    try {
+      await trx.step(
+        () => query`
+        UPSERT { _key: ${user._key} }
+          INSERT { 
+            phoneDetails: null,
+            phoneValidated: false,
+            tfaSendMethod: ${tfaSendMethod}
+          }
+          UPDATE {
+            phoneDetails: null,
+            phoneValidated: false,
+            tfaSendMethod: ${tfaSendMethod}
+          }
+          IN users
+      `,
+      )
+    } catch (err) {
+      console.error(
+        `Trx step error occurred well removing phone number for user: ${user._key}: ${err}`,
+      )
+      throw new Error(
+        i18n._(t`Unable to remove phone number. Please try again.`),
+      )
+    }
+
+    try {
+      await trx.commit()
+    } catch (err) {
+      console.error(
+        `Trx commit error occurred well removing phone number for user: ${user._key}: ${err}`,
+      )
+      throw new Error(
+        i18n._(t`Unable to remove phone number. Please try again.`),
+      )
+    }
+
+    console.info(`User: ${user._key} successfully removed their phone number.`)
+    return {
+      _type: 'result',
+      status: i18n._(t`Phone number has been successfully removed.`),
+    }
+  },
+})
