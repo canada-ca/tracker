c09e37de8a5ba091358d7dcffccfa7925e7816c7
Thomas Nickerson <64759920+ThomasNickerson@users.noreply.github.com>
2021-04-08 10:37:29 -0300

    Update dmarcReportSummaryTable fixture, tests pass

--- frontend/src/fixtures/dmarcReportSummaryTable.js
+++ frontend/src/fixtures/dmarcReportSummaryTable.js
@@ -1,52 +1,48 @@
 export const rawDmarcReportSummaryTableData = {
   data: {
-    findMyDomains: {
+    findMyDmarcSummaries: {
       edges: [
         {
           node: {
             id: 'testid1=',
-            domain: 'domain1.ca',
-            dmarcSummaryByPeriod: {
-              month: 'LAST30DAYS',
-              year: '2021',
-              domain: { domain: 'domain1.ca' },
-              categoryPercentages: {
-                failPercentage: 31.5,
-                fullPassPercentage: 1.88,
-                passDkimOnlyPercentage: 4.51,
-                passSpfOnlyPercentage: 4.12,
-                totalMessages: 7803,
-                __typename: 'CategoryPercentages',
-              },
-              __typename: 'Period',
+            domain: {
+              domain: 'domain1.ca',
+              __typename: 'Domain',
             },
-            __typename: 'Domain',
+            month: 'LAST30DAYS',
+            year: '2021',
+            categoryPercentages: {
+              failPercentage: 31.5,
+              fullPassPercentage: 1.88,
+              passDkimOnlyPercentage: 4.51,
+              passSpfOnlyPercentage: 4.12,
+              totalMessages: 7803,
+              __typename: 'CategoryPercentages',
+            },
+            __typename: 'DmarcSummary',
           },
-
-          __typename: 'DomainEdge',
+          __typename: 'DmarcSummaryEdge',
         },
         {
           node: {
             id: 'testid2=',
-            domain: 'domain2.ca',
-            dmarcSummaryByPeriod: {
-              month: 'LAST30DAYS',
-              year: '2021',
-              domain: { domain: 'domain2.ca' },
-              categoryPercentages: {
-                failPercentage: 31.5,
-                fullPassPercentage: 1.88,
-                passDkimOnlyPercentage: 4.51,
-                passSpfOnlyPercentage: 4.12,
-                totalMessages: 7803,
-                __typename: 'CategoryPercentages',
-              },
-              __typename: 'Period',
+            domain: {
+              domain: 'domain2.ca',
+              __typename: 'Domain',
             },
-            __typename: 'Domain',
+            month: 'LAST30DAYS',
+            year: '2021',
+            categoryPercentages: {
+              failPercentage: 31.5,
+              fullPassPercentage: 1.88,
+              passDkimOnlyPercentage: 4.51,
+              passSpfOnlyPercentage: 4.12,
+              totalMessages: 7803,
+              __typename: 'CategoryPercentages',
+            },
+            __typename: 'DmarcSummary',
           },
-
-          __typename: 'DomainEdge',
+          __typename: 'DmarcSummaryEdge',
         },
       ],
       pageInfo: {
@@ -56,7 +52,7 @@ export const rawDmarcReportSummaryTableData = {
         startCursor: 'startcursor=',
         __typename: 'PageInfo',
       },
-      __typename: 'DomainConnection',
+      __typename: 'DmarcSummaryConnection',
     },
   },
 }
