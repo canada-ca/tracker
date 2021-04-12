3fee4f6771f8675ef39a588a63e4e5124c7b9de7
Thomas Nickerson <64759920+ThomasNickerson@users.noreply.github.com>
2021-04-12 10:40:18 -0300

    Use single source of truth for setup.py version

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -1,5 +1,10 @@
 import setuptools
 
+# Get the version from single source
+version = {}
+with open("tracker_client/__version__.py") as version_file:
+    exec(version_file.read(), version)
+
 requirements = [
     "aiohttp>=3.7.1,<3.8.0",
     "gql>=3.0.0a5",
@@ -11,18 +16,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tracker_client",
-    version="1.0.0-alpha.1",
+    version=version["__version__"],
     author="Thomas Nickerson",
     author_email="contact@cyber.gc.ca",
     description="A Python client for the GoC Tracker API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/canada-ca/tracker/tree/master/clients/python",
-    license = "MIT",
+    license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
