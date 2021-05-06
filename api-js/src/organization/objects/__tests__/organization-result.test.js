1c85d743615ec35f878d5d3f126be7be8793d6d2
nsdeschenes <nsdeschenes@outlook.com>
2021-05-06 08:02:36 -0300

    update org result obj

--- api-js/src/organization/objects/__tests__/organization-result.test.js
+++ api-js/src/organization/objects/__tests__/organization-result.test.js
@@ -1,6 +1,7 @@
 import { GraphQLString } from 'graphql'
 
 import { organizationResultType } from '../organization-result'
+import { organizationType } from '../organization'
 
 describe('given the organizationResultType object', () => {
   describe('testing the field definitions', () => {
@@ -10,6 +11,12 @@ describe('given the organizationResultType object', () => {
       expect(demoType).toHaveProperty('status')
       expect(demoType.status.type).toMatchObject(GraphQLString)
     })
+    it('has an organization field', () => {
+      const demoType = organizationResultType.getFields()
+
+      expect(demoType).toHaveProperty('organization')
+      expect(demoType.organization.type).toMatchObject(organizationType)
+    })
   })
 
   describe('testing the field resolvers', () => {
@@ -18,6 +25,17 @@ describe('given the organizationResultType object', () => {
         const demoType = organizationResultType.getFields()
 
         expect(demoType.status.resolve({ status: 'status' })).toEqual('status')
+      })
+    })
+    describe('testing the organization resolver', () => {
+      it('returns the resolved field', () => {
+        const demoType = organizationResultType.getFields()
+
+        expect(
+          demoType.organization.resolve({
+            organization: { id: 1, name: 'org name' },
+          }),
+        ).toEqual({ id: 1, name: 'org name' })
       })
     })
   })
