48f9846633324edd974a912933a82b6bfdaa94ab
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-05-11 11:22:38 -0300

    Fix toConstantCase regex

--- frontend/src/helpers/toConstantCase.js
+++ frontend/src/helpers/toConstantCase.js
@@ -1,3 +1,9 @@
 export const toConstantCase = (str) => {
-  return str.toUpperCase().replace(/ /g, '_')
+  // ex.  toConstantCase('fullPassPercentage') === 'FULL_PASS_PERCENTAGE'
+  // ex.  toConstantCase('full Pass Percentage') === 'FULL_PASS_PERCENTAGE'
+  // ex.  toConstantCase('full pass percentage') === 'FULL_PASS_PERCENTAGE'
+  return str
+    .replace(/([A-Z]|\s([a-z]))/g, '_$1')
+    .replace(/\s+/g, '')
+    .toUpperCase()
 }
