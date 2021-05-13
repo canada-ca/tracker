18a2efefc51f35d81b01d1e97ec30744cf50f1f6
Mike Williamson <mike@korora.ca>
2021-05-13 16:38:11 -0400

    Add explicit test for JWT expiry time (#2222)
    This commit adds a test proving that jwt expires in 1 hour by default.

--- api-js/src/auth/__tests__/generate-jwt.test.js
+++ api-js/src/auth/__tests__/generate-jwt.test.js
@@ -3,16 +3,30 @@ import { tokenize } from '../index'
 
 const { AUTHENTICATED_KEY } = process.env
 
-describe('given a token generator', () => {
-  describe('given a set of parameters token can be encoded', () => {
-    it('returns a valid encoded token', () => {
+describe('tokenize()', () => {
+  describe('when passed an object of parameters', () => {
+    it('encodes them into the token', () => {
       const token = tokenize({ parameters: { userKey: 1 } })
 
       const decoded = jwt.verify(token, String(AUTHENTICATED_KEY))
       expect(decoded.parameters.userKey).toEqual(1)
     })
   })
-  describe('given no parameters, token can still be encoded', () => {
+})
+
+describe('tokenize()', () => {
+  describe('when no iat/exp parameters are passed', () => {
+    it('expires in 1 hour (3600 seconds) by default', () => {
+      const token = tokenize({secret: 'foo' })
+
+      const decoded = jwt.verify(token, 'foo')
+      expect(decoded.exp - decoded.iat).toEqual(3600)
+    })
+  })
+})
+
+describe('tokenize()', () => {
+  describe('given no parameters', () => {
     it('returns a valid encoded token', () => {
       const token = tokenize({})
 
