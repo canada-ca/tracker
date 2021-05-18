5aec05d8ddc7f53ef9f5732e92e031dae764e89b
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-05-18 12:45:05 -0300

    Reset cache on language change

--- frontend/src/LocaleSwitcher.js
+++ frontend/src/LocaleSwitcher.js
@@ -3,51 +3,62 @@ import { useLingui } from '@lingui/react'
 import { locales, activate } from './i18n.config'
 import svgGlobe from './images/vector-globe.svg'
 import { Box, PseudoBox, VisuallyHidden, Image } from '@chakra-ui/core'
+import { ApolloConsumer } from '@apollo/client'
 
 const Toggler = props => {
   const { locale } = props // eslint-disable-line
   return (
-    <PseudoBox
-      as="button"
-      key={locale}
-      padding={0}
-      onClick={() => activate(locale)}
-      _focus={{
-        outline: `3px solid accent`,
-      }}
-      bg="primary"
-      color="#fff"
-    >
-      <VisuallyHidden>{locales[locale]}</VisuallyHidden>
-      <PseudoBox
-        aria-hidden
-        fontSize="sm"
-        pl={2}
-        py={1}
-        textTransform="uppercase"
-        d={{ base: 'none', md: 'flex' }}
-        alignItems="center"
-        justifyContent="center"
-        _hover={{ color: 'accent', border: '1px solid', borderColor: 'accent' }}
-      >
-        {locales[locale]}
-        <Image src={svgGlobe} px={2} alt={'SVG Globe'} />
-      </PseudoBox>
-      <PseudoBox
-        aria-hidden
-        d={{ base: 'flex', md: 'none' }}
-        bg="gray.100"
-        color="primary"
-        textTransform="uppercase"
-        fontWeight="bold"
-        fontSize="lg"
-        size={10}
-        alignItems="center"
-        justifyContent="center"
-      >
-        {locale}
-      </PseudoBox>
-    </PseudoBox>
+    <ApolloConsumer>
+      {client => (
+        <PseudoBox
+          as='button'
+          key={locale}
+          padding={0}
+          onClick={() => {
+            activate(locale).then(() => client.resetStore())
+          }}
+          _focus={{
+            outline: `3px solid accent`,
+          }}
+          bg='primary'
+          color='#fff'
+        >
+          <VisuallyHidden>{locales[locale]}</VisuallyHidden>
+          <PseudoBox
+            aria-hidden
+            fontSize='sm'
+            pl={2}
+            py={1}
+            textTransform='uppercase'
+            d={{ base: 'none', md: 'flex' }}
+            alignItems='center'
+            justifyContent='center'
+            _hover={{
+              color: 'accent',
+              border: '1px solid',
+              borderColor: 'accent',
+            }}
+          >
+            {locales[locale]}
+            <Image src={svgGlobe} px={2} alt={'SVG Globe'} />
+          </PseudoBox>
+          <PseudoBox
+            aria-hidden
+            d={{ base: 'flex', md: 'none' }}
+            bg='gray.100'
+            color='primary'
+            textTransform='uppercase'
+            fontWeight='bold'
+            fontSize='lg'
+            size={10}
+            alignItems='center'
+            justifyContent='center'
+          >
+            {locale}
+          </PseudoBox>
+        </PseudoBox>)}
+
+    </ApolloConsumer>
   )
 }
 
@@ -55,8 +66,8 @@ export function LocaleSwitcher() {
   const { i18n } = useLingui()
   return (
     <Box>
-      {i18n.locale === 'en' && <Toggler locale="fr" />}
-      {i18n.locale === 'fr' && <Toggler locale="en" />}
+      {i18n.locale === 'en' && <Toggler locale='fr' />}
+      {i18n.locale === 'fr' && <Toggler locale='en' />}
     </Box>
   )
 }
