cea240fd4dc447fd39569cb9f544b0d04ad2bcbd
nsdeschenes <nc575894@dal.ca>
2021-05-27 06:33:02 -0300

    Update Core Service Collection Creations (#2286)
    * update core service create_collections
    
    * update replication factor to 3
    
    * update database options
    
    * update dmarc-report database options

--- services/dmarc-report/database-options.js
+++ services/dmarc-report/database-options.js
@@ -2,107 +2,33 @@ const databaseOptions = ({ rootPass }) => [
   { type: 'user', username: 'root', password: rootPass },
   {
     type: 'documentcollection',
-    name: 'users',
-  },
-  {
-    type: 'documentcollection',
     name: 'organizations',
+    options: { replicationFactor: 3, writeConcern: 1, numberOfShards: 6 },
   },
   {
     type: 'documentcollection',
     name: 'domains',
-  },
-  {
-    type: 'documentcollection',
-    name: 'dkim',
-  },
-  {
-    type: 'documentcollection',
-    name: 'dkimResults',
-  },
-  {
-    type: 'documentcollection',
-    name: 'dmarc',
-  },
-  {
-    type: 'documentcollection',
-    name: 'spf',
-  },
-  {
-    type: 'documentcollection',
-    name: 'https',
-  },
-  {
-    type: 'documentcollection',
-    name: 'ssl',
-  },
-  {
-    type: 'documentcollection',
-    name: 'dkimGuidanceTags',
-  },
-  {
-    type: 'documentcollection',
-    name: 'dmarcGuidanceTags',
-  },
-  {
-    type: 'documentcollection',
-    name: 'spfGuidanceTags',
-  },
-  {
-    type: 'documentcollection',
-    name: 'httpsGuidanceTags',
-  },
-  {
-    type: 'documentcollection',
-    name: 'sslGuidanceTags',
-  },
-  {
-    type: 'documentcollection',
-    name: 'chartSummaries',
+    options: { replicationFactor: 3, writeConcern: 1, numberOfShards: 6 },
   },
   {
     type: 'documentcollection',
     name: 'dmarcSummaries',
-  },
-  {
-    type: 'edgecollection',
-    name: 'affiliations',
+    options: { replicationFactor: 3, writeConcern: 1, numberOfShards: 6 },
   },
   {
     type: 'edgecollection',
     name: 'claims',
-  },
-  {
-    type: 'edgecollection',
-    name: 'domainsDKIM',
-  },
-  {
-    type: 'edgecollection',
-    name: 'dkimToDkimResults',
-  },
-  {
-    type: 'edgecollection',
-    name: 'domainsDMARC',
-  },
-  {
-    type: 'edgecollection',
-    name: 'domainsSPF',
-  },
-  {
-    type: 'edgecollection',
-    name: 'domainsHTTPS',
-  },
-  {
-    type: 'edgecollection',
-    name: 'domainsSSL',
+    options: { replicationFactor: 3, writeConcern: 1, numberOfShards: 6 },
   },
   {
     type: 'edgecollection',
     name: 'ownership',
+    options: { replicationFactor: 3, writeConcern: 1, numberOfShards: 6 },
   },
   {
     type: 'edgecollection',
     name: 'domainsToDmarcSummaries',
+    options: { replicationFactor: 3, writeConcern: 1, numberOfShards: 6 },
   },
 ]
 
