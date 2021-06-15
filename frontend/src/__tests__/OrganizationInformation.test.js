340553b7e382af7841160e382c60779ce26a12e5
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-06-15 17:07:57 -0300

    Code cleanup

--- frontend/src/__tests__/OrganizationInformation.test.js
+++ frontend/src/__tests__/OrganizationInformation.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ThemeProvider, theme } from '@chakra-ui/core'
 import { MemoryRouter } from 'react-router-dom'
-import { render, waitFor, screen } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
 import { MockedProvider } from '@apollo/client/testing'
 import OrganizationInformation from '../OrganizationInformation'
 import { REMOVE_ORGANIZATION, UPDATE_ORGANIZATION } from '../graphql/mutations'
