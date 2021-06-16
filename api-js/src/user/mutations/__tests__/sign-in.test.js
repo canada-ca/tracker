b03e73d483b5784a432b9df2ebe0f3960d9f6704
nsdeschenes <nsdeschenes@outlook.com>
2021-06-16 07:02:37 -0300

    update tests broken because of new refresh token

--- api-js/src/user/mutations/__tests__/sign-in.test.js
+++ api-js/src/user/mutations/__tests__/sign-in.test.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs'
 import { graphql, GraphQLSchema, GraphQLError } from 'graphql'
 import { setupI18n } from '@lingui/core'
 import { v4 as uuidv4 } from 'uuid'
+import jwt from 'jsonwebtoken'
 
 import englishMessages from '../../../locale/en/messages'
 import frenchMessages from '../../../locale/fr/messages'
@@ -72,6 +73,8 @@ describe('authenticate user account', () => {
         query,
         collections,
         transaction,
+        jwt,
+        uuidv4,
         auth: {
           bcrypt,
           tokenize,
