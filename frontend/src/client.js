2450e0f73422da10c6883449887c1611ad18280a
Kyle Sullivan <47400288+FestiveKyle@users.noreply.github.com>
2021-06-18 15:24:31 -0300

    Fe fix admin panel not showing all orgs (#2467)
    * Fix admin page showing limited list of organizations
    
    * Fix client uri

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -11,7 +11,7 @@ export function createCache() {
         fields: {
           findMyDomains: relayStylePagination(),
           findMyDmarcSummaries: relayStylePagination(),
-          findMyOrganizations: relayStylePagination(),
+          findMyOrganizations: relayStylePagination(['isAdmin']),
         },
       },
       Organization: {
