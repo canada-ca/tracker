72e6d290b9dbca288f0856f519eebaa6b47c80c8
nsdeschenes <nsdeschenes@outlook.com>
2021-06-24 09:35:11 -0300

    continued work on leaving orgs

--- api-js/src/affiliation/mutations/leave-organization.js
+++ api-js/src/affiliation/mutations/leave-organization.js
@@ -27,7 +27,7 @@ export const leaveOrganization = new mutationWithClientMutationId({
       query,
       collections,
       transaction,
-      auth: { userRequired, verifiedRequired },
+      auth: { checkOrgOwner, checkPermission, userRequired, verifiedRequired },
       loaders: { loadOrgByKey },
       validators: { cleanseInput },
     },
@@ -45,32 +45,151 @@ export const leaveOrganization = new mutationWithClientMutationId({
       return {
         _type: 'error',
         code: 400,
-        description: i18n._(t``),
+        description: i18n._(t`Unable leave organization.`),
       }
     }
 
-    // check to see if user belongs to org
-    let affiliatedCursor
+
+    // check to see if org owner
+    const owner = await checkOrgOwner({ orgId: org._id })
+
+    // Generate list of collections names
+    const collectionStrings = []
+    for (const property in collections) {
+      collectionStrings.push(property.toString())
+    }
+
+    // Setup Trans action
+    const trx = await transaction(collectionStrings)
+
+    if (!owner) {
+      try {
+        await trx.step(
+          () => query`
+            WITH affiliations, organizations, users
+            FOR v, e IN 1..1 OUTBOUND ${org._id} affiliations
+              FILTER e._to == ${user._id}
+              REMOVE { _key: e._key } IN affiliations
+          `,
+        )
+      } catch (err) {
+        console.error(
+          `Trx step error occurred when removing user: ${user._key} affiliation with org: ${org._key} err: ${err}`,
+        )
+        throw new Error(i18n._(t`Unable leave organization. Please try again.`))
+      }
+    } else {
+      try {
+        await Promise.all([
+          trx.step(() => {
+            query`
+              WITH claims, dkim, domains, domainsDKIM, organizations
+              LET domainEdges = (FOR v, e IN 1..1 ANY ${org._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+              FOR domainEdge in domainEdges
+                LET dkimEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsDKIM RETURN { edgeKey: e._key, dkimId: e._to })
+                LET removeDkimEdges = (FOR dkimEdge IN dkimEdges REMOVE dkimEdge.edgeKey IN domainsDKIM)
+                LET removeDkim = (FOR dkimEdge IN dkimEdges LET key = PARSE_IDENTIFIER(dkimEdge.dkimId).key REMOVE key IN dkim)
+              RETURN true
+            `
+          }),
+          trx.step(() => {
+            query`
+              WITH claims, dmarc, domains, domainsDMARC, organizations
+              LET domainEdges = (FOR v, e IN 1..1 ANY ${org._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+              FOR domainEdge in domainEdges
+                LET dmarcEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsDMARC RETURN { edgeKey: e._key, dmarcId: e._to })
+                LET removeDmarcEdges = (FOR dmarcEdge IN dmarcEdges REMOVE dmarcEdge.edgeKey IN domainsDMARC)
+                LET removeDmarc = (FOR dmarcEdge IN dmarcEdges LET key = PARSE_IDENTIFIER(dmarcEdge.dmarcId).key REMOVE key IN dmarc)
+              RETURN true
+            `
+          }),
+          trx.step(() => {
+            query`
+              WITH claims, domains, domainsSPF, organizations, spf
+              LET domainEdges = (FOR v, e IN 1..1 ANY ${org._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+              FOR domainEdge in domainEdges
+                LET spfEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsSPF RETURN { edgeKey: e._key, spfId: e._to })
+                LET removeSpfEdges = (FOR spfEdge IN spfEdges REMOVE spfEdge.edgeKey IN domainsSPF)
+                LET removeSpf = (FOR spfEdge IN spfEdges LET key = PARSE_IDENTIFIER(spfEdge.spfId).key REMOVE key IN spf)
+              RETURN true
+            `
+          }),
+          trx.step(() => {
+            query`
+              WITH claims, domains, domainsHTTPS, https, organizations
+              LET domainEdges = (FOR v, e IN 1..1 ANY ${org._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+              FOR domainEdge in domainEdges
+                LET httpsEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsHTTPS RETURN { edgeKey: e._key, httpsId: e._to })
+                LET removeHttpsEdges = (FOR httpsEdge IN httpsEdges REMOVE httpsEdge.edgeKey IN domainsHTTPS)
+                LET removeHttps = (FOR httpsEdge IN httpsEdges LET key = PARSE_IDENTIFIER(httpsEdge.httpsId).key REMOVE key IN https)
+              RETURN true
+            `
+          }),
+          trx.step(() => {
+            query`
+              WITH claims, domains, domainsSSL, organizations, ssl
+              LET domainEdges = (FOR v, e IN 1..1 ANY ${org._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+              FOR domainEdge in domainEdges
+                LET sslEdges = (FOR v, e IN 1..1 ANY domainEdge.domainId domainsSSL RETURN { edgeKey: e._key, sslId: e._to})
+                LET removeSslEdges = (FOR sslEdge IN sslEdges REMOVE sslEdge.edgeKey IN domainsSSL)
+                LET removeSsl = (FOR sslEdge IN sslEdges LET key = PARSE_IDENTIFIER(sslEdge.sslId).key REMOVE key IN ssl)
+              RETURN true
+            `
+          }),
+        ])
+      } catch (err) {
+        console.error(
+          `Trx step error occurred while attempting to remove scan results for org: ${org._key}, when user: ${user._key} attempted to leave. error: ${err}`,
+        )
+        throw new Error(i18n._(t`Unable leave organization. Please try again.`))
+      }
+
+      try {
+        await Promise.all([
+          trx.step(() => {
+            query`
+              WITH claims, domains, organizations
+              LET domainEdges = (FOR v, e IN 1..1 ANY ${org._id} claims RETURN { edgeKey: e._key, domainId: e._to })
+              LET removeDomainEdges = (FOR domainEdge in domainEdges REMOVE domainEdge.edgeKey IN claims)
+              LET removeDomain = (FOR domainEdge in domainEdges LET key = PARSE_IDENTIFIER(domainEdge.domainId).key REMOVE key IN domains)
+              RETURN true
+            `
+          }),
+          trx.step(() => {
+            query`
+              WITH affiliations, organizations, users
+              LET userEdges = (FOR v, e IN 1..1 ANY ${org._id} affiliations RETURN { edgeKey: e._key, userKey: e._to })
+              LET removeUserEdges = (FOR userEdge IN userEdges REMOVE userEdge.edgeKey IN affiliations)
+              RETURN true
+            `
+          }),
+          trx.step(() => {
+            query`
+              WITH organizations
+              REMOVE ${org._key} IN organizations
+            `
+          }),
+        ])
+      } catch (err) {
+        console.error(
+          `Trx step error occurred while attempting to remove domain, affiliations, and the org for org: ${org._key}, when user: ${user._key} attempted to leave. error: ${err}`,
+        )
+        throw new Error(i18n._(t`Unable leave organization. Please try again.`))
+      }
+    }
+
     try {
-      affiliatedCursor = await query`
-        WITH affiliations, organizations, users
-        FOR v, e IN 1..1 INBOUND ${user._id} affiliations
-          FILTER e._from == ${org._id}
-          RETURN e
-      `
+      await trx.commit()
     } catch (err) {
-      console.error(`query error`)
-      throw new Error(i18n._(t``))
+      console.error(
+        `Trx commit error occurred when user: ${user._key} attempted to leave org: ${org._key}. error: ${err}`,
+      )
+      throw new Error(i18n._(t`Unable leave organization. Please try again.`))
     }
 
-    if (affiliatedCursor.count === 0) {
-      console.warn(`no affiliation`)
-      return {
-        _type: 'error',
-        code: 400,
-        description: i18n._(t``),
-      }
+    return {
+      _type: 'regular',
+      status: i18n._(t``),
     }
-    console.log('affiliation found')
   },
 })
