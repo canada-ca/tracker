3104810628addb0483c29a98cee59766d0fb4755
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-06-30 08:04:24 -0300

    Request only the latest scan information for guidance

--- frontend/src/graphql/queries.js
+++ frontend/src/graphql/queries.js
@@ -109,7 +109,7 @@ export const GET_GUIDANCE_TAGS_OF_DOMAIN = gql`
       dmarcPhase
       hasDMARCReport
       web {
-        https(first: 10, orderBy: { field: TIMESTAMP, direction: DESC }) {
+        https(first: 1, orderBy: { field: TIMESTAMP, direction: DESC }) {
           edges {
             cursor
             node {
@@ -177,7 +177,7 @@ export const GET_GUIDANCE_TAGS_OF_DOMAIN = gql`
             }
           }
         }
-        ssl(first: 10, orderBy: { field: TIMESTAMP, direction: DESC }) {
+        ssl(first: 1, orderBy: { field: TIMESTAMP, direction: DESC }) {
           edges {
             cursor
             node {
@@ -251,7 +251,7 @@ export const GET_GUIDANCE_TAGS_OF_DOMAIN = gql`
         }
       }
       email {
-        dkim(first: 10, orderBy: { field: TIMESTAMP, direction: DESC }) {
+        dkim(first: 1, orderBy: { field: TIMESTAMP, direction: DESC }) {
           edges {
             cursor
             node {
@@ -322,7 +322,7 @@ export const GET_GUIDANCE_TAGS_OF_DOMAIN = gql`
             }
           }
         }
-        dmarc(first: 10, orderBy: { field: TIMESTAMP, direction: DESC }) {
+        dmarc(first: 1, orderBy: { field: TIMESTAMP, direction: DESC }) {
           edges {
             cursor
             node {
@@ -389,7 +389,7 @@ export const GET_GUIDANCE_TAGS_OF_DOMAIN = gql`
             }
           }
         }
-        spf(first: 10, orderBy: { field: TIMESTAMP, direction: DESC }) {
+        spf(first: 1, orderBy: { field: TIMESTAMP, direction: DESC }) {
           edges {
             cursor
             node {
@@ -874,7 +874,7 @@ export const PAGINATED_DMARC_REPORT = gql`
     $year: Year!
     $after: String
     $first: Int
-    ) {
+  ) {
     findDomainByDomain(domain: $domain) {
       id
       dmarcSummaryByPeriod(month: $month, year: $year) {
