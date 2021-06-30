400505cf5c748ffc9f88b7b0530d96d53846458b
Adam Prins <AdamPrins@cmail.carleton.ca>
2021-06-30 11:39:19 -0400

    Change to Grid Layout

--- frontend/src/UserCard.js
+++ frontend/src/UserCard.js
@@ -1,50 +1,31 @@
 import React from 'react'
-import { Badge, Box, Text, PseudoBox } from '@chakra-ui/core'
+import { Badge, Grid, Text } from '@chakra-ui/core'
 import { string } from 'prop-types'
 
 export function UserCard({ userName, displayName, role }) {
   return (
-    <PseudoBox width="100%" p="8">
-      <Box
-        align="center"
-        mb={['1', '0']}
-        width="100%"
-      >
-        <Box
-          width={{sm:"100%", md:"50%"}}
-          display={{sm:"block", md:"inline-block"}}
-          align={{sm:"left", md:"center"}}
+    <Grid
+      templateColumns={{ base: 'auto', md: '45% auto auto' }}
+      columnGap="1.5rem"
+    >
+      <Text fontSize="md" wordBreak="break-all" ml={{ md: '1rem' }}>
+        {userName}
+      </Text>
+      <Text>{displayName}</Text>
+      {role && (
+        <Badge
+          color="primary"
+          bg="transparent"
+          borderColor="primary"
+          borderWidth="1px"
+          alignSelf={{ md: 'center' }}
+          mr={{ md: '1rem' }}
+          justifySelf={{ base: 'start', md: 'end' }}
         >
-          <Text fontSize="md" wordBreak="break-all">
-            {userName}
-          </Text>
-        </Box>
-        <Box
-          width={{sm:"100%", md:"40%"}}
-          display={{sm:"block", md:"inline-block"}}
-          align={{sm:"left", md:"center"}}
-        >
-          {displayName}
-        </Box>
-        <Box
-          width={{sm:"100%", md:"10%"}}
-          display={{sm:"block", md:"inline-block"}}
-          align={{sm:"left", md:"center"}}
-        >
-          {role && (
-            <Badge
-              color="primary"
-              bg="transparent"
-              borderColor="primary"
-              borderWidth="1px"
-              ml="auto"
-            >
-              {role}
-            </Badge>
-          )}
-        </Box>
-      </Box>
-    </PseudoBox>
+          {role}
+        </Badge>
+      )}
+    </Grid>
   )
 }
 
