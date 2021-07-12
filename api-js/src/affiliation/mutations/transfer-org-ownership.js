57a37c8de9187de88bb4d0cf5200ac29cad497c6
nsdeschenes <nsdeschenes@outlook.com>
2021-07-12 11:39:15 -0300

    create transferOrgOwnership mutation

--- api-js/src/affiliation/mutations/transfer-org-ownership.js
+++ api-js/src/affiliation/mutations/transfer-org-ownership.js
@@ -6,158 +6,223 @@ import { transferOrgOwnershipUnion } from '../unions'
 
 export const transferOrgOwnership = new mutationWithClientMutationId({
   name: 'TransferOrgOwnership',
-  description: 'This mutation allows a user to transfer org ownership to another user in the given org.',
+  description:
+    'This mutation allows a user to transfer org ownership to another user in the given org.',
   inputFields: () => ({
     orgId: {
       type: GraphQLNonNull(GraphQLID),
-      description: '',
+      description:
+        'Id of the organization the user is looking to transfer ownership of.',
     },
     userId: {
       type: GraphQLNonNull(GraphQLID),
-      description: '',
+      description:
+        'Id of the user that the org ownership is being transferred to.',
     },
   }),
-	outputFields: () => ({
-		result: {
-			type: transferOrgOwnershipUnion,
-			description: 
-				'`TransferOrgOwnershipUnion` resolving to either a `TransferOrgOwnershipResult` or `AffilitionError`.',
-			resolve: (payload) => payload,
-		}
-	}),
-	mutateAndGetPayload: async (
-		args,
-		{
-			i18n,
-			query,
-			collections,
-			transaction,
-			auth: { 
-				checkOrgOwner,
-				checkUserBelongsToOrg,
-				userRequired,
-				verifiedRequired
-			},
-			loaders: { loadOrgByKey, loadUserByKey },
-			validators: { cleanseInput },
-		},
-	) => {
-		const { id: orgKey } = fromGlobalId(cleanseInput(args.orgId))
-		const { id: userTransferKey } = fromGlobalId(cleanseInput(args.userId))
+  outputFields: () => ({
+    result: {
+      type: transferOrgOwnershipUnion,
+      description:
+        '`TransferOrgOwnershipUnion` resolving to either a `TransferOrgOwnershipResult` or `AffiliationError`.',
+      resolve: (payload) => payload,
+    },
+  }),
+  mutateAndGetPayload: async (
+    args,
+    {
+      i18n,
+      query,
+      collections,
+      transaction,
+      auth: { checkOrgOwner, userRequired, verifiedRequired },
+      loaders: { loadOrgByKey, loadUserByKey },
+      validators: { cleanseInput },
+    },
+  ) => {
+    // cleanse inputs
+    const { id: orgKey } = fromGlobalId(cleanseInput(args.orgId))
+    const { id: userTransferKey } = fromGlobalId(cleanseInput(args.userId))
 
-		const requestingUser = await userRequired()
+    // protect mutation from un-authed users
+    const requestingUser = await userRequired()
 
-		verifiedRequired({ user: requestingUser })
+    // ensure that user has email verified their account
+    verifiedRequired({ user: requestingUser })
 
-		const org = await loadOrgByKey.load(orgKey)
+    // load the requested org
+    const org = await loadOrgByKey.load(orgKey)
 
-		if (typeof org === 'undefined') {
-			console.warn(`User: ${requestingUser._key} attempted to transfer org ownership of an undefined org.`)
-			return {
-				_type: 'error',
-				code: 400,
-				description: i18n._(t`Unable to transfer ownership of undefined organization.`)
-			}
-		}
+    // ensure requested org is not undefined
+    if (typeof org === 'undefined') {
+      console.warn(
+        `User: ${requestingUser._key} attempted to transfer org ownership of an undefined org.`,
+      )
+      return {
+        _type: 'error',
+        code: 400,
+        description: i18n._(
+          t`Unable to transfer ownership of undefined organization.`,
+        ),
+      }
+    }
+    // ensure org is not verified
+    else if (org.verified) {
+      console.warn(
+        `User: ${requestingUser._key} attempted to transfer ownership of a verified org: ${org.slug}.`,
+      )
+      return {
+        _type: 'error',
+        code: 400,
+        description: i18n._(
+          t`Unable to transfer ownership of a verified organization.`,
+        ),
+      }
+    }
 
-		if (org.verified) {
-			console.warn(`User: ${requestingUser._key} attempted to transfer ownership of a verified org: ${org._key}.`)
-			return {
-				_type: 'error',
-				code: 400,
-				description: i18n._(t`Unable to transfer ownership of a verified organization.`))
-			}
-		}
+    // get org owner bool value
+    const owner = await checkOrgOwner({ orgId: org._id })
 
-		const requestedUser = await loadUserByKey.load(userTransferKey)
+    // check to see if requesting user is the org owner
+    if (!owner) {
+      console.warn(
+        `User: ${requestingUser._key} attempted to transfer org: ${org.slug} ownership but does not have current ownership.`,
+      )
+      return {
+        _type: 'error',
+        code: 400,
+        description: i18n._(
+          t`Permission Denied: Please contact org owner to transfer ownership.`,
+        ),
+      }
+    }
 
-		if (typeof requestedUser === 'undefined') {
-			console.warn(`User: ${requestingUser._key} attempted to transfer org: ${org._key} ownership to an undefined user.`)
-			return {
-				_type: 'error',
-				code: 400,
-				description i18n._(t`Unable to transfer ownership of an org to an undefined user.`)
-			}
-		}
-		
-		const owner = await checkOrgOwner({ orgId: org._id })
+    // get the user that is org ownership is being transferred to
+    const requestedUser = await loadUserByKey.load(userTransferKey)
 
-		// check to see if requested user belongs to org
-		let affiliationCursor
-		try {
-			affiliationCursor = await query`
+    // check to ensure requested user is not undefined
+    if (typeof requestedUser === 'undefined') {
+      console.warn(
+        `User: ${requestingUser._key} attempted to transfer org: ${org.slug} ownership to an undefined user.`,
+      )
+      return {
+        _type: 'error',
+        code: 400,
+        description: i18n._(
+          t`Unable to transfer ownership of an org to an undefined user.`,
+        ),
+      }
+    }
+
+    // query db for requested user affiliation to org
+    let affiliationCursor
+    try {
+      affiliationCursor = await query`
 				WITH affiliations, organizations, users
 				FOR v, e IN 1..1 OUTBOUND ${org._id} affiliations
 					FILTER e._to == ${requestedUser._id}
 					RETURN e
 			`
-		} catch (err) {
-			console.error(`Database error occurred for user: ${requestingUser._key} when they were attmepting to `)
-			throw new Error(i18n._(t`Unable to transfer organization ownership. Please try again.`))
-		}
-	
-		if (affiliationCursor.count < 1) {
-			console.warn(`User: ${requestingUser._key} attempted to transfer org: ${org._key} ownership to user: ${requestedUser} but they are not affiliated with the org.`)
-			return {
-				_type: 'error',
-				code: 400,
-				description: i18n._(t`Unable to transfer ownership to a user outside the org. Please invite the user and try again.`),
-			}
-		}
+    } catch (err) {
+      console.error(
+        `Database error occurred for user: ${requestingUser._key} when they were attempting to transfer org: ${org.slug} ownership to user: ${requestedUser._key}: ${err}`,
+      )
+      throw new Error(
+        i18n._(t`Unable to transfer organization ownership. Please try again.`),
+      )
+    }
 
-		// remove current org owners role
-		try {
-			await trx.step(
-				() =>
-				  query`
-						WITH affiliations, organizations, users
-						FOR aff IN affilitions
-							FILTER aff._from == ${org._id}
-							AND aff._to == ${requestingUser._id}
-							UPDATE aff WITH {
-								owner: false,
-							} IN affiliations
-							RETURN aff
-					`
-			)
-		} catch (err) {
-			console.error(`Trx step error occurred while removing org: ${org._key} for user: ${requestingUser._key}.`)
-			throw new Error(i18n._(t`Unable to transfer organization ownership. Please try again.`))
-		}
+    // check to see if requested user belongs to org
+    if (affiliationCursor.count < 1) {
+      console.warn(
+        `User: ${requestingUser._key} attempted to transfer org: ${org.slug} ownership to user: ${requestedUser._key} but they are not affiliated with the org.`,
+      )
+      return {
+        _type: 'error',
+        code: 400,
+        description: i18n._(
+          t`Unable to transfer ownership to a user outside the org. Please invite the user and try again.`,
+        ),
+      }
+    }
 
-		// set new org owner
-		try {
-			await trx.step(
-				() =>
-					query`
+    // Generate list of collections names
+    const collectionStrings = []
+    for (const property in collections) {
+      collectionStrings.push(property.toString())
+    }
+
+    // Setup Trans action
+    const trx = await transaction(collectionStrings)
+
+    // remove current org owners role
+    try {
+      await trx.step(
+        () =>
+          query`
 						WITH affiliations, organizations, users
 						FOR aff IN affiliations
 							FILTER aff._from == ${org._id}
-							AND aff._to == ${requestedUser._id}
-							UPDATE aff WITH {
+							FILTER aff._to == ${requestingUser._id}
+							UPDATE { _key: aff._key } WITH {
+								owner: false,
+							} IN affiliations
+							RETURN aff
+					`,
+      )
+    } catch (err) {
+      console.error(
+        `Trx step error occurred for user: ${requestingUser._key} when they were attempting to transfer org: ${org.slug} ownership to user: ${requestedUser._key}: ${err}`,
+      )
+      throw new Error(
+        i18n._(t`Unable to transfer organization ownership. Please try again.`),
+      )
+    }
+
+    // set new org owner
+    try {
+      await trx.step(
+        () =>
+          query`
+						WITH affiliations, organizations, users
+						FOR aff IN affiliations
+							FILTER aff._from == ${org._id}
+							FILTER aff._to == ${requestedUser._id}
+							UPDATE { _key: aff._key } WITH {
 								owner: true,
 							} IN affiliations
 							RETURN aff
-					`
-			)
-		} catch (err) {
-			console.error(`Trx step error occurred while setting org: ${org._key} new owner to user: ${requestedUser._key}.`)
-			throw new Error(i18n._(t`Unable to transfer organization ownership. Please try again.`))
-		}
+					`,
+      )
+    } catch (err) {
+      console.error(
+        `Trx step error occurred for user: ${requestingUser._key} when they were attempting to transfer org: ${org.slug} ownership to user: ${requestedUser._key}: ${err}`,
+      )
+      throw new Error(
+        i18n._(t`Unable to transfer organization ownership. Please try again.`),
+      )
+    }
 
-		try {
-			await trx.commit()
-		} catch (err) {
-			console.error(``)
-			throw new Error(i18n._(t`Unable to transfer organization ownership. Please try again.`))
-		}
+    // commit changes to the db
+    try {
+      await trx.commit()
+    } catch (err) {
+      console.error(
+        `Trx commit error occurred for user: ${requestingUser._key} when they were attempting to transfer org: ${org.slug} ownership to user: ${requestedUser._key}: ${err}`,
+      )
+      throw new Error(
+        i18n._(t`Unable to transfer organization ownership. Please try again.`),
+      )
+    }
 
-		console.info(
-						`User: ${requestingUser._key} successfully transfer org: ${org._key} ownership to user: ${requestedUser._key}.`
-		)
-		return {
-			_type: 'regular'
-			status: i18n._(t``)
-		}
-	}
+    console.info(
+      `User: ${requestingUser._key} successfully transfer org: ${org.slug} ownership to user: ${requestedUser._key}.`,
+    )
+    return {
+      _type: 'regular',
+      status: i18n._(
+        t`Successfully transferred org: ${org.slug} ownership to user: ${requestedUser.userName}`,
+      ),
+    }
+  },
 })
