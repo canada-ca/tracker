465c3c83a39441a66b4ef892515256f888a45c0b
Ethan Foss <fossliam@outlook.com>
2021-07-13 18:22:39 -0300

    Amends OTS logic within scanners and queues

--- services/queues/ots-result/tests/test_result_queue.py
+++ services/queues/ots-result/tests/test_result_queue.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 from pretend import stub
 from result_queue import Server, dispatch_dns, dispatch_https, dispatch_ssl
@@ -29,7 +30,7 @@ def test_enqueue_dns(client):
         "selectors": ["selector1._domainkey", "selector2._domainkey"],
     }
 
-    res = client.post("/dns", json=test_payload)
+    res = client.post("/dns", json=json.dumps(test_payload))
 
     assert res.data.decode("utf-8") == "DNS result processing request enqueued."
 
@@ -41,7 +42,7 @@ def test_enqueue_https(client):
         "domain": "cyber.gc.ca",
     }
 
-    res = client.post("/https", json=test_payload)
+    res = client.post("/https", json=json.dumps(test_payload))
 
     assert res.data.decode("utf-8") == "HTTPS result processing request enqueued."
 
@@ -53,6 +54,6 @@ def test_enqueue_ssl(client):
         "domain": "cyber.gc.ca",
     }
 
-    res = client.post("/ssl", json=test_payload)
+    res = client.post("/ssl", json=json.dumps(test_payload))
 
     assert res.data.decode("utf-8") == "SSL result processing request enqueued."
