da751a97d67b3087a86768f0176f63f069f0bb4f
Adam Prins <AdamPrins@cmail.carleton.ca>
2021-07-16 09:42:25 -0400

    Remove commented code from Accordion theme

--- frontend/src/theme/components/Accordion.js
+++ frontend/src/theme/components/Accordion.js
@@ -17,25 +17,3 @@ const Accordion = {
 }
 
 export default Accordion
-
-/*
-justifyContent: 'center',
-fontWeight: 'semibold',
-rounded: 'md',
-transition: 'all 0.2s cubic-bezier(.08,.52,.52,1)',
-overflow: 'hidden',
-px: '4',
-py: '2',
-color: 'gray.50',
-bg: 'primary',
-_hover: {
-  bg: 'primary2',
-},
-_active: {
-  boxShadow: 'outline',
-},
-_focus: {
-  boxShadow: 'outline',
-},
-_disabled: { opacity: '0.4', cursor: 'not-allowed', boxShadow: 'none' },
-*/
