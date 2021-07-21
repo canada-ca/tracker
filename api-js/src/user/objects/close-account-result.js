455f7a8a7aeb806b863b664ca2a10989c72e54cb
nsdeschenes <nc575894@dal.ca>
2021-07-21 11:46:21 -0300

    Close Your Account Today (#2662)
    * create close account objects
    
    * create close account union
    
    * start working on mutation
    
    * more progress
    
    * update translations
    
    * update translations
    
    * fix up the close account mutation
    
    * finish up the testing for close account mutation
    
    * update gql descriptions
    
    * filter on the domain id as well
    
    * update translations
    
    * update error message
    
    * update tests

---
+++ api-js/src/user/objects/close-account-result.js
@@ -0,0 +1,14 @@
+import { GraphQLObjectType, GraphQLString } from 'graphql'
+
+export const closeAccountResult = new GraphQLObjectType({
+  name: 'CloseAccountResult',
+  description:
+    'This object is used to inform the user of the status of closing their account.',
+  fields: () => ({
+    status: {
+      type: GraphQLString,
+      description: 'Status of closing the users account.',
+      resolve: ({ status }) => status,
+    },
+  }),
+})
---
+++ api-js/src/user/unions/__tests__/close-account-union.test.js
@@ -0,0 +1,45 @@
+import { closeAccountError, closeAccountResult } from '../../objects'
+import { closeAccountUnion } from '../close-account-union'
+
+describe('given the closeAccountUnion', () => {
+  describe('testing the field types', () => {
+    it('contains closeAccountResult', () => {
+      const demoType = closeAccountUnion.getTypes()
+
+      expect(demoType).toContain(closeAccountResult)
+    })
+    it('contains closeAccountError', () => {
+      const demoType = closeAccountUnion.getTypes()
+
+      expect(demoType).toContain(closeAccountError)
+    })
+  })
+  describe('testing the field selection', () => {
+    describe('testing the closeAccountResult', () => {
+      it('returns the correct type', () => {
+        const obj = {
+          _type: 'regular',
+          authResult: {},
+        }
+
+        expect(closeAccountUnion.resolveType(obj)).toMatchObject(
+          closeAccountResult,
+        )
+      })
+    })
+    describe('testing the closeAccountError', () => {
+      it('returns the correct type', () => {
+        const obj = {
+          _type: 'error',
+          error: 'sign-in-error',
+          code: 401,
+          description: 'text',
+        }
+
+        expect(closeAccountUnion.resolveType(obj)).toMatchObject(
+          closeAccountError,
+        )
+      })
+    })
+  })
+})
