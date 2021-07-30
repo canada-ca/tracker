eed05c631fba93828e05d481401c85cc12bc31e4
lcampbell2 <64781228+lcampbell2@users.noreply.github.com>
2021-07-30 13:13:17 -0300

    Fix app reload on translate (#2725)
    * rearrange index.js components and move refresh mutation to prevent reload on translate
    
    * Undo previous changes, move i18nProvider inside App.js
    
    * add trycatch to silent refresh
    
    * move loading conditional
    
    * move i18nProvider back to index.js in separate component with App.js
    
    * remove commented out code
    
    * move useEffect changes to index.js
    
    * remove loading
    
    * `history.replace(from)` takes user to requested page on active refresh and doesn't affect user on silent refresh
    
    * fix missing import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,90 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
-import { I18nProvider } from '@lingui/react'
-import { i18n } from '@lingui/core'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { BrowserRouter as Router } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  useHistory,
+  useLocation,
+} from 'react-router-dom'
 import canada from './theme/canada'
 import { client, currentUserVar } from './client'
-import { ApolloProvider } from '@apollo/client'
-import { UserVarProvider } from './UserState'
+import { ApolloProvider, useMutation } from '@apollo/client'
+import { UserVarProvider, useUserVar } from './UserState'
+import { I18nProvider } from '@lingui/react'
+import { i18n } from '@lingui/core'
+import { REFRESH_TOKENS } from './graphql/mutations'
+import { activate } from './i18n.config'
+
+const I18nApp = () => {
+  const { currentUser, login } = useUserVar()
+  const location = useLocation()
+  const { from } = location.state || { from: { pathname: '/' } }
+  const history = useHistory()
+
+  const [refreshTokens] = useMutation(REFRESH_TOKENS, {
+    onError(error) {
+      console.error(error.message)
+    },
+    onCompleted({ refreshTokens }) {
+      if (refreshTokens.result.__typename === 'AuthResult') {
+        if (!currentUser.jwt) {
+          // User not logged in yet, set up environment (redirect and lang)
+          if (refreshTokens.result.user.preferredLang === 'ENGLISH')
+            activate('en')
+          else if (refreshTokens.result.user.preferredLang === 'FRENCH')
+            activate('fr')
+        }
+        login({
+          jwt: refreshTokens.result.authToken,
+          tfaSendMethod: refreshTokens.result.user.tfaSendMethod,
+          userName: refreshTokens.result.user.userName,
+        })
+        if (from.pathname !== '/') history.replace(from)
+      }
+      // Non server error occurs
+      else if (refreshTokens.result.__typename === 'AuthenticateError') {
+        // Could not authenticate
+      } else {
+        console.warn('Incorrect authenticate.result typename.')
+      }
+    },
+  })
+
+  useEffect(() => {
+    if (currentUser?.jwt) {
+      const jwtPayload = currentUser.jwt.split('.')[1]
+      const payloadDecoded = window.atob(jwtPayload)
+      const jwtExpiryTimeSeconds = JSON.parse(payloadDecoded).exp
+      // using seconds as that's what the api uses
+      const currentTimeSeconds = Math.floor(new Date().getTime() / 1000)
+      const jwtExpiresAfterSeconds = jwtExpiryTimeSeconds - currentTimeSeconds
+      const timeoutID = setTimeout(() => {
+        refreshTokens()
+      }, (jwtExpiresAfterSeconds - 60) * 1000)
+      return () => {
+        clearTimeout(timeoutID)
+      }
+    } else {
+      refreshTokens()
+    }
+  }, [currentUser, refreshTokens])
+
+  return (
+    <I18nProvider i18n={i18n}>
+      <App />
+    </I18nProvider>
+  )
+}
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <UserVarProvider userVar={currentUserVar}>
       <ChakraProvider theme={canada}>
-        <I18nProvider i18n={i18n}>
-          <Router>
-            <App />
-          </Router>
-        </I18nProvider>
+        <Router>
+          <I18nApp />
+        </Router>
       </ChakraProvider>
     </UserVarProvider>
   </ApolloProvider>,
