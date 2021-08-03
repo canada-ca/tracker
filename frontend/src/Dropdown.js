883ef30fc7401cbbf2592d227b63ab59f86f33f0
lcampbell2 <l.campbell144768@gmail.com>
2021-08-03 15:30:46 -0300

    fix alignment

--- frontend/src/Dropdown.js
+++ frontend/src/Dropdown.js
@@ -7,6 +7,7 @@ import {
   Input,
   InputGroup,
   InputRightElement,
+  Flex,
 } from '@chakra-ui/react'
 
 export function Dropdown({
@@ -116,7 +117,7 @@ export function Dropdown({
         }}
       >
         <label>
-          <Stack
+          <Flex
             flexDirection={{ base: 'column', md: labelDirection }}
             align="center"
           >
@@ -153,7 +154,7 @@ export function Dropdown({
                 />
               </InputRightElement>
             </InputGroup>
-          </Stack>
+          </Flex>
         </label>
       </Box>
       <Box
