abd8827840c1b49ca969c18ef3eef468409299bc
Adam <55841241+AdamPrins@users.noreply.github.com>
2021-08-04 11:42:13 -0400

    Leave Org Button (#2750)
    * Primary implementation
    
    * fix orgName and result type
    
    * move button to summary tab
    
    * fix leaveOrg to leaveOrganization
    
    * change button placement
    
    * add redirect
    
    * remove space
    
    * add loading state

--- frontend/src/OrganizationDetails.js
+++ frontend/src/OrganizationDetails.js
@@ -1,20 +1,32 @@
 import React from 'react'
-import { useQuery } from '@apollo/client'
-import { Trans } from '@lingui/macro'
+import { useQuery, useMutation } from '@apollo/client'
+import { t, Trans } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
 import {
   Box,
+  Button,
+  Flex,
   Heading,
   IconButton,
-  Stack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
+  useDisclosure,
+  useToast,
 } from '@chakra-ui/react'
 import { ArrowLeftIcon, CheckCircleIcon } from '@chakra-ui/icons'
 import { ORG_DETAILS_PAGE } from './graphql/queries'
-import { Link as RouteLink, useParams } from 'react-router-dom'
+import { LEAVE_ORG } from './graphql/mutations'
+import { Link as RouteLink, useParams, useHistory } from 'react-router-dom'
 import { OrganizationSummary } from './OrganizationSummary'
 import { ErrorBoundary } from 'react-error-boundary'
 import { ErrorFallbackMessage } from './ErrorFallbackMessage'
@@ -25,6 +37,9 @@ import { useDocumentTitle } from './useDocumentTitle'
 
 export default function OrganizationDetails() {
   const { orgSlug } = useParams()
+  const toast = useToast()
+  const history = useHistory()
+  const { i18n } = useLingui()
 
   useDocumentTitle(`${orgSlug}`)
 
@@ -32,6 +47,63 @@ export default function OrganizationDetails() {
     variables: { slug: orgSlug },
     errorPolicy: 'ignore', // allow partial success
   })
+
+  const [leaveOrganization, { loading: loadingLeaveOrg }] = useMutation(
+    LEAVE_ORG,
+    {
+      onError(error) {
+        toast({
+          title: i18n._(t`An error occurred.`),
+          description: error.message,
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+          position: 'top-left',
+        })
+      },
+      onCompleted({ leaveOrganization }) {
+        if (leaveOrganization.result.__typename === 'LeaveOrganizationResult') {
+          toast({
+            title: i18n._(t`Organization left successfully`),
+            description: i18n._(t`You have successfully left ${orgSlug}`),
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+            position: 'top-left',
+          })
+          leaveOrgOnClose()
+          history.push('/organizations')
+        } else if (leaveOrganization.result.__typename === 'AffiliationError') {
+          toast({
+            title: i18n._(t`Unable to leave organization.`),
+            description: leaveOrganization.result.description,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+            position: 'top-left',
+          })
+        } else {
+          toast({
+            title: i18n._(t`Incorrect send method received.`),
+            description: i18n._(
+              t`Incorrect leaveOrganization.result typename.`,
+            ),
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+            position: 'top-left',
+          })
+          console.log('Incorrect leaveOrganization.result typename.')
+        }
+      },
+    },
+  )
+
+  const {
+    isOpen: leaveOrgIsOpen,
+    onOpen: leaveOrgOnOpen,
+    onClose: leaveOrgOnClose,
+  } = useDisclosure()
 
   if (loading) {
     return (
@@ -45,7 +117,12 @@ export default function OrganizationDetails() {
 
   return (
     <Box w="100%" px={4}>
-      <Stack isInline align="center" mb="4">
+      <Flex
+        flexDirection="row"
+        align="center"
+        mb="4"
+        flexWrap={{ base: 'wrap', md: 'nowrap' }}
+      >
         <IconButton
           icon={<ArrowLeftIcon />}
           as={RouteLink}
@@ -53,14 +130,36 @@ export default function OrganizationDetails() {
           color="gray.900"
           fontSize="2xl"
           aria-label="back to organizations"
+          mr="0.5rem"
+          order={0}
         />
-        <Heading as="h1" textAlign={{ base: 'center', md: 'left' }}>
+        <Heading
+          as="h1"
+          textAlign={{ base: 'center', md: 'left' }}
+          mr={{ base: '0', md: '0.5rem' }}
+          order={{ base: 2, md: 1 }}
+          flexBasis={{ base: '100%', md: 'auto' }}
+        >
           {orgName}
+          {data?.organization?.verified && (
+            <>
+              {' '}
+              <CheckCircleIcon color="blue.500" boxSize="icons.lg" />
+            </>
+          )}
         </Heading>
-        {data?.organization?.verified && (
-          <CheckCircleIcon color="blue.500" size="icons.lg" />
-        )}
-      </Stack>
+        <Button
+          variant="danger"
+          order={{ base: 1, md: 2 }}
+          onClick={() => {
+            leaveOrgOnOpen()
+          }}
+          flexShrink={0}
+          ml="auto"
+        >
+          <Trans> Leave Organization </Trans>
+        </Button>
+      </Flex>
       <Tabs isFitted>
         <TabList mb="4">
           <Tab>
@@ -102,6 +201,47 @@ export default function OrganizationDetails() {
           )}
         </TabPanels>
       </Tabs>
+
+      <Modal
+        isOpen={leaveOrgIsOpen}
+        onClose={leaveOrgOnClose}
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent pb={4}>
+          <ModalHeader>
+            <Trans>Leave Organization</Trans>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Trans>
+              Are you sure you wish to leave {orgName}? You will have to be
+              invited back in to access it.
+            </Trans>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variant="primaryOutline" mr="4" onClick={leaveOrgOnClose}>
+              <Trans>Cancel</Trans>
+            </Button>
+
+            <Button
+              variant="primary"
+              mr="4"
+              onClick={async () => {
+                await leaveOrganization({
+                  variables: {
+                    orgId: data?.organization?.id,
+                  },
+                })
+              }}
+              isLoading={loadingLeaveOrg}
+            >
+              <Trans>Confirm</Trans>
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   )
 }
