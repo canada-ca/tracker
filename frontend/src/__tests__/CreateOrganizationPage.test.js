e84a55a01bcdd8a0409ab7e235eb2475b04407c2
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-08-10 17:28:49 -0300

    Add more tests to CreateOrganizationPage.test.js

--- frontend/src/__tests__/CreateOrganizationPage.test.js
+++ frontend/src/__tests__/CreateOrganizationPage.test.js
@@ -10,6 +10,7 @@ import { setupI18n } from '@lingui/core'
 import { UserVarProvider } from '../UserState'
 import { makeVar } from '@apollo/client'
 import { en } from 'make-plural/plurals'
+import userEvent from '@testing-library/user-event'
 
 const i18n = setupI18n({
   locale: 'en',
@@ -25,11 +26,30 @@ const mocks = [
   {
     request: {
       query: CREATE_ORGANIZATION,
+      variables: {
+        nameEN: 'Test Org Name EN',
+        nameFR: 'Test Org Name FR',
+        acronymEN: 'TESTACREN',
+        acronymFR: 'TESTACRFR',
+        cityEN: 'Test Org City EN',
+        cityFR: 'Test Org City FR',
+        provinceEN: 'Test Org Province EN',
+        provinceFR: 'Test Org Province FR',
+        countryEN: 'Test Org Country EN',
+        countryFR: 'Test Org Country FR',
+        zoneEN: '',
+        zoneFR: '',
+        sectorEN: '',
+        sectorFR: '',
+      },
     },
     result: {
       data: {
-        organization: {
-          name: 'New Test Org',
+        createOrganization: {
+          result: {
+            name: 'Test Org Name EN',
+            __typename: 'Organization',
+          },
         },
       },
     },
@@ -133,6 +153,190 @@ describe('<CreateOrganizationPage />', () => {
       })
 
       await waitFor(() => expect(getByText(errorMessage)).toBeInTheDocument())
+    })
+  })
+  describe('submits proper org creation data', () => {
+    it('successfully create org and displays message', async () => {
+      const { getByRole, findByText } = render(
+        <MockedProvider mocks={mocks}>
+          <UserVarProvider
+            userVar={makeVar({
+              jwt: null,
+              tfaSendMethod: null,
+              userName: null,
+            })}
+          >
+            <ChakraProvider theme={theme}>
+              <I18nProvider i18n={i18n}>
+                <MemoryRouter initialEntries={['/']} initialIndex={0}>
+                  <CreateOrganizationPage />
+                </MemoryRouter>
+              </I18nProvider>
+            </ChakraProvider>
+          </UserVarProvider>
+        </MockedProvider>,
+      )
+
+      const nameENInput = getByRole('textbox', { name: /Name \(English\)/ })
+      const nameFRInput = getByRole('textbox', { name: /Name \(French\)/ })
+
+      const acronymENInput = getByRole('textbox', {
+        name: /Acronym \(English\)/,
+      })
+      const acronymFRInput = getByRole('textbox', {
+        name: /Acronym \(French\)/,
+      })
+
+      const cityENInput = getByRole('textbox', { name: /City \(English\)/ })
+      const cityFRInput = getByRole('textbox', { name: /City \(French\)/ })
+
+      const provinceENInput = getByRole('textbox', {
+        name: /Province \(English\)/,
+      })
+      const provinceFRInput = getByRole('textbox', {
+        name: /Province \(French\)/,
+      })
+
+      const countryENInput = getByRole('textbox', {
+        name: /Country \(English\)/,
+      })
+      const countryFRInput = getByRole('textbox', {
+        name: /Country \(French\)/,
+      })
+
+      userEvent.type(nameENInput, 'Test Org Name EN')
+      userEvent.type(nameFRInput, 'Test Org Name FR')
+
+      userEvent.type(acronymENInput, 'TESTACREN')
+      userEvent.type(acronymFRInput, 'TESTACRFR')
+
+      userEvent.type(cityENInput, 'Test Org City EN')
+      userEvent.type(cityFRInput, 'Test Org City FR')
+
+      userEvent.type(provinceENInput, 'Test Org Province EN')
+      userEvent.type(provinceFRInput, 'Test Org Province FR')
+
+      userEvent.type(countryENInput, 'Test Org Country EN')
+      userEvent.type(countryFRInput, 'Test Org Country FR')
+
+      const createOrganizationButton = getByRole('button', {
+        name: /Create Organization/,
+      })
+      userEvent.click(createOrganizationButton)
+
+      const orgCreationToast = await findByText(/Test Org Name EN was created/)
+      await waitFor(() => expect(orgCreationToast).toBeVisible())
+    })
+  })
+
+  describe('submits improper org creation data', () => {
+    const improperCreationMocks = [
+      {
+        request: {
+          query: CREATE_ORGANIZATION,
+          variables: {
+            nameEN: 'Test Org Name EN',
+            nameFR: 'Test Org Name FR',
+            acronymEN: 'TESTACREN',
+            acronymFR: 'TESTACRFR',
+            cityEN: 'Test Org City EN',
+            cityFR: 'Test Org City FR',
+            provinceEN: 'Test Org Province EN',
+            provinceFR: 'Test Org Province FR',
+            countryEN: 'Test Org Country EN',
+            countryFR: 'Test Org Country FR',
+            zoneEN: '',
+            zoneFR: '',
+            sectorEN: '',
+            sectorFR: '',
+          },
+        },
+        result: {
+          data: {
+            createOrganization: {
+              result: {
+                code: 400,
+                description: 'Improper Information Given',
+                __typename: 'OrganizationError',
+              },
+            },
+          },
+        },
+      },
+    ]
+
+    it('displays error received from api', async () => {
+      const { getByRole, findByText } = render(
+        <MockedProvider mocks={improperCreationMocks}>
+          <UserVarProvider
+            userVar={makeVar({
+              jwt: null,
+              tfaSendMethod: null,
+              userName: null,
+            })}
+          >
+            <ChakraProvider theme={theme}>
+              <I18nProvider i18n={i18n}>
+                <MemoryRouter initialEntries={['/']} initialIndex={0}>
+                  <CreateOrganizationPage />
+                </MemoryRouter>
+              </I18nProvider>
+            </ChakraProvider>
+          </UserVarProvider>
+        </MockedProvider>,
+      )
+
+      const nameENInput = getByRole('textbox', {
+        name: /Name \(English\)/,
+      })
+      const nameFRInput = getByRole('textbox', { name: /Name \(French\)/ })
+
+      const acronymENInput = getByRole('textbox', {
+        name: /Acronym \(English\)/,
+      })
+      const acronymFRInput = getByRole('textbox', {
+        name: /Acronym \(French\)/,
+      })
+
+      const cityENInput = getByRole('textbox', { name: /City \(English\)/ })
+      const cityFRInput = getByRole('textbox', { name: /City \(French\)/ })
+
+      const provinceENInput = getByRole('textbox', {
+        name: /Province \(English\)/,
+      })
+      const provinceFRInput = getByRole('textbox', {
+        name: /Province \(French\)/,
+      })
+
+      const countryENInput = getByRole('textbox', {
+        name: /Country \(English\)/,
+      })
+      const countryFRInput = getByRole('textbox', {
+        name: /Country \(French\)/,
+      })
+
+      userEvent.type(nameENInput, 'Test Org Name EN')
+      userEvent.type(nameFRInput, 'Test Org Name FR')
+
+      userEvent.type(acronymENInput, 'TESTACREN')
+      userEvent.type(acronymFRInput, 'TESTACRFR')
+
+      userEvent.type(cityENInput, 'Test Org City EN')
+      userEvent.type(cityFRInput, 'Test Org City FR')
+
+      userEvent.type(provinceENInput, 'Test Org Province EN')
+      userEvent.type(provinceFRInput, 'Test Org Province FR')
+
+      userEvent.type(countryENInput, 'Test Org Country EN')
+      userEvent.type(countryFRInput, 'Test Org Country FR')
+
+      const createOrganizationButton = getByRole('button', {
+        name: /Create Organization/,
+      })
+      userEvent.click(createOrganizationButton)
+
+      const orgCreationToast = await findByText(/Improper Information Given/)
+      await waitFor(() => expect(orgCreationToast).toBeVisible())
     })
   })
 })
