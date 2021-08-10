a1dd9d3702d965c8961a0fd4e3b1999008cfb7cd
nsdeschenes <nsdeschenes@outlook.com>
2021-08-10 12:57:56 -0300

    export new functions

--- scripts/insert-json/src/index.js
+++ scripts/insert-json/src/index.js
@@ -1,5 +1,17 @@
+const { checkClaimCount } = require('./check-claims')
+const { checkDomain } = require('./check-domain')
+const { checkOrganization } = require('./check-organization')
+const { createClaim } = require('./create-claim')
+const { createDomain } = require('./create-domain')
+const { createOrganization } = require('./create-organization')
 const slugify = require('./slugify')
 
 module.exports = {
+  checkClaimCount,
+  checkDomain,
+  checkOrganization,
+  createClaim,
+  createDomain,
+  createOrganization,
   ...slugify,
 }
