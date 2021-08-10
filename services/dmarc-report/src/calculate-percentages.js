c7f55e9027e5ad3e208b3310a7616a0e526a8d0d
nsdeschenes <nsdeschenes@outlook.com>
2021-08-10 10:14:47 -0300

    go back to providing a single decimal place

--- services/dmarc-report/src/calculate-percentages.js
+++ services/dmarc-report/src/calculate-percentages.js
@@ -5,72 +5,24 @@ const calculatePercentages = ({ fail, pass, passDkimOnly, passSpfOnly }) => {
   )
 
   // calculate base percentages
-  const failPercentage = fail <= 0 ? 0 : Number((fail / total) * 100)
+  const failPercentage = Number(fail <= 0 ? 0 : Number((fail / total) * 100).toFixed(1))
 
-  const passPercentage = pass <= 0 ? 0 : Number((pass / total) * 100)
+  const passPercentage = Number(pass <= 0 ? 0 : Number((pass / total) * 100).toFixed(1))
 
   const passDkimOnlyPercentage =
-    passDkimOnly <= 0 ? 0 : Number((passDkimOnly / total) * 100)
+    Number(passDkimOnly <= 0 ? 0 : Number((passDkimOnly / total) * 100).toFixed(1))
 
   const passSpfOnlyPercentage =
-    passSpfOnly <= 0 ? 0 : Number((passSpfOnly / total) * 100)
+    Number(passSpfOnly <= 0 ? 0 : Number((passSpfOnly / total) * 100).toFixed(1))
 
-  const percentages = [
-    failPercentage,
-    passPercentage,
-    passDkimOnlyPercentage,
-    passSpfOnlyPercentage,
-  ]
-
-  // do a basic rounding of all percentages
-  const rounded = percentages.map((x) => Math.floor(x))
-
-  // get the sum of the current rounded percentages
-  const afterRoundSum = rounded.reduce((pre, curr) => pre + curr, 0)
-
-  // get the amount of numbers larger or equal to 1
-  const countMutableItems = rounded.filter((x) => x >= 1).length
-
-  // get the current error rate based off of the expected total and calculated total
-  const errorRate = 100 - afterRoundSum
-
-  // calculate the amount needed to add to the rounded percentages
-  const deductPortion = Math.ceil(errorRate / countMutableItems)
-
-  // sort the rounded percentages in desc order
-  const biggest = [...rounded]
-    .sort((a, b) => b - a)
-    .slice(0, Math.min(Math.abs(errorRate), countMutableItems))
-
-  // get the modified percentages
-  const [
-    calculatedFailPercentage,
-    calculatedPassPercentage,
-    calculatedPassDkimOnlyPercentage,
-    calculatedPassSpfOnlyPercentage,
-  ] = rounded.map((x) => {
-    // get the index of the current percentage
-    const indexOfX = biggest.indexOf(x)
-    // if its not the largest index
-    if (indexOfX >= 0) {
-      // add the calculated amount based off of error rate
-      x += deductPortion
-      // remove modified value from array
-      biggest.splice(indexOfX, 1)
-      // return modified value
-      return x
-    }
-    // return current value
-    return x
-  })
 
   return {
     totalMessages: total,
     percentages: {
-      fail: calculatedFailPercentage,
-      pass: calculatedPassPercentage,
-      passDkimOnly: calculatedPassDkimOnlyPercentage,
-      passSpfOnly: calculatedPassSpfOnlyPercentage,
+      fail: failPercentage,
+      pass: passPercentage,
+      passDkimOnly: passDkimOnlyPercentage,
+      passSpfOnly: passSpfOnlyPercentage,
     },
   }
 }
