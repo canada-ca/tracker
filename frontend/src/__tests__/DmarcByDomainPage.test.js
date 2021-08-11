a66b04bcbc3fcda6d5d2d2f6c98f7111d0753e44
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-08-11 13:04:24 -0300

    Add more tests to DmarcByDomainPage.test.js

--- frontend/src/__tests__/DmarcByDomainPage.test.js
+++ frontend/src/__tests__/DmarcByDomainPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ChakraProvider, theme } from '@chakra-ui/react'
-import { render, waitFor } from '@testing-library/react'
+import { render, waitFor, screen } from '@testing-library/react'
 import { MockedProvider } from '@apollo/client/testing'
 import { PAGINATED_DMARC_REPORT_SUMMARY_TABLE as FORWARD } from '../graphql/queries'
 import { I18nProvider } from '@lingui/react'
@@ -10,6 +10,9 @@ import DmarcByDomainPage from '../DmarcByDomainPage'
 import { rawDmarcReportSummaryTableData } from '../fixtures/dmarcReportSummaryTable'
 import { makeVar } from '@apollo/client'
 import { en } from 'make-plural/plurals'
+import matchMediaSize from '../helpers/matchMedia'
+import { MemoryRouter } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
 
 const i18n = setupI18n({
   locale: 'en',
@@ -21,15 +24,10 @@ const i18n = setupI18n({
   },
 })
 
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-  })),
-})
+matchMediaSize()
 
 describe('<DmarcByDomainPage />', () => {
+  const currentYear = new Date().getFullYear()
   const mocks = [
     {
       request: {
@@ -38,14 +36,64 @@ describe('<DmarcByDomainPage />', () => {
           first: 10,
           month: 'LAST30DAYS',
           year: '2021',
-          search: '',
           orderBy: {
             field: 'TOTAL_MESSAGES',
             direction: 'DESC',
           },
         },
       },
-      result: rawDmarcReportSummaryTableData,
+      result: { data: rawDmarcReportSummaryTableData },
+    },
+    {
+      request: {
+        query: FORWARD,
+        variables: {
+          first: 10,
+          month: 'AUGUST',
+          year: currentYear.toString(),
+          orderBy: {
+            field: 'TOTAL_MESSAGES',
+            direction: 'DESC',
+          },
+        },
+      },
+      result: {
+        data: {
+          findMyDmarcSummaries: {
+            pageInfo: {
+              hasNextPage: false,
+              hasPreviousPage: true,
+              startCursor: 'YXJyYXljb25uZWN0aW9uOjA=',
+              endCursor: 'YXJyYXljb25uZWN0aW9uOjk=',
+              __typename: 'PageInfo',
+            },
+            edges: [
+              {
+                node: {
+                  id: 'a26b94b0-a290-4912-a4ea-7563b07430f6',
+                  month: 'AUGUST',
+                  year: 2019,
+                  domain: {
+                    domain: 'another.domain',
+                    __typename: 'Domain',
+                  },
+                  categoryPercentages: {
+                    failPercentage: 7,
+                    fullPassPercentage: 76,
+                    passDkimOnlyPercentage: 14,
+                    passSpfOnlyPercentage: 3,
+                    totalMessages: 9501,
+                    __typename: 'CategoryPercentages',
+                  },
+                  __typename: 'DmarcSummary',
+                },
+                __typename: 'DmarcSummaryEdge',
+              },
+            ],
+            __typename: 'DmarcSummaryConnection',
+          },
+        },
+      },
     },
   ]
 
@@ -81,5 +129,51 @@ describe('<DmarcByDomainPage />', () => {
       </MockedProvider>,
     )
     await waitFor(() => getByText(/Showing data for period:/i))
+  })
+  it('renders table with data', async () => {
+    const { findByRole, findByText } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <UserVarProvider
+          userVar={makeVar({ jwt: null, tfaSendMethod: null, userName: null })}
+        >
+          <ChakraProvider theme={theme}>
+            <I18nProvider i18n={i18n}>
+              <MemoryRouter initialEntries={['/']} initialIndex={0}>
+                <DmarcByDomainPage />
+              </MemoryRouter>
+            </I18nProvider>
+          </ChakraProvider>
+        </UserVarProvider>
+      </MockedProvider>,
+    )
+    await findByRole('table')
+    await findByText(/erna.org/)
+  })
+  it('can change period', async () => {
+    const { findByRole, findByText, getByRole } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <UserVarProvider
+          userVar={makeVar({ jwt: null, tfaSendMethod: null, userName: null })}
+        >
+          <ChakraProvider theme={theme}>
+            <I18nProvider i18n={i18n}>
+              <MemoryRouter initialEntries={['/']} initialIndex={0}>
+                <DmarcByDomainPage />
+              </MemoryRouter>
+            </I18nProvider>
+          </ChakraProvider>
+        </UserVarProvider>
+      </MockedProvider>,
+    )
+    await findByRole('table')
+    await findByText(/erna.org/)
+
+    const periodSelector = getByRole('combobox', {
+      name: /showing data for period:/i,
+    })
+
+    userEvent.selectOptions(periodSelector, `AUGUST, ${currentYear}`)
+
+    await findByText(/another.domain/)
   })
 })
