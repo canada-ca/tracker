b0679bdf5a54dd6d6eb0164c2bf5e698e69d6033
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-08-11 17:08:36 -0300

    Add more tests to DmarcReportSummaryGraph.test.js

--- frontend/src/__tests__/DmarcReportSummaryGraph.test.js
+++ frontend/src/__tests__/DmarcReportSummaryGraph.test.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { theme, ChakraProvider } from '@chakra-ui/react'
-import { render, waitFor } from '@testing-library/react'
+import { render, waitFor, screen } from '@testing-library/react'
 import { I18nProvider } from '@lingui/react'
 import { setupI18n } from '@lingui/core'
 import { DmarcReportSummaryGraph } from '../DmarcReportSummaryGraph'
 import { formattedBarData } from '../fixtures/summaryListData'
+import userEvent from '@testing-library/user-event'
+import canada from '../theme/canada'
 
 // ** need to mock the ResizeObserver and polute the window object to avoid errors
 class ResizeObserver {
@@ -43,6 +45,80 @@ describe('<DmarcReportTimeGraph />', () => {
         </I18nProvider>
       </ChakraProvider>,
     )
-    await waitFor(() => getByText(/Fail DKIM/i))
+    getByText(/Aug-20/i, { selector: 'text' })
+  })
+
+  describe('is vertical', () => {
+    it('shows tooltip', async () => {
+      const { getByText, container, queryByText } = render(
+        <ChakraProvider theme={canada}>
+          <I18nProvider i18n={i18n}>
+            <DmarcReportSummaryGraph data={formattedBarData} />
+          </I18nProvider>
+        </ChakraProvider>,
+      )
+      getByText(/Aug-21/i, { selector: 'text' })
+
+      const { strong } = canada.colors
+
+      // query bottom-left "strong" rectangle
+      const firstStrongRect = container.querySelector(`rect[fill="${strong}"]`)
+      expect(firstStrongRect).toBeInTheDocument()
+
+      expect(
+        queryByText(
+          (_content, element) => element.textContent === 'Pass:2,048',
+        ),
+      ).not.toBeInTheDocument()
+
+      userEvent.hover(firstStrongRect)
+
+      await waitFor(() =>
+        expect(
+          getByText(
+            (_content, element) => element.textContent === 'Pass:2,048',
+          ),
+        ).toBeInTheDocument(),
+      )
+    })
+  })
+  describe('is horizontal', () => {
+    it('shows tooltip', async () => {
+      const { getByText, container, queryByText, getByRole } = render(
+        <ChakraProvider theme={canada}>
+          <I18nProvider i18n={i18n}>
+            <DmarcReportSummaryGraph data={formattedBarData} />
+          </I18nProvider>
+        </ChakraProvider>,
+      )
+      getByText(/Aug-21/i, { selector: 'text' })
+
+      const graphDirectionSelect = getByRole('combobox', {
+        name: /Graph direction:/i,
+      })
+      userEvent.selectOptions(graphDirectionSelect, 'Horizontal View')
+
+      const { strong } = canada.colors
+
+      // query bottom-left "strong" rectangle
+      const firstStrongRect = container.querySelector(`rect[fill="${strong}"]`)
+      expect(firstStrongRect).toBeInTheDocument()
+
+      expect(
+        queryByText(
+          (_content, element) => element.textContent === 'Pass:2,048',
+        ),
+      ).not.toBeInTheDocument()
+
+      userEvent.hover(firstStrongRect)
+
+      await waitFor(() =>
+        expect(
+          getByText(
+            (_content, element) => element.textContent === 'Pass:2,048',
+          ),
+        ).toBeInTheDocument(),
+      )
+    })
   })
 })
--- frontend/src/fixtures/summaryListData.js
+++ frontend/src/fixtures/summaryListData.js
@@ -3,179 +3,342 @@ import { t } from '@lingui/macro'
 export const rawSummaryListData = {
   dmarcReportSummaryList: [
     {
-      month: 'August',
+      month: 'AUGUST',
+      year: 2021,
+      domain: {
+        domain: 'alba.org',
+        __typename: 'Domain',
+      },
+      categoryTotals: {
+        passSpfOnly: 422,
+        passDkimOnly: 87,
+        fullPass: 4163,
+        fail: 5019,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 52,
+        fullPassPercentage: 43,
+        passDkimOnlyPercentage: 1,
+        passSpfOnlyPercentage: 4,
+        totalMessages: 9691,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
+    },
+    {
+      month: 'JULY',
+      year: 2021,
+      domain: {
+        domain: 'jodie.com',
+        __typename: 'Domain',
+      },
+      categoryTotals: {
+        passSpfOnly: 202,
+        passDkimOnly: 108,
+        fullPass: 121,
+        fail: 448,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 51,
+        fullPassPercentage: 14,
+        passDkimOnlyPercentage: 12,
+        passSpfOnlyPercentage: 23,
+        totalMessages: 879,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
+    },
+    {
+      month: 'JUNE',
+      year: 2021,
+      domain: {
+        domain: 'royal.org',
+        __typename: 'Domain',
+      },
+      categoryTotals: {
+        passSpfOnly: 399,
+        passDkimOnly: 755,
+        fullPass: 4691,
+        fail: 188,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 3,
+        fullPassPercentage: 78,
+        passDkimOnlyPercentage: 13,
+        passSpfOnlyPercentage: 7,
+        totalMessages: 6033,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
+    },
+    {
+      month: 'MAY',
+      year: 2021,
+      domain: {
+        domain: 'landen.biz',
+        __typename: 'Domain',
+      },
+      categoryTotals: {
+        passSpfOnly: 4,
+        passDkimOnly: 30,
+        fullPass: 42,
+        fail: 60,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 44,
+        fullPassPercentage: 31,
+        passDkimOnlyPercentage: 22,
+        passSpfOnlyPercentage: 3,
+        totalMessages: 136,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
+    },
+    {
+      month: 'APRIL',
+      year: 2021,
+      domain: {
+        domain: 'jason.biz',
+        __typename: 'Domain',
+      },
+      categoryTotals: {
+        passSpfOnly: 865,
+        passDkimOnly: 11,
+        fullPass: 1223,
+        fail: 326,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 13,
+        fullPassPercentage: 50,
+        passDkimOnlyPercentage: 0,
+        passSpfOnlyPercentage: 36,
+        totalMessages: 2425,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
+    },
+    {
+      month: 'MARCH',
+      year: 2021,
+      domain: {
+        domain: 'quinton.biz',
+        __typename: 'Domain',
+      },
+      categoryTotals: {
+        passSpfOnly: 1015,
+        passDkimOnly: 1203,
+        fullPass: 5045,
+        fail: 2253,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 24,
+        fullPassPercentage: 53,
+        passDkimOnlyPercentage: 13,
+        passSpfOnlyPercentage: 11,
+        totalMessages: 9516,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
+    },
+    {
+      month: 'FEBRUARY',
+      year: 2021,
+      domain: {
+        domain: 'german.org',
+        __typename: 'Domain',
+      },
+      categoryTotals: {
+        passSpfOnly: 126,
+        passDkimOnly: 335,
+        fullPass: 7190,
+        fail: 43,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 1,
+        fullPassPercentage: 93,
+        passDkimOnlyPercentage: 4,
+        passSpfOnlyPercentage: 2,
+        totalMessages: 7694,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
+    },
+    {
+      month: 'JANUARY',
+      year: 2021,
+      domain: {
+        domain: 'kianna.info',
+        __typename: 'Domain',
+      },
+      categoryTotals: {
+        passSpfOnly: 819,
+        passDkimOnly: 848,
+        fullPass: 4368,
+        fail: 180,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 3,
+        fullPassPercentage: 70,
+        passDkimOnlyPercentage: 14,
+        passSpfOnlyPercentage: 13,
+        totalMessages: 6215,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
+    },
+    {
+      month: 'DECEMBER',
       year: 2020,
-      categoryTotals: {
-        passSpfOnly: 4187,
-        passDkimOnly: 100,
-        fullPass: 4878,
-        fail: 12,
-        total: 0,
+      domain: {
+        domain: 'taryn.biz',
+        __typename: 'Domain',
       },
+      categoryTotals: {
+        passSpfOnly: 72,
+        passDkimOnly: 48,
+        fullPass: 2020,
+        fail: 64,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 3,
+        fullPassPercentage: 92,
+        passDkimOnlyPercentage: 2,
+        passSpfOnlyPercentage: 3,
+        totalMessages: 2204,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
     },
     {
-      month: 'July',
+      month: 'NOVEMBER',
       year: 2020,
-      categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
+      domain: {
+        domain: 'alverta.net',
+        __typename: 'Domain',
       },
+      categoryTotals: {
+        passSpfOnly: 435,
+        passDkimOnly: 400,
+        fullPass: 1635,
+        fail: 126,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 5,
+        fullPassPercentage: 63,
+        passDkimOnlyPercentage: 15,
+        passSpfOnlyPercentage: 17,
+        totalMessages: 2596,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
     },
     {
-      month: 'June',
+      month: 'OCTOBER',
       year: 2020,
-      categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
+      domain: {
+        domain: 'merl.biz',
+        __typename: 'Domain',
       },
+      categoryTotals: {
+        passSpfOnly: 3252,
+        passDkimOnly: 205,
+        fullPass: 3006,
+        fail: 109,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 2,
+        fullPassPercentage: 46,
+        passDkimOnlyPercentage: 3,
+        passSpfOnlyPercentage: 49,
+        totalMessages: 6572,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
     },
     {
-      month: 'May',
+      month: 'SEPTEMBER',
       year: 2020,
-      categoryTotals: {
-        passSpfOnly: 762,
-        passDkimOnly: 96,
-        fullPass: 2211,
-        fail: 32,
-        total: 0,
+      domain: {
+        domain: 'eula.name',
+        __typename: 'Domain',
       },
+      categoryTotals: {
+        passSpfOnly: 2087,
+        passDkimOnly: 189,
+        fullPass: 179,
+        fail: 57,
+        __typename: 'CategoryTotals',
+      },
+      categoryPercentages: {
+        failPercentage: 2,
+        fullPassPercentage: 7,
+        passDkimOnlyPercentage: 8,
+        passSpfOnlyPercentage: 83,
+        totalMessages: 2512,
+        __typename: 'CategoryPercentages',
+      },
+      __typename: 'DmarcSummary',
     },
     {
-      month: 'April',
+      month: 'AUGUST',
       year: 2020,
-      categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
+      domain: {
+        domain: 'frida.name',
+        __typename: 'Domain',
       },
-    },
-    {
-      month: 'March',
-      year: 2020,
       categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
+        passSpfOnly: 1292,
+        passDkimOnly: 1313,
+        fullPass: 2048,
+        fail: 4238,
+        __typename: 'CategoryTotals',
       },
-    },
-    {
-      month: 'February',
-      year: 2020,
-      categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
+      categoryPercentages: {
+        failPercentage: 48,
+        fullPassPercentage: 23,
+        passDkimOnlyPercentage: 15,
+        passSpfOnlyPercentage: 15,
+        totalMessages: 8891,
+        __typename: 'CategoryPercentages',
       },
-    },
-    {
-      month: 'January',
-      year: 2020,
-      categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
-      },
-    },
-    {
-      month: 'December',
-      year: 2019,
-      categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
-      },
-    },
-    {
-      month: 'November',
-      year: 2019,
-      categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
-      },
-    },
-    {
-      month: 'October',
-      year: 2019,
-      categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
-      },
-    },
-    {
-      month: 'September',
-      year: 2019,
-      categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
-      },
-    },
-    {
-      month: 'August',
-      year: 2019,
-      categoryTotals: {
-        passSpfOnly: 5853,
-        passDkimOnly: 5020,
-        fullPass: 5994,
-        fail: 1031,
-        total: 4,
-      },
+      __typename: 'DmarcSummary',
     },
   ],
 }
 
 const strengths = {
-  strong: [
-    {
-      name: 'fullPass',
-      displayName: t`Pass`,
-    },
-  ],
-  moderate: [
-    {
-      name: 'failDkim',
-      displayName: t`Fail DKIM`,
-    },
-    {
-      name: 'failSpf',
-      displayName: t`Fail SPF`,
-    },
-  ],
-  weak: [
-    {
-      name: 'fail',
-      displayName: t`Fail`,
-    },
-  ],
+  fullPass: `Pass`,
+  fullPassPercentage: `Pass`,
+
+  passSpfOnly: `Fail DKIM`,
+  passSpfOnlyPercentage: `Fail DKIM`,
+
+  passDkimOnly: `Fail SPF`,
+  passDkimOnlyPercentage: `Fail SPF`,
+
+  fail: `Fail`,
+  failPercentage: `Fail`,
 }
 
 export const formattedBarData = {
   periods: rawSummaryListData.dmarcReportSummaryList.map((entry) => {
-    return { month: entry.month, year: entry.year, ...entry.categoryTotals }
+    return {
+      month: entry.month,
+      year: entry.year,
+      ...entry.categoryTotals,
+      ...entry.categoryPercentages,
+    }
   }),
+  strengths: strengths,
 }
-formattedBarData.strengths = strengths
