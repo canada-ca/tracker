9ab36cb12edc14fea13cfe30638f7da46a55100a
Adam Prins <AdamPrins@cmail.carleton.ca>
2021-08-12 08:00:46 -0400

    favicon pulls from root directory

--- frontend/src/html.js
+++ frontend/src/html.js
@@ -21,26 +21,26 @@ export default () => `
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="images/apple-touch-icon.png"
+        href="/images/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="images/favicon-32x32.png"
+        href="/images/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="images/favicon-16x16.png"
+        href="/images/favicon-16x16.png"
       />
       <link
         rel="mask-icon"
-        href="images/safari-pinned-tab.svg"
+        href="/images/safari-pinned-tab.svg"
         color="#e65225"
       />
-      <link rel="shortcut icon" href="images/favicon.ico" />
+      <link rel="shortcut icon" href="/images/favicon.ico" />
       <link rel="manifest" href="manifest.json" />
       <meta name="msapplication-TileColor" content="#2e2e40" />
       <meta name="msapplication-config" content="./browserconfig.xml" />
