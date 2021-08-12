d7f1037605a768a6a25689d62fd741beee6c25dd
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-08-12 18:07:56 -0300

    Fix AdminPage.test.js

--- frontend/src/__tests__/AdminPage.test.js
+++ frontend/src/__tests__/AdminPage.test.js
@@ -4,9 +4,15 @@ import { theme, ChakraProvider } from '@chakra-ui/react'
 import { I18nProvider } from '@lingui/react'
 import { setupI18n } from '@lingui/core'
 import { MockedProvider } from '@apollo/client/testing'
-import { ADMIN_AFFILIATIONS, IS_USER_SUPER_ADMIN } from '../graphql/queries'
+import {
+  ADMIN_AFFILIATIONS,
+  IS_USER_SUPER_ADMIN,
+  ORGANIZATION_INFORMATION,
+  PAGINATED_ORG_AFFILIATIONS_ADMIN_PAGE,
+  PAGINATED_ORG_DOMAINS_ADMIN_PAGE,
+} from '../graphql/queries'
 import AdminPage from '../AdminPage'
-import { waitFor, render } from '@testing-library/react'
+import { waitFor, render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { makeVar } from '@apollo/client'
 import { en } from 'make-plural/plurals'
@@ -78,6 +84,132 @@ describe('<AdminPage />', () => {
       result: {
         data: {
           isUserSuperAdmin: false,
+        },
+      },
+    },
+    {
+      request: {
+        query: ORGANIZATION_INFORMATION,
+        variables: { orgSlug: 'Wolf-Group' },
+      },
+      result: {
+        data: {
+          findOrganizationBySlug: {
+            id: 'dd725149-d6ca-4f46-9a08-a6cf984b719e',
+            acronym: 'WOLFGROUP',
+            name: 'Wolf Group',
+            slug: 'Wolf-Group',
+            zone: 'WGZone',
+            sector: 'WGSector',
+            country: 'WGCountry',
+            province: 'WGProvince',
+            city: 'WGCity',
+            verified: true,
+            __typename: 'Organization',
+          },
+        },
+      },
+    },
+    {
+      request: {
+        query: PAGINATED_ORG_DOMAINS_ADMIN_PAGE,
+        variables: { orgSlug: 'Wolf-Group', first: 10, search: '' },
+      },
+      result: {
+        data: {
+          findOrganizationBySlug: {
+            id: 'dd725149-d6ca-4f46-9a08-a6cf984b719e',
+            name: 'Wolf Group',
+            domains: {
+              edges: [
+                {
+                  node: {
+                    id: 'e86770be-b13e-4bee-b833-6a2e31add85c',
+                    domain: 'antonia.name',
+                    lastRan: null,
+                    selectors: ['selector9._domainkey', 'selector7._domainkey'],
+                    __typename: 'Domain',
+                  },
+                  __typename: 'DomainEdge',
+                },
+                {
+                  node: {
+                    id: '11494bbf-1ed6-4edb-b96f-3fed6f36a226',
+                    domain: 'blaise.biz',
+                    lastRan: null,
+                    selectors: ['selector3._domainkey', 'selector5._domainkey'],
+                    __typename: 'Domain',
+                  },
+                  __typename: 'DomainEdge',
+                },
+              ],
+              pageInfo: {
+                hasNextPage: false,
+                hasPreviousPage: false,
+                startCursor: 'Hello World',
+                endCursor: 'Hello World',
+                __typename: 'PageInfo',
+              },
+              totalCount: 2,
+              __typename: 'DomainConnection',
+            },
+            __typename: 'Organization',
+          },
+        },
+      },
+    },
+    {
+      request: {
+        query: PAGINATED_ORG_AFFILIATIONS_ADMIN_PAGE,
+        variables: { orgSlug: 'Wolf-Group', first: 10, search: '' },
+      },
+      result: {
+        data: {
+          findOrganizationBySlug: {
+            id: 'dd725149-d6ca-4f46-9a08-a6cf984b719e',
+            affiliations: {
+              edges: [
+                {
+                  node: {
+                    id: '4f105cd9-e625-4fa7-9617-e574bff4cb2a',
+                    permission: 'USER',
+                    user: {
+                      id: '8f014942-f585-49cf-98fa-069f136aab73',
+                      userName: 'Joseph.Grimes@hotmail.com',
+                      displayName: 'Larry Klein',
+                      __typename: 'SharedUser',
+                    },
+                    __typename: 'Affiliation',
+                  },
+                  __typename: 'AffiliationEdge',
+                },
+                {
+                  node: {
+                    id: '9772d2ad-4687-423c-9c6e-4daa1f716591',
+                    permission: 'ADMIN',
+                    user: {
+                      id: '4a8ee5ca-a255-48bb-a3a1-1a3c74a4b6e2',
+                      userName: 'Roosevelt_Windler57@yahoo.com',
+                      displayName: 'Woodrow Jerde Jr.',
+                      __typename: 'SharedUser',
+                    },
+                    __typename: 'Affiliation',
+                  },
+                  __typename: 'AffiliationEdge',
+                },
+              ],
+              pageInfo: {
+                hasNextPage: false,
+                hasPreviousPage: false,
+                startCursor: 'Hello World',
+                endCursor: 'Hello World',
+                __typename: 'PageInfo',
+              },
+              totalCount: 2,
+              __typename: 'AffiliationConnection',
+            },
+            __typename: 'Organization',
+          },
         },
       },
     },
@@ -201,7 +333,9 @@ describe('<AdminPage />', () => {
       const orgEntry = getByText(/Wolf Group/)
       userEvent.click(orgEntry)
 
-      await findByText(/Slug: Wolf-Group/)
+      await waitFor(() => {
+        expect(getByText(/Slug:/i)).toHaveTextContent(/Slug: Wolf-Group/i)
+      })
     })
   })
 })
