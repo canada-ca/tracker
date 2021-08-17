7dfc99a38b4cb1cbd1349808335d2b48edb1b0be
nsdeschenes <nsdeschenes@outlook.com>
2021-08-17 06:52:23 -0300

    create factory function for load-ownerships

--- services/dmarc-report/src/loaders/load-ownerships.js
+++ services/dmarc-report/src/loaders/load-ownerships.js
@@ -2,28 +2,30 @@ const { GITHUB_TOKEN, GITHUB_URL } = process.env
 
 const { GET_FILE_CONTENTS } = require('../graphql')
 
-const loadDomainOwnership = async ({ fetch }) => {
-  let repoInfo
+const loadDomainOwnership =
+  ({ fetch }) =>
+  async () => {
+    let repoInfo
 
-  try {
-    repoInfo = await fetch(GITHUB_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `bearer ${GITHUB_TOKEN}`,
-      },
-      body: JSON.stringify({ query: GET_FILE_CONTENTS }),
-    }).then((response) => response.json())
+    try {
+      repoInfo = await fetch(GITHUB_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `bearer ${GITHUB_TOKEN}`,
+        },
+        body: JSON.stringify({ query: GET_FILE_CONTENTS }),
+      }).then((response) => response.json())
 
-    const domainOwnership = JSON.parse(repoInfo.data.repository.object.text)
+      const domainOwnership = JSON.parse(repoInfo.data.repository.object.text)
 
-    return domainOwnership.Federal.TBS
-  } catch (err) {
-    console.error(
-      `Error occurred while fetching domain ownership information: ${err}`,
-    )
+      return domainOwnership.Federal.TBS
+    } catch (err) {
+      console.error(
+        `Error occurred while fetching domain ownership information: ${err}`,
+      )
+    }
   }
-}
 
 module.exports = {
   loadDomainOwnership,
