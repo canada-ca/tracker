fc31701c5bea0addc26d7740b5a2d3b50e799562
nsdeschenes <nsdeschenes@outlook.com>
2021-09-09 10:04:16 -0300

    revert back to using regex for domain scalar

--- api-js/src/scalars/domain.js
+++ api-js/src/scalars/domain.js
@@ -1,15 +1,20 @@
 import { Kind, GraphQLError, GraphQLScalarType } from 'graphql'
-import psl from 'psl'
 
 const validate = (value) => {
   if (typeof value !== typeof 'string') {
     throw new TypeError(`Value is not a string: ${typeof value}`)
   }
-  if (!psl.isValid(value)) {
+
+  value = value.toLowerCase()
+
+  const DOMAIN_REGEX =
+    /\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/
+
+  if (!DOMAIN_REGEX.test(value)) {
     throw new TypeError(`Value is not a valid domain: ${value}`)
   }
 
-  return value.toLowerCase()
+  return value
 }
 
 export const Domain = new GraphQLScalarType({
