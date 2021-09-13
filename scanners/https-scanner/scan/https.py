184a974d4ca2916a6f73042c1fb1ff5b81c3a17e
Mike Williamson <mike@korora.ca>
2021-09-13 15:35:44 -0400

    initialize_external_data() at startup
    This commit moves the call to initialize_external_data() so that it executes
    when the event loop starts.

--- scanners/https-scanner/scan/https.py
+++ scanners/https-scanner/scan/https.py
@@ -24,11 +24,118 @@ from sslyze.plugins.certificate_info._certificate_utils import (
 from .models import Domain, Endpoint
 from .query_crlite import query_crlite
 
+
+def load_preload_list():
+    preload_json = None
+
+    # Downloads the chromium preloaded domain list and sets it to a global set
+    file_url = "https://chromium.googlesource.com/chromium/src/net/+/master/http/transport_security_state_static.json?format=TEXT"
+
+    try:
+        request = requests.get(file_url)
+    except (requests.exceptions.SSLError, requests.exceptions.ConnectionError) as err:
+        logging.debug("Failed to fetch preload list: {}".format(file_url))
+        logging.debug("{}".format(err))
+        return []
+
+    raw = request.content
+
+    # To avoid parsing the contents of the file out of the source tree viewer's
+    # HTML, we download it as a raw file. googlesource.com Base64-encodes the
+    # file to avoid potential content injection issues, so we need to decode it
+    # before using it. https://code.google.com/p/gitiles/issues/detail?id=7
+    raw = base64.b64decode(raw).decode("utf-8")
+
+    # The .json file contains '//' comments, which are not actually valid JSON,
+    # and confuse Python's JSON decoder. Begone, foul comments!
+    raw = "".join([re.sub(r"^\s*//.*$", "", line) for line in raw.splitlines()])
+
+    preload_json = json.loads(raw)
+
+    # For our purposes, we only care about entries that includeSubDomains
+    fully_preloaded = []
+    for entry in preload_json["entries"]:
+        if entry.get("include_subdomains", False) is True:
+            fully_preloaded.append(entry["name"])
+
+    return fully_preloaded
+
+
+def load_preload_pending():
+    """
+    Fetch the Chrome preload pending list.
+    """
+    pending_url = "https://hstspreload.org/api/v2/pending"
+
+    try:
+        request = requests.get(pending_url)
+    except (requests.exceptions.SSLError, requests.exceptions.ConnectionError) as err:
+        logging.debug("Failed to fetch pending preload list: {}".format(pending_url))
+        logging.debug("{}".format(err))
+        return []
+
+    # TODO: abstract Py 2/3 check out to utils
+    if sys.version_info[0] < 3:
+        raw = request.content
+    else:
+        raw = str(request.content, "utf-8")
+
+    pending_json = json.loads(raw)
+
+    pending = []
+    for entry in pending_json:
+        if entry.get("include_subdomains", False) is True:
+            pending.append(entry["name"])
+
+    return pending
+
+
+def load_suffix_list():
+    # File does not exist, download current list and cache it at given location.
+    try:
+        cache_file = publicsuffix.fetch()
+    except urllib.error.URLError as err:
+        logging.debug("Unable to download the Public Suffix List...")
+        return []
+    content = cache_file.readlines()
+    suffixes = publicsuffix.PublicSuffixList(content)
+    return suffixes, content
+
+def initialize_external_data():
+    """
+    This function serves to load all of third party external data.
+    This can be called explicitly by a library, as part of the setup needed
+    before calling other library functions, or called as part of running
+    inspect_domains() or CLI operation.
+    If values are passed in to this function, they will be assigned to
+    be the cached values. This allows a caller of the Python API to manage
+    cached data in a customized way.
+    It also potentially allows clients to pass in subsets of these lists,
+    for testing or novel performance reasons.
+    Otherwise, if the --cache-third-parties=[DIR] flag specifies a directory,
+    all downloaded third party data will be cached in a directory, and
+    used from cache on the next pshtt run instead of hitting the network.
+    If no values are passed in, and no --cache-third-parties flag is used,
+    then no cached third party data will be created or used, and pshtt will
+    download the latest data from those third party sources.
+    """
+    global preload_list, preload_pending, suffix_list
+
+    preload_list = load_preload_list()
+
+    preload_pending = load_preload_pending()
+
+    suffix_list, raw_content = load_suffix_list()
+
+
 suffix_list = None
 preload_pending = None
 preload_list = None
 STORE = "Mozilla"
 
+print(f"loading suffix list, hsts preload and pending lists.")
+initialize_external_data()
+print(f"lists loaded.")
 
 def result_for(domain):
 
@@ -767,7 +874,6 @@ def did_domain_error(domain):
 def run(domains):
     results = {}
 
-    initialize_external_data()
 
     for base_domain in domains:
 
@@ -1371,105 +1477,3 @@ def https_check(endpoint):
         endpoint.https_valid = False
 
 
-def initialize_external_data():
-    """
-    This function serves to load all of third party external data.
-    This can be called explicitly by a library, as part of the setup needed
-    before calling other library functions, or called as part of running
-    inspect_domains() or CLI operation.
-    If values are passed in to this function, they will be assigned to
-    be the cached values. This allows a caller of the Python API to manage
-    cached data in a customized way.
-    It also potentially allows clients to pass in subsets of these lists,
-    for testing or novel performance reasons.
-    Otherwise, if the --cache-third-parties=[DIR] flag specifies a directory,
-    all downloaded third party data will be cached in a directory, and
-    used from cache on the next pshtt run instead of hitting the network.
-    If no values are passed in, and no --cache-third-parties flag is used,
-    then no cached third party data will be created or used, and pshtt will
-    download the latest data from those third party sources.
-    """
-    global preload_list, preload_pending, suffix_list
-
-    preload_list = load_preload_list()
-
-    preload_pending = load_preload_pending()
-
-    suffix_list, raw_content = load_suffix_list()
-
-
-def load_preload_list():
-    preload_json = None
-
-    # Downloads the chromium preloaded domain list and sets it to a global set
-    file_url = "https://chromium.googlesource.com/chromium/src/net/+/master/http/transport_security_state_static.json?format=TEXT"
-
-    try:
-        request = requests.get(file_url)
-    except (requests.exceptions.SSLError, requests.exceptions.ConnectionError) as err:
-        logging.debug("Failed to fetch preload list: {}".format(file_url))
-        logging.debug("{}".format(err))
-        return []
-
-    raw = request.content
-
-    # To avoid parsing the contents of the file out of the source tree viewer's
-    # HTML, we download it as a raw file. googlesource.com Base64-encodes the
-    # file to avoid potential content injection issues, so we need to decode it
-    # before using it. https://code.google.com/p/gitiles/issues/detail?id=7
-    raw = base64.b64decode(raw).decode("utf-8")
-
-    # The .json file contains '//' comments, which are not actually valid JSON,
-    # and confuse Python's JSON decoder. Begone, foul comments!
-    raw = "".join([re.sub(r"^\s*//.*$", "", line) for line in raw.splitlines()])
-
-    preload_json = json.loads(raw)
-
-    # For our purposes, we only care about entries that includeSubDomains
-    fully_preloaded = []
-    for entry in preload_json["entries"]:
-        if entry.get("include_subdomains", False) is True:
-            fully_preloaded.append(entry["name"])
-
-    return fully_preloaded
-
-
-def load_preload_pending():
-    """
-    Fetch the Chrome preload pending list.
-    """
-    pending_url = "https://hstspreload.org/api/v2/pending"
-
-    try:
-        request = requests.get(pending_url)
-    except (requests.exceptions.SSLError, requests.exceptions.ConnectionError) as err:
-        logging.debug("Failed to fetch pending preload list: {}".format(pending_url))
-        logging.debug("{}".format(err))
-        return []
-
-    # TODO: abstract Py 2/3 check out to utils
-    if sys.version_info[0] < 3:
-        raw = request.content
-    else:
-        raw = str(request.content, "utf-8")
-
-    pending_json = json.loads(raw)
-
-    pending = []
-    for entry in pending_json:
-        if entry.get("include_subdomains", False) is True:
-            pending.append(entry["name"])
-
-    return pending
-
-
-def load_suffix_list():
-    # File does not exist, download current list and cache it at given location.
-    try:
-        cache_file = publicsuffix.fetch()
-    except urllib.error.URLError as err:
-        logging.debug("Unable to download the Public Suffix List...")
-        return []
-    content = cache_file.readlines()
-    suffixes = publicsuffix.PublicSuffixList(content)
-    return suffixes, content
