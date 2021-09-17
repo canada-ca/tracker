f34068e3b7d2cea034b44e55184b90443b222085
Mike Williamson <mike@korora.ca>
2021-09-17 14:32:56 -0400

    Make tls-processor deployable (#2975)
    This commit adds kubernetes config and cleans up the args to nats so the
    service starts.

--- scanners/tls-processor/tls_processor/__init__.py
+++ scanners/tls-processor/tls_processor/__init__.py
@@ -180,14 +180,6 @@ def process_results(results, domain_key, user_key, shared_id):
 
 
 async def run(loop):
-    parser = argparse.ArgumentParser()
-
-    # e.g. nats-sub hello -s nats://127.0.0.1:4222
-    parser.add_argument("subject", default="domains", nargs="?")
-    parser.add_argument("-s", "--servers", default=[], action="append")
-    parser.add_argument("-q", "--queue", default="https")
-    parser.add_argument("--creds", default="")
-    args = parser.parse_args()
 
     nc = NATS()
 
@@ -224,24 +216,17 @@ async def run(loop):
             f"{PUBLISH_TO}.{domain_key}.tls.processed", json.dumps(processed).encode()
         )
 
-    options = {
-        "loop": loop,
-        "error_cb": error_cb,
-        "closed_cb": closed_cb,
-        "reconnected_cb": reconnected_cb,
-    }
-
-    if len(args.creds) > 0:
-        options["user_credentials"] = args.creds
-
     try:
-        if len(args.servers) > 0:
-            options["servers"] = SERVERS
 
-        await nc.connect(**options)
+        await nc.connect(
+            loop=loop,
+            error_cb=error_cb,
+            closed_cb=closed_cb,
+            reconnected_cb=reconnected_cb,
+            servers=SERVERS,
+        )
     except Exception as e:
-        print(e)
-        show_usage_and_die()
+        print(f"Nats error callback: {e}")
 
     print(f"Connected to NATS at {nc.connected_url.netloc}...")
 
