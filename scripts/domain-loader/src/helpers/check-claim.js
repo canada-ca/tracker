908fb4baf709534ecd9c95c44f351dc40416c674
Kyle Sullivan <141261s@acadiau.ca>
2021-11-03 13:23:06 -0300

    Await entire result

--- scripts/domain-loader/src/helpers/check-claim.js
+++ scripts/domain-loader/src/helpers/check-claim.js
@@ -1,11 +1,13 @@
 const checkClaim = async ({ query, domainId, orgId }) => {
   console.info(`\t Checking domain claims ...`)
-  return await query`
+  return await (
+    await query`
     WITH claims, domains, organizations
     FOR v,e IN 1..1 ANY ${domainId} claims
     FILTER v._id == ${orgId}
       RETURN v
   `
+  ).all()
 }
 
 module.exports = {
