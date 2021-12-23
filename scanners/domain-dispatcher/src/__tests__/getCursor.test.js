b6bdb35484f098d492c0ca93b8a63511787eb876
Mike Williamson <mike@korora.ca>
2021-12-23 16:36:34 -0500

    Get tests working with the env vars that exist

--- scanners/domain-dispatcher/src/__tests__/getCursor.test.js
+++ scanners/domain-dispatcher/src/__tests__/getCursor.test.js
@@ -2,25 +2,53 @@ import { Database } from 'arangojs'
 import { getCursor } from '../getCursor.js'
 
 const {
-  DB_URL: url,
-  DB_NAME: databaseName,
+  DB_HOST: host,
+  DB_PORT: port,
+  DB_NAME,
+  DB_COLLECTION: collectionName,
   DB_PASS: password,
   DB_USER: username,
 } = process.env
 
-const db = new Database({ url, databaseName, auth: { username, password } })
+const sys = new Database({
+  url: `http://${host}:${port}`,
+  auth: { username, password },
+})
 
+const databaseName = `${DB_NAME}-${Date.now()}`
+
+let db, collection
 
 describe('domain-dispatcher', () => {
-  describe('getCursor', () => {
-    const domains = db.collection('domains')
+  beforeAll(async () => {
+    await sys.createDatabase(databaseName, {
+      users: [{ username: 'root' }],
+      precaptureStackTraces: true,
+    })
 
+    db = new Database({
+      url: `http://${host}:${port}`,
+      databaseName,
+      auth: { username, password },
+      precaptureStackTraces: true,
+    })
+
+    collection = db.collection(collectionName)
+    await collection.create()
+  })
+
+  afterAll(async () => {
+    await sys.dropDatabase(databaseName)
+  })
+
+  describe('getCursor', () => {
     afterEach(async () => {
-      await domains.truncate()
+      await collection.truncate()
     })
 
     describe('with 0 domains in the collection', () => {
       it('produces a cursor with a count of 0', async () => {
+        const domains = db.collection(collectionName)
         const cursor = await getCursor({ db, collection: domains })
         expect(cursor.count).toEqual(0)
       })
@@ -28,9 +56,9 @@ describe('domain-dispatcher', () => {
 
     describe('with 1 domain in the collection', () => {
       it('produces a cursor with a count of 1', async () => {
-        await domains.save({ domain: 'cyber.gc.ca' })
+        await collection.save({ domain: 'cyber.gc.ca' })
 
-        const cursor = await getCursor({ db, collection: domains })
+        const cursor = await getCursor({ db, collection })
         expect(cursor.count).toEqual(1)
       })
     })
