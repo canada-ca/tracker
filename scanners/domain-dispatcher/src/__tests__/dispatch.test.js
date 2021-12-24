a9a2d7094a104ab882758568c650a096c35f32c2
Mike Williamson <mike@korora.ca>
2021-12-24 11:18:25 -0500

    Include domain hash when publishing domain events

--- scanners/domain-dispatcher/src/__tests__/dispatch.test.js
+++ scanners/domain-dispatcher/src/__tests__/dispatch.test.js
@@ -20,12 +20,14 @@ describe('domain-dispatcher', () => {
                   user_key: 1,
                   domain: 'tbs-sct.gc.ca',
                   domain_key: 'domains/1',
+                  hash: '8a237c3e4033c5f9ec06f28edddbccd6',
                   shared_id: 1234,
                 },
                 {
                   user_key: 1,
                   domain: 'cyber.gc.ca',
                   domain_key: 'domains/2',
+                  hash: '56f36ef5e84af6eef45299d58890bfe5',
                   shared_id: 1235,
                 },
               ]
@@ -46,6 +48,52 @@ describe('domain-dispatcher', () => {
         expect(publish).toHaveBeenCalledTimes(2)
       })
 
+      it('includes the domain hash when publishing', async () => {
+        const publish = jest.fn()
+        const logger = {
+          info: jest.fn(),
+          error: jest.fn(),
+        }
+
+        const asyncIterable = {
+          batches: {
+            async *[Symbol.asyncIterator]() {
+              yield [
+                {
+                  user_key: 1,
+                  domain: 'tbs-sct.gc.ca',
+                  _key: '1',
+                  hash: '8a237c3e4033c5f9ec06f28edddbccd6',
+                  shared_id: 1234,
+                },
+              ]
+            },
+          },
+        }
+
+        await dispatch({
+          cursor: asyncIterable,
+          endpoints: [
+            'http://dns-scanner.scanners.svc.cluster.local',
+            'http://https-scanner.scanners.svc.cluster.local',
+          ],
+          publish,
+          logger,
+        })
+        // 1 batch of 2 domains = 2 dispatches
+        expect(publish).toHaveBeenCalledWith({
+          channel: 'domains.1',
+          msg: {
+            domain: 'tbs-sct.gc.ca',
+            domain_key: '1',
+            hash: '8a237c3e4033c5f9ec06f28edddbccd6',
+            selectors: [],
+            shared_id: null,
+            user_key: null,
+          },
+        })
+      })
+
       it('handles errors', async () => {
         const logger = {
           info: jest.fn(),
@@ -60,12 +108,14 @@ describe('domain-dispatcher', () => {
                   user_key: 1,
                   domain: 'tbs-sct.gc.ca',
                   domain_key: 'domains/1',
+                  hash: '8a237c3e4033c5f9ec06f28edddbccd6',
                   shared_id: 1234,
                 },
                 {
                   user_key: 1,
                   domain: 'cyber.gc.ca',
                   domain_key: 'domains/2',
+                  hash: '56f36ef5e84af6eef45299d58890bfe5',
                   shared_id: 1235,
                 },
               ]
