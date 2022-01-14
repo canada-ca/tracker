7e698de92be663b7724768258e22073b792d0947
Kyle Sullivan <47400288+FestiveKyle@users.noreply.github.com>
2022-01-14 16:45:06 -0400

    Fix 'loginRequiredBool' import (#3401)

--- api/src/organization/loaders/__tests__/load-organization-connections-by-user-id.test.js
+++ api/src/organization/loaders/__tests__/load-organization-connections-by-user-id.test.js
@@ -2270,7 +2270,7 @@ describe('given the load organization connections by user id function', () => {
                   query,
                   userKey: user._key,
                   cleanseInput,
-                  auth: { loginRequired: true },
+                  auth: { loginRequiredBool: true },
                   language: 'en',
                   i18n,
                 })
@@ -4514,7 +4514,7 @@ describe('given the load organization connections by user id function', () => {
                   query,
                   userKey: user._key,
                   cleanseInput,
-                  auth: { loginRequired: true },
+                  auth: { loginRequiredBool: true },
                   language: 'fr',
                   i18n,
                 })
