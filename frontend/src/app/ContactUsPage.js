8944a902ac3d40fdb82af93fddfc2452ed5d5a28
Kyle Sullivan <47400288+FestiveKyle@users.noreply.github.com>
2022-01-20 16:01:06 -0400

    Update wording on contact us page (#3414)

--- frontend/src/app/ContactUsPage.js
+++ frontend/src/app/ContactUsPage.js
@@ -8,46 +8,37 @@ export default function ContactUsPage() {
       <Heading>
         <Trans>Contact the Tracker Team</Trans>
       </Heading>
-      <Divider borderBottomColor="gray.900" mb="2" />
-      <Box fontSize="xl">
-        <Text fontWeight="bold" fontSize="2xl">
-          <Trans>General Public</Trans>
-        </Text>
-        <Text mb="2">
-          <Trans>
-            Individuals with questions about the accuracy of their domain’s
-            compliance data may contact the TBS Cyber Security mailbox.
-          </Trans>
-        </Text>
+      <Divider borderBottomColor="gray.900" mb="4" />
+      <Box fontSize="xl" p="4">
         <Text fontWeight="bold" fontSize="2xl">
           <Trans>Government of Canada Employees</Trans>
         </Text>
-        <Text mb="2">
+        <Text mb="8">
           <Trans>
             Individuals from a departmental information technology group may
-            contact the TBS Cyber Security mailbox for interpretations of this
-            ITPIN.
+            contact the TBS Cyber Security mailbox for ITPIN interpretation and
+            domain management.
           </Trans>
         </Text>
-        <Text fontWeight="bold" mb="2">
+        <Text fontWeight="bold" mb="4">
           <Trans>
             Note that compliance data does not automatically refresh.
             Modifications to domains could take 24 hours to update.
           </Trans>
         </Text>
+        <Button
+          variant="primary"
+          size="lg"
+          mb="2"
+          as={Link}
+          href={
+            'mailto:zzTBSCybers@tbs-sct.gc.ca?subject=Tracker%20Issue%20Report'
+          }
+          isExternal={true}
+        >
+          <Trans>Contact Us</Trans>
+        </Button>
       </Box>
-      <Button
-        variant="primary"
-        size="lg"
-        mb="2"
-        as={Link}
-        href={
-          'mailto:zzTBSCybers@tbs-sct.gc.ca?subject=Tracker%20Issue%20Report'
-        }
-        isExternal={true}
-      >
-        <Trans>Contact Us</Trans>
-      </Button>
     </Box>
   )
 }
