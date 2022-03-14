a268cee602b63a1b4a296e060a82e25e08b07816
lcampbell2 <l.campbell144768@gmail.com>
2022-03-14 10:17:46 -0300

    add logo to header

--- frontend/src/app/TopBanner.js
+++ frontend/src/app/TopBanner.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { t, Trans } from '@lingui/macro'
-import { Box, Button, Flex, useToast } from '@chakra-ui/react'
+import { Box, Button, Flex, useToast, Image } from '@chakra-ui/react'
 import { Link as RouteLink } from 'react-router-dom'
 import { useMutation } from '@apollo/client'
 
@@ -10,10 +10,13 @@ import { Layout } from '../components/Layout'
 import { useUserVar } from '../utilities/userState'
 import { SIGN_OUT } from '../graphql/mutations'
 import { PhaseBanner } from './PhaseBanner'
+import trackerLogo from '../images/Asset4.svg'
+import { useLingui } from '@lingui/react'
 
 export const TopBanner = (props) => {
   const { isLoggedIn, logout } = useUserVar()
   const toast = useToast()
+  const { i18n } = useLingui()
 
   const [signOut] = useMutation(SIGN_OUT, {
     onError(error) {
@@ -42,11 +45,32 @@ export const TopBanner = (props) => {
   return (
     <Layout>
       <Flex align="center" fontFamily="body" {...props}>
-        <PhaseBanner phase={<Trans>BETA</Trans>} ml="auto" mr="2">
+        <Box
+          mt={{ base: '4', lg: '8' }}
+          mb="4"
+          mx="4"
+          w="15%"
+          display={{ base: 'none', md: 'initial' }}
+        >
+          <Image
+            src={trackerLogo}
+            alt={
+              i18n.locale === 'en'
+                ? 'Symbol of the Government of Canada'
+                : 'Symbole du gouvernement du Canada'
+            }
+          />
+        </Box>
+
+        <PhaseBanner
+          phase={<Trans>BETA</Trans>}
+          ml={{ base: '0', md: 'auto' }}
+          mr="2"
+        >
           <Trans>This is a new service, we are constantly improving.</Trans>
         </PhaseBanner>
 
-        <Box py="4" mx="2">
+        <Box py="4" mx="2" ml={{ base: 'auto', md: '0' }}>
           <LocaleSwitcher />
         </Box>
 
