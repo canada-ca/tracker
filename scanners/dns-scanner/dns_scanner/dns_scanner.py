f211a98340618a8adf7faffa253cc99bc9acb2ba
Kyle Sullivan <141261s@acadiau.ca>
2022-03-22 11:00:13 -0300

    Fix imports after merge

--- scanners/dns-scanner/dns_scanner/dns_scanner.py
+++ scanners/dns-scanner/dns_scanner/dns_scanner.py
@@ -1,23 +1,11 @@
-import os
 from dataclasses import dataclass
 import time
 
 import dns.resolver
-import logging
-import nacl
-import base64
-import tldextract
-import dkim
 from checkdmarc import *
-from dns import resolver
-from dkim import dnsplug, crypto, KeyFormatError
-from dkim.util import InvalidTagValueList
-from dns.resolver import NoAnswer
 from dns.resolver import NXDOMAIN, NoAnswer
 
 from .email_scanners import DKIMScanner, DMARCScanner
-
-# from email_scanners import DMARCScanner, DKIMScanner
 
 logger = logging.getLogger(__name__)
 
