43f51b7ad38f9c521c01323c354ec57bb790f6a6
lcampbell2 <l.campbell144768@gmail.com>
2022-03-29 14:16:53 -0300

    fix isLoggedIn prop

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -156,7 +156,7 @@ export function App() {
           <Suspense fallback={<LoadingMessage />}>
             <Switch>
               <Page exact path="/" title={t`Home`}>
-                <LandingPage isLoggedIn={isLoggedIn} />
+                <LandingPage isLoggedIn={isLoggedIn()} />
               </Page>
 
               <Page
