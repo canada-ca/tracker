8d1ad5651ddc524b55757f404dc30069291186b2
lcampbell2 <l.campbell144768@gmail.com>
2022-06-03 13:22:28 -0300

    update TopBanner tests

--- frontend/src/app/__tests__/TopBanner.test.js
+++ frontend/src/app/__tests__/TopBanner.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { theme, ChakraProvider } from '@chakra-ui/react'
-import { cleanup, render } from '@testing-library/react'
+import { cleanup, fireEvent, render, waitFor } from '@testing-library/react'
 import { I18nProvider } from '@lingui/react'
 import { setupI18n } from '@lingui/core'
 import { MockedProvider } from '@apollo/client/testing'
@@ -10,6 +10,7 @@ import { makeVar } from '@apollo/client'
 import { TopBanner } from '../TopBanner'
 
 import { UserVarProvider } from '../../utilities/userState'
+import { SIGN_OUT } from '../../graphql/mutations'
 
 const i18n = setupI18n({
   locale: 'en',
@@ -24,7 +25,7 @@ const i18n = setupI18n({
 describe('<TopBanner />', () => {
   afterEach(cleanup)
 
-  it('renders using the language prop correctly', () => {
+  it('renders text', () => {
     const { getByText } = render(
       <MockedProvider>
         <UserVarProvider
@@ -33,7 +34,7 @@ describe('<TopBanner />', () => {
           <ChakraProvider theme={theme}>
             <MemoryRouter initialEntries={['/']}>
               <I18nProvider i18n={i18n}>
-                <TopBanner lang="en" />
+                <TopBanner />
               </I18nProvider>
             </MemoryRouter>
           </ChakraProvider>
@@ -41,5 +42,92 @@ describe('<TopBanner />', () => {
       </MockedProvider>,
     )
     expect(getByText('This is a new service, we are constantly improving.'))
+  })
+
+  describe('user is logged in', () => {
+    describe('on SignOut', () => {
+      it('succeeds', async () => {
+        const mocks = [
+          {
+            request: {
+              query: SIGN_OUT,
+            },
+            result: {
+              data: {
+                signOut: {
+                  status: 'wfewgwgew',
+                },
+              },
+            },
+          },
+        ]
+
+        const { queryByText, getByText } = render(
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <UserVarProvider
+              userVar={makeVar({
+                jwt: 'fwgegvgerdf',
+                tfaSendMethod: 'none',
+                userName: 'user@test.ca',
+              })}
+            >
+              <ChakraProvider theme={theme}>
+                <MemoryRouter initialEntries={['/']}>
+                  <I18nProvider i18n={i18n}>
+                    <TopBanner />
+                  </I18nProvider>
+                </MemoryRouter>
+              </ChakraProvider>
+            </UserVarProvider>
+          </MockedProvider>,
+        )
+
+        const signOutBtn = getByText('Sign Out')
+        fireEvent.click(signOutBtn)
+
+        await waitFor(() => {
+          expect(queryByText('You have successfully been signed out.'))
+        })
+      })
+      it('fails', async () => {
+        const mocks = [
+          {
+            request: {
+              query: SIGN_OUT,
+            },
+            result: {
+              error: {
+                errors: [{ message: 'foobar' }],
+              },
+            },
+          },
+        ]
+        const { queryByText, getByText } = render(
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <UserVarProvider
+              userVar={makeVar({
+                jwt: 'fwgegvgerdf',
+                tfaSendMethod: 'none',
+                userName: 'user@test.ca',
+              })}
+            >
+              <ChakraProvider theme={theme}>
+                <MemoryRouter initialEntries={['/']}>
+                  <I18nProvider i18n={i18n}>
+                    <TopBanner />
+                  </I18nProvider>
+                </MemoryRouter>
+              </ChakraProvider>
+            </UserVarProvider>
+          </MockedProvider>,
+        )
+        const signOutBtn = getByText('Sign Out')
+        fireEvent.click(signOutBtn)
+
+        await waitFor(() => {
+          expect(queryByText('An error occured when you attempted to sign out'))
+        })
+      })
+    })
   })
 })
