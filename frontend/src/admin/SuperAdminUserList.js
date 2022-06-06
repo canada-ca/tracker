41369415d247dc352df43ec864c2263411ecc37e
lcampbell2 <l.campbell144768@gmail.com>
2022-06-06 09:50:03 -0300

    add close account button to SA userlist

--- frontend/src/admin/SuperAdminUserList.js
+++ frontend/src/admin/SuperAdminUserList.js
@@ -6,14 +6,24 @@ import {
   AccordionPanel,
   Badge,
   Box,
+  Button,
   Flex,
   IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Stack,
   Text,
   useDisclosure,
+  useToast,
 } from '@chakra-ui/react'
 
 import { FIND_MY_USERS } from '../graphql/queries'
+import { CLOSE_ACCOUNT, SIGN_OUT } from '../graphql/mutations'
 import { LoadingMessage } from '../components/LoadingMessage'
 import { ErrorFallbackMessage } from '../components/ErrorFallbackMessage'
 import { RelayPaginationControls } from '../components/RelayPaginationControls'
@@ -24,6 +34,10 @@ import { CheckCircleIcon, EditIcon, MinusIcon } from '@chakra-ui/icons'
 import { SearchBox } from '../components/SearchBox'
 import { UserListModal } from './UserListModal'
 import { string } from 'prop-types'
+import { FormField } from '../components/fields/FormField'
+import { createValidationSchema } from '../utilities/fieldRequirements'
+import { Formik } from 'formik'
+import { useMutation } from '@apollo/client'
 
 export function SuperAdminUserList({ permission }) {
   const [mutation, setMutation] = useState()
@@ -32,14 +46,75 @@ export function SuperAdminUserList({ permission }) {
   const [searchTerm, setSearchTerm] = useState('')
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
   const [usersPerPage, setUsersPerPage] = useState(10)
+  const [closeAccountUser, setCloseAccountUser] = useState({
+    userName: '',
+    displayName: '',
+    userId: '',
+  })
 
+  const toast = useToast()
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const {
+    isOpen: closeAccountIsOpen,
+    onOpen: closeAccountOnOpen,
+    onClose: closeAccountOnClose,
+  } = useDisclosure()
 
   const memoizedSetDebouncedSearchTermCallback = useCallback(() => {
     setDebouncedSearchTerm(searchTerm)
   }, [searchTerm])
 
   useDebouncedFunction(memoizedSetDebouncedSearchTermCallback, 500)
+
+  const [closeAccount, { loading: loadingCloseAccount }] = useMutation(
+    CLOSE_ACCOUNT,
+    {
+      refetchQueries: ['FindMyUsers'],
+      awaitRefetchQueries: true,
+
+      onError(error) {
+        toast({
+          title: t`Unable to close this account.`,
+          description: error.message,
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+          position: 'top-left',
+        })
+      },
+      onCompleted({ closeAccount }) {
+        if (closeAccount.result.__typename === 'CloseAccountResult') {
+          toast({
+            title: t`Account Closed Successfully`,
+            description: t`Tracker account has been successfully closed.`,
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+            position: 'top-left',
+          })
+          closeAccountOnClose()
+        } else if (closeAccount.result.__typename === 'CloseAccountError') {
+          toast({
+            title: t`Unable to close the account.`,
+            description: closeAccount.result.description,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+            position: 'top-left',
+          })
+        } else {
+          toast({
+            title: t`Incorrect send method received.`,
+            description: t`Incorrect closeAccount.result typename.`,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+            position: 'top-left',
+          })
+        }
+      },
+    },
+  )
 
   const {
     loading,
@@ -153,7 +228,6 @@ export function SuperAdminUserList({ permission }) {
                           console.log(`Removed user from org ${orgName}`)
                         }}
                         p={2}
-                        m={0}
                         icon={<MinusIcon />}
                       />
                       <IconButton
@@ -165,7 +239,6 @@ export function SuperAdminUserList({ permission }) {
                           console.log(`Edit user in org ${orgName}`)
                         }}
                         p={2}
-                        m={0}
                         icon={<EditIcon />}
                       />
                     </Stack>
@@ -224,37 +297,129 @@ export function SuperAdminUserList({ permission }) {
 
           return (
             <AccordionItem key={userId}>
-              <AccordionButton
-                width="100%"
-                p="4"
-                alignItems={{ base: 'flex-start', md: 'center' }}
-                flexDirection={{ base: 'column', md: 'row' }}
-                _hover={{ bg: 'gray.100' }}
-                mb="2"
-                borderWidth="1px"
-                borderColor="black"
-                rounded="md"
-              >
-                <Flex w="100%" textAlign="left">
-                  <Text minW="33%">{userName}</Text>
-                  <Text minW="25%">{displayName}</Text>
-                  <Box minW="25%">
-                    <Badge
-                      variant="solid"
-                      bg={emailValidated ? 'strong' : 'weak'}
-                      pt={1}
-                      mr={{ md: '1rem' }}
-                      justifySelf={{ base: 'start', md: 'end' }}
-                    >
-                      <Trans>Verified</Trans>
-                    </Badge>
-                  </Box>
-                  <Text minW="17%">
-                    <Trans>Affiliations:</Trans> {totalCount}
-                  </Text>
+              <Box>
+                <Flex w="100%">
+                  <AccordionButton
+                    width="100%"
+                    p="4"
+                    alignItems={{ base: 'flex-start', md: 'center' }}
+                    flexDirection={{ base: 'column', md: 'row' }}
+                    _hover={{ bg: 'gray.100' }}
+                    mb="2"
+                    borderWidth="1px"
+                    borderColor="black"
+                    rounded="md"
+                  >
+                    <Flex w="100%" textAlign="left">
+                      <Text minW="33%">{userName}</Text>
+                      <Text minW="25%">{displayName}</Text>
+                      <Box minW="25%">
+                        <Badge
+                          variant="solid"
+                          bg={emailValidated ? 'strong' : 'weak'}
+                          pt={1}
+                          mr={{ md: '1rem' }}
+                          justifySelf={{ base: 'start', md: 'end' }}
+                        >
+                          <Trans>Verified</Trans>
+                        </Badge>
+                      </Box>
+                      <Text minW="17%">
+                        <Trans>Affiliations:</Trans> {totalCount}
+                      </Text>
+                    </Flex>
+                  </AccordionButton>
+                  <Button
+                    alignSelf="center"
+                    variant="danger"
+                    onClick={() => {
+                      setCloseAccountUser({ userId, userName, displayName })
+                      closeAccountOnOpen()
+                    }}
+                    w={{ base: '100%', md: 'auto' }}
+                    mx="2"
+                    mb="2"
+                  >
+                    <Trans>Close Account</Trans>
+                  </Button>
                 </Flex>
-              </AccordionButton>
-              <AccordionPanel>{userAffiliations}</AccordionPanel>
+                <AccordionPanel>{userAffiliations}</AccordionPanel>
+              </Box>
+
+              <Modal
+                isOpen={closeAccountIsOpen}
+                onClose={closeAccountOnClose}
+                motionPreset="slideInBottom"
+              >
+                <Formik
+                  validateOnBlur={false}
+                  initialValues={{
+                    matchEmail: '',
+                  }}
+                  initialTouched={{
+                    matchEmail: true,
+                  }}
+                  validationSchema={createValidationSchema(['matchEmail'], {
+                    matches: closeAccountUser.userName,
+                  })}
+                  onSubmit={async () => {
+                    await closeAccount({
+                      variables: { userId: closeAccountUser.userId },
+                    })
+                  }}
+                >
+                  {({ handleSubmit }) => (
+                    <form onSubmit={handleSubmit}>
+                      <ModalOverlay />
+                      <ModalContent pb={4}>
+                        <ModalHeader>
+                          <Trans>Close Account</Trans>
+                        </ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                          <Trans>
+                            This action CANNOT be reversed, are you sure you
+                            wish to to close the account{' '}
+                            {closeAccountUser.displayName}?
+                          </Trans>
+
+                          <Text mb="1rem">
+                            <Trans>
+                              Enter "{closeAccountUser.userName}" below to
+                              confirm removal. This field is case-sensitive.
+                            </Trans>
+                          </Text>
+
+                          <FormField
+                            name="matchEmail"
+                            label={t`User Email`}
+                            placeholder={closeAccountUser.userName}
+                          />
+                        </ModalBody>
+
+                        <ModalFooter>
+                          <Button
+                            variant="primaryOutline"
+                            mr="4"
+                            onClick={closeAccountOnClose}
+                          >
+                            <Trans>Cancel</Trans>
+                          </Button>
+
+                          <Button
+                            variant="primary"
+                            mr="4"
+                            type="submit"
+                            isLoading={loadingCloseAccount}
+                          >
+                            <Trans>Confirm</Trans>
+                          </Button>
+                        </ModalFooter>
+                      </ModalContent>
+                    </form>
+                  )}
+                </Formik>
+              </Modal>
             </AccordionItem>
           )
         },
