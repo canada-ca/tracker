6d5b3b20fb385de9b6a806f693587dfa821e4594
lcampbell <l.campbell144768@gmail.com>
2022-09-23 09:35:18 -0300

    handles single variant for removal or addition

--- frontend/src/app/ABTestWrapper.js
+++ frontend/src/app/ABTestWrapper.js
@@ -1,25 +1,39 @@
 import React from 'react'
-import { any } from 'prop-types'
+import { any, string } from 'prop-types'
 import { useUserVar } from '../utilities/userState'
 
 const isInsiderUser = ({ userName }) => {
   return userName.endsWith('@tbs-sct.gc.ca')
 }
 
-export function ABTestingWrapper({ children }) {
+export function ABTestingWrapper({ children, insiderVariantName = 'B' }) {
   const { currentUser } = useUserVar()
-  let childIndex
+  let childIndex = 0
 
-  if (isInsiderUser({ userName: currentUser?.userName || '' })) {
-    childIndex = children.findIndex((variant) => variant.props.name === 'B')
-  } else {
-    childIndex = children.findIndex((variant) => variant.props.name === 'A')
+  // only one variant
+  if (!children.length) {
+    if (isInsiderUser({ userName: currentUser?.userName || '' })) {
+      if (children.props.name === insiderVariantName) return <>{children}</>
+      else return <></>
+    } else {
+      if (children.props.name !== insiderVariantName) return <>{children}</>
+      else return <></>
+    }
   }
-
-  console.log(children)
+  // A + B variants
+  if (isInsiderUser({ userName: currentUser?.userName || '' })) {
+    childIndex = children.findIndex(
+      (variant) => variant.props.name === insiderVariantName,
+    )
+  } else {
+    childIndex = children.findIndex(
+      (variant) => variant.props.name !== insiderVariantName,
+    )
+  }
   return <>{children[childIndex]}</>
 }
 
 ABTestingWrapper.propTypes = {
+  insiderVariantName: string,
   children: any,
 }
