4f00cf7ecbccd207b5eb0e04dd94cccebec53a89
Kyle Sullivan <141261s@acadiau.ca>
2022-10-21 11:03:04 -0300

    Add more details after switching to composition

---
+++ frontend/src/guidance/DetailList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box, Text } from '@chakra-ui/react'
+import { t, Trans } from '@lingui/macro'
+
+export function DetailList({ details }) {
+  const mappedDetails = details.map((detail, idx) => {
+    return (
+      <Text key={`${detail.category}-${idx}`}>
+        <b>{detail.category}</b>: {detail.description}
+      </Text>
+    )
+  })
+
+  return (
+    <Box bg="gray.100" px="2" py="1">
+      {mappedDetails}
+    </Box>
+  )
+}
+
+DetailList.propTypes = {
+  details: PropTypes.arrayOf(
+    PropTypes.shape({
+      category: PropTypes.string,
+      description: PropTypes.string,
+    }),
+  ).isRequired,
+}
--- frontend/src/guidance/WebGuidance.js
+++ frontend/src/guidance/WebGuidance.js
@@ -5,10 +5,23 @@ import { ScanCard } from './NewScanCard'
 import { t } from '@lingui/macro'
 import { ScanDetails } from './NewScanCategoryDetails'
 import { GuidanceTagList } from './GuidanceTagList'
+import { StrengthCategory } from './StrengthCategory'
+import { Accordion } from '@chakra-ui/react'
+import { DetailList } from './DetailList'
 
 const WebGuidance = ({ webScan }) => {
   const httpsScan = webScan.https.edges[0].node
   const tlsScan = webScan.ssl.edges[0].node
+
+  const httpDetailList = []
+  httpDetailList.push({
+    category: t`Implementation`,
+    description: httpsScan.implementation,
+  })
+  httpDetailList.push({
+    category: t`Enforcement`,
+    description: httpsScan.enforced,
+  })
 
   return (
     <>
@@ -17,19 +30,57 @@ const WebGuidance = ({ webScan }) => {
         description={t`Web Scan Results`}
         title={t`Results for scans of web technologies (TLS, HTTPS).`}
       >
-        <ScanDetails title={`HTTPS`}>
+        <ScanDetails title={t`HTTPS`}>
+          <DetailList details={httpDetailList} />
           <GuidanceTagList
             positiveTags={httpsScan.positiveGuidanceTags.edges}
             neutralTags={httpsScan.neutralGuidanceTags.edges}
             negativeTags={httpsScan.negativeGuidanceTags.edges}
           />
         </ScanDetails>
-        <ScanDetails title={`TLS`}>
+        <ScanDetails title={t`TLS`}>
           <GuidanceTagList
             positiveTags={tlsScan.positiveGuidanceTags.edges}
             neutralTags={tlsScan.neutralGuidanceTags.edges}
             negativeTags={tlsScan.negativeGuidanceTags.edges}
           />
+          <Accordion allowMultiple defaultIndex={[0, 1]}>
+            <ScanDetails title={t`Ciphers`}>
+              <StrengthCategory
+                type="ciphers"
+                strength="strong"
+                items={tlsScan.strongCiphers}
+              />
+              <StrengthCategory
+                type="ciphers"
+                strength="acceptable"
+                items={tlsScan.acceptableCiphers}
+              />
+              <StrengthCategory
+                type="ciphers"
+                strength="weak"
+                items={tlsScan.weakCiphers}
+              />
+            </ScanDetails>
+
+            <ScanDetails title={t`Curves`}>
+              <StrengthCategory
+                type="ciphers"
+                strength="strong"
+                items={tlsScan.strongCiphers}
+              />
+              <StrengthCategory
+                type="ciphers"
+                strength="acceptable"
+                items={tlsScan.acceptableCiphers}
+              />
+              <StrengthCategory
+                type="ciphers"
+                strength="weak"
+                items={tlsScan.weakCiphers}
+              />
+            </ScanDetails>
+          </Accordion>
         </ScanDetails>
       </ScanCard>
     </>
