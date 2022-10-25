323d2ce1316fd73355d98223bc4f8a24b316f081
lcampbell <l.campbell144768@gmail.com>
2022-10-25 14:14:42 -0300

    move wrapper inside page

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -338,6 +338,22 @@ export function App() {
               )}
             </Page>
 
+            <Page path="/my-tracker/:activeTab?" title={t`myTracker`}>
+              {isLoggedIn() ? (
+                <ABTestingWrapper insiderVariantName="B">
+                  <ABTestVariant name="B">
+                    <MyTrackerPage />
+                  </ABTestVariant>
+                </ABTestingWrapper>
+              ) : (
+                <Redirect
+                  to={{
+                    pathname: '/sign-in',
+                  }}
+                />
+              )}
+            </Page>
+
             <Page path="/validate/:verifyToken" title={t`Email Verification`}>
               {() => <EmailValidationPage />}
             </Page>
@@ -357,22 +373,6 @@ export function App() {
             </Page>
 
             <Page component={PageNotFound} title="404" />
-
-            <ABTestingWrapper insiderVariantName="B">
-              <ABTestVariant name="B">
-                <Page path="/my-tracker/:activeTab?" title={t`myTracker`}>
-                  {isLoggedIn() ? (
-                    <MyTrackerPage />
-                  ) : (
-                    <Redirect
-                      to={{
-                        pathname: '/sign-in',
-                      }}
-                    />
-                  )}
-                </Page>
-              </ABTestVariant>
-            </ABTestingWrapper>
           </Switch>
         </Suspense>
       </Main>
