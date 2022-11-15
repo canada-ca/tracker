3d12a5a387eff8ef7e49ed39557890864fdb153e
Kyle Sullivan <47400288+FestiveKyle@users.noreply.github.com>
2022-11-15 13:40:55 -0400

    Allow wildcard dkim selectors (#4134)
    Co-authored-by: Kyle Sullivan <sullivan.kyle@protonmail.com>

--- api/src/scalars/selector.js
+++ api/src/scalars/selector.js
@@ -1,7 +1,7 @@
 import { Kind, GraphQLError, GraphQLScalarType } from 'graphql'
 
 const validate = (value) => {
-  const SLUG_REGEX = /^[a-zA-Z0-9](\.?[a-zA-Z0-9])*$/
+  const SLUG_REGEX = /^(?:[a-zA-Z0-9](\.?[a-zA-Z0-9])*|\*)$/
   if (typeof value !== typeof 'string') {
     throw new TypeError(`Value is not a string: ${typeof value}`)
   }
@@ -15,7 +15,7 @@ const validate = (value) => {
 export const Selectors = new GraphQLScalarType({
   name: 'Selector',
   description:
-    'A field that conforms to a DKIM selector. Only alphanumeric characters and periods are allowed, string must also start and end with alphanumeric characters',
+    'A field that conforms to a DKIM selector. Must be either a single asterisk or a string where only alphanumeric characters and periods are allowed, string must also start and end with alphanumeric characters',
   serialize: validate,
   parseValue: validate,
 
