c0aa669786bf731d704ed50e02bfc0a5d79ee7f3
lcampbell <l.campbell144768@gmail.com>
2023-01-19 14:16:28 -0400

    fix empty webResults handling

--- frontend/src/guidance/GuidancePage.js
+++ frontend/src/guidance/GuidancePage.js
@@ -103,7 +103,7 @@ function GuidancePage() {
           </Tab>
         </TabList>
         <TabPanels>
-          <TabPanel>{webScan.edges.length === 0 ? noScanData : <WebGuidance webResults={webResults} />}</TabPanel>
+          <TabPanel>{webScan.length === 0 ? noScanData : <WebGuidance webResults={webResults} />}</TabPanel>
           <TabPanel>
             {dnsScan.edges.length === 0 ? (
               noScanData
