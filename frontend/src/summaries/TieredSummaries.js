d913f0d11f5cd984382d457a6ef13d27ff52744d
lcampbell <l.campbell144768@gmail.com>
2023-04-18 09:57:32 -0300

    move arrow btns to top of TieredSummaries component

--- frontend/src/summaries/TieredSummaries.js
+++ frontend/src/summaries/TieredSummaries.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Flex, Tabs, TabPanels, TabPanel, IconButton, Text } from '@chakra-ui/react'
+import { Flex, Tabs, TabPanels, TabPanel, IconButton, Text, Box } from '@chakra-ui/react'
 import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
 
 import { LandingPageSummaries } from '../landing/LandingPageSummaries'
@@ -19,45 +19,41 @@ export function TieredSummaries() {
   }
 
   return (
-    <Flex align="center" w="100%">
-      <IconButton
-        borderColor="gray.900"
-        bg="gray.50"
-        borderWidth="1px"
-        rounded="full"
-        icon={<ArrowLeftIcon />}
-        onClick={handleBackBtn}
-      />
+    <Box>
+      <Flex align="center" w="100%" mb="2" justifyContent="space-between">
+        <IconButton
+          borderColor="gray.900"
+          bg="gray.50"
+          borderWidth="1px"
+          rounded="full"
+          icon={<ArrowLeftIcon />}
+          onClick={handleBackBtn}
+        />
+        <Text mb="2" textAlign="center" fontWeight="bold" fontSize="xl">
+          <Trans>Tier {tabIndex + 1}</Trans>
+        </Text>
+        <IconButton
+          borderColor="gray.900"
+          bg="gray.50"
+          borderWidth="1px"
+          rounded="full"
+          icon={<ArrowRightIcon />}
+          onClick={handleFwdBtn}
+        />
+      </Flex>
       <Tabs index={tabIndex} isLazy>
         <TabPanels>
           <TabPanel>
-            <Text mb="2" textAlign="center" fontWeight="bold" fontSize="xl">
-              <Trans>Tier 1</Trans>
-            </Text>
             <LandingPageSummaries />
           </TabPanel>
           <TabPanel>
-            <Text mb="2" textAlign="center" fontWeight="bold" fontSize="xl">
-              <Trans>Tier 2</Trans>
-            </Text>
             <TierTwoSummaries />
           </TabPanel>
           <TabPanel>
-            <Text mb="2" textAlign="center" fontWeight="bold" fontSize="xl">
-              <Trans>Tier 3</Trans>
-            </Text>
             <TierThreeSummaries />
           </TabPanel>
         </TabPanels>
       </Tabs>
-      <IconButton
-        borderColor="gray.900"
-        bg="gray.50"
-        borderWidth="1px"
-        rounded="full"
-        icon={<ArrowRightIcon />}
-        onClick={handleFwdBtn}
-      />
-    </Flex>
+    </Box>
   )
 }
