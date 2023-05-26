1415b7b5aa61d633e0d0d7509e92aec650b26ab6
lcampbell <l.campbell144768@gmail.com>
2023-05-26 10:00:23 -0300

    revert changes to loadOrgDomainStatuses

--- api/src/organization/loaders/load-organization-domain-statuses.js
+++ api/src/organization/loaders/load-organization-domain-statuses.js
@@ -1,13 +1,10 @@
 import { t } from '@lingui/macro'
-import { logActivity } from '../../audit-logs/mutations/log-activity'
 
 export const loadOrganizationDomainStatuses =
-  ({ query, collections, transaction, userKey, i18n, auth: { userRequired, verifiedRequired } }) =>
+  ({ query, userKey, i18n }) =>
   async ({ orgId }) => {
-    const user = await userRequired()
-    verifiedRequired({ user })
-
     let domains
+
     try {
       domains = (
         await query`
@@ -23,45 +20,6 @@ export const loadOrganizationDomainStatuses =
       console.error(`Database error occurred when user: ${userKey} running loadOrganizationDomainStatuses: ${err}`)
       throw new Error(i18n._(t`Unable to load organization domain statuses. Please try again.`))
     }
-
-    // Get org names to use in email
-    let orgCursor
-    try {
-      orgCursor = await query`RETURN DOCUMENT(organizations, ${orgId})`
-    } catch (err) {
-      console.error(`Database error occurred when user: ${userKey} running loadOrganizationDomainStatuses: ${err}`)
-      throw new Error(i18n._(t`Unable to load organization domain statuses. Please try again.`))
-    }
-
-    let org
-    try {
-      org = await orgCursor.next()
-    } catch (err) {
-      console.error(`Cursor error occurred when user: ${userKey} running loadOrganizationDomainStatuses: ${err}`)
-      throw new Error(i18n._(t`Unable to load organization domain statuses. Please try again.`))
-    }
-
-    await logActivity({
-      transaction,
-      collections,
-      query,
-      initiatedBy: {
-        id: user._key,
-        userName: user.userName,
-      },
-      action: 'export',
-      target: {
-        resource: {
-          en: org.orgDetails.en.name,
-          fr: org.orgDetails.fr.name,
-        },
-        organization: {
-          id: org._key,
-          name: org.orgDetails.en.name,
-        },
-        resourceType: 'organization', // user, org, domain
-      },
-    })
 
     return domains
   }
