1b4272013d3b03534976c5a2b63a258750fe60be
lcampbell <l.campbell144768@gmail.com>
2023-05-30 15:24:54 -0300

    send to service account if no admins

--- services/org-footprint/src/org-footprint-service.js
+++ services/org-footprint/src/org-footprint-service.js
@@ -12,22 +12,22 @@ const orgFootprintService = async ({ query, log, notifyClient }) => {
     // if new audit logs exist
     if (auditLogs.length > 0) {
       // get list of org admins
-
-      const orgAdmins = await getOrgAdmins({ query, orgKey })
-      // if org admins exist
-      if (orgAdmins.length > 0) {
+      if (REDIRECT_TO_SERVICE_ACCOUNT_EMAIL) {
         const orgNames = await getBilingualOrgNames({ query, orgKey })
-        if (REDIRECT_TO_SERVICE_ACCOUNT_EMAIL) {
-          log(`Sending recent activity email to service account: ${orgKey}`)
-          // send email to service account
-          const user = {
-            userName: SERVICE_ACCOUNT_EMAIL,
-            displayName: 'Service Account',
-            preferredLang: 'en',
-            _key: 'service-account',
-          }
-          await sendOrgFootprintEmail({ notifyClient, user, auditLogs, orgNames })
-        } else {
+        log(`Sending recent activity email to service account: ${orgKey}`)
+        // send email to service account
+        const user = {
+          userName: SERVICE_ACCOUNT_EMAIL,
+          displayName: 'Service Account',
+          preferredLang: 'en',
+          _key: 'service-account',
+        }
+        await sendOrgFootprintEmail({ notifyClient, user, auditLogs, orgNames })
+      } else {
+        const orgAdmins = await getOrgAdmins({ query, orgKey })
+        // if org admins exist
+        if (orgAdmins.length > 0) {
+          const orgNames = await getBilingualOrgNames({ query, orgKey })
           log(`Sending recent activity email to admins of org: ${orgKey}`)
           // send email to org admins
           for (const user of orgAdmins) {
