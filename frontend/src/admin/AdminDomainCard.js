20e3c58513aa7df74689c326682cc0a8624392a6
lcampbell <l.campbell144768@gmail.com>
2023-06-01 17:01:49 -0300

    restyle admin domain cards

--- frontend/src/admin/AdminDomainCard.js
+++ frontend/src/admin/AdminDomainCard.js
@@ -1,61 +1,44 @@
 import React from 'react'
 import { Trans } from '@lingui/macro'
 import { array, bool, string } from 'prop-types'
-import { Flex, Grid, Link, ListItem, Stack, Tag, TagLabel, Text } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-
-import { sanitizeUrl } from '../utilities/sanitizeUrl'
+import { Flex, ListItem, Tag, TagLabel, Text } from '@chakra-ui/react'
 
 export function AdminDomainCard({ url, tags, isHidden, isArchived, rcode, ...rest }) {
   return (
     <ListItem {...rest}>
-      <Grid templateColumns={{ base: 'auto', md: '40% 60%' }} columnGap="1.5rem">
-        <Stack isInline align="center">
-          <Text fontWeight="bold">
-            <Trans>Domain:</Trans>
-          </Text>
-          <Link
-            ml="auto"
-            // TODO: have the API enforce a scheme
-            // so we don't need to guess badly here.
-            href={`http://${sanitizeUrl(url)}`}
-            isExternal
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {url}
-            <ExternalLinkIcon mx="2px" aria-hidden="true" />
-          </Link>
-        </Stack>
-        <Flex>
+      <Flex align="center">
+        <Text fontWeight="bold">{url}</Text>
+        <Flex ml="auto" align="center">
           {tags?.map((tag, idx) => {
             return (
-              <Tag key={idx} m="1" borderRadius="full" borderWidth="1px" borderColor="gray.900">
-                <TagLabel mx="auto">{tag}</TagLabel>
+              <Tag key={idx} m="0.5" bg="gray.50" borderWidth="1px" borderColor="gray.900">
+                <TagLabel textColor="primary" fontWeight="bold" mx="auto">
+                  {tag}
+                </TagLabel>
               </Tag>
             )
           })}
           {rcode === 'NXDOMAIN' && (
             <Tag colorScheme="red" mr="auto" alignSelf="center">
-              NXDOMAIN
+              <TagLabel fontWeight="bold">NXDOMAIN</TagLabel>
             </Tag>
           )}
           {isHidden && (
-            <Tag m="1" borderRadius="full" borderWidth="1px" borderColor="gray.900">
-              <TagLabel mx="auto">
-                <Trans>Hidden</Trans>
+            <Tag colorScheme="blue" mx="1">
+              <TagLabel fontWeight="bold">
+                <Trans>HIDDEN</Trans>
               </TagLabel>
             </Tag>
           )}
           {isArchived && (
-            <Tag m="1" borderRadius="full" borderWidth="1px" borderColor="gray.900">
-              <TagLabel mx="auto">
-                <Trans>Archived</Trans>
+            <Tag colorScheme="red" mx="1">
+              <TagLabel fontWeight="bold">
+                <Trans>ARCHIVED</Trans>
               </TagLabel>
             </Tag>
           )}
         </Flex>
-      </Grid>
+      </Flex>
     </ListItem>
   )
 }
