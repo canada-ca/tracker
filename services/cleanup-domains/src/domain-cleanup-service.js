2b333ea038ea05a2faa6d1de24bb65d2246eebce
lcampbell <l.campbell144768@gmail.com>
2023-06-09 14:37:36 -0300

    add trycatch

--- services/cleanup-domains/src/domain-cleanup-service.js
+++ services/cleanup-domains/src/domain-cleanup-service.js
@@ -5,54 +5,86 @@ const domainCleanupService = async ({ query, log }) => {
   const cleanupDomains = await getNXDomains({ query, log })
 
   for (const domain of cleanupDomains) {
-    console.log(domain._id)
+    console.log(domain.domain)
     // remove claims
-    await removeEdges({
-      query,
-      vertexSelectorId: domain._id,
-      direction: 'ANY',
-      edgeCollection: 'claims',
-    })
+    try {
+      await removeEdges({
+        query,
+        vertexSelectorId: domain._id,
+        direction: 'ANY',
+        edgeCollection: 'claims',
+      })
+    } catch (err) {
+      console.error(`Error while removing claims for domain: ${domain._key}, error: ${err})`)
+      continue
+    }
     // remove ownerships
-    await removeEdges({
-      query,
-      vertexSelectorId: domain._id,
-      edgeCollection: 'ownership',
-      direction: 'ANY',
-    })
+    try {
+      await removeEdges({
+        query,
+        vertexSelectorId: domain._id,
+        edgeCollection: 'ownership',
+        direction: 'ANY',
+      })
+    } catch (err) {
+      console.error(`Error while removing ownerships for domain: ${domain._key}, error: ${err})`)
+      continue
+    }
+
     // remove dmarc summaries
-    await removeEdges({
-      db,
-      vertexSelectorId: domain._id,
-      edgeCollection: 'domainsToDmarcSummaries',
-      direction: 'ANY',
-      vertexCollection: 'dmarcSummaries',
-      removeVertices: true,
-    })
-    // script: remove scans
-    // web
-    await query`
-            WITH web, webScan, domains
-            FOR webV, domainsWebEdge IN 1..1 OUTBOUND ${domain._id} domainsWeb
-              FOR webScanV, webToWebScansV In 1..1 OUTBOUND webV._id webToWebScans
-                REMOVE webScanV IN webScan
-                REMOVE webToWebScansV IN webToWebScans
-                OPTIONS { waitForSync: true }
-              REMOVE webV IN web
-              REMOVE domainsWebEdge IN domainsWeb
-              OPTIONS { waitForSync: true }
-          `
-    // dns
-    await removeEdges({
-      db,
-      vertexSelectorId: domain._id,
-      edgeCollection: 'domainsDNS',
-      direction: 'ANY',
-      vertexCollection: 'dns',
-      removeVertices: true,
-    })
+    try {
+      await removeEdges({
+        db,
+        vertexSelectorId: domain._id,
+        edgeCollection: 'domainsToDmarcSummaries',
+        direction: 'ANY',
+        vertexCollection: 'dmarcSummaries',
+        removeVertices: true,
+      })
+    } catch (err) {
+      console.error(`Error while removing DMARC summaries for domain: ${domain._key}, error: ${err})`)
+      continue
+    }
+    // remove web scans
+    try {
+      await (
+        await query`
+        WITH web, webScan, domains
+        FOR webV, domainsWebEdge IN 1..1 OUTBOUND ${domain._id} domainsWeb
+          FOR webScanV, webToWebScansV In 1..1 OUTBOUND webV._id webToWebScans
+            REMOVE webScanV IN webScan
+            REMOVE webToWebScansV IN webToWebScans
+            OPTIONS { waitForSync: true }
+          REMOVE webV IN web
+          REMOVE domainsWebEdge IN domainsWeb
+          OPTIONS { waitForSync: true }
+      `
+      ).all()
+    } catch (err) {
+      console.error(`Error while removing web scans for domain: ${domain._key}, error: ${err})`)
+      continue
+    }
+    // remove dns scans
+    try {
+      await removeEdges({
+        db,
+        vertexSelectorId: domain._id,
+        edgeCollection: 'domainsDNS',
+        direction: 'ANY',
+        vertexCollection: 'dns',
+        removeVertices: true,
+      })
+    } catch (err) {
+      console.error(`Error while removing dns scans for domain: ${domain._key}, error: ${err})`)
+      continue
+    }
     // remove domain
-    await (await query`REMOVE ${domain._key} IN domains`).all()
+    try {
+      await (await query`REMOVE ${domain._key} IN domains`).all()
+    } catch (err) {
+      console.error(`Error while removing domain: ${domain._key}, error: ${err})`)
+      continue
+    }
     console.log(`Domain "${domain.domain}" and related data successfully removed`)
   }
 }
