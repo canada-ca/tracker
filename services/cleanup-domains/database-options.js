087f803b27ae55ecab0d42a1141a54b8ba0477b8
lcampbell <l.campbell144768@gmail.com>
2023-06-12 12:02:31 -0300

    domainCleanupService tests

--- services/cleanup-domains/database-options.js
+++ services/cleanup-domains/database-options.js
@@ -6,6 +6,11 @@ const databaseOptions = ({ rootPass }) => [
     options: { replicationFactor: 3, writeConcern: 1, numberOfShards: 6 },
   },
   {
+    type: 'documentcollection',
+    name: 'organizations',
+    options: { replicationFactor: 3, writeConcern: 1, numberOfShards: 6 },
+  },
+  {
     type: 'edgecollection',
     name: 'claims',
     options: { replicationFactor: 3, writeConcern: 1, numberOfShards: 6 },
