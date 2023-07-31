7438190f64e8f4f1d0659583b1a46e5a712584a3
Luke Campbell <64781228+lcampbell2@users.noreply.github.com>
2023-07-31 07:56:12 -0300

    Bugfix separate close account functions (#4721)
    * separate close-account mutations into self and other
    
    * refactor close-account tests
    
    * refactor frontend mutation calls

--- api/src/user/mutations/close-account.js
+++ api/src/user/mutations/close-account.js
@@ -5,9 +5,98 @@ import { logActivity } from '../../audit-logs/mutations/log-activity'
 
 import { closeAccountUnion } from '../unions'
 
-export const closeAccount = new mutationWithClientMutationId({
-  name: 'CloseAccount',
-  description: `This mutation allows a user to close their account, or a super admin to close another user's account.`,
+export const closeAccountSelf = new mutationWithClientMutationId({
+  name: 'CloseAccountSelf',
+  description: `This mutation allows a user to close their account.`,
+  outputFields: () => ({
+    result: {
+      type: closeAccountUnion,
+      description: '`CloseAccountUnion` returning either a `CloseAccountResult`, or `CloseAccountError` object.',
+      resolve: (payload) => payload,
+    },
+  }),
+  mutateAndGetPayload: async (
+    args,
+    { i18n, query, collections, transaction, auth: { userRequired }, validators: { cleanseInput } },
+  ) => {
+    let submittedUserId
+    if (args?.userId) {
+      submittedUserId = fromGlobalId(cleanseInput(args.userId)).id
+    }
+
+    const user = await userRequired()
+
+    const userId = user._id
+    const targetUserName = user.userName
+
+    // Setup Trans action
+    const trx = await transaction(collections)
+
+    try {
+      await trx.step(
+        () => query`
+          WITH affiliations, organizations, users
+          FOR v, e IN 1..1 INBOUND ${userId} affiliations
+            REMOVE { _key: e._key } IN affiliations
+            OPTIONS { waitForSync: true }
+        `,
+      )
+    } catch (err) {
+      console.error(
+        `Trx step error occurred when removing users remaining affiliations when user: ${user._key} attempted to close account: ${userId}: ${err}`,
+      )
+      throw new Error(i18n._(t`Unable to close account. Please try again.`))
+    }
+
+    try {
+      await trx.step(
+        () => query`
+          WITH users
+          REMOVE PARSE_IDENTIFIER(${userId}).key
+          IN users OPTIONS { waitForSync: true }
+        `,
+      )
+    } catch (err) {
+      console.error(
+        `Trx step error occurred when removing user: ${user._key} attempted to close account: ${userId}: ${err}`,
+      )
+      throw new Error(i18n._(t`Unable to close account. Please try again.`))
+    }
+
+    try {
+      await trx.commit()
+    } catch (err) {
+      console.error(`Trx commit error occurred when user: ${user._key} attempted to close account: ${userId}: ${err}`)
+      throw new Error(i18n._(t`Unable to close account. Please try again.`))
+    }
+
+    console.info(`User: ${user._key} successfully closed user: ${userId} account.`)
+    await logActivity({
+      transaction,
+      collections,
+      query,
+      initiatedBy: {
+        id: user._key,
+        userName: user.userName,
+        role: submittedUserId ? 'SUPER_ADMIN' : '',
+      },
+      action: 'delete',
+      target: {
+        resource: targetUserName, // name of resource being acted upon
+        resourceType: 'user', // user, org, domain
+      },
+    })
+
+    return {
+      _type: 'regular',
+      status: i18n._(t`Successfully closed account.`),
+    }
+  },
+})
+
+export const closeAccountOther = new mutationWithClientMutationId({
+  name: 'CloseAccountOther',
+  description: `This mutation allows a super admin to close another user's account.`,
   inputFields: () => ({
     userId: {
       type: GraphQLID,
@@ -42,36 +131,32 @@ export const closeAccount = new mutationWithClientMutationId({
 
     let userId = ''
     let targetUserName = ''
-    if (submittedUserId) {
-      const permission = await checkSuperAdmin()
-      if (!permission) {
-        console.warn(
-          `User: ${user._key} attempted to close user: ${submittedUserId} account, but requesting user is not a super admin.`,
-        )
-        return {
-          _type: 'error',
-          code: 400,
-          description: i18n._(t`Permission error: Unable to close other user's account.`),
-        }
-      }
 
-      const checkUser = await loadUserByKey.load(submittedUserId)
-      if (typeof checkUser === 'undefined') {
-        console.warn(
-          `User: ${user._key} attempted to close user: ${submittedUserId} account, but requested user is undefined.`,
-        )
-        return {
-          _type: 'error',
-          code: 400,
-          description: i18n._(t`Unable to close account of an undefined user.`),
-        }
+    const permission = await checkSuperAdmin()
+    if (!permission) {
+      console.warn(
+        `User: ${user._key} attempted to close user: ${submittedUserId} account, but requesting user is not a super admin.`,
+      )
+      return {
+        _type: 'error',
+        code: 400,
+        description: i18n._(t`Permission error: Unable to close other user's account.`),
       }
-      userId = checkUser._id
-      targetUserName = checkUser.userName
-    } else {
-      userId = user._id
-      targetUserName = user.userName
     }
+
+    const checkUser = await loadUserByKey.load(submittedUserId)
+    if (typeof checkUser === 'undefined') {
+      console.warn(
+        `User: ${user._key} attempted to close user: ${submittedUserId} account, but requested user is undefined.`,
+      )
+      return {
+        _type: 'error',
+        code: 400,
+        description: i18n._(t`Unable to close account of an undefined user.`),
+      }
+    }
+    userId = checkUser._id
+    targetUserName = checkUser.userName
 
     // Setup Trans action
     const trx = await transaction(collections)
