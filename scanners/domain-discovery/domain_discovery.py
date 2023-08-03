30258e5d108e1f5b3c60f22fcb54fca2fe8670bc
lcampbell <l.campbell144768@gmail.com>
2023-08-03 09:02:12 -0300

    add NATS pubsub

--- scanners/domain-discovery/domain_discovery.py
+++ scanners/domain-discovery/domain_discovery.py
@@ -3,6 +3,16 @@ import logging
 import os
 import shutil
 from arango import ArangoClient
+from dotenv import load_dotenv
+
+import asyncio
+import nats
+import functools
+import json
+import signal
+import traceback
+
+load_dotenv()
 
 logging.basicConfig(
     level=logging.INFO, format="[%(asctime)s :: %(name)s :: %(levelname)s] %(message)s"
@@ -14,6 +24,12 @@ DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
 DB_URL = os.getenv("DB_URL")
 
+NAME = os.getenv("NAME", "domain-discovery")
+SUBSCRIBE_TO = os.getenv("SUBSCRIBE_TO")
+PUBLISH_TO = os.getenv("PUBLISH_TO")
+QUEUE_GROUP = os.getenv("QUEUE_GROUP")
+SERVERLIST = os.getenv("NATS_SERVERS")
+SERVERS = SERVERLIST.split(",")
 
 # Establish DB connection
 arango_client = ArangoClient(hosts=DB_URL)
@@ -30,6 +46,7 @@ def process_subdomains(base_domain, orgId):
     subdomains = f.readlines()
     subdomains = [domain.strip() for domain in subdomains]
     claimed_domains = get_claimed_domains(orgId)
+    domains_to_scan = []
     for subdomain in subdomains:
         if subdomain not in claimed_domains:
             logging.info("Adding {subdomain} to DB".format(subdomain=subdomain))
@@ -52,6 +69,7 @@ def process_subdomains(base_domain, orgId):
                         "archived": False,
                     }
                 )
+                domains_to_scan.append(domainInsert)
             else:
                 domainInsert = checkDomain
             db.collection("claims").insert(
@@ -62,7 +80,7 @@ def process_subdomains(base_domain, orgId):
                     "tags": [],
                 }
             )
-            # TODO publish to NATS
+    return domains_to_scan
 
 
 def get_claimed_domains(orgId):
@@ -70,7 +88,6 @@ def get_claimed_domains(orgId):
     cursor = db.aql.execute(
         "FOR v, e IN 1..1 OUTBOUND @val claims RETURN v.domain",
         bind_vars={"val": orgId},
-        batch_size=2,
         count=True,
     )
     return [document for document in cursor]
@@ -85,13 +102,98 @@ def domain_discovery(domain="", orgId=""):
 
     if domain == "":
         print("No domain provided")
+    elif orgId == "":
+        print("No orgId provided")
     else:
         print("Running domain discovery for {domain}".format(domain=domain))
         subdomain_enumeration(domain)
-        process_subdomains(domain, orgId)
+        results = process_subdomains(domain, orgId)
         os.chdir("../")
         shutil.rmtree("domains")
+        return results
+
+
+async def run(loop):
+    async def error_cb(error):
+        logger.error(error)
+
+    async def closed_cb():
+        logger.info("Connection to NATS is closed.")
+        await asyncio.sleep(0.1)
+        loop.stop()
+
+    async def reconnected_cb():
+        logger.info(f"Connected to NATS at {nc.connected_url.netloc}...")
+
+    nc = await nats.connect(
+        error_cb=error_cb,
+        closed_cb=closed_cb,
+        reconnected_cb=reconnected_cb,
+        servers=SERVERS,
+        name=NAME,
+    )
+
+    logger.info(f"Connected to NATS at {nc.connected_url.netloc}...")
+
+    async def subscribe_handler(msg):
+        await asyncio.sleep(0.01)
+        subject = msg.subject
+        reply = msg.reply
+        data = msg.data.decode()
+        logger.info(f"Received a message on '{subject} {reply}': {data}")
+        payload = json.loads(msg.data)
+
+        domain = payload.get("domain")
+        orgId = payload.get("orgId")
+
+        results = domain_discovery(domain, orgId)
+
+        for domain in results:
+            domain_key = domain["_key"]
+            try:
+                await nc.publish(
+                    f"{PUBLISH_TO}.{domain_key}",
+                    json.dumps(
+                        {
+                            "domain": domain["domain"],
+                            "selectors": domain["selectors"],
+                            "domain_key": domain_key,
+                        }
+                    ).encode(),
+                )
+
+            except Exception as e:
+                logging.error(
+                    f"Inserting processed results: {str(e)} \n\nFull traceback: {traceback.format_exc()}"
+                )
+                return
+
+            # logging.info(
+            #     f"DNS Scans inserted into database: {json.dumps(processed_results)}"
+            # )
+
+    await nc.subscribe(subject=SUBSCRIBE_TO, queue=QUEUE_GROUP, cb=subscribe_handler)
+
+    def ask_exit(sig_name):
+        logger.error(f"Got signal {sig_name}: exit")
+        if nc.is_closed:
+            return
+        loop.create_task(nc.close())
+
+    for signal_name in {"SIGINT", "SIGTERM"}:
+        loop.add_signal_handler(
+            getattr(signal, signal_name), functools.partial(ask_exit, signal_name)
+        )
+
+
+def main():
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(run(loop))
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
 
 
 if __name__ == "__main__":
-    domain_discovery("domain.gc.ca", "organizations/1234")
+    main()
