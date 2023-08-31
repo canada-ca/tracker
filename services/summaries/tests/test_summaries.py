180ddaab0ab3bdd9955a378bfda2832b332984d2
lcampbell <l.campbell144768@gmail.com>
2023-08-31 10:29:30 -0300

    fetch summary by key

--- services/summaries/tests/test_summaries.py
+++ services/summaries/tests/test_summaries.py
@@ -124,7 +124,7 @@ claims.insert({"_from": org["_id"], "_to": domain3["_id"], "hidden": False})
 def test_update_chart_summaries():
     update_chart_summaries(host="http://testdb:8529", name="test", user="", password="")
 
-    summary = db.collection("chartSummaries").get({"date": date.today()})
+    summary = db.collection("chartSummaries").get({"_key": "testorg"})
     assert summary["https"] == {
         "pass": 2,
         "fail": 1,
