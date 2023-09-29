e58b3130f8ac3132014a2089b420aa883e380a22
lcampbell <l.campbell144768@gmail.com>
2023-09-29 15:17:02 -0300

    use env var to include service account in footprint updates

--- services/org-footprint/src/org-footprint-service.js
+++ services/org-footprint/src/org-footprint-service.js
@@ -11,28 +11,25 @@ const orgFootprintService = async ({ query, log, notifyClient }) => {
     const auditLogs = await getNewAuditLogs({ query, orgKey })
     // if new audit logs exist
     if (auditLogs.length > 0) {
+      // get list of org admins
+      const orgAdmins = await getOrgAdmins({ query, orgKey })
       if (REDIRECT_TO_SERVICE_ACCOUNT_EMAIL) {
-        const orgNames = await getBilingualOrgNames({ query, orgKey })
-        log(`Sending recent activity email to service account: ${orgKey}`)
         // send email to service account
-        const user = {
+        orgAdmins.push({
           userName: SERVICE_ACCOUNT_EMAIL,
           displayName: 'Service Account',
           preferredLang: 'en',
           _key: 'service-account',
-        }
-        await sendOrgFootprintEmail({ notifyClient, user, auditLogs, orgNames })
-      } else {
-        // get list of org admins
-        const orgAdmins = await getOrgAdmins({ query, orgKey })
-        // if org admins exist
-        if (orgAdmins.length > 0) {
-          const orgNames = await getBilingualOrgNames({ query, orgKey })
-          log(`Sending recent activity email to admins of org: ${orgKey}`)
-          // send email to org admins
-          for (const user of orgAdmins) {
-            await sendOrgFootprintEmail({ notifyClient, user, auditLogs, orgNames })
-          }
+        })
+      }
+      // if org admins exist
+      if (orgAdmins.length > 0) {
+        log(`Sending recent activity email to admins of org: ${orgKey}`)
+        const orgNames = await getBilingualOrgNames({ query, orgKey })
+
+        // send email to org admins
+        for (const user of orgAdmins) {
+          await sendOrgFootprintEmail({ notifyClient, user, auditLogs, orgNames })
         }
       }
     }
