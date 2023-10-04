04580359fb79920fe38af57666169d0299077c83
lcampbell <l.campbell144768@gmail.com>
2023-10-04 15:10:20 -0300

    create get org admins query"

--- services/progress-report/src/database/index.js
+++ services/progress-report/src/database/index.js
@@ -1,4 +1,5 @@
 const { findChartSummaries } = require('./find-chart-summaries')
 const { findOrgSummaries } = require('./find-verified-org-summaries')
+const { getOrgAdmins } = require('./get-org-admins')
 
-module.exports = { findChartSummaries, findOrgSummaries }
+module.exports = { findChartSummaries, findOrgSummaries, getOrgAdmins }
