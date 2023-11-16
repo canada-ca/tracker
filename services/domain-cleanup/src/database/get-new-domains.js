41e67ecd878d25d95b5268346170078b6184a523
Luke Campbell <64781228+lcampbell2@users.noreply.github.com>
2023-11-16 13:06:28 -0400

    Remove new tags after 120 days (#4932)
    * add firstSeen value to claim during create-domain
    
    * when updating domain, add firstSeen value to claim if undefined
    
    * add service that removes NEW tag from claims that are 120 days old
    
    * get-new-domains tests
    
    * add trycatch to services so they can run individually

---
+++ services/domain-cleanup/src/database/get-new-domains.js
@@ -0,0 +1,25 @@
+const getNewDomains = async ({ query, _log }) => {
+  let cursor
+  try {
+    cursor = await query`
+            FOR c IN claims
+                FILTER { en: "NEW", fr: "NOUVEAU" } IN c.tags
+                RETURN c
+        `
+  } catch (err) {
+    throw new Error(`Database error occurred while trying to find new domains: ${err}`)
+  }
+
+  let newDomains
+  try {
+    newDomains = await cursor.all()
+  } catch (err) {
+    throw new Error(`Cursor error occurred while trying to find new domains: ${err}`)
+  }
+
+  return newDomains
+}
+
+module.exports = {
+  getNewDomains,
+}
--- services/domain-cleanup/src/database/index.js
+++ services/domain-cleanup/src/database/index.js
@@ -1,5 +1,6 @@
 const { getNXDomains } = require('./get-nxdomains')
 const { findDomainClaims } = require('./find-domain-claims')
 const { findUnclaimedDomains } = require('./find-unclaimed-domains')
+const { getNewDomains } = require('./get-new-domains')
 
-module.exports = { getNXDomains, findDomainClaims, findUnclaimedDomains }
+module.exports = { getNXDomains, findDomainClaims, findUnclaimedDomains, getNewDomains }
