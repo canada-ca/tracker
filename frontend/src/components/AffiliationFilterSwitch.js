53e0d36c2041a53e00b38efb0be9e2055a888134
lcampbell <l.campbell144768@gmail.com>
2023-12-18 12:30:13 -0400

    add tooltip to switch

--- frontend/src/components/AffiliationFilterSwitch.js
+++ frontend/src/components/AffiliationFilterSwitch.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import { Flex, Switch } from '@chakra-ui/react'
+import { Flex, Switch, Tooltip } from '@chakra-ui/react'
 import { bool, func } from 'prop-types'
 import { UserIcon } from '../theme/Icons'
 import { useUserVar } from '../utilities/userState'
+import { t } from '@lingui/macro'
 
 export function AffiliationFilterSwitch({ isAffiliated, setIsAffiliated }) {
   const { isLoggedIn } = useUserVar()
   return (
     isLoggedIn() && (
       <Flex align="center" my="2">
-        <Switch
-          isFocusable={true}
-          aria-label="Show only affiliated resources"
-          mx="2"
-          defaultChecked={isAffiliated}
-          onChange={(e) => setIsAffiliated(e.target.checked)}
-        />
-        <UserIcon color="gray.900" size="lg" />
+        <Tooltip label={t`Filter list to affiliated resources only.`}>
+          <Switch
+            isFocusable={true}
+            aria-label="Filter list to affiliated resources only."
+            mx="2"
+            defaultChecked={isAffiliated}
+            onChange={(e) => setIsAffiliated(e.target.checked)}
+          />
+          <UserIcon color="gray.900" size="lg" />
+        </Tooltip>
       </Flex>
     )
   )
