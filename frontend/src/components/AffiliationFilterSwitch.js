4c7cd01aad471338b441ce4979b31da0040b33bf
lcampbell <l.campbell144768@gmail.com>
2023-12-19 08:11:58 -0400

    add AB test wrapper to new switch

--- frontend/src/components/AffiliationFilterSwitch.js
+++ frontend/src/components/AffiliationFilterSwitch.js
@@ -4,23 +4,28 @@ import { bool, func } from 'prop-types'
 import { UserIcon } from '../theme/Icons'
 import { t } from '@lingui/macro'
 import { useUserVar } from '../utilities/userState'
+import { ABTestVariant, ABTestWrapper } from '../app/ABTestWrapper'
 
 export function AffiliationFilterSwitch({ isAffiliated, setIsAffiliated }) {
   const { isLoggedIn } = useUserVar()
   if (!isLoggedIn()) return null
   return (
-    <Tooltip label={t`Filter list to affiliated resources only.`}>
-      <Flex align="center" my="2">
-        <Switch
-          isFocusable={true}
-          aria-label="Filter list to affiliated resources only."
-          mx="2"
-          defaultChecked={isAffiliated}
-          onChange={(e) => setIsAffiliated(e.target.checked)}
-        />
-        <UserIcon color="gray.900" size="lg" />
-      </Flex>
-    </Tooltip>
+    <ABTestWrapper insiderVariantName="B">
+      <ABTestVariant name="B">
+        <Tooltip label={t`Filter list to affiliated resources only.`}>
+          <Flex align="center" my="2">
+            <Switch
+              isFocusable={true}
+              aria-label="Filter list to affiliated resources only."
+              mx="2"
+              defaultChecked={isAffiliated}
+              onChange={(e) => setIsAffiliated(e.target.checked)}
+            />
+            <UserIcon color="gray.900" size="lg" />
+          </Flex>
+        </Tooltip>
+      </ABTestVariant>
+    </ABTestWrapper>
   )
 }
 
