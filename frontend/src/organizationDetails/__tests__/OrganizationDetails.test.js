52e2d043f3bcf9fc69777735be3f24d0a183b4b7
lcampbell <l.campbell144768@gmail.com>
2024-01-04 11:58:37 -0400

    add tests

--- frontend/src/organizationDetails/__tests__/OrganizationDetails.test.js
+++ frontend/src/organizationDetails/__tests__/OrganizationDetails.test.js
@@ -45,90 +45,90 @@ window.resizeTo = function resizeTo(width, height) {
 
 describe('<OrganizationDetails />', () => {
   describe('given the url /organisations/tbs-sct-gc-ca', () => {
-    it('displays details using the tbs-sct-gc-ca slug', async () => {
-      window.resizeTo(1024, 768)
+    const name = 'Treasury Board Secretariat'
 
-      const name = 'Treasury Board Secretariat'
-
-      const mocks = [
-        {
-          request: {
-            query: ORG_DETAILS_PAGE,
-            variables: { slug: 'tbs-sct-gc-ca' },
-          },
-          result: {
-            data: {
-              organization: {
-                id: 'ODk3MDg5MzI2MA==',
-                name,
-                lastRan: 'today',
-                acronym: 'TBS',
-                domainCount: 1,
-                city: 'Ottawa',
-                province: 'ON',
-                verified: true,
-                summaries: {
-                  https: {
-                    total: 54386,
-                    categories: [
-                      {
-                        name: 'pass',
-                        count: 7435,
-                        percentage: 50,
-                      },
-                      {
-                        name: 'fail',
-                        count: 7435,
-                        percentage: 43.5,
-                      },
-                    ],
-                  },
-                  dmarcPhase: {
-                    total: 5355,
-                    categories: [
-                      {
-                        name: 'not implemented',
-                        count: 611,
-                        percentage: 11.409897292250234,
-                      },
-                      {
-                        name: 'assess',
-                        count: 410,
-                        percentage: 7.65639589169001,
-                      },
-                      {
-                        name: 'deploy',
-                        count: 1751,
-                        percentage: 32.698412698412696,
-                      },
-                      {
-                        name: 'enforce',
-                        count: 1248,
-                        percentage: 23.30532212885154,
-                      },
-                      {
-                        name: 'maintain',
-                        count: 1335,
-                        percentage: 24.92997198879552,
-                      },
-                    ],
-                  },
-                },
-                affiliations: {
-                  totalCount: 5,
-                  edges: [
+    const mocks = [
+      {
+        request: {
+          query: ORG_DETAILS_PAGE,
+          variables: { slug: 'tbs-sct-gc-ca' },
+        },
+        result: {
+          data: {
+            organization: {
+              id: 'ODk3MDg5MzI2MA==',
+              name,
+              lastRan: 'today',
+              acronym: 'TBS',
+              domainCount: 1,
+              city: 'Ottawa',
+              province: 'ON',
+              verified: true,
+              userHasPermission: false,
+              summaries: {
+                https: {
+                  total: 54386,
+                  categories: [
                     {
-                      node: {
-                        id: 'ABCDEF38924712398',
-                      },
+                      name: 'pass',
+                      count: 7435,
+                      percentage: 50,
+                    },
+                    {
+                      name: 'fail',
+                      count: 7435,
+                      percentage: 43.5,
+                    },
+                  ],
+                },
+                dmarcPhase: {
+                  total: 5355,
+                  categories: [
+                    {
+                      name: 'not implemented',
+                      count: 611,
+                      percentage: 11.409897292250234,
+                    },
+                    {
+                      name: 'assess',
+                      count: 410,
+                      percentage: 7.65639589169001,
+                    },
+                    {
+                      name: 'deploy',
+                      count: 1751,
+                      percentage: 32.698412698412696,
+                    },
+                    {
+                      name: 'enforce',
+                      count: 1248,
+                      percentage: 23.30532212885154,
+                    },
+                    {
+                      name: 'maintain',
+                      count: 1335,
+                      percentage: 24.92997198879552,
                     },
                   ],
                 },
               },
+              affiliations: {
+                totalCount: 5,
+                edges: [
+                  {
+                    node: {
+                      id: 'ABCDEF38924712398',
+                    },
+                  },
+                ],
+              },
             },
           },
         },
-      ]
+      },
+    ]
+    it('displays details using the tbs-sct-gc-ca slug', async () => {
+      window.resizeTo(1024, 768)
 
       const { getByText } = render(
         <ChakraProvider theme={theme}>
@@ -141,10 +141,7 @@ describe('<OrganizationDetails />', () => {
                   userName: null,
                 })}
               >
-                <MemoryRouter
-                  initialEntries={['/organizations/tbs-sct-gc-ca']}
-                  initialIndex={0}
-                >
+                <MemoryRouter initialEntries={['/organizations/tbs-sct-gc-ca']} initialIndex={0}>
                   <Route path="/organizations/:orgSlug">
                     <OrganizationDetails />
                   </Route>
@@ -160,104 +157,132 @@ describe('<OrganizationDetails />', () => {
         expect(getByText('Summary')).toBeInTheDocument()
       })
     })
-  })
-})
-
-describe('<OrganizationDetails />', () => {
-  describe('for a non-admin user', () => {
-    it('displays details using the treasury-board-of-canada-secretariat slug', async () => {
+    it('displays the request invite buton for unaffiliated users', async () => {
       window.resizeTo(1024, 768)
+      const { queryByRole, getByText } = render(
+        <ChakraProvider theme={theme}>
+          <I18nProvider i18n={i18n}>
+            <MockedProvider mocks={mocks} addTypename={false}>
+              <UserVarProvider
+                userVar={makeVar({
+                  jwt: 'asdf1234',
+                  tlfaSendMethod: null,
+                  userName: 'justauser',
+                })}
+              >
+                <MemoryRouter initialEntries={['/organizations/tbs-sct-gc-ca']} initialIndex={0}>
+                  <Route path="/organizations/:orgSlug">
+                    <OrganizationDetails />
+                  </Route>
+                </MemoryRouter>
+              </UserVarProvider>
+            </MockedProvider>
+          </I18nProvider>
+        </ChakraProvider>,
+      )
 
-      const mocks = [
-        {
-          request: {
-            query: ORG_DETAILS_PAGE,
-            variables: { slug: 'treasury-board-of-canada-secretariat' },
-          },
-          result: {
-            errors: [
-              {
-                message:
-                  'cannot query affiliations on organization without admin permission or higher.',
-                locations: [{ line: 33, column: 5 }],
-                path: ['organization', 'affiliations'],
-                extensions: { code: 'INTERNAL_SERVER_ERROR' },
-              },
-            ],
-            data: {
-              organization: {
-                id: 'b3JnYW5pemF0aW9uczoyMTY2MDUy',
-                name: 'Treasury Board of Canada Secretariat',
-                lastRan: 'today',
-                acronym: 'TBS',
-                domainCount: 82,
-                city: 'Ottawa',
-                province: 'Ontario',
-                verified: true,
-                summaries: {
-                  https: {
-                    total: 82,
-                    categories: [
-                      {
-                        name: 'pass',
-                        count: 5,
-                        percentage: 6.1,
-                        __typename: 'SummaryCategory',
-                      },
-                      {
-                        name: 'fail',
-                        count: 77,
-                        percentage: 93.9,
-                        __typename: 'SummaryCategory',
-                      },
-                    ],
-                    __typename: 'CategorizedSummary',
-                  },
-                  dmarcPhase: {
-                    total: 5355,
-                    categories: [
-                      {
-                        name: 'not implemented',
-                        count: 611,
-                        percentage: 11.409897292250234,
-                        __typename: 'SummaryCategory',
-                      },
-                      {
-                        name: 'assess',
-                        count: 410,
-                        percentage: 7.65639589169001,
-                        __typename: 'SummaryCategory',
-                      },
-                      {
-                        name: 'deploy',
-                        count: 1751,
-                        percentage: 32.698412698412696,
-                        __typename: 'SummaryCategory',
-                      },
-                      {
-                        name: 'enforce',
-                        count: 1248,
-                        percentage: 23.30532212885154,
-                        __typename: 'SummaryCategory',
-                      },
-                      {
-                        name: 'maintain',
-                        count: 1335,
-                        percentage: 24.92997198879552,
-                        __typename: 'SummaryCategory',
-                      },
-                    ],
-                    __typename: 'CategorizedSummary',
-                  },
-                  __typename: 'OrganizationSummary',
+      await waitFor(() => {
+        expect(getByText(name)).toBeInTheDocument()
+        expect(getByText('Summary')).toBeInTheDocument()
+      })
+
+      const requestInviteButton = queryByRole('button', { name: /Request Invite/ })
+      expect(requestInviteButton).toBeInTheDocument()
+    })
+  })
+  describe('for a non-admin user', () => {
+    const mocks = [
+      {
+        request: {
+          query: ORG_DETAILS_PAGE,
+          variables: { slug: 'treasury-board-of-canada-secretariat' },
+        },
+        result: {
+          errors: [
+            {
+              message: 'cannot query affiliations on organization without admin permission or higher.',
+              locations: [{ line: 33, column: 5 }],
+              path: ['organization', 'affiliations'],
+              extensions: { code: 'INTERNAL_SERVER_ERROR' },
+            },
+          ],
+          data: {
+            organization: {
+              id: 'b3JnYW5pemF0aW9uczoyMTY2MDUy',
+              name: 'Treasury Board of Canada Secretariat',
+              lastRan: 'today',
+              acronym: 'TBS',
+              domainCount: 82,
+              city: 'Ottawa',
+              province: 'Ontario',
+              verified: true,
+              userHasPermission: true,
+              summaries: {
+                https: {
+                  total: 82,
+                  categories: [
+                    {
+                      name: 'pass',
+                      count: 5,
+                      percentage: 6.1,
+                      __typename: 'SummaryCategory',
+                    },
+                    {
+                      name: 'fail',
+                      count: 77,
+                      percentage: 93.9,
+                      __typename: 'SummaryCategory',
+                    },
+                  ],
+                  __typename: 'CategorizedSummary',
                 },
-                affiliations: null,
-                __typename: 'Organization',
+                dmarcPhase: {
+                  total: 5355,
+                  categories: [
+                    {
+                      name: 'not implemented',
+                      count: 611,
+                      percentage: 11.409897292250234,
+                      __typename: 'SummaryCategory',
+                    },
+                    {
+                      name: 'assess',
+                      count: 410,
+                      percentage: 7.65639589169001,
+                      __typename: 'SummaryCategory',
+                    },
+                    {
+                      name: 'deploy',
+                      count: 1751,
+                      percentage: 32.698412698412696,
+                      __typename: 'SummaryCategory',
+                    },
+                    {
+                      name: 'enforce',
+                      count: 1248,
+                      percentage: 23.30532212885154,
+                      __typename: 'SummaryCategory',
+                    },
+                    {
+                      name: 'maintain',
+                      count: 1335,
+                      percentage: 24.92997198879552,
+                      __typename: 'SummaryCategory',
+                    },
+                  ],
+                  __typename: 'CategorizedSummary',
+                },
+                __typename: 'OrganizationSummary',
               },
+              affiliations: null,
+              __typename: 'Organization',
             },
           },
         },
-      ]
+      },
+    ]
+    it('displays details using the treasury-board-of-canada-secretariat slug', async () => {
+      window.resizeTo(1024, 768)
 
       const { queryByText } = render(
         <ChakraProvider theme={theme}>
@@ -270,12 +295,7 @@ describe('<OrganizationDetails />', () => {
                   userName: 'justauser',
                 })}
               >
-                <MemoryRouter
-                  initialEntries={[
-                    '/organizations/treasury-board-of-canada-secretariat',
-                  ]}
-                  initialIndex={0}
-                >
+                <MemoryRouter initialEntries={['/organizations/treasury-board-of-canada-secretariat']} initialIndex={0}>
                   <Route path="/organizations/:orgSlug">
                     <OrganizationDetails />
                   </Route>
@@ -288,6 +308,33 @@ describe('<OrganizationDetails />', () => {
 
       await waitFor(() => {
         expect(queryByText(/Ottawa/)).not.toBeInTheDocument()
+      })
+    })
+    it('does not display the request invite buton for affiliated users', async () => {
+      const { queryByText } = render(
+        <ChakraProvider theme={theme}>
+          <I18nProvider i18n={i18n}>
+            <MockedProvider mocks={mocks} addTypename={false}>
+              <UserVarProvider
+                userVar={makeVar({
+                  jwt: 'asdf1234',
+                  tlfaSendMethod: null,
+                  userName: 'justauser',
+                })}
+              >
+                <MemoryRouter initialEntries={['/organizations/treasury-board-of-canada-secretariat']} initialIndex={0}>
+                  <Route path="/organizations/:orgSlug">
+                    <OrganizationDetails />
+                  </Route>
+                </MemoryRouter>
+              </UserVarProvider>
+            </MockedProvider>
+          </I18nProvider>
+        </ChakraProvider>,
+      )
+
+      await waitFor(() => {
+        expect(queryByText(/Request Invite/)).not.toBeInTheDocument()
       })
     })
   })
