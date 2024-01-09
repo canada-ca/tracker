c3e70359d8dfd4c2e7a62ce21feff6098503445a
Luke Campbell <64781228+lcampbell2@users.noreply.github.com>
2024-01-09 14:40:38 -0500

    bring AffilationFilterSwitch to A stream (#5047)

--- frontend/src/components/AffiliationFilterSwitch.js
+++ frontend/src/components/AffiliationFilterSwitch.js
@@ -4,28 +4,23 @@ import { bool, func } from 'prop-types'
 import { UserIcon } from '../theme/Icons'
 import { t } from '@lingui/macro'
 import { useUserVar } from '../utilities/userState'
-import { ABTestVariant, ABTestWrapper } from '../app/ABTestWrapper'
 
 export function AffiliationFilterSwitch({ isAffiliated, setIsAffiliated }) {
   const { isLoggedIn } = useUserVar()
   if (!isLoggedIn()) return null
   return (
-    <ABTestWrapper insiderVariantName="B">
-      <ABTestVariant name="B">
-        <Tooltip label={t`Filter list to affiliated resources only.`} hasArrow>
-          <Flex align="center" my="2" maxW="5rem">
-            <Switch
-              isFocusable={true}
-              aria-label="Filter list to affiliated resources only."
-              mx="2"
-              defaultChecked={isAffiliated}
-              onChange={(e) => setIsAffiliated(e.target.checked)}
-            />
-            <UserIcon color="gray.900" size="lg" />
-          </Flex>
-        </Tooltip>
-      </ABTestVariant>
-    </ABTestWrapper>
+    <Tooltip label={t`Filter list to affiliated resources only.`} hasArrow>
+      <Flex align="center" my="2" maxW="5rem">
+        <Switch
+          isFocusable={true}
+          aria-label="Filter list to affiliated resources only."
+          mx="2"
+          defaultChecked={isAffiliated}
+          onChange={(e) => setIsAffiliated(e.target.checked)}
+        />
+        <UserIcon color="gray.900" size="lg" />
+      </Flex>
+    </Tooltip>
   )
 }
 
