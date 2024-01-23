d2040d93401d4a4a2777b8fbfa5ad41d724cdd6b
lcampbell <l.campbell144768@gmail.com>
2024-01-23 16:04:01 -0400

    change KUSTO_CLUSTER secret value, build link in kusto_client.py

--- azure-defender-easm/kusto_client.py
+++ azure-defender-easm/kusto_client.py
@@ -6,12 +6,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 KUSTO_CLUSTER = os.getenv("KUSTO_CLUSTER")
+REGION = os.getenv("REGION")
 KUSTO_DATABASE = os.getenv("KUSTO_DATABASE")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 AUTHORITY_ID = os.getenv("TENANT_ID")
 
 KCSB_DATA = KustoConnectionStringBuilder.with_aad_application_key_authentication(
-    KUSTO_CLUSTER, CLIENT_ID, CLIENT_SECRET, AUTHORITY_ID
+    f"https://{KUSTO_CLUSTER}.{REGION}.kusto.windows.net",
+    CLIENT_ID,
+    CLIENT_SECRET,
+    AUTHORITY_ID,
 )
 KUSTO_CLIENT = KustoClient(KCSB_DATA)
