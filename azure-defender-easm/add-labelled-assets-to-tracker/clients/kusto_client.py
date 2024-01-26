d2d75b46348a44a5d4e5a6fefb404f41e7668432
lcampbell <l.campbell144768@gmail.com>
2024-01-26 16:14:36 -0400

    init service dir

---
+++ azure-defender-easm/add-labelled-assets-to-tracker/clients/kusto_client.py
@@ -0,0 +1,42 @@
+from azure.kusto.data import KustoClient, KustoConnectionStringBuilder
+from azure.kusto.data.helpers import dataframe_from_result_table
+
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+KUSTO_CLUSTER = os.getenv("KUSTO_CLUSTER")
+REGION = os.getenv("REGION")
+KUSTO_DATABASE = os.getenv("KUSTO_DATABASE")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+AUTHORITY_ID = os.getenv("TENANT_ID")
+
+KCSB_DATA = KustoConnectionStringBuilder.with_aad_application_key_authentication(
+    f"https://{KUSTO_CLUSTER}.{REGION}.kusto.windows.net",
+    CLIENT_ID,
+    CLIENT_SECRET,
+    AUTHORITY_ID,
+)
+KUSTO_CLIENT = KustoClient(KCSB_DATA)
+
+
+def get_labelled_org_assets_from_org_key(org_key):
+    query = f"""
+    EasmAsset
+    | where TimeGeneratedValue > ago(24h)
+    | where AssetType == 'HOST'
+    | where Labels == '["{org_key}"]'
+    | project AssetName, AssetUuid, Labels
+    """
+    try:
+        response = KUSTO_CLIENT.execute(KUSTO_DATABASE, query)
+        data = dataframe_from_result_table(response.primary_results[0]).to_dict(
+            orient="records"
+        )
+        return data
+    except Exception as e:
+        print(f"Failed to get labelled assets from org key: {e}")
+        return []
