9fe6d8be2fb80544c4d67679b25b0e1b27d37d33
lcampbell <l.campbell144768@gmail.com>
2024-03-22 09:44:56 -0300

    only send progress reports on preview stream

--- services/progress-report/src/database/get-org-admins.js
+++ services/progress-report/src/database/get-org-admins.js
@@ -5,6 +5,7 @@ const getOrgAdmins = async ({ query, orgId }) => {
         FOR v, e IN 1..1 OUTBOUND ${orgId} affiliations
             FILTER e.permission == "admin" OR e.permission == "owner"
             FILTER v.receiveUpdateEmails == true
+            FILTER v.insideUser == true
             RETURN v
     `
   } catch (err) {
