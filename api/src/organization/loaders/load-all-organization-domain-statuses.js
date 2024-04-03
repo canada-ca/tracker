0590d15213c51922527fe2bb824cbeec13b354ae
lcampbell <l.campbell144768@gmail.com>
2024-04-03 08:45:27 -0300

    add filter options to domain statuses loader

--- api/src/organization/loaders/load-all-organization-domain-statuses.js
+++ api/src/organization/loaders/load-all-organization-domain-statuses.js
@@ -1,61 +1,108 @@
 import { t } from '@lingui/macro'
+import { aql } from 'arangojs'
 
 export const loadAllOrganizationDomainStatuses =
   ({ query, userKey, i18n }) =>
-  async ({ blocked }) => {
-    let statuses
-
-    try {
-      if (blocked) {
-        statuses = (
-          await query`
-          WITH domains, organizations
-          FOR org IN organizations
-              FILTER org.orgDetails.en.acronym != "SA"
-              FOR domain, claim IN 1..1 OUTBOUND org._id claims
-                  FILTER domain.blocked == true
-                  RETURN {
-                      "Organization name (English)": org.orgDetails.en.name,
-                      "Nom de l'organisation (Français)": org.orgDetails.fr.name,
-                      "Domain": domain.domain,
-                      "HTTPS": domain.status.https,
-                      "HSTS": domain.status.hsts,
-                      "Ciphers": domain.status.ciphers,
-                      "Curves": domain.status.curves,
-                      "Protocols": domain.status.protocols,
-                      "SPF": domain.status.spf,
-                      "DKIM": domain.status.dkim,
-                      "DMARC": domain.status.dmarc
-                  }
-      `
-        ).all()
-      } else {
-        statuses = (
-          await query`
-          WITH domains, organizations
-          FOR org IN organizations
-              FILTER org.orgDetails.en.acronym != "SA"
-              FOR domain, claim IN 1..1 OUTBOUND org._id claims
-                  RETURN {
-                      "Organization name (English)": org.orgDetails.en.name,
-                      "Nom de l'organisation (Français)": org.orgDetails.fr.name,
-                      "Domain": domain.domain,
-                      "HTTPS": domain.status.https,
-                      "HSTS": domain.status.hsts,
-                      "Ciphers": domain.status.ciphers,
-                      "Curves": domain.status.curves,
-                      "Protocols": domain.status.protocols,
-                      "SPF": domain.status.spf,
-                      "DKIM": domain.status.dkim,
-                      "DMARC": domain.status.dmarc
-                  }
-      `
-        ).all()
-      }
-    } catch (err) {
-      console.error(`Database error occurred when user: ${userKey} running loadAllOrganizationDomainStatuses: ${err}`)
-      throw new Error(i18n._(t`Unable to load all organization domain statuses. Please try again.`))
+  async ({ filters }) => {
+    let domains
+    let domainFilters = aql`FILTER v.archived != true`
+    if (typeof filters !== 'undefined') {
+      filters.forEach(({ filterCategory, comparison, filterValue }) => {
+        if (comparison === '==') {
+          comparison = aql`==`
+        } else {
+          comparison = aql`!=`
+        }
+        if (filterCategory === 'dmarc-status') {
+          domainFilters = aql`
+          ${domainFilters}
+          FILTER v.status.dmarc ${comparison} ${filterValue}
+        `
+        } else if (filterCategory === 'dkim-status') {
+          domainFilters = aql`
+          ${domainFilters}
+          FILTER v.status.dkim ${comparison} ${filterValue}
+        `
+        } else if (filterCategory === 'https-status') {
+          domainFilters = aql`
+          ${domainFilters}
+          FILTER v.status.https ${comparison} ${filterValue}
+        `
+        } else if (filterCategory === 'spf-status') {
+          domainFilters = aql`
+          ${domainFilters}
+          FILTER v.status.spf ${comparison} ${filterValue}
+        `
+        } else if (filterCategory === 'ciphers-status') {
+          domainFilters = aql`
+          ${domainFilters}
+          FILTER v.status.ciphers ${comparison} ${filterValue}
+        `
+        } else if (filterCategory === 'curves-status') {
+          domainFilters = aql`
+          ${domainFilters}
+          FILTER v.status.curves ${comparison} ${filterValue}
+        `
+        } else if (filterCategory === 'hsts-status') {
+          domainFilters = aql`
+          ${domainFilters}
+          FILTER v.status.hsts ${comparison} ${filterValue}
+        `
+        } else if (filterCategory === 'protocols-status') {
+          domainFilters = aql`
+          ${domainFilters}
+          FILTER v.status.protocols ${comparison} ${filterValue}
+        `
+        } else if (filterCategory === 'certificates-status') {
+          domainFilters = aql`
+          ${domainFilters}
+          FILTER v.status.certificates ${comparison} ${filterValue}
+        `
+        } else if (filterCategory === 'tags') {
+          if (filterValue === 'nxdomain') {
+            domainFilters = aql`
+            ${domainFilters}
+            FILTER v.rcode ${comparison} "NXDOMAIN"
+          `
+          } else if (filterValue === 'blocked') {
+            domainFilters = aql`
+            ${domainFilters}
+            FILTER v.blocked ${comparison} true
+          `
+          } else if (filterValue === 'wildcard-sibling') {
+            domainFilters = aql`
+            ${domainFilters}
+            FILTER v.wildcardSibling ${comparison} true
+          `
+          } else if (filterValue === 'scan-pending') {
+            domainFilters = aql`
+            ${domainFilters}
+            FILTER domain.webScanPending ${comparison} true
+          `
+          }
+        }
+      })
     }
 
-    return statuses
+    try {
+      domains = (
+        await query`
+          WITH domains
+          FOR d IN domains
+            ${domainFilters}
+            RETURN {
+              domain: d.domain,
+              status: d.status,
+              rcode: d.rcode,
+              blocked: d.blocked,
+              wildcardSibling: d.wildcardSibling,
+            }
+          `
+      ).all()
+    } catch (err) {
+      console.error(`Database error occurred when user: ${userKey} running loadOrganizationDomainStatuses: ${err}`)
+      throw new Error(i18n._(t`Unable to load organization domain statuses. Please try again.`))
+    }
+
+    return domains
   }
