5e4b3de1bfd907f285bc6067fdc3b9b065e8871e
lcampbell <l.campbell144768@gmail.com>
2024-04-22 10:14:07 -0300

    bugfix: remove error message from withSuperAdmin

--- frontend/src/app/withSuperAdmin.js
+++ frontend/src/app/withSuperAdmin.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import { useQuery } from '@apollo/client'
 import { IS_USER_SUPER_ADMIN } from '../graphql/queries'
-import { ErrorFallbackMessage } from '../components/ErrorFallbackMessage'
 import { LoadingMessage } from '../components/LoadingMessage'
-import { useUserVar } from '../utilities/userState'
 
 const withSuperAdmin = (Component) => {
   const WrappedComponent = (props) => {
-    const { isLoggedIn } = useUserVar()
-    const { loading, error, data: { isUserSuperAdmin } = {} } = useQuery(IS_USER_SUPER_ADMIN)
+    const { loading, data: { isUserSuperAdmin } = {} } = useQuery(IS_USER_SUPER_ADMIN)
 
-    if (!isLoggedIn) return null
     if (loading) return <LoadingMessage />
-    if (error) return <ErrorFallbackMessage error={error} />
-
     return isUserSuperAdmin ? <Component {...props} /> : null
   }
 
