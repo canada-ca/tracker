684664a4b5b8018093079a8881eb4add89856879
FestiveKyle <sullivan.kyle@pm.me>
2024-05-28 10:26:41 -0300

    Use sendsEmail property and add new "sends email" dkim tags

--- scanners/dns-processor/service.py
+++ scanners/dns-processor/service.py
@@ -147,22 +147,6 @@ def check_mx_diff(processed_results, domain_id):
 
     return mx_record_diff
 
-
-def domain_sends_mail(domain_id):
-    check_mail_cursor = db.aql.execute(
-        """
-        FOR v, e IN 1..1 OUTBOUND @domain_id domainsToDmarcSummaries
-            FILTER e.startDate == "thirtyDays"
-            LIMIT 1
-            RETURN [ v.categoryTotals.pass, v.categoryTotals.passDkimOnly, v.categoryTotals.passSpfOnly ] ANY > 0
-    """,
-        bind_vars={"domain_id": domain_id},
-    )
-    if check_mail_cursor.empty():
-        return None
-    return check_mail_cursor.next()
-
-
 async def run(loop):
     async def error_cb(error):
         logger.error(error)
@@ -199,9 +183,12 @@ async def run(loop):
         shared_id = payload.get("shared_id")
 
         try:
-            results["domain_sends_mail"] = domain_sends_mail(f"domains/{domain_key}")
+            domain = db.collection("domains").get({"_key": domain_key})
         except Exception as e:
-            logger.error(f"Error while checking if domain sends mail: {str(e)}")
+            logger.error(f"Error while fetching domain: {str(e)}")
+            return
+
+        results["sends_email"] = domain.get("sendsEmail", "unknown")
 
         processed_results = process_results(results)
         try:
@@ -227,7 +214,6 @@ async def run(loop):
                     snake_to_camel(processed_results)
                 )
 
-                domain = db.collection("domains").get({"_key": domain_key})
                 db.collection("domainsDNS").insert(
                     {
                         "_from": domain["_id"],
