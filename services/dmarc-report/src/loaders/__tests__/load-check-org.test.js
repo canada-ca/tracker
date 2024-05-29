01365e49b31423feb235075e335a048cff02587a
FestiveKyle <sullivan.kyle@pm.me>
2024-05-29 14:38:15 -0300

    Fix load-check-org.test.js

--- services/dmarc-report/src/loaders/__tests__/load-check-org.test.js
+++ services/dmarc-report/src/loaders/__tests__/load-check-org.test.js
@@ -6,7 +6,7 @@ const { databaseOptions } = require('../../../database-options')
 const { DB_PASS: rootPass, DB_URL: url } = process.env
 
 describe('given the loadCheckOrg function', () => {
-  let query, drop, truncate, collections, org
+  let query, drop, truncate, collections, arangoCtx, org
   beforeAll(async () => {
     ;({ query, drop, truncate, collections } = await ensure({
       type: 'database',
@@ -15,6 +15,7 @@ describe('given the loadCheckOrg function', () => {
       rootPassword: rootPass,
       options: databaseOptions({ rootPass }),
     }))
+    arangoCtx = { query, collections }
   })
 
   beforeEach(async () => {
@@ -36,7 +37,8 @@ describe('given the loadCheckOrg function', () => {
   })
 
   it('returns the org from the db', async () => {
-    const checkOrg = await loadCheckOrg({ query })({
+    const checkOrg = await loadCheckOrg({
+      arangoCtx,
       orgAcronymEn: 'ACR',
     })
 
