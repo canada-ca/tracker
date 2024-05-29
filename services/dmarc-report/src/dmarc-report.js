6be1cef5a495655e1696f0858750abdc2b094f82
FestiveKyle <sullivan.kyle@pm.me>
2024-05-29 00:04:30 -0300

    Call arango async

--- services/dmarc-report/src/dmarc-report.js
+++ services/dmarc-report/src/dmarc-report.js
@@ -1,4 +1,3 @@
-const { updateDomain } = require('./update-domain')
 const { calculatePercentages } = require('./utils')
 
 module.exports.dmarcReport = async ({
@@ -7,7 +6,6 @@ module.exports.dmarcReport = async ({
   removeOwnership,
   removeSummary,
   loadArangoDates,
-  loadArangoThirtyDaysCount,
   loadCheckOrg,
   loadCheckDomain,
   loadOrgOwner,
@@ -17,12 +15,133 @@ module.exports.dmarcReport = async ({
   upsertSummary,
   cosmosDates,
   currentDate,
-  loadTables,
   container,
   UPDATE_ALL_DATES,
 }) => {
   // get org acronyms
   const orgAcronyms = Object.keys(ownerships)
+
+  async function updateDomainFunc(domain, orgAcronym, orgAcronymEn, queryResults) {
+    // check to see if domain exists
+    const checkDomain = await loadCheckDomain({ domain })
+    if (!checkDomain) {
+      console.warn(`\t${domain} cannot be found in the datastore`)
+      return
+    }
+
+    console.info(`\tWorking on domain: ${domain}`)
+
+    // get the current owner of the domain
+    const orgOwner = await loadOrgOwner({
+      domain,
+    })
+
+    // if the domain is not owned create ownership
+    if (!orgOwner) {
+      console.info(`\t\tAssigning ${domain} ownership to: ${String(orgAcronym)}`)
+      await createOwnership({ domain, orgAcronymEn })
+    }
+    // if the domain is owned by another org, remove ownership and assign a new one
+    else if (orgOwner !== orgAcronymEn) {
+      console.info(`\t\tRemoving ${domain} ownership from: ${orgOwner}`)
+      await removeOwnership({ domain, orgAcronymEn })
+
+      console.info(`\t\tAssigning ${domain} ownership to: ${String(orgAcronym)}`)
+      await createOwnership({ domain, orgAcronymEn })
+    } else {
+      console.info(`\t\tOwnership of ${domain} is already assigned to ${String(orgAcronym)}`)
+    }
+
+    const arangoDates = await loadArangoDates({ domain })
+    for (const date of arangoDates) {
+      if (cosmosDates.indexOf(date) === -1) {
+        // remove periods in arango
+        console.info(`\t\tRemoving ${date} for ${domain}`)
+        await removeSummary({
+          domain,
+          date,
+        })
+      }
+    }
+
+    const domainData = queryResults.resources.filter((resource) => resource.domain === domain)
+    if (!domainData.length > 0) {
+      console.warn(`\t${domain} cannot be found in the summaries container`)
+    }
+
+    for (const date of cosmosDates) {
+      const arangoDate = date === 'thirty_days' ? 'thirtyDays' : date
+      const summaryData = domainData.find((resource) => resource.id === date)
+      let summaryDataToInput
+      if (!summaryData) {
+        summaryDataToInput = {
+          categoryTotals: {
+            pass: 0,
+            fail: 0,
+            passDkimOnly: 0,
+            passSpfOnly: 0,
+          },
+          detailTables: {
+            dkimFailure: [],
+            dmarcFailure: [],
+            fullPass: [],
+            spfFailure: [],
+          },
+        }
+      } else {
+        summaryDataToInput = {
+          categoryTotals: {
+            pass: summaryData.category_totals.pass || 0,
+            fail: summaryData.category_totals.fail || 0,
+            passDkimOnly: summaryData.category_totals['pass-dkim-only'] || 0,
+            passSpfOnly: summaryData.category_totals['pass-spf-only'] || 0,
+          },
+          detailTables: {
+            dkimFailure: summaryData.detail_tables.dkim_failure || [],
+            dmarcFailure: summaryData.detail_tables.dmarc_failure || [],
+            fullPass: summaryData.detail_tables.full_pass || [],
+            spfFailure: summaryData.detail_tables.spf_failure || [],
+          },
+        }
+      }
+
+      const { totalMessages, categoryPercentages } = calculatePercentages({ ...summaryDataToInput.categoryTotals })
+      summaryDataToInput.categoryPercentages = categoryPercentages
+      summaryDataToInput.totalMessages = totalMessages
+
+      if (arangoDates.indexOf(date) === -1) {
+        console.info(`\t\tCreating ${date} for ${domain}`)
+        await createSummary({
+          domain,
+          date: arangoDate,
+          summaryData: summaryDataToInput,
+        })
+      } else if (date === currentDate || UPDATE_ALL_DATES) {
+        console.info(`\t\tUpserting ${date} for ${domain}`)
+        await upsertSummary({
+          domain,
+          date: arangoDate,
+          summaryData: summaryDataToInput,
+        })
+      }
+
+      if (date === 'thirty_days') {
+        // update domain mail status
+        let sendsEmail = 'false'
+        if (
+          [
+            summaryDataToInput.categoryTotals.pass,
+            summaryDataToInput.categoryTotals.passDkimOnly,
+            summaryDataToInput.categoryTotals.passSpfOnly,
+          ].some((total) => total > 0)
+        ) {
+          sendsEmail = 'true'
+        }
+        console.info(`\t\tUpdating domain mail status for ${domain} to ${sendsEmail}`)
+        await updateDomainMailStatus({ domain, sendsEmail })
+      }
+    }
+  }
 
   // loop through orgs
   for (const orgAcronym of orgAcronyms) {
@@ -36,7 +155,7 @@ module.exports.dmarcReport = async ({
 
     console.info(`Updating DMARC summary info for org: ${String(orgAcronym)}`)
 
-    const batchSize = 20
+    const batchSize = 60
 
     for (let i = 0; i < ownerships[orgAcronym].length; i += batchSize) {
       // Batch update domains, process 20 at a time
@@ -56,127 +175,8 @@ module.exports.dmarcReport = async ({
         })
         .fetchAll()
 
-      for await (const domain of domains) {
-        // check to see if domain exists
-        const checkDomain = await loadCheckDomain({ domain })
-        if (!checkDomain) {
-          console.warn(`\t${domain} cannot be found in the datastore`)
-          continue
-        }
-
-        console.info(`\tWorking on domain: ${domain}`)
-
-        // get the current owner of the domain
-        const orgOwner = await loadOrgOwner({
-          domain,
-        })
-
-        // if the domain is not owned create ownership
-        if (!orgOwner) {
-          console.info(`\t\tAssigning ${domain} ownership to: ${String(orgAcronym)}`)
-          await createOwnership({ domain, orgAcronymEn })
-        }
-        // if the domain is owned by another org, remove ownership and assign a new one
-        else if (orgOwner !== orgAcronymEn) {
-          console.info(`\t\tRemoving ${domain} ownership from: ${orgOwner}`)
-          await removeOwnership({ domain, orgAcronymEn })
-
-          console.info(`\t\tAssigning ${domain} ownership to: ${String(orgAcronym)}`)
-          await createOwnership({ domain, orgAcronymEn })
-        } else {
-          console.info(`\t\tOwnership of ${domain} is already assigned to ${String(orgAcronym)}`)
-        }
-
-        const arangoDates = await loadArangoDates({ domain })
-        for (const date of arangoDates) {
-          if (cosmosDates.indexOf(date) === -1) {
-            // remove periods in arango
-            console.info(`\t\tRemoving ${date} for ${domain}`)
-            await removeSummary({
-              domain,
-              date,
-            })
-          }
-        }
-
-        const domainData = queryResults.resources.filter((resource) => resource.domain === domain)
-        if (!domainData.length > 0) {
-          console.warn(`\t${domain} cannot be found in the summaries container`)
-        }
-
-        for (const date of cosmosDates) {
-          const arangoDate = date === 'thirty_days' ? 'thirtyDays' : date
-          const summaryData = domainData.find((resource) => resource.id === date)
-          let summaryDataToInput
-          if (!summaryData) {
-            summaryDataToInput = {
-              categoryTotals: {
-                pass: 0,
-                fail: 0,
-                passDkimOnly: 0,
-                passSpfOnly: 0,
-              },
-              detailTables: {
-                dkimFailure: [],
-                dmarcFailure: [],
-                fullPass: [],
-                spfFailure: [],
-              },
-            }
-          } else {
-            summaryDataToInput = {
-              categoryTotals: {
-                pass: summaryData.category_totals.pass || 0,
-                fail: summaryData.category_totals.fail || 0,
-                passDkimOnly: summaryData.category_totals['pass-dkim-only'] || 0,
-                passSpfOnly: summaryData.category_totals['pass-spf-only'] || 0,
-              },
-              detailTables: {
-                dkimFailure: summaryData.detail_tables.dkim_failure || [],
-                dmarcFailure: summaryData.detail_tables.dmarc_failure || [],
-                fullPass: summaryData.detail_tables.full_pass || [],
-                spfFailure: summaryData.detail_tables.spf_failure || [],
-              },
-            }
-          }
-
-          const { totalMessages, categoryPercentages } = calculatePercentages({ ...summaryDataToInput.categoryTotals })
-          summaryDataToInput.categoryPercentages = categoryPercentages
-          summaryDataToInput.totalMessages = totalMessages
-
-          if (arangoDates.indexOf(date) === -1) {
-            console.info(`\t\tCreating ${date} for ${domain}`)
-            await createSummary({
-              domain,
-              date: arangoDate,
-              summaryData: summaryDataToInput,
-            })
-          } else if (date === currentDate || UPDATE_ALL_DATES) {
-            console.info(`\t\tUpserting ${date} for ${domain}`)
-            await upsertSummary({
-              domain,
-              date: arangoDate,
-              summaryData: summaryDataToInput,
-            })
-          }
-
-          if (date === 'thirty_days') {
-            // update domain mail status
-            let sendsEmail = 'false'
-            if (
-              [
-                summaryDataToInput.categoryTotals.pass,
-                summaryDataToInput.categoryTotals.passDkimOnly,
-                summaryDataToInput.categoryTotals.passSpfOnly,
-              ].some((total) => total > 0)
-            ) {
-              sendsEmail = 'true'
-            }
-            console.info(`\t\tUpdating domain mail status for ${domain} to ${sendsEmail}`)
-            await updateDomainMailStatus({ domain, sendsEmail })
-          }
-        }
-      }
+      const promises = domains.map((domain) => updateDomainFunc(domain, orgAcronym, orgAcronymEn, queryResults))
+      await Promise.all(promises)
     }
   }
   // Update send status for all domains without ownership
