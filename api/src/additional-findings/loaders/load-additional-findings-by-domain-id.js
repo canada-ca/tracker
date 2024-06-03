c88039572258a3d00eef79b890c9b18ca78efc95
lcampbell <l.campbell144768@gmail.com>
2024-06-03 12:13:42 -0300

    add additional-finding dir with object and loader

---
+++ api/src/additional-findings/loaders/load-additional-findings-by-domain-id.js
@@ -0,0 +1,37 @@
+import { t } from '@lingui/macro'
+
+export const loadAdditionalFindingsByDomainId =
+  ({ query, userKey, i18n }) =>
+  async ({ domainId }) => {
+    if (domainId === undefined) {
+      console.warn(`User: ${userKey} did not set \`domainId\` argument for: loadAdditionalFindingsByDomainId.`)
+      throw new Error(i18n._(t`You must provide a \`domainId\` to retrieve a domain's EASM findings.`))
+    }
+
+    let cursor
+    try {
+      cursor = await query`
+        WITH additionalFindings, domains
+        FOR finding IN additionalFindings
+            FILTER finding.domain == ${domainId}
+            RETURN finding
+    `
+    } catch (err) {
+      console.error(
+        `Database error occurred while user: ${userKey} was trying to gather EASM findings for domain: ${domainId}. Error: ${err}`,
+      )
+      throw new Error(i18n._(t`Unable to load EASM findings. Please try again.`))
+    }
+
+    let finding
+    try {
+      finding = await cursor.next()
+    } catch (err) {
+      console.error(
+        `Cursor error occurred while user: ${userKey} was trying to gather EASM findings for domain: ${domainId}. Error: ${err}`,
+      )
+      throw new Error(i18n._(t`Unable to load EASM findings. Please try again.`))
+    }
+
+    return finding
+  }
