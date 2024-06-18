1abe5d3359629121222188cea2748f846fb45747
lcampbell <l.campbell144768@gmail.com>
2024-06-18 11:39:44 -0300

    remove unused vulnerable dependency from frontend

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -4,8 +4,6 @@ import { onError } from '@apollo/client/link/error'
 import { getMainDefinition, relayStylePagination } from '@apollo/client/utilities'
 import { setContext } from '@apollo/client/link/context'
 import { i18n } from '@lingui/core'
-import { WebSocketLink } from '@apollo/client/link/ws'
-import { SubscriptionClient } from 'subscriptions-transport-ws'
 import { REFRESH_TOKENS } from './graphql/mutations'
 import { RetryLink } from '@apollo/client/link/retry'
 
@@ -144,31 +142,12 @@ const errorLink = onError(({ graphQLErrors, operation, forward }) => {
 
 const httpLinkWithHeaders = headersLink.concat(httpLink)
 
-export const wsClient = new SubscriptionClient(
-  process.env.NODE_ENV === 'production' ? `wss://${window.location.host}/graphql` : 'ws://localhost:3000/graphql',
-  {
-    lazy: true,
-    reconnect: true,
-    connectionParams: () => {
-      return {
-        AcceptLanguage: i18n.locale,
-        authorization: currentUserVar().jwt,
-      }
-    },
-  },
-)
-
 const retryLink = new RetryLink()
-const wsLink = new WebSocketLink(wsClient)
 
-const splitLink = ApolloLink.from([retryLink, errorLink]).split(
-  ({ query }) => {
-    const definition = getMainDefinition(query)
-    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-  },
-  wsLink,
-  httpLinkWithHeaders,
-)
+const splitLink = ApolloLink.from([retryLink, errorLink]).split(({ query }) => {
+  const definition = getMainDefinition(query)
+  return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+}, httpLinkWithHeaders)
 
 export const client = new ApolloClient({
   link: splitLink,
