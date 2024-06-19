19b0f2b3c8db9b479df35ac52e3ecd95f60fb2cc
Luke Campbell <64781228+lcampbell2@users.noreply.github.com>
2024-06-19 08:29:11 -0300

    Security: remove ws subscription dependencies (#5416)
    * remove unused vulnerable dependency from api
    
    * remove unused vulnerable dependency from frontend
    
    * remove wsClient from App.js
    
    * remove split link from client

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,11 +1,9 @@
 import 'isomorphic-unfetch'
-import { ApolloClient, createHttpLink, InMemoryCache, makeVar, ApolloLink } from '@apollo/client'
+import { ApolloClient, createHttpLink, InMemoryCache, makeVar, from } from '@apollo/client'
 import { onError } from '@apollo/client/link/error'
-import { getMainDefinition, relayStylePagination } from '@apollo/client/utilities'
+import { relayStylePagination } from '@apollo/client/utilities'
 import { setContext } from '@apollo/client/link/context'
 import { i18n } from '@lingui/core'
-import { WebSocketLink } from '@apollo/client/link/ws'
-import { SubscriptionClient } from 'subscriptions-transport-ws'
 import { REFRESH_TOKENS } from './graphql/mutations'
 import { RetryLink } from '@apollo/client/link/retry'
 
@@ -110,9 +108,7 @@ const httpLink = createHttpLink({
 
 const headersLink = setContext(({ operationName }, { headers }) => {
   const language = i18n.locale
-
   const authorization = currentUserVar().jwt && operationName !== 'RefreshTokens' ? currentUserVar().jwt : ''
-
   return {
     headers: {
       ...headers,
@@ -122,10 +118,12 @@ const headersLink = setContext(({ operationName }, { headers }) => {
   }
 })
 
+const httpLinkWithHeaders = headersLink.concat(httpLink)
+
 const errorLink = onError(({ graphQLErrors, operation, forward }) => {
   if (graphQLErrors)
-    graphQLErrors.forEach(async ({ extensions }) => {
-      if (extensions.code === 'UNAUTHENTICATED') {
+    graphQLErrors.map(async ({ extensions: { code } }) => {
+      if (code === 'UNAUTHENTICATED') {
         // Modify the operation context with a new token
         const oldHeaders = operation.getContext().headers
         const newToken = await refreshTokens()
@@ -142,35 +140,10 @@ const errorLink = onError(({ graphQLErrors, operation, forward }) => {
     })
 })
 
-const httpLinkWithHeaders = headersLink.concat(httpLink)
-
-export const wsClient = new SubscriptionClient(
-  process.env.NODE_ENV === 'production' ? `wss://${window.location.host}/graphql` : 'ws://localhost:3000/graphql',
-  {
-    lazy: true,
-    reconnect: true,
-    connectionParams: () => {
-      return {
-        AcceptLanguage: i18n.locale,
-        authorization: currentUserVar().jwt,
-      }
-    },
-  },
-)
-
 const retryLink = new RetryLink()
-const wsLink = new WebSocketLink(wsClient)
-
-const splitLink = ApolloLink.from([retryLink, errorLink]).split(
-  ({ query }) => {
-    const definition = getMainDefinition(query)
-    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-  },
-  wsLink,
-  httpLinkWithHeaders,
-)
+const additiveLink = from([retryLink, errorLink, httpLinkWithHeaders])
 
 export const client = new ApolloClient({
-  link: splitLink,
+  link: additiveLink,
   cache,
 })
