9aaa4e41dc6850cea7ad6f8156132bbb8516c1fe
lcampbell <l.campbell144768@gmail.com>
2024-08-19 09:04:02 -0300

    add negative guidance tag counts to summaries

--- services/summaries/summaries.py
+++ services/summaries/summaries.py
@@ -208,6 +208,8 @@ def update_org_summaries(host=DB_URL, name=DB_NAME, user=DB_USER, password=DB_PA
         dmarc_phase_enforce = 0
         dmarc_phase_maintain = 0
 
+        negative_tags = {}
+
         claims = db.collection("claims").find({"_from": org["_id"]})
         for claim in claims:
             domain = db.collection("domains").get({"_id": claim["_to"]})
@@ -298,6 +300,15 @@ def update_org_summaries(host=DB_URL, name=DB_NAME, user=DB_USER, password=DB_PA
                 elif phase == "maintain":
                     dmarc_phase_maintain = dmarc_phase_maintain + 1
 
+                # Negative tags
+                domain_negative_tags = get_domain_negative_findings(db, domain["_id"])
+                # print(domain_negative_tags)
+                for tag in domain_negative_tags:
+                    if tag in negative_tags:
+                        negative_tags[tag] += 1
+                    else:
+                        negative_tags[tag] = 1
+
         dmarc_phase_total = (
             dmarc_phase_not_implemented
             + dmarc_phase_assess
@@ -305,6 +316,8 @@ def update_org_summaries(host=DB_URL, name=DB_NAME, user=DB_USER, password=DB_PA
             + dmarc_phase_enforce
             + dmarc_phase_maintain
         )
+
+        print(negative_tags)
 
         summary_data = {
             "date": date.today().isoformat(),
@@ -377,8 +390,31 @@ def update_org_summaries(host=DB_URL, name=DB_NAME, user=DB_USER, password=DB_PA
     logging.info(f"Organization summary value update completed.")
 
 
+def get_domain_negative_findings(db, domain_id):
+    cursor = db.aql.execute(
+        """
+                        LET emailTags = (
+                            FOR dnsScan, dnsE IN 1 OUTBOUND @domain_id domainsDNS
+                                SORT dnsScan.timestamp DESC
+                                LIMIT 1
+                                RETURN FLATTEN([dnsScan.dmarc.negativeTags, dnsScan.dkim.negativeTags, dnsScan.spf.negativeTags])
+                        )[0]
+                        LET webTags = (
+                            FOR web, webE IN 1 OUTBOUND @domain_id domainsWeb
+                                SORT web.timestamp DESC
+                                LIMIT 1
+                                FOR webScan, webScanE IN 1 OUTBOUND web webToWebScans
+                                    RETURN FLATTEN([webScan.results.tlsResult.negativeTags, webScan.results.connectionResults.negativeTags])
+                        )
+                        RETURN FLATTEN([emailTags, webTags], 5)
+                    """,
+        bind_vars={"domain_id": domain_id},
+    )
+    return cursor.next()
+
+
 if __name__ == "__main__":
     logging.info("Summary service started")
-    update_chart_summaries()
+    # update_chart_summaries()
     update_org_summaries()
     logging.info(f"Summary service shutting down...")
