d878be7eebc4c5f219b6d5b9e953e98f76aea929
lcampbell <l.campbell144768@gmail.com>
2024-09-18 16:55:00 -0300

    init query for top 25 report

---
+++ api/src/additional-findings/queries/get-top-25-report.js
@@ -0,0 +1,43 @@
+import { GraphQLString } from 'graphql'
+
+export const getTop25Reports = {
+  type: GraphQLString,
+  description: 'CSV formatted output of top 25 reports.',
+  resolve: async (
+    _,
+    args,
+    {
+      userKey,
+      auth: { checkSuperAdmin, userRequired, verifiedRequired, superAdminRequired },
+      loaders: { loadTop25Reports },
+    },
+  ) => {
+    const user = await userRequired()
+    verifiedRequired({ user })
+
+    const isSuperAdmin = await checkSuperAdmin()
+    superAdminRequired({ user, isSuperAdmin })
+
+    const top25Reports = await loadTop25Reports({ ...args })
+
+    console.info(`User ${userKey} successfully retrieved all top 25 reports.`)
+
+    if (top25Reports === undefined) return top25Reports
+
+    const headers = ['orgName', 'orgAcronym', 'assetCount']
+    let csvOutput = headers.join(',')
+    let totalAssetCount = 0
+    top25Reports.forEach((domainStatus) => {
+      const csvLine = headers
+        .map((header) => {
+          return `"${domainStatus[header]}"`
+        })
+        .join(',')
+      csvOutput += `\n${csvLine}`
+      totalAssetCount += domainStatus.assetCount
+    })
+    csvOutput += `\nGovernment of Canada,GC,${totalAssetCount}`
+
+    return csvOutput
+  },
+}
