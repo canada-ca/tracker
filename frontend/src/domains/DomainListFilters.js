2e6d4327fd33826e84f063b2f24dbe858d7de604
lcampbell <l.campbell144768@gmail.com>
2024-09-25 12:37:39 -0300

    fix linting

--- frontend/src/domains/DomainListFilters.js
+++ frontend/src/domains/DomainListFilters.js
@@ -4,7 +4,6 @@ import { Formik } from 'formik'
 import React from 'react'
 import { getRequirement, schemaToValidation } from '../utilities/fieldRequirements'
 import { array, func } from 'prop-types'
-import { ABTestVariant, ABTestWrapper } from '../app/ABTestWrapper'
 
 export function DomainListFilters({ filters, setFilters, statusOptions, filterTagOptions, assetStateOptions = [] }) {
   const validationSchema = schemaToValidation({
