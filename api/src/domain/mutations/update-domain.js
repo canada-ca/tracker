12a2ac6ec613e4b323f463261df5cd2b7aa7c505
Luke Campbell <64781228+lcampbell2@users.noreply.github.com>
2024-10-08 11:54:44 -0300

    Feature: Update easm asset state (#5788)
    * send tracker asset state to add-domain-to-easm service
    
    * add func to update asset state in EASM when modified in tracker
    
    * update asset state when asset found in EASM
    
    * conditionally publish updated domain to service
    
    * add publish to update-domain.test

--- api/src/domain/mutations/update-domain.js
+++ api/src/domain/mutations/update-domain.js
@@ -57,6 +57,7 @@ export const updateDomain = new mutationWithClientMutationId({
       collections,
       transaction,
       userKey,
+      publish,
       auth: { checkPermission, userRequired, verifiedRequired, tfaRequired },
       validators: { cleanseInput },
       loaders: { loadDomainByKey, loadOrgByKey },
@@ -255,7 +256,7 @@ export const updateDomain = new mutationWithClientMutationId({
     console.info(`User: ${userKey} successfully updated domain: ${domainId}.`)
 
     const updatedProperties = []
-    if (typeof assetState !== 'undefined') {
+    if (typeof assetState !== 'undefined' && assetState !== claim.assetState) {
       updatedProperties.push({
         name: 'assetState',
         oldValue: claim.assetState,
@@ -319,6 +320,24 @@ export const updateDomain = new mutationWithClientMutationId({
           updatedProperties: [{ name: 'archived', oldValue: domain.archived, newValue: archived }],
         },
       })
+    }
+
+    if (typeof assetState !== 'undefined' && assetState !== claim.assetState) {
+      try {
+        await publish({
+          channel: 'scans.add_domain_to_easm',
+          msg: {
+            domain: returnDomain.domain,
+            assetState,
+            domain_key: returnDomain._key,
+            hash: returnDomain.hash,
+            user_key: null, // only used for One Time Scans
+            shared_id: null, // only used for One Time Scans
+          },
+        })
+      } catch (err) {
+        console.error(`Error publishing to NATS for domain ${returnDomain._key}: ${err}`)
+      }
     }
 
     returnDomain.id = returnDomain._key
