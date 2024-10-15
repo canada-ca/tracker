94c7368454114a834ce4be20ad28cd2030c090d5
Luke Campbell <64781228+lcampbell2@users.noreply.github.com>
2024-10-15 13:26:41 -0300

    Display landing page tour (#5789)
    * uncomment tour components on landing page
    
    * hide step progress in tours
    
    * add translations
    
    * add TourProvider to App.test.js
    
    * move 'Start Tour' btn to unauth'd top banner
    
    * translations
    
    * add i18nProvider to tests

--- frontend/src/app/TopBanner.js
+++ frontend/src/app/TopBanner.js
@@ -18,7 +18,7 @@ import { PhaseBanner } from './PhaseBanner'
 import { useLingui } from '@lingui/react'
 import { ABTestWrapper, ABTestVariant } from './ABTestWrapper'
 import { bool } from 'prop-types'
-// import { TourButton } from '../userOnboarding/components/TourButton'
+import { TourButton } from '../userOnboarding/components/TourButton'
 
 export const TopBanner = ({ initialLoading, ...props }) => {
   const { isLoggedIn, logout } = useUserVar()
@@ -96,10 +96,8 @@ export const TopBanner = ({ initialLoading, ...props }) => {
         </PhaseBanner>
 
         <Flex align="center" ml="auto">
-          <Box py="4" mx="2" ml={{ base: 'auto', md: '0' }}>
-            <LocaleSwitcher />
-          </Box>
-
+          <TourButton />
+          <LocaleSwitcher py="4" mx="2" ml={{ base: 'auto', md: '0' }} />
           {initialLoading ? (
             <>
               <Skeleton display={{ base: 'none', md: 'inline' }} mr="2">
@@ -125,7 +123,6 @@ export const TopBanner = ({ initialLoading, ...props }) => {
               >
                 <Trans>Sign Out</Trans>
               </Button>
-              {/* <TourButton /> */}
             </>
           ) : (
             <>
