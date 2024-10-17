895c93bc6720f5f26c93e201ef9d82309763c6bb
lcampbell <l.campbell144768@gmail.com>
2024-10-17 16:02:43 -0300

    fix tests

--- api/src/guidance-tag/loaders/__tests__/load-guidance-tags-connections.test.js
+++ api/src/guidance-tag/loaders/__tests__/load-guidance-tags-connections.test.js
@@ -1,5 +1,4 @@
 const { loadGuidanceTagSummaryConnectionsByTagId } = require('../load-guidance-tags-connections')
-const { toGlobalId } = require('graphql-relay')
 
 describe('loadGuidanceTagSummaryConnectionsByTagId', () => {
   let query, userKey, cleanseInput, i18n, language
@@ -16,9 +15,6 @@ describe('loadGuidanceTagSummaryConnectionsByTagId', () => {
 
   it('successfully retrieves data', async () => {
     const guidanceTags = { tag1: 3, tag2: 5 }
-    const after = toGlobalId('guidanceTag', 'tag1')
-    const before = toGlobalId('guidanceTag', 'tag2')
-    const first = 1
     const orderBy = { field: 'tag-id', direction: 'ASC' }
 
     query.mockResolvedValueOnce({
@@ -28,10 +24,6 @@ describe('loadGuidanceTagSummaryConnectionsByTagId', () => {
           { _key: '2', tagName: 'Tag 2' },
         ],
         totalCount: 2,
-        hasNextPage: false,
-        hasPreviousPage: false,
-        startKey: '1',
-        endKey: '2',
       }),
     })
 
@@ -43,55 +35,14 @@ describe('loadGuidanceTagSummaryConnectionsByTagId', () => {
       language,
     })
 
-    const result = await loader({
-      guidanceTags,
-      after,
-      before,
-      first,
-      orderBy,
-    })
-
+    const result = await loader({ guidanceTags, orderBy })
     expect(result).toEqual({
-      edges: [
-        { cursor: toGlobalId('guidanceTag', '1'), node: { _key: '1', tagName: 'Tag 1' } },
-        { cursor: toGlobalId('guidanceTag', '2'), node: { _key: '2', tagName: 'Tag 2' } },
+      guidanceTags: [
+        { _key: '1', tagName: 'Tag 1' },
+        { _key: '2', tagName: 'Tag 2' },
       ],
       totalCount: 2,
-      pageInfo: {
-        hasNextPage: false,
-        hasPreviousPage: false,
-        startCursor: toGlobalId('guidanceTag', '1'),
-        endCursor: toGlobalId('guidanceTag', '2'),
-      },
     })
-  })
-
-  it('throws an error if neither first nor last is provided', async () => {
-    const loader = loadGuidanceTagSummaryConnectionsByTagId({
-      query,
-      userKey,
-      cleanseInput,
-      i18n,
-      language,
-    })
-
-    await expect(loader({ guidanceTags: {} })).rejects.toThrow(
-      'You must provide a `first` or `last` value to properly paginate the `GuidanceTag` connection.',
-    )
-  })
-
-  it('throws an error if both first and last are provided', async () => {
-    const loader = loadGuidanceTagSummaryConnectionsByTagId({
-      query,
-      userKey,
-      cleanseInput,
-      i18n,
-      language,
-    })
-
-    await expect(loader({ guidanceTags: {}, first: 1, last: 1 })).rejects.toThrow(
-      'Passing both `first` and `last` to paginate the `GuidanceTag` connection is not supported.',
-    )
   })
 
   it('throws an error if there is a database error', async () => {
@@ -105,9 +56,7 @@ describe('loadGuidanceTagSummaryConnectionsByTagId', () => {
       language,
     })
 
-    await expect(loader({ guidanceTags: {}, first: 1 })).rejects.toThrow(
-      'Unable to load guidance tag(s). Please try again.',
-    )
+    await expect(loader({ guidanceTags: {} })).rejects.toThrow('Unable to load guidance tag(s). Please try again.')
   })
 
   it('throws an error if there is a cursor error', async () => {
@@ -123,8 +72,6 @@ describe('loadGuidanceTagSummaryConnectionsByTagId', () => {
       language,
     })
 
-    await expect(loader({ guidanceTags: {}, first: 1 })).rejects.toThrow(
-      'Unable to load guidance tag(s). Please try again.',
-    )
+    await expect(loader({ guidanceTags: {} })).rejects.toThrow('Unable to load guidance tag(s). Please try again.')
   })
 })
