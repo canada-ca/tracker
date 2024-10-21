a74ce3ddc9a8b3749e2b0d6aba257793762a9580
Luke Campbell <64781228+lcampbell2@users.noreply.github.com>
2024-10-21 14:11:05 -0300

    Feature: Add negative findings summary to org summary obj (#5809)
    * init loader
    
    * add total count to guidance tag objs
    
    * add 'negativeFindings' to org summary obj
    
    * add tests for loader
    
    * translations
    
    * return full list of guidance tags, remove edge-based pagination
    
    * fix tests
    
    * fix tests
    
    * fix permissions error message
    
    * fix potential invalid aql when order-by options not given

--- api/src/guidance-tag/objects/guidance-tag-connection.js
+++ api/src/guidance-tag/objects/guidance-tag-connection.js
@@ -1,16 +1,18 @@
-import {GraphQLInt} from 'graphql'
-import {connectionDefinitions} from 'graphql-relay'
+import { GraphQLInt, GraphQLObjectType, GraphQLList } from 'graphql'
+import { guidanceTagType } from './guidance-tag'
 
-import {guidanceTagType} from './guidance-tag'
-
-export const guidanceTagConnection = connectionDefinitions({
-  name: 'GuidanceTag',
-  nodeType: guidanceTagType,
-  connectionFields: () => ({
+export const guidanceTagConnection = new GraphQLObjectType({
+  name: 'GuidanceTagConnection',
+  fields: () => ({
+    guidanceTags: {
+      type: new GraphQLList(guidanceTagType),
+      description: '',
+      resolve: ({ guidanceTags }) => guidanceTags,
+    },
     totalCount: {
       type: GraphQLInt,
       description: 'The total amount of guidance tags for a given scan type.',
-      resolve: ({totalCount}) => totalCount,
+      resolve: ({ totalCount }) => totalCount,
     },
   }),
 })
