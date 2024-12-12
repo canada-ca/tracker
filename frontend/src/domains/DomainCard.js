0056c14adcf75e7f4d65a0a222f26ca78f6fa6e5
Luke Campbell <64781228+lcampbell2@users.noreply.github.com>
2024-12-12 12:24:24 -0500

    Feature: Add wildcard entry to frontend (#5985)
    * add wildcardEntry to gql queries
    
    * add wildcardEntry as filter option and conditional logic for DomainCard
    
    * add wildcardEntry as conditional logic for GuidancePage
    
    * conditionally show wildcard entry/sibling badge
    
    * translations

--- frontend/src/domains/DomainCard.js
+++ frontend/src/domains/DomainCard.js
@@ -23,6 +23,7 @@ function DomainCard({
   rcode,
   blocked,
   wildcardSibling,
+  wildcardEntry,
   webScanPending,
   hasEntrustCertificate,
   userHasPermission,
@@ -146,8 +147,8 @@ function DomainCard({
             <ABTestWrapper insiderVariantName="B">
               <ABTestVariant name="B">
                 {wildcardSibling && (
-                  <Badge ml="2" colorScheme="blue" variant="subtle" alignSelf="center">
-                    <Trans>Wildcard</Trans>*
+                  <Badge ml="2" colorScheme={wildcardEntry ? 'red' : 'blue'} variant="subtle" alignSelf="center">
+                    {wildcardEntry ? <Trans>Wildcard Entry</Trans> : <Trans>Wildcard Sibling</Trans>}
                   </Badge>
                 )}
                 {hasEntrustCertificate && (
@@ -288,6 +289,7 @@ DomainCard.propTypes = {
   isArchived: bool,
   blocked: bool,
   wildcardSibling: bool,
+  wildcardEntry: bool,
   webScanPending: bool,
   hasEntrustCertificate: bool,
   userHasPermission: bool,
