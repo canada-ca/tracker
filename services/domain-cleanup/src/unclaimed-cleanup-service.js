ea3677d348867ff5ca4a714cd91cc34feb01b758
Luke Campbell <64781228+lcampbell2@users.noreply.github.com>
2025-01-27 13:12:07 -0500

    Improve logging in domain cleanup service (#6076)
    * add start and end logs to main function calls
    
    * add better logging to unclaimed domain cleanup service

--- services/domain-cleanup/src/unclaimed-cleanup-service.js
+++ services/domain-cleanup/src/unclaimed-cleanup-service.js
@@ -3,10 +3,11 @@ const { findDomainClaims, findUnclaimedDomains } = require('./database')
 
 const unclaimedCleanupService = async ({ query, log }) => {
   const unclaimedDomains = await findUnclaimedDomains({ query, orgId: UNCLAIMED_ORG_ID })
-  unclaimedDomains.forEach(async ({ domain, _id }) => {
-    const claims = await findDomainClaims({ query, domainId: _id })
+  log(`Found ${unclaimedDomains.length} unclaimed domains`)
+  unclaimedDomains.forEach(async ({ domain, _id: domainId }) => {
+    const claims = await findDomainClaims({ query, domainId })
     if (claims.length > 1) {
-      log(`${domain} has been claimed. Removing from unclaimed org.`)
+      log(`Attempting to remove ${domain} from the 'Unclaimed' org...`)
       try {
         await (
           await query`
@@ -15,6 +16,7 @@ const unclaimedCleanupService = async ({ query, log }) => {
                   REMOVE e IN claims
               `
         ).all()
+        log(`Successfully removed ${domain} from the 'Unclaimed' org.`)
       } catch (err) {
         console.error(`Error while removing claims for domain: ${domain._key}, error: ${err})`)
       }
