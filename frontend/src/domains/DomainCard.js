e7376425f9e82697caed8d2b7028e1c94c7edbc5
lcampbell <l.campbell144768@gmail.com>
2025-03-05 12:28:18 -0400

    fix passing state to DomainCard links

--- frontend/src/domains/DomainCard.js
+++ frontend/src/domains/DomainCard.js
@@ -218,10 +218,8 @@ function DomainCard({
               className="view-results-button"
               variant="primary"
               as={RouteLink}
-              to={{
-                pathname: isLoggedIn() ? `/domains/${url}` : '/sign-in',
-                state: { from: location.pathname },
-              }}
+              to={isLoggedIn() ? `/domains/${url}` : '/sign-in'}
+              state={{ from: location.pathname }}
               px="10"
             >
               <Text whiteSpace="noWrap">
@@ -233,6 +231,7 @@ function DomainCard({
                 variant="primary"
                 as={RouteLink}
                 to={`/domains/${url}/dmarc-report/LAST30DAYS/${new Date().getFullYear()}`}
+                state={{ from: location.pathname }}
               >
                 <Text whiteSpace="noWrap">
                   <Trans>DMARC Report</Trans>
