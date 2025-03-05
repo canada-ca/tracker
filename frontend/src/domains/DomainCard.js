5861b739e25e5f762d707e669f275abbf4163ac6
lcampbell <l.campbell144768@gmail.com>
2025-03-05 17:34:14 -0400

    keep search params for guidance page back button

--- frontend/src/domains/DomainCard.js
+++ frontend/src/domains/DomainCard.js
@@ -32,6 +32,7 @@ function DomainCard({
   const location = useLocation()
   const toast = useToast()
   const { isLoggedIn, isEmailValidated } = useUserVar()
+  const searchParams = new URLSearchParams(window.location.search)
 
   const [favouriteDomain] = useMutation(FAVOURITE_DOMAIN, {
     onError: ({ message }) => {
@@ -219,7 +220,7 @@ function DomainCard({
               variant="primary"
               as={RouteLink}
               to={isLoggedIn() ? `/domains/${url}` : '/sign-in'}
-              state={{ from: location.pathname }}
+              state={{ from: location.pathname, searchParams: `?${searchParams.toString()}` }}
               px="10"
             >
               <Text whiteSpace="noWrap">
