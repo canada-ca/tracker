99e48dfe202fffaeb5755e2170e2e473353ab1e7
lcampbell <l.campbell144768@gmail.com>
2025-03-07 14:45:41 -0400

    remove unneeded useMemo and useCallback

--- frontend/src/utilities/useSearchParam.js
+++ frontend/src/utilities/useSearchParam.js
@@ -1,35 +1,35 @@
-import { useEffect, useMemo, useCallback } from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
 function useSearchParam({ name, validOptions, defaultValue }) {
   const { search } = useLocation()
   const navigate = useNavigate()
 
-  const searchParams = useMemo(() => new URLSearchParams(search), [search])
+  const searchParams = React.useMemo(() => {
+    return new URLSearchParams(search)
+  }, [search])
 
-  const parseVal = useCallback((value) => {
+  const parseVal = (value) => {
     try {
       return JSON.parse(value)
     } catch {
       return value
     }
-  }, [])
+  }
 
   const value = searchParams.get(name) || defaultValue
-  const searchValue = useMemo(() => {
-    return !validOptions || validOptions.includes(value) ? parseVal(value) : defaultValue
-  }, [value, validOptions, parseVal, defaultValue])
+  const searchValue = !validOptions || validOptions.includes(value) ? parseVal(value) : defaultValue
 
-  const setSearchParams = useCallback(
+  const setSearchParams = React.useCallback(
     (value) => {
       if (Array.isArray(value)) {
-        if (!value || value.length === 0) {
+        if (value == null || value.length === 0) {
           searchParams.delete(name)
         } else {
           searchParams.set(name, JSON.stringify(value))
         }
       } else {
-        if (!value || (validOptions && !validOptions.includes(value))) {
+        if (value == null || value === '' || (validOptions && !validOptions.includes(value))) {
           searchParams.delete(name)
         } else {
           searchParams.set(name, value)
@@ -41,14 +41,16 @@ function useSearchParam({ name, validOptions, defaultValue }) {
   )
 
   useEffect(() => {
-    if (!validOptions || validOptions.includes(value)) {
-      if (value === null || value === '') {
+    if ((validOptions && !validOptions.includes(value)) || value === '') {
+      if (value != null) {
+        setSearchParams(null)
+      } else {
         setSearchParams(defaultValue)
       }
-    } else {
+    } else if (value === null || value.length === 0) {
       setSearchParams(defaultValue)
     }
-  }, [value, validOptions, defaultValue, setSearchParams])
+  }, [value, validOptions, defaultValue, setSearchParams, name])
 
   return { searchValue, setSearchParams }
 }
