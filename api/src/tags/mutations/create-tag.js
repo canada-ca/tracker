8592cd2c8f622ef571f8400f525b94d232db14fe
lcampbell <l.campbell144768@gmail.com>
2025-04-01 10:50:20 -0300

    add ownership input val to create-tag with new permissions for org owned tags

--- api/src/tags/mutations/create-tag.js
+++ api/src/tags/mutations/create-tag.js
@@ -1,7 +1,8 @@
-import { GraphQLNonNull, GraphQLBoolean, GraphQLString } from 'graphql'
-import { mutationWithClientMutationId } from 'graphql-relay'
+import { GraphQLNonNull, GraphQLID, GraphQLBoolean, GraphQLString } from 'graphql'
+import { fromGlobalId, mutationWithClientMutationId } from 'graphql-relay'
 import { t } from '@lingui/macro'
 import { createTagUnion } from '../unions'
+import { TagOwnershipEnums } from '../../enums'
 
 export const createTag = new mutationWithClientMutationId({
   name: 'CreateTag',
@@ -31,6 +32,14 @@ export const createTag = new mutationWithClientMutationId({
       description: 'Value used to decide if users should see the tag.',
       type: GraphQLBoolean,
     },
+    ownership: {
+      description: '',
+      type: new GraphQLNonNull(TagOwnershipEnums),
+    },
+    orgId: {
+      description: '',
+      type: GraphQLID,
+    },
   }),
   outputFields: () => ({
     result: {
@@ -47,8 +56,8 @@ export const createTag = new mutationWithClientMutationId({
       collections,
       transaction,
       userKey,
-      auth: { userRequired, checkSuperAdmin, superAdminRequired, verifiedRequired },
-      loaders: { loadTagByTagId },
+      auth: { userRequired, verifiedRequired, checkPermission, checkSuperAdmin, superAdminRequired },
+      loaders: { loadTagByTagId, loadOrgByKey },
       validators: { cleanseInput },
     },
   ) => {
@@ -56,15 +65,14 @@ export const createTag = new mutationWithClientMutationId({
     const user = await userRequired()
     verifiedRequired({ user })
 
-    const isSuperAdmin = await checkSuperAdmin()
-    superAdminRequired({ user, isSuperAdmin })
-
     // Cleanse input
     const tagId = cleanseInput(args.tagId)
     const labelEn = cleanseInput(args.labelEn)
     const labelFr = cleanseInput(args.labelFr)
     const descriptionEn = cleanseInput(args.descriptionEn)
     const descriptionFr = cleanseInput(args.descriptionFr)
+    const ownership = cleanseInput(args.ownership)
+    const { type: _orgType, id: orgId } = fromGlobalId(cleanseInput(args.orgId))
 
     const insertTag = {
       tagId: tagId.toLowerCase(),
@@ -74,6 +82,7 @@ export const createTag = new mutationWithClientMutationId({
         fr: descriptionFr || '',
       },
       visible: args?.isVisible || false,
+      ownership,
     }
 
     const tag = await loadTagByTagId.load(insertTag.tagId)
@@ -115,8 +124,74 @@ export const createTag = new mutationWithClientMutationId({
       }
     }
 
+    if (ownership === 'global') {
+      const isSuperAdmin = await checkSuperAdmin()
+      superAdminRequired({ user, isSuperAdmin })
+    }
+
     // Setup Transaction
     const trx = await transaction(collections)
+
+    if (ownership === 'org') {
+      if (typeof orgId === 'undefined') {
+        console.warn(
+          `User: ${userKey} attempted to create a tag: ${insertTag.tagId}, however organization-owned tags must have a valid organization.`,
+        )
+        return {
+          _type: 'error',
+          code: 400,
+          description: i18n._(t`Unable to create tag, tagId already in use.`),
+        }
+      } else {
+        // Check to see if org exists
+        const org = await loadOrgByKey.load(orgId)
+        if (typeof org === 'undefined') {
+          console.warn(`User: ${userKey} attempted to create a tag to an organization: ${orgId} that does not exist.`)
+          return {
+            _type: 'error',
+            code: 400,
+            description: i18n._(t`Unable to create tag in unknown organization.`),
+          }
+        }
+
+        const permission = await checkPermission({ orgId })
+        if (permission !== 'super_admin') {
+          insertTag.ownership = 'pending'
+          console.warn(
+            `User: ${userKey} attempted to create a tag in: ${org.slug}, however they do not have permission to do so.`,
+          )
+        } else if (!['admin', 'owner'].includes(permission)) {
+          console.warn(
+            `User: ${userKey} attempted to create a tag in: ${org.slug}, however they do not have permission to do so.`,
+          )
+          return {
+            _type: 'error',
+            code: 400,
+            description: i18n._(t`Permission Denied: Please contact organization admin for help with creating tag.`),
+          }
+        }
+        // add tagId to org tags
+        const updatedOrgDetails = {
+          tags: [...org?.tags, tagId],
+        }
+        try {
+          await trx.step(
+            async () =>
+              await query`
+            WITH organizations
+            UPSERT { _key: ${org._key} }
+              INSERT ${updatedOrgDetails}
+              UPDATE ${updatedOrgDetails}
+              IN organizations
+          `,
+          )
+        } catch (err) {
+          console.error(`Transaction error occurred while upserting org: ${org}, err: ${err}`)
+          await trx.abort()
+          throw new Error(i18n._(t`Unable to update organization. Please try again.`))
+        }
+      }
+    }
 
     try {
       await trx.step(
