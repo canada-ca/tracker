2f0bacb6e14ccb7850c77c4a6034aba726a3cd9c
lcampbell <l.campbell144768@gmail.com>
2025-04-09 15:32:53 -0300

    add permission checks for super admins

--- api/src/tags/objects/tag.js
+++ api/src/tags/objects/tag.js
@@ -1,5 +1,6 @@
-import { GraphQLBoolean, GraphQLObjectType, GraphQLString } from 'graphql'
+import { GraphQLBoolean, GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql'
 import { TagOwnershipEnums } from '../../enums/tag-ownership'
+import { organizationType } from '../../organization/objects'
 
 export const tagType = new GraphQLObjectType({
   name: 'Tag',
@@ -28,6 +29,38 @@ export const tagType = new GraphQLObjectType({
       type: TagOwnershipEnums,
       description: '',
       resolve: ({ ownership }) => ownership,
+    },
+    organizations: {
+      type: new GraphQLList(organizationType),
+      description: '',
+      resolve: async (
+        { tagId, organizations, ownership },
+        _,
+        {
+          userKey,
+          auth: { userRequired, verifiedRequired, checkSuperAdmin, superAdminRequired },
+          loaders: { loadOrgByKey },
+        },
+      ) => {
+        const user = await userRequired()
+        verifiedRequired({ user })
+
+        const isSuperAdmin = await checkSuperAdmin()
+        superAdminRequired({ user, isSuperAdmin })
+
+        if (ownership === 'global') return []
+
+        const orgs = []
+        for (const orgId of organizations) {
+          const org = await loadOrgByKey.load(orgId)
+          if (!org) continue
+          orgs.push(org)
+        }
+
+        console.info(`User: ${userKey} successfully retrieved affiliated orgs for tag: ${tagId}.`)
+
+        return orgs
+      },
     },
   }),
 })
