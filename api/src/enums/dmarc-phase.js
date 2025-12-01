a313526eeb5b2d189a940894c2d0389843fab5c5
lcampbell2 <l.campbell144768@gmail.com>
2025-12-01 15:05:54 -0400

    add descriptions to dmarc phase enums

--- api/src/enums/dmarc-phase.js
+++ api/src/enums/dmarc-phase.js
@@ -2,23 +2,23 @@ import { GraphQLEnumType } from 'graphql'
 
 export const DmarcPhaseEnum = new GraphQLEnumType({
   name: 'DmarcPhaseEnum',
-  description: '',
+  description: 'Phases of DMARC implementation.',
   values: {
     ASSESS: {
       value: 'assess',
-      description: '',
+      description: 'Assess domains and DMARC status.',
     },
     DEPLOY: {
       value: 'deploy',
-      description: '',
+      description: 'Deploy SPF and DKIM records.',
     },
     ENFORCE: {
       value: 'enforce',
-      description: '',
+      description: 'Enforce DMARC policies.',
     },
     MAINTAIN: {
       value: 'maintain',
-      description: '',
+      description: 'Maintain DMARC and update records.',
     },
   },
 })
