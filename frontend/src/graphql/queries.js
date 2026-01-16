097b3669b480a50abfe2b1b08ddf75977461dcbb
lcampbell2 <l.campbell144768@gmail.com>
2026-01-16 10:37:45 -0400

    remove diff viewer from guidance page

--- frontend/src/graphql/queries.js
+++ frontend/src/graphql/queries.js
@@ -343,23 +343,6 @@ export const DOMAIN_GUIDANCE_PAGE = gql`
       dmarcPhase
       hasDMARCReport
       userHasPermission
-      mxRecordDiff(limit: 10, orderBy: { field: TIMESTAMP, direction: DESC }) {
-        totalCount
-        edges {
-          node {
-            id
-            timestamp
-            mxRecords {
-              hosts {
-                preference
-                hostname
-                addresses
-              }
-              warnings
-            }
-          }
-        }
-      }
       dnsScan(limit: 1, orderBy: { field: TIMESTAMP, direction: DESC }) {
         edges {
           cursor
