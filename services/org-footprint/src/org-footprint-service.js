95715e9104d3671313cef1c82a65537e72304625
lcampbell2 <l.campbell144768@gmail.com>
2026-03-11 11:22:25 -0300

    add check for pending CVD enrollment and notify owners

--- services/org-footprint/src/org-footprint-service.js
+++ services/org-footprint/src/org-footprint-service.js
@@ -5,8 +5,10 @@ const {
   getBilingualOrgNames,
   getPendingOrgUserCount,
   getOrgUsers,
+  getPendingCvdEnrollmentCount,
+  getOrgOwner,
 } = require('./database')
-const { sendOrgFootprintEmail, sendPendingOrgUsersEmail } = require('./notify')
+const { sendOrgFootprintEmail, sendPendingOrgUsersEmail, sendPendingCvdEnrollmentEmail } = require('./notify')
 const logger = require('logger')
 
 const { SERVICE_ACCOUNT_EMAIL, REDIRECT_TO_SERVICE_ACCOUNT_EMAIL } = process.env
@@ -14,6 +16,7 @@ const { SERVICE_ACCOUNT_EMAIL, REDIRECT_TO_SERVICE_ACCOUNT_EMAIL } = process.env
 const orgFootprintService = async ({ query, notifyClient }) => {
   const orgKeys = await getAllOrgKeys({ query })
   for (const orgKey of orgKeys) {
+    const orgNames = await getBilingualOrgNames({ query, orgKey })
     const auditLogs = await getNewAuditLogs({ query, orgKey })
     if (auditLogs.length > 0) {
       const orgUsers = await getOrgUsers({ query, orgKey })
@@ -25,7 +28,6 @@ const orgFootprintService = async ({ query, notifyClient }) => {
         })
       }
 
-      const orgNames = await getBilingualOrgNames({ query, orgKey })
       logger.info({ orgKey, auditLogCount: auditLogs.length }, 'Sending recent activity email to admins of org')
       for (const user of orgUsers) {
         await sendOrgFootprintEmail({ notifyClient, user, auditLogs, orgNames })
@@ -34,12 +36,18 @@ const orgFootprintService = async ({ query, notifyClient }) => {
 
     const pendingUserCount = await getPendingOrgUserCount({ query, orgKey })
     if (pendingUserCount > 0) {
-      const orgNames = await getBilingualOrgNames({ query, orgKey })
       const orgAdmins = await getOrgAdmins({ query, orgKey })
       logger.info({ orgKey, pendingUserCount }, 'Sending pending users email to admins of org')
       for (const admin of orgAdmins) {
         await sendPendingOrgUsersEmail({ notifyClient, user: admin, orgNames })
       }
+    }
+
+    const pendingEnrollmentCount = await getPendingCvdEnrollmentCount({ query, orgKey })
+    if (pendingEnrollmentCount > 0) {
+      const orgOwner = await getOrgOwner({ query, orgKey })
+      logger.info({ orgKey, pendingEnrollmentCount }, 'Sending pending CVD enrollments email to owner of org')
+      await sendPendingCvdEnrollmentEmail({ notifyClient, user: orgOwner, orgNames })
     }
   }
 }
