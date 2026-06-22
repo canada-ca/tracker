629aa257dc993a28dc5f625f18887eb3e06093b6
lcampbell2 <l.campbell144768@gmail.com>
2026-06-22 17:32:29 -0300

    refactor(user-queries): use context.dataSources.user

--- api/src/user/queries/is-user-super-admin.js
+++ api/src/user/queries/is-user-super-admin.js
@@ -1,34 +1,10 @@
-import {GraphQLBoolean} from 'graphql'
-import {t} from '@lingui/macro'
+import { GraphQLBoolean } from 'graphql'
 
 export const isUserSuperAdmin = {
   type: GraphQLBoolean,
   description: 'Query used to check if the user has a super admin role.',
-  resolve: async (_, __, {i18n, query, userKey, auth: {userRequired}}) => {
+  resolve: async (_, __, { auth: { userRequired }, dataSources: { user: userDataSource } }) => {
     const user = await userRequired()
-
-    let userAdmin
-    try {
-      userAdmin = await query`
-      WITH users, affiliations
-      FOR v, e IN 1..1 INBOUND ${user._id} affiliations
-      FILTER e.permission == "super_admin"
-      LIMIT 1
-      RETURN e.permission
-      `
-    } catch (err) {
-      console.error(
-        `Database error occurred when user: ${userKey} was seeing if they were a super admin, err: ${err}`,
-      )
-      throw new Error(
-        i18n._(t`Unable to verify if user is a super admin, please try again.`),
-      )
-    }
-
-    if (userAdmin.count > 0) {
-      return true
-    }
-
-    return false
+    return userDataSource.isSuperAdmin({ userId: user._id })
   },
 }
