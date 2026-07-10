2a3a92424eaf43fca67d38c084c8875b7d17dbd1
lcampbell2 <l.campbell144768@gmail.com>
2026-07-10 10:08:51 -0300

    match cname fingerprints and emit finding

--- scanners/subdomain-takeover/internal/detect/rules.go
+++ scanners/subdomain-takeover/internal/detect/rules.go
@@ -2,13 +2,15 @@ package detect
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"strings"
 )
 
 type CNAMEHit struct {
 	Matched    bool
 	Provider   string
 	ReasonCode string
-	RuleID     string
 	NeedsNX    bool
 }
 
@@ -16,7 +18,6 @@ type NSHit struct {
 	Matched            bool
 	Provider           string
 	ReasonCode         string
-	RuleID             string
 	ProviderVulnerable bool
 	RegistrarMismatch  bool
 }
@@ -24,26 +25,67 @@ type NSHit struct {
 func MatchCNAMEFingerprints(evidence CNAMEEvidence, fingerprints []CNAMEProviderFingerprint) *CNAMEHit {
 	for _, fp := range fingerprints {
 		if fp.ContainsTarget(evidence.Target) {
-			if fp.Nxdomain {
-				fmt.Println("check for nxdomain on A record")
+			hit := CNAMEHit{
+				Matched:    false,
+				Provider:   fp.Name,
+				ReasonCode: "",
+				NeedsNX:    fp.Nxdomain,
+			}
+			if hit.NeedsNX {
+				hit.ReasonCode = "CNAME_TARGET_MATCH_MISSING_NXDOMAIN"
 				if evidence.QueryAnswer.A == "NXDOMAIN" {
-					fmt.Println("Bingo")
+					hit.Matched = true
+					hit.ReasonCode = "CNAME_DANGLING_NXDOMAIN"
 				}
-			} // else {
-			// 	fmt.Println("check for other fingerprint")
-			// }
+			} else {
+				hit.ReasonCode = "CNAME_TARGET_MATCH_MISSING_BODY_FINGERPRINT"
+				if urlBodyContainsFingerprint(evidence.Domain, fp.Fingerprint) {
+					hit.Matched = true
+					hit.ReasonCode = "CNAME_PROVIDER_FINGERPRINT_BODY_MATCH"
+				}
+			}
+			return &hit
 		}
 	}
+	return nil
 }
 
 func MatchNSProviderRules(evidence NSEvidence, fingerprints []NSProviderFingerprint) *NSHit {
-	return
+	return &NSHit{}
 }
 
-func ShouldEmitCNAME(evidence CNAMEEvidence, hit *CNAMEHit) bool {
+func ShouldEmitCNAME(hit *CNAMEHit) bool {
+	if hit == nil {
+		return false
+	}
+	if hit.Matched {
+		return true
+	}
 	return false
 }
 
 func ShouldEmitNSHijack(evidence NSEvidence, hit *NSHit) bool {
+	return false
+}
+
+func urlBodyContainsFingerprint(domain string, fp string) bool {
+	// curl domain and look for matching fp in body
+	url := fmt.Sprintf("http://%s", domain)
+	res, err := http.Get(url)
+	if err != nil {
+		// log error
+		return false
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		// log err
+		return false
+	}
+
+	if strings.Contains(string(body), fp) {
+		return true
+	}
 	return false
 }
