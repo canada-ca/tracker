efbf4a0c2535a98792897579ae203be042ded456
lcampbell2 <l.campbell144768@gmail.com>
2026-07-10 16:16:14 -0300

    move logger out of config.go

--- scanners/subdomain-takeover/internal/config/config.go
+++ scanners/subdomain-takeover/internal/config/config.go
@@ -1,37 +1,25 @@
 package config
 
 import (
-	"os"
-
 	"github.com/kelseyhightower/envconfig"
 	"github.com/rs/zerolog"
 )
 
 type Config struct {
-	NatsUrl     string        `envconfig:"NATS_URL" default:"nats://localhost:4222"`
-	NatsStream  string        `envconfig:"NATS_STREAM" default:"SCANS"`
+	NATSURL     string        `envconfig:"NATS_URL" default:"nats://localhost:4222"`
+	NATSStream  string        `envconfig:"NATS_STREAM" default:"SCANS"`
 	SubjectIn   string        `envconfig:"SUBJECT_IN" default:"scans.dns_scanner_results"`
 	SubjectOut  string        `envconfig:"SUBJECT_OUT" default:"scans.findings.upsert"`
 	DurableName string        `envconfig:"DURABLE_NAME" default:"subdomain_takeover"`
 	WorkerCount int           `envconfig:"WORKER_COUNT" default:"5"`
 	LogLevel    zerolog.Level `envconfig:"LOG_LEVEL" default:"info"`
-	Logger      zerolog.Logger
 }
 
-func InitConfig() *Config {
+func Load() (*Config, error) {
 	var cfg Config
 	if err := envconfig.Process("", &cfg); err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	zerolog.SetGlobalLevel(cfg.LogLevel)
-
-	// Create multiple output steams for zerolog
-	multi := zerolog.MultiLevelWriter(zerolog.ConsoleWriter{Out: os.Stderr})
-
-	logger := zerolog.New(multi).With().Timestamp().Logger()
-
-	cfg.Logger = logger
-
-	return &cfg
+	return &cfg, nil
 }
