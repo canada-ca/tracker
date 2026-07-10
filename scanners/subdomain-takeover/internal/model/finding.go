0900dc1caced07aec602b5091e5a4b96b918eab4
lcampbell2 <l.campbell144768@gmail.com>
2026-07-10 16:19:47 -0300

    add RecordType enums

--- scanners/subdomain-takeover/internal/model/finding.go
+++ scanners/subdomain-takeover/internal/model/finding.go
@@ -1,9 +1,16 @@
 package model
 
+type RecordType string
+
+const (
+	RecordTypeCNAME RecordType = "CNAME"
+	RecordTypeNS    RecordType = "NS"
+)
+
 type Finding struct {
 	Domain      string `json:"domain"`
 	DomainKey   string `json:"domain_key"`
-	RecordType  string `json:"record_type"`
+	RecordType  RecordType `json:"record_type"`
 	Target      string `json:"target"`
 	Provider    string `json:"provider"`
 	LameType    string `json:"lame_type"`
