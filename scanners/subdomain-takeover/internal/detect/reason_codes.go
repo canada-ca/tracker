acfeda6bf752ef93fb7d4907aee33495b988d659
lcampbell2 <l.campbell144768@gmail.com>
2026-08-04 14:19:54 -0300

    subdomain-takeover: add final detection architecture and rules

---
+++ scanners/subdomain-takeover/internal/detect/reason_codes.go
@@ -0,0 +1,17 @@
+package detect
+
+type ReasonCode string
+
+const (
+	ReasonCNAMEDanglingNXDOMAIN             ReasonCode = "CNAME_DANGLING_NXDOMAIN"
+	ReasonCNAMEProviderFingerprintBodyMatch ReasonCode = "CNAME_PROVIDER_FINGERPRINT_BODY_MATCH"
+	ReasonCNAMETargetMatchMissingNXDOMAIN   ReasonCode = "CNAME_TARGET_MATCH_MISSING_NXDOMAIN"
+	ReasonCNAMETargetMatchMissingBodyFP     ReasonCode = "CNAME_TARGET_MATCH_MISSING_BODY_FINGERPRINT"
+
+	ReasonNSFullLameProviderVulnerable    ReasonCode = "NS_FULL_LAME_PROVIDER_VULNERABLE"
+	ReasonNSPartialLameProviderVulnerable ReasonCode = "NS_PARTIAL_LAME_PROVIDER_VULNERABLE"
+	ReasonNSRegistrarProviderMatch        ReasonCode = "NS_REGISTRAR_PROVIDER_MATCH"
+	ReasonNSRegistrarContextInsufficient  ReasonCode = "NS_REGISTRAR_CONTEXT_INSUFFICIENT"
+	ReasonNSLameProviderUnknown           ReasonCode = "NS_LAME_PROVIDER_UNKNOWN"
+	ReasonNSProviderMatchOnly             ReasonCode = "NS_PROVIDER_MATCH_ONLY"
+)
