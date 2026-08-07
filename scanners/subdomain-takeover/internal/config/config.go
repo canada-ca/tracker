2380ed173600730a2d890a79e4c1b77024ff0502
lcampbell2 <l.campbell144768@gmail.com>
2026-08-07 13:51:29 -0300

    refactor emitted payload structure from subdomain-takeover to match processor

--- scanners/subdomain-takeover/internal/config/config.go
+++ scanners/subdomain-takeover/internal/config/config.go
@@ -9,7 +9,7 @@ type Config struct {
 	NATSURL     string        `envconfig:"NATS_URL" default:"nats://localhost:4222"`
 	NATSStream  string        `envconfig:"NATS_STREAM" default:"SCANS"`
 	SubjectIn   string        `envconfig:"SUBJECT_IN" default:"scans.dns_scanner_results"`
-	SubjectOut  string        `envconfig:"SUBJECT_OUT" default:"scans.findings.subdomain_takeover"`
+	SubjectOut  string        `envconfig:"SUBJECT_OUT" default:"scans.findings.subdomain-takeover"`
 	DurableName string        `envconfig:"DURABLE_NAME" default:"subdomain_takeover"`
 	WorkerCount int           `envconfig:"WORKER_COUNT" default:"5"`
 	LogLevel    zerolog.Level `envconfig:"LOG_LEVEL" default:"info"`
